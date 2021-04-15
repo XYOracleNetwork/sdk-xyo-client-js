48569d18dd0ab441d37a006cecf6ea98958b4330
Arie Trouw <arie.trouw@xy.company>
2021-04-15 16:59:44 -0700

    Ready for test deploy

--- src/ArchivistApi.ts
+++ src/ArchivistApi.ts
@@ -1,6 +1,5 @@
 import { ApiConfig } from '@xyo-network/sdk-xyo-js'
 import axios from 'axios'
-import shajs from 'sha.js'
 
 import { XyoEntryJson } from './models'
 
@@ -32,22 +31,6 @@ class XyoArchivistApi {
         headers: this.headers,
       })
     ).data
-  }
-
-  static sortObject<T extends Record<string, any>>(obj: T) {
-    const result: Record<string, any> = {} as Record<string, any>
-    Object.keys(obj)
-      .sort()
-      .forEach((key) => {
-        result[key] = obj[key]
-      })
-    return result as T
-  }
-
-  static hash(entry: XyoEntryJson) {
-    const sortedEntry = this.sortObject<XyoEntryJson>(entry)
-    const stringObject = JSON.stringify(sortedEntry)
-    return shajs('sha256').update(stringObject).digest('hex')
   }
 
   static get(config: ApiConfig) {

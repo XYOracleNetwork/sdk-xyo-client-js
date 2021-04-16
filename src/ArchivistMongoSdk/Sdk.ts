0d35b2cc9bdbb08d4a14b6a599aefe7e8144f72c
Arie Trouw <arie.trouw@xy.company>
2021-04-16 13:14:29 -0700

    mongo update

--- src/ArchivistMongoSdk/Sdk.ts
+++ src/ArchivistMongoSdk/Sdk.ts
@@ -1,7 +1,19 @@
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
+import { Collection } from 'mongodb'
 
 import { XyoBoundWitnessJson } from '../models'
 
-class MongoSdk<T> extends BaseMongoSdk<XyoBoundWitnessJson<T>> {}
+class MongoSdk<T> extends BaseMongoSdk<XyoBoundWitnessJson<T>> {
+  public async insert(item: XyoBoundWitnessJson<T>) {
+    return await this.useCollection(async (collection: Collection<XyoBoundWitnessJson<T>>) => {
+      const result = await collection.insertOne(item)
+      if (result.result.ok) {
+        return result.insertedId
+      } else {
+        throw Error('Insert Failed')
+      }
+    })
+  }
+}
 
 export default MongoSdk

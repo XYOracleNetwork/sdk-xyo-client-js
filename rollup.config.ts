3d04fb104487814697e81b145bd6c54ba0fc8198
Arie Trouw <arie.trouw@xy.company>
2021-04-18 12:41:24 -0700

    rollup fixed?

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,4 +1,7 @@
-import typescript from 'rollup-plugin-typescript2'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
 
 import pkg from './package.json'
 
@@ -16,5 +19,5 @@ export default {
       sourcemap: true,
     },
   ],
-  plugins: [typescript()],
+  plugins: [commonjs(), nodeResolve(), json(), typescript()],
 }

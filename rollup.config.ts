b3039c22224125f0df55501dc19243804e0dde55
Arie Trouw <arie.trouw@xy.company>
2021-04-19 17:23:52 -0700

    Back to old rollup and jest

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,4 @@
-import commonjs from '@rollup/plugin-commonjs'
-import json from '@rollup/plugin-json'
-import resolve from '@rollup/plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript'
+import typescript from 'rollup-plugin-typescript2'
 
 import pkg from './package.json'
 
@@ -21,5 +18,5 @@ export default {
       sourcemap: true,
     },
   ],
-  plugins: [commonjs(), resolve({ preferBuiltins: true }), json(), typescript({ tsconfig: './tsconfig.json' })],
+  plugins: [typescript()],
 }

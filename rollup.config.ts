90de53d59890fabb762959694faae6e3e621f676
Arie Trouw <arie.trouw@xy.company>
2021-05-26 09:24:06 -0700

    mjs -> js

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,20 +1,21 @@
-import typescript from 'rollup-plugin-typescript2'
+import typescript from '@rollup/plugin-typescript'
 
-export default {
-  input: './src/index.ts',
-  output: [
-    {
-      exports: 'auto',
-      file: './dist/index.cjs',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      exports: 'auto',
-      file: './dist/index.mjs',
-      format: 'es',
-      sourcemap: true,
-    },
-  ],
-  plugins: [typescript()],
-}
+export default [
+  {
+    external: ['axios', '@xyo-network/sdk-xyo-js', '@xyo-network/sdk-xyo-mongo-js', 'sha.js'],
+    input: './src/index.ts',
+    output: [
+      {
+        file: './dist/index.cjs',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: './dist/index.js',
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript()],
+  },
+]

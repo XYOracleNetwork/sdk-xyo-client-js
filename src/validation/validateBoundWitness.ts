249fb30bde39dac0271906fcb02cdd3f7728da6a
Arie Trouw <arie.trouw@xy.compay>
2021-08-12 16:43:06 -0700

    timestamp epoch validation update

--- src/validation/validateBoundWitness.ts
+++ src/validation/validateBoundWitness.ts
@@ -5,8 +5,8 @@ import validateBoundWitnessBodyHash from './validateBoundWitnessBodyHash'
 
 const validateBoundWitness = (bw: XyoBoundWitness) => {
   assertEx(bw._timestamp !== undefined, 'Missing _timestamp')
-  assertEx((bw._timestamp ?? 0) > 1609459200, '_timestamp is before year 2021')
-  assertEx((bw._timestamp ?? 4102444800) < 4102444800, '_timestamp is after year 2100')
+  assertEx((bw._timestamp ?? 0) > 1609459200000, '_timestamp is before year 2021')
+  assertEx((bw._timestamp ?? 4102444800000) < 4102444800000, '_timestamp is after year 2100')
 
   validateBoundWitnessBodyHash(bw)
 }

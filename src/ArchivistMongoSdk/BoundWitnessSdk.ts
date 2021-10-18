ea709b37e8ccedbe40207b04c3aca35c3c0eba45
Arie Trouw <arie.trouw@xy.compay>
2021-10-18 16:04:29 -0700

    block index use?

--- src/ArchivistMongoSdk/BoundWitnessSdk.ts
+++ src/ArchivistMongoSdk/BoundWitnessSdk.ts
@@ -15,7 +15,7 @@ class BoundWitnessSdk extends BaseMongoSdk<XyoBoundWitness> {
   public async findRecent(limit = 20) {
     return await this.useCollection(async (collection: Collection<XyoBoundWitness>) => {
       return await collection
-        .find({ archive: this._archive })
+        .find({ _archive: this._archive })
         .sort({ _archive: -1, _timestamp: -1 })
         .limit(limit)
         .toArray()

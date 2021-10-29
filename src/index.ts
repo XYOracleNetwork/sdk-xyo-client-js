867bc968a8528e8fa6639e30b64b74acdc35e7dc
Arie Trouw <arie.trouw@xy.compay>
2021-10-28 18:11:15 -0700

    neo4j driver

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './ArchivistApi'
 export * from './ArchivistMongoSdk'
+export * from './ArchivistNeo4jSdk'
 export * from './BoundWitness'
 export * from './models'
 export * from './Payload'

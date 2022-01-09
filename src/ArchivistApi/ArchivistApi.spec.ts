73ee0219526dd98d0412dd17284f47e2bd0352d7
Joel Carter <joelbcarter@gmail.com>
2022-01-09 09:16:21 -0600

    Streamline tests

--- src/ArchivistApi/ArchivistApi.spec.ts
+++ src/ArchivistApi/ArchivistApi.spec.ts
@@ -2,57 +2,75 @@ import { AxiosError } from 'axios'
 
 import { XyoAddress } from '../Address'
 import { XyoBoundWitnessBuilder } from '../BoundWitness'
-import { XyoPayload } from '../models'
+import { XyoBoundWitness, XyoPayload } from '../models'
 import { XyoArchivistApi } from './ArchivistApi'
 import { XyoArchivistApiConfig } from './ArchivistApiConfig'
 
-test('checking happy path', async () => {
-  const payload: XyoPayload = {
-    number_field: 1,
-    object_field: {
-      number_value: 2,
-      string_value: 'yo',
-    },
-    schema: 'network.xyo.test',
-    string_field: 'there',
-    timestamp: 1618603439107,
-  }
+const payload: XyoPayload = {
+  number_field: 1,
+  object_field: {
+    number_value: 2,
+    string_value: 'yo',
+  },
+  schema: 'network.xyo.test',
+  string_field: 'there',
+  timestamp: 1618603439107,
+}
 
-  const config: XyoArchivistApiConfig = {
-    apiDomain: process.env.API_DOMAIN || 'https://api.archivist.xyo.network',
-    archive: 'test',
-  }
+const config: XyoArchivistApiConfig = {
+  apiDomain: process.env.API_DOMAIN || 'https://api.archivist.xyo.network',
+  archive: 'test',
+}
 
-  const address = XyoAddress.random()
+describe('XyoArchivistApi', () => {
+  describe('get', () => {
+    it('returns a new XyoArchivistApi', () => {
+      const api = XyoArchivistApi.get(config)
+      expect(api).toBeDefined()
+    })
+    describe('with no token', () => {
+      it('is not authenticated', () => {
+        const api = XyoArchivistApi.get(config)
+        expect(api.authenticated).toEqual(false)
+      })
+    })
+  })
+  describe('postBoundWitness', () => {
+    it('posts a single bound witness', async () => {
+      const builder = new XyoBoundWitnessBuilder({ inlinePayloads: true })
+        .witness(XyoAddress.random(), null)
+        .payload('network.xyo.test', payload)
+      const api = XyoArchivistApi.get(config)
+      const boundWitness: XyoBoundWitness = builder.build()
 
-  let builder = new XyoBoundWitnessBuilder()
-  expect(builder).toBeDefined()
-  builder = builder.witness(address, null)
-  expect(builder).toBeDefined()
-
-  builder = builder.payload('network.xyo.test', payload)
-  expect(builder).toBeDefined()
-
-  const json = builder.build()
-  expect(json).toBeDefined()
-
-  const api = XyoArchivistApi.get(config)
-  expect(api).toBeDefined()
-  expect(api.authenticated).toEqual(false)
-  try {
-    const postBoundWitnessResult = await api.postBoundWitness(json)
-    expect(postBoundWitnessResult.boundWitnesses).toEqual(1)
-  } catch (ex) {
-    const error = ex as AxiosError
-    console.log(JSON.stringify(error.response?.data, null, 2))
-    throw ex
-  }
-  try {
-    const postBoundWitnessesResult = await api.postBoundWitnesses([json, json])
-    expect(postBoundWitnessesResult.boundWitnesses).toEqual(2)
-  } catch (ex) {
-    const error = ex as AxiosError
-    console.log(JSON.stringify(error.response?.data, null, 2))
-    throw ex
-  }
-}, 40000)
+      try {
+        const postBoundWitnessResult = await api.postBoundWitness(boundWitness)
+        expect(postBoundWitnessResult.boundWitnesses).toEqual(1)
+        expect(postBoundWitnessResult.payloads).toEqual(1)
+      } catch (ex) {
+        const error = ex as AxiosError
+        console.log(JSON.stringify(error.response?.data, null, 2))
+        throw ex
+      }
+    }, 40000)
+  })
+  describe('postBoundWitnesses', () => {
+    it('posts multiple bound witnesses', async () => {
+      const builder = new XyoBoundWitnessBuilder({ inlinePayloads: true })
+        .witness(XyoAddress.random(), null)
+        .payload('network.xyo.test', payload)
+      const api = XyoArchivistApi.get(config)
+      const json = builder.build()
+      const boundWitnesses: XyoBoundWitness[] = [json, json]
+      try {
+        const postBoundWitnessesResult = await api.postBoundWitnesses(boundWitnesses)
+        expect(postBoundWitnessesResult.boundWitnesses).toEqual(2)
+        expect(postBoundWitnessesResult.payloads).toEqual(2)
+      } catch (ex) {
+        const error = ex as AxiosError
+        console.log(JSON.stringify(error.response?.data, null, 2))
+        throw ex
+      }
+    }, 40000)
+  })
+})

4255a5993ffeb2ed956e45c38f62989f0e49d863
Arie Trouw <arie.trouw@xylabs.com>
2022-02-07 12:58:48 -0800

    PayloadBuilder

---
+++ src/Payload/Builder/Builder.ts
@@ -0,0 +1,45 @@
+import shajs from 'sha.js'
+
+import { XyoPayload } from '../../models'
+import { sortObject } from '../../sortObject'
+
+export interface XyoPayloadBuilderOptions {
+  schema: string
+}
+
+export class XyoPayloadBuilder {
+  private _fields?: Record<string, unknown> = {}
+  private _schema: string
+
+  constructor({ schema }: XyoPayloadBuilderOptions) {
+    this._schema = schema
+  }
+
+  public fields(fields: Record<string, unknown>) {
+    this._fields = { ...this._fields, ...fields }
+    return this
+  }
+
+  public hashableFields(): XyoPayload {
+    return {
+      ...this._fields,
+      schema: this._schema,
+    }
+  }
+
+  public build(): XyoPayload {
+    const hashableFields = this.hashableFields() as unknown as Record<string, unknown>
+    const _hash = XyoPayloadBuilder.hash(hashableFields)
+    return { ...hashableFields, _client: 'js', _hash, schema: this._schema } as XyoPayload
+  }
+
+  static sortedStringify<T extends Record<string, unknown>>(obj: T) {
+    const sortedEntry = sortObject<T>(obj)
+    return JSON.stringify(sortedEntry)
+  }
+
+  static hash<T extends Record<string, unknown>>(obj: T) {
+    const stringObject = XyoPayloadBuilder.sortedStringify<T>(obj)
+    return shajs('sha256').update(stringObject).digest('hex')
+  }
+}

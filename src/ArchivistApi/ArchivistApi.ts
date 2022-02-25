71720434f16300b4b04e47a2ff2abbe6084dc7ee
JoelBCarter <joelbcarter@gmail.com>
2022-02-24 21:24:35 -0600

    Change method name to plural

--- src/ArchivistApi/ArchivistApi.ts
+++ src/ArchivistApi/ArchivistApi.ts
@@ -104,7 +104,7 @@ class XyoArchivistApi {
     ).data
   }
 
-  public async getBoundWitnessBefore(timestamp: number, limit = 20) {
+  public async getBoundWitnessesBefore(timestamp: number, limit = 20) {
     assertEx(limit > 0, 'min limit = 1')
     assertEx(limit <= 100, 'max limit = 100')
     const params = { order: 'desc', timestamp }
@@ -116,7 +116,7 @@ class XyoArchivistApi {
     ).data
   }
 
-  public async getBoundWitnessAfter(timestamp: string, limit = 20) {
+  public async getBoundWitnessesAfter(timestamp: string, limit = 20) {
     assertEx(limit > 0, 'min limit = 1')
     assertEx(limit <= 100, 'max limit = 100')
     const params = { order: 'asc', timestamp }

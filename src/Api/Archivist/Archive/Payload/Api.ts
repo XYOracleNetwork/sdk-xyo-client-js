639eaa4b498cbf9ec4bdff4913284bba2c99533c
Arie Trouw <arie.trouw@xylabs.com>
2022-03-15 20:11:05 -0700

    simplified api call classes

--- src/Api/Archivist/Archive/Payload/Api.ts
+++ src/Api/Archivist/Archive/Payload/Api.ts
@@ -4,50 +4,33 @@ import { XyoPayload } from '../../../../models'
 import { XyoArchivistApiBase } from '../../Base'
 
 export class XyoArchivistArchivePayloadApi extends XyoArchivistApiBase {
+  private buildPath(endPoint: string) {
+    return `${this.config.apiDomain}/archive/${this.config.archive}/payload/${endPoint}`
+  }
+
+  private async getPath<T>(endPoint: string) {
+    return (await this.axios.get<T>(this.buildPath(endPoint), this.axiosRequestConfig)).data
+  }
+
   public async getStats() {
-    return (
-      await this.axios.get<{ count: number }>(
-        `${this.config.apiDomain}/archive/${this.config.archive}/payload/stats`,
-        this.axiosRequestConfig
-      )
-    ).data
+    return await this.getPath<{ count: number }>('/stats')
   }
 
   public async getByHash(hash: string) {
-    return (
-      await this.axios.get<XyoPayload[]>(
-        `${this.config.apiDomain}/archive/${this.config.archive}/payload/hash/${hash}`,
-        this.axiosRequestConfig
-      )
-    ).data
+    return await this.getPath<XyoPayload[]>(`/hash/${hash}`)
   }
 
   public async repairByHash(hash: string) {
-    return (
-      await this.axios.get<XyoPayload[]>(
-        `${this.config.apiDomain}/archive/${this.config.archive}/payload/hash/${hash}/repair`,
-        this.axiosRequestConfig
-      )
-    ).data
+    return await this.getPath<XyoPayload[]>(`/hash/${hash}/repair`)
   }
 
   public async getMostRecent(limit = 20) {
     assertEx(limit > 0, 'min limit = 1')
     assertEx(limit <= 100, 'max limit = 100')
-    return (
-      await this.axios.get<XyoPayload[]>(
-        `${this.config.apiDomain}/archive/${this.config.archive}/payload/recent/${limit}`,
-        this.axiosRequestConfig
-      )
-    ).data
+    return await this.getPath<XyoPayload[]>(`/recent/${limit}`)
   }
 
   public async getSample(size: number) {
-    return (
-      await this.axios.get<XyoPayload[]>(
-        `${this.config.apiDomain}/archive/${this.config.archive}/payload/sample/${size}`,
-        this.axiosRequestConfig
-      )
-    ).data
+    return await this.getPath<XyoPayload[]>(`/sample/${size}`)
   }
 }

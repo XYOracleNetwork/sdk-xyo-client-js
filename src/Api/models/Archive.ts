04a94977ef4b632478127a1e9114ea78a899d634
Arie Trouw <arie.trouw@xylabs.com>
2022-03-30 09:47:41 -0700

    XyoArchive access control updated

--- src/Api/models/Archive.ts
+++ src/Api/models/Archive.ts
@@ -1,20 +1,28 @@
 import { XyoArchivePath } from './ArchivePath'
 
 export type AccessLevel = 'public' | 'private'
+export type AccessType = 'read' | 'remove' | 'create' | 'list'
 export type ExpirationWindow = 'create' | 'read'
 
-export interface SchemaConfig {
-  read?: AccessLevel
-  write?: AccessLevel
-  list?: AccessLevel
+export interface XyoSchemaAccessControl {
+  access?: Record<AccessType, AccessLevel>
   expiration?: {
     window?: ExpirationWindow
     delay?: number
   }
 }
 
+export interface XyoAccessControl {
+  /**
+   * Set access controls on a per schema basis.
+   * '*' can be used as a wildcard
+   * Access control is set from a generic to specific direction if
+   * multiple wildcard versions apply to a schema
+   */
+  schema?: Record<string, XyoSchemaAccessControl>
+}
+
 export interface XyoArchive extends XyoArchivePath {
   user?: string
-  accessControl?: boolean
-  schemaConfig?: Record<string, SchemaConfig>
+  accessControl?: XyoAccessControl | boolean
 }

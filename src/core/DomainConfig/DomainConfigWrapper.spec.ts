e27dbb6219402fd94e3dc43001903ec265460ce1
Arie Trouw <arie.trouw@xylabs.com>
2022-04-20 16:12:04 -0700

    test updated

--- src/core/DomainConfig/DomainConfigWrapper.spec.ts
+++ src/core/DomainConfig/DomainConfigWrapper.spec.ts
@@ -3,12 +3,12 @@ import { XyoDomainConfigWrapper } from './DomainConfigWrapper'
 describe('DomainConfigWrapper', () => {
   test('Valid', async () => {
     const wrapper = await XyoDomainConfigWrapper.discover('network.xyo.domain')
-    expect(wrapper?.config.schema).toBe('network.xyo.domain')
+    expect(wrapper?.payload.schema).toBe('network.xyo.domain')
     await wrapper?.fetch()
-    expect(wrapper?.definitions?.length).toBe(2)
+    expect(wrapper?.aliases?.length).toBe(2)
   })
   test('InValid', async () => {
     const wrapper = await XyoDomainConfigWrapper.discover('blahblah.blahblah.blahblah')
-    expect(wrapper?.config.schema).toBe(undefined)
+    expect(wrapper?.payload.schema).toBe(undefined)
   })
 })

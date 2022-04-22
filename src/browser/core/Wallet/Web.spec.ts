695f49dc617ac115681770acec715b5da6ea0980
Arie Trouw <arie.trouw@xylabs.com>
2022-04-22 14:14:11 -0700

    Fixed getAccount

--- src/browser/core/Wallet/Web.spec.ts
+++ src/browser/core/Wallet/Web.spec.ts
@@ -2,7 +2,7 @@ import { XyoWebWallet } from './Web'
 
 test('all', () => {
   const wallet = new XyoWebWallet('test phrase')
-  const account = wallet.getAccount(0)
+  const account = wallet.getAccount(5)
 
   expect(account).toBeDefined()
   expect(account.addressValue.length).toBe(20)
--- src/core/Wallet/Base/Base.spec.ts
+++ src/core/Wallet/Base/Base.spec.ts
@@ -2,7 +2,7 @@ import { XyoWalletBase } from './Base'
 
 test('all', () => {
   const wallet = new XyoWalletBase('test phrase')
-  const account = wallet.getAccount(0)
+  const account = wallet.getAccount(10)
 
   expect(account).toBeDefined()
   expect(account.addressValue.length).toBe(20)

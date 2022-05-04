de525702b92f2933d8019404305be2b4013de4db
Arie Trouw <arie.trouw@xylabs.com>
2022-05-04 16:45:02 -0700

    bch improvements

--- src/core/BoundWitness/models/XyoBoundWitnessSchema.ts
+++ src/core/BoundWitness/models/XyoBoundWitnessSchema.ts
@@ -3,19 +3,21 @@
 
 import { XyoBoundWitnessMetaSchemaProperties } from './Meta'
 
-const XyoBoundWitnessSchema = {
-  $id: 'https://schemas.xyo.network/2.0/boundwitness',
-  additionalProperties: false,
-  properties: {
-    ...XyoBoundWitnessMetaSchemaProperties,
-    addresses: { items: { type: 'string' }, type: 'array' },
-    payload_hashes: { items: { type: 'string' }, type: 'array' },
-    payload_schemas: { items: { type: 'string' }, type: 'array' },
-    previous_hashes: { items: { nullable: true, type: 'string' }, type: 'array' },
-    schema: { type: 'string' },
-  },
-  required: ['addresses', 'payload_hashes', 'payload_schemas', 'previous_hashes', 'schema'],
-  type: 'object',
+const XyoBoundWitnessSchema = () => {
+  return {
+    $id: 'https://schemas.xyo.network/2.0/boundwitness',
+    additionalProperties: false,
+    properties: {
+      ...XyoBoundWitnessMetaSchemaProperties(),
+      addresses: { items: { type: 'string' }, type: 'array' },
+      payload_hashes: { items: { type: 'string' }, type: 'array' },
+      payload_schemas: { items: { type: 'string' }, type: 'array' },
+      previous_hashes: { items: { nullable: true, type: 'string' }, type: 'array' },
+      schema: { type: 'string' },
+    },
+    required: ['addresses', 'payload_hashes', 'payload_schemas', 'previous_hashes', 'schema'],
+    type: 'object',
+  }
 }
 
 export { XyoBoundWitnessSchema }
--- src/network/knownDiviners.ts
+++ src/network/knownDiviners.ts
@@ -1,19 +1,23 @@
 import { XyoNodeConfig } from '../shared'
 
-const beta: XyoNodeConfig = {
-  name: 'XYO Location (beta)',
-  slug: 'beta-location-diviner-xyo-network',
-  type: 'diviner',
-  uri: 'https://beta.api.location.diviner.xyo.network',
-  web: 'https://beta.explore.xyo.network',
+const beta = (): XyoNodeConfig => {
+  return {
+    name: 'XYO Location (beta)',
+    slug: 'beta-location-diviner-xyo-network',
+    type: 'diviner',
+    uri: 'https://beta.api.location.diviner.xyo.network',
+    web: 'https://beta.explore.xyo.network',
+  }
 }
 
-const main: XyoNodeConfig = {
-  name: 'XYO Location',
-  slug: 'location-diviner-xyo-network',
-  type: 'diviner',
-  uri: 'https://api.location.diviner.xyo.network',
-  web: 'https://explore.xyo.network',
+const main = (): XyoNodeConfig => {
+  return {
+    name: 'XYO Location',
+    slug: 'location-diviner-xyo-network',
+    type: 'diviner',
+    uri: 'https://api.location.diviner.xyo.network',
+    web: 'https://explore.xyo.network',
+  }
 }
 
-export const knownDiviners: XyoNodeConfig[] = [beta, main]
+export const knownDiviners = (): XyoNodeConfig[] => [beta(), main()]

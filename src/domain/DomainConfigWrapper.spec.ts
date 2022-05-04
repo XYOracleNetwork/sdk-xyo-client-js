8713d16910e36382cfd92dddc98b69b9251240f3
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-05-03 20:08:05 -0400

    use proxy if provided regardless of environment (#466)

--- src/domain/DomainConfigWrapper.spec.ts
+++ src/domain/DomainConfigWrapper.spec.ts
@@ -2,25 +2,23 @@ import { XyoDomainConfigWrapper } from './DomainConfigWrapper'
 
 describe('DomainConfigWrapper', () => {
   test('Valid-discover', async () => {
-    const wrapper = await XyoDomainConfigWrapper.discover('network.xyo.domain', process.env.API_DOMAIN)
+    const wrapper = await XyoDomainConfigWrapper.discover('network.xyo.domain')
     expect(wrapper?.payload.schema).toBe('network.xyo.domain')
     await wrapper?.fetch()
     expect(wrapper?.aliases?.length).toBe(3)
     expect(wrapper?.aliases?.[0].huri).toBeDefined()
   })
-  /*
   test('Valid-discover-proxy', async () => {
-    const wrapper = await XyoDomainConfigWrapper.discoverRootFileWithProxy('xyo.network', `${process.env.API_DOMAIN}/domain`)
+    const wrapper = await XyoDomainConfigWrapper.discover('network.xyo.domain', `${process.env.API_DOMAIN}/domain`)
     expect(wrapper?.payload.schema).toBe('network.xyo.domain')
     await wrapper?.fetch()
-    expect(wrapper?.aliases?.length).toBe(2)
+    expect(wrapper?.aliases?.length).toBe(3)
     expect(wrapper?.aliases?.[0].huri).toBeDefined()
   })
   test('Invalid-discover-proxy', async () => {
     const wrapper = await XyoDomainConfigWrapper.discoverRootFileWithProxy('xyoo.network', `${process.env.API_DOMAIN}/domain`)
     expect(wrapper?.payload.schema).toBeUndefined()
   })
-  */
   test('Valid-discover-direct', async () => {
     const wrapper = await XyoDomainConfigWrapper.discoverRootFileDirect('xyo.network')
     expect(wrapper?.payload.schema).toBe('network.xyo.domain')

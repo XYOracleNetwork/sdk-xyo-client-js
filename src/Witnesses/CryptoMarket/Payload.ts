008dde8e4d48c852c2725dbba4277e807d9ab9f3
Arie Trouw <arie.trouw@xylabs.com>
2022-05-09 16:55:42 -0700

    cryptomarket witness

--- src/Witnesses/CryptoMarket/Payload.ts
+++ src/Witnesses/CryptoMarket/Payload.ts
@@ -1,10 +1,13 @@
 import { XyoPayload } from '../../core'
 
 export interface XyoCryptoMarketAsset {
-  usd: number
+  usd?: number
+  eur?: number
+  btc?: number
+  eth?: number
 }
 
 export interface XyoCryptoMarketPayload extends XyoPayload {
   timestamp: number
-  assets: XyoCryptoMarketAsset[]
+  assets?: XyoCryptoMarketAsset[]
 }
--- src/Witnesses/CryptoMarket/Witness.ts
+++ src/Witnesses/CryptoMarket/Witness.ts
@@ -4,7 +4,10 @@ import { XyoWitness } from '../../core'
 import { XyoCryptoMarketPayload } from './Payload'
 
 interface CoinGeckoSimplePrice {
-  usd: number
+  usd?: number
+  eur?: number
+  btc?: number
+  eth?: number
 }
 
 interface Coin {
@@ -12,17 +15,27 @@ interface Coin {
   symbol?: string
 }
 
+interface Currency {
+  name?: string
+  symbol?: string
+}
+
 export class XyoCryptoMarketWitness extends XyoWitness<XyoCryptoMarketPayload> {
   private coins: Coin[]
-  constructor(coins: Coin[] = [{ name: 'bitcoin' }, { name: 'ethereum' }, { name: 'xyo-network' }]) {
+  private currencies: Currency[]
+  constructor(
+    coins: Coin[] = [{ name: 'bitcoin' }, { name: 'ethereum' }, { name: 'xyo-network' }],
+    currencies: Currency[] = [{ symbol: 'usd' }, { symbol: 'eur' }, { symbol: 'btc' }, { symbol: 'eth' }]
+  ) {
     super({
       schema: XyoCryptoMarketWitness.schema,
     })
     this.coins = coins
+    this.currencies = currencies
   }
 
   override async observe(): Promise<XyoCryptoMarketPayload> {
-    const result = await axios.get<CoinGeckoSimplePrice[]>(`https://api.coingecko.com/api/v3/simple/price?ids=${this.coins.join('&')}&vs_currencies=usd`)
+    const result = await axios.get<CoinGeckoSimplePrice[]>(`https://api.coingecko.com/api/v3/simple/price?ids=${this.coins.join(',')}&vs_currencies=${this.currencies.join(',')}`)
     return await super.observe({ assets: result.data, timestamp: Date.now() })
   }
 

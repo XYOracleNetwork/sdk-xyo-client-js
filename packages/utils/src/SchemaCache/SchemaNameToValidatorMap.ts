c2aa3ee205833fd9bb10688c446fdab929a0ff40
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-05-18 08:12:59 -0500

    Export map instead of func to allow return type narrowing

--- packages/utils/src/SchemaCache/SchemaNameToValidatorMap.ts
+++ packages/utils/src/SchemaCache/SchemaNameToValidatorMap.ts
@@ -1,9 +1,10 @@
-import { XyoBoundWitness, XyoPayload, XyoSchemaPayload } from '@xyo-network/core'
+import { XyoPayload, XyoSchemaPayload } from '@xyo-network/core'
+import { XyoDomainConfig } from '@xyo-network/domain'
 
 export type PayloadValidator<T extends XyoPayload = XyoPayload> = (x: XyoPayload) => x is T
 
 export interface XyoSchemaNameToValidatorMap {
-  'network.xyo.boundwitness': PayloadValidator<XyoBoundWitness>
+  'network.xyo.domain': PayloadValidator<XyoDomainConfig>
   'network.xyo.payload': PayloadValidator<XyoPayload>
   'network.xyo.schema': PayloadValidator<XyoSchemaPayload>
 }

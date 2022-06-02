142701b3567417345978b98e6b6ba4214e8ad6b1
Arie Trouw <arie.trouw@xylabs.com>
2022-06-01 18:57:30 -0700

    targetSchema for witnesses

--- packages/core/src/Witness/Witness.ts
+++ packages/core/src/Witness/Witness.ts
@@ -2,4 +2,5 @@ import { XyoPayload } from '../Payload'
 
 export abstract class XyoWitness<T extends XyoPayload = XyoPayload> {
   abstract observe(fields?: Partial<T>): Promise<T>
+  abstract get targetSchema(): string
 }

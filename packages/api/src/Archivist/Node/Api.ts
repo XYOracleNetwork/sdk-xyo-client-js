234a0fdc9ff401b644ac0d537d3cf853dbb197f3
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-08 13:49:51 -0500

    Method for query & await result

--- packages/api/src/Archivist/Node/Api.ts
+++ packages/api/src/Archivist/Node/Api.ts
@@ -8,10 +8,26 @@ export class XyoArchivistNodeApi<
   D extends XyoBoundWitness | XyoBoundWitness[] = XyoBoundWitness | XyoBoundWitness[],
   C extends WithArchive<XyoApiConfig> = WithArchive<XyoApiConfig>
 > extends XyoApiSimple<string[][], D, XyoApiSimpleQuery, C> {
-  public queryResult(id: string): XyoApiSimple<XyoPayload> {
-    return new XyoApiSimple<XyoPayload>({
+  /**
+   * Get the result of a previously issued query (if available)
+   * @param queryId Query ID from a previously issued query
+   * @returns The result of the query (if available)
+   */
+  public result<T extends XyoPayload = XyoPayload>(queryId: string): XyoApiSimple<XyoPayload> {
+    return new XyoApiSimple<T>({
       ...this.config,
-      root: `/query/${id}/`,
+      root: `/query/${queryId}/`,
     })
+  }
+
+  /**
+   * Issue the supplied queries and wait (non-blocking) for the results
+   * @param data The queries to issue
+   * @returns The results for the issued queries
+   */
+  public async perform(data: D) {
+    const ids = await this.post(data)
+    // TODO: Polling interval for long running, etc.
+    return ids?.length ? Promise.all(ids?.map((bw) => Promise.all(bw.map((p) => this.result(p).get())))) : []
   }
 }

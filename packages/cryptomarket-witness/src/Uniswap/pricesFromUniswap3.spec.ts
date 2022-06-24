0d78f812cd2a7c5680302c876b4078ec12283cb9
Arie Trouw <arie.trouw@xylabs.com>
2022-06-23 18:39:36 -0700

    Uniswap Witness honors query

--- packages/cryptomarket-witness/src/Uniswap/pricesFromUniswap3.spec.ts
+++ packages/cryptomarket-witness/src/Uniswap/pricesFromUniswap3.spec.ts
@@ -1,11 +1,11 @@
 import { InfuraProvider } from '@ethersproject/providers'
 
-import { pricesFromUniswap3, uniswapPoolContracts } from './pricesFromUniswap3'
+import { createUniswapPoolContracts, pricesFromUniswap3, UniswapPoolContracts } from './pricesFromUniswap3'
 
 describe('procesFromUniswap3', () => {
   test('observe', async () => {
     const provider = new InfuraProvider('homestead', { projectId: process.env.INFURA_PROJECT_ID, projectSecret: process.env.INFURA_PROJECT_SECRET })
-    const pairs = await pricesFromUniswap3(uniswapPoolContracts(provider))
+    const pairs = await pricesFromUniswap3(createUniswapPoolContracts(provider, UniswapPoolContracts))
     expect(pairs.length).toBeGreaterThan(1)
   })
 })

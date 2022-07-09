0c5084a323fd4c23625a137b1416e540f9fded67
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-08 20:05:37 -0500

    Extract interfaces to separate files

--- packages/ethereum-witness/src/Etherchain/v1/getV1GasFromEtherchain.ts
+++ packages/ethereum-witness/src/Etherchain/v1/getV1GasFromEtherchain.ts
@@ -1,13 +1,6 @@
 import axios from 'axios'
 
-export interface EtherchainGasPriceResultV1 {
-  safeLow: number
-  standard: number
-  fast: number
-  fastest: number
-  currentBaseFee: number
-  recommendedBaseFee: number
-}
+import { EtherchainGasPriceResultV1 } from './EtherchainGasPriceResultV1'
 
 export const getV1GasFromEtherchain = async (): Promise<EtherchainGasPriceResultV1> => {
   const etherchainGasPrices = (await axios.get<EtherchainGasPriceResultV1>('https://www.etherchain.org/api/gasPriceOracle')).data
--- packages/ethereum-witness/src/Etherchain/v2/Payload.ts
+++ packages/ethereum-witness/src/Etherchain/v2/Payload.ts
@@ -3,11 +3,13 @@ import { XyoPayload, XyoQueryPayload } from '@xyo-network/payload'
 export type XyoEthereumGasEtherchainQueryPayloadV2 = XyoQueryPayload
 
 export interface XyoEthereumGasEtherchainPayloadV2 extends XyoPayload {
-  timestamp: number
-  safeLow: number
-  standard: number
-  fast: number
-  fastest: number
-  currentBaseFee: number
-  recommendedBaseFee: number
+  code: number
+  data: {
+    fast: number
+    priceUSD: number
+    rapid: number
+    slow: number
+    standard: number
+    timestamp: number
+  }
 }

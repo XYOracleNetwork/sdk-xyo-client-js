17fb2e296918ba539cfcaba5c4925311c7e625bd
Arie Trouw <arie.trouw@xylabs.com>
2022-07-19 16:34:02 -0700

    archivist implementations

--- packages/api/src/Archivist/Model/Archivist.ts
+++ packages/api/src/Archivist/Model/Archivist.ts
@@ -1,12 +1,14 @@
 export interface QueryableArchivist<TQueryResponse, TQuery> {
-  find(query: TQuery): Promise<TQueryResponse>
+  find(query: TQuery): Promise<TQueryResponse> | TQueryResponse
 }
 export interface ReadArchivist<TReadResponse, TId = string> {
-  get(id: TId): Promise<TReadResponse>
+  get(id: TId): Promise<TReadResponse | undefined> | TReadResponse | undefined
 }
 
-export interface WriteArchivist<TWriteResponse, TWrite> {
-  insert(item: TWrite): Promise<TWriteResponse>
+export interface WriteArchivist<TWriteResponse, TWrite, TId = string> {
+  insert(item: TWrite): Promise<TWriteResponse> | TWriteResponse
+  delete?(id: TId): Promise<boolean> | boolean
+  clear?(): void
 }
 
 export type ReadWriteArchivist<TWriteResponse, TWrite, TReadResponse = TWriteResponse, TId = string> = ReadArchivist<TReadResponse, TId> & WriteArchivist<TWriteResponse, TWrite>

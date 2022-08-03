7f6f2e0206671573cacac8cec92d39b09e8025bf
Arie Trouw <arie.trouw@xylabs.com>
2022-08-03 14:24:30 -0700

    validateSignature exposed

--- packages/sdk/boundwitness/src/Validator/Validator.ts
+++ packages/sdk/boundwitness/src/Validator/Validator.ts
@@ -1,4 +1,3 @@
-import { assertEx } from '@xylabs/sdk-js'
 import { XyoAddressValue } from '@xyo-network/account'
 import { XyoHasher, XyoValidatorBase } from '@xyo-network/core'
 
@@ -8,16 +7,6 @@ import { XyoBoundWitnessMetaValidator } from './MetaValidator'
 
 const validateArraysSameLength = (a: unknown[], b: unknown[], message = 'Array length mismatch') => {
   return a.length != b.length ? [Error(`${message} []`)] : []
-}
-
-const validateSignature = (hash: string, address: string, signature?: string) => {
-  if (!signature) {
-    return [Error(`Missing signature [${address}]`)]
-  }
-  if (!new XyoAddressValue(address).verify(hash, signature)) {
-    return [Error(`Invalid signature [${address}] [${signature}]`)]
-  }
-  return []
 }
 
 class XyoBoundWitnessValidator<T extends XyoBoundWitnessWithPartialMeta = XyoBoundWitnessWithPartialMeta> extends XyoValidatorBase<T> {
@@ -37,7 +26,7 @@ class XyoBoundWitnessValidator<T extends XyoBoundWitnessWithPartialMeta = XyoBou
     return [
       ...validateArraysSameLength(this.obj._signatures ?? [], this.obj.addresses, 'Length mismatch: address/_signature'),
       ...this.obj.addresses.reduce<Error[]>((errors, address, index) => {
-        errors.push(...validateSignature(this.hash, address, this.obj._signatures?.[index]))
+        errors.push(...XyoBoundWitnessValidator.validateSignature(this.hash, address, this.obj._signatures?.[index]))
         return errors
       }, []),
     ]
@@ -48,6 +37,16 @@ class XyoBoundWitnessValidator<T extends XyoBoundWitnessWithPartialMeta = XyoBou
     errors.push(...this.meta.validate(), ...this.body.validate())
     errors.push(...this.signatures())
     return errors
+  }
+
+  static validateSignature(hash: string, address: string, signature?: string) {
+    if (!signature) {
+      return [Error(`Missing signature [${address}]`)]
+    }
+    if (!new XyoAddressValue(address).verify(hash, signature)) {
+      return [Error(`Invalid signature [${address}] [${signature}]`)]
+    }
+    return []
   }
 }
 

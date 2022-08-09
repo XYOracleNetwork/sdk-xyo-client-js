42b51a442fd0dd5b0d9c9620e0739e952448c730
Arie Trouw <arie.trouw@xylabs.com>
2022-08-09 12:44:53 -0700

    changed Diviner type

--- packages/sdk/diviner/src/Diviner/Payload/XyoPayloadDiviner.ts
+++ packages/sdk/diviner/src/Diviner/Payload/XyoPayloadDiviner.ts
@@ -1,8 +1,6 @@
 import { XyoAbstractDiviner } from '../Diviner'
-import { XyoPayloadDivinerPayload } from './XyoPayloadDivinerPayload'
 import { XyoPayloadDivinerQueryPayload } from './XyoPayloadDivinerQueryPayload'
 
 export abstract class XyoPayloadDiviner<
-  TTargetPayload extends XyoPayloadDivinerPayload = XyoPayloadDivinerPayload,
   TQueryPayload extends XyoPayloadDivinerQueryPayload = XyoPayloadDivinerQueryPayload
-> extends XyoAbstractDiviner<TTargetPayload, TQueryPayload> {}
+> extends XyoAbstractDiviner<TQueryPayload> {}

c402ad84375b1b67ac448489128f4d4793e63cbc
Arie Trouw <arie.trouw@xylabs.com>
2022-08-17 10:09:47 -0700

    diviner config / deploy

--- packages/modules/diviner/src/Payload/Huri/Diviner.ts
+++ packages/modules/diviner/src/Payload/Huri/Diviner.ts
@@ -5,26 +5,25 @@ import { XyoDivinerConfig } from '../../Abstract'
 import { XyoDivinerQueryPayload } from '../../Diviner'
 import { profile } from '../lib'
 import { XyoPayloadDiviner } from '../XyoPayloadDiviner'
-import { XyoPayloadDivinerQueryPayload } from '../XyoPayloadDivinerQueryPayload'
 
 export type XyoHuriPayloadDivinerConfig = XyoDivinerConfig<{
   schema: 'network.xyo.diviner.payload.huri.config'
-  huriOptions: HuriOptions
-  huri: string
+  options?: HuriOptions
 }>
 
 export type XyoHuriPayloadDivinerQuery = XyoDivinerQueryPayload<{
   schema: 'network.xyo.diviner.payload.huri.query'
+  options?: HuriOptions
   huri: string
 }>
 
 export class XyoHuriPayloadDiviner extends XyoPayloadDiviner<XyoHuriPayloadDivinerQuery, XyoHuriPayloadDivinerConfig> {
   protected get options() {
-    return this.config.huriOptions
+    return this.config.options
   }
 
-  override async query(query: XyoPayloadDivinerQueryPayload): Promise<[XyoBoundWitness, XyoPayload[]]> {
-    const huri = new Huri(query.huri, this.options)
+  override async query(query: XyoHuriPayloadDivinerQuery): Promise<[XyoBoundWitness, XyoPayload[]]> {
+    const huri = new Huri(query.huri, query.options ?? this.options)
     const [payload = null] = await profile(async () => await huri.fetch())
     const payloads = payload ? [payload] : []
     return [this.bindPayloads(payloads), payloads]

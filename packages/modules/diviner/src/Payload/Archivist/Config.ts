d060847e7cfd488743444b25a24cb4323f079a33
Arie Trouw <arie.trouw@xylabs.com>
2022-08-24 10:56:18 -0700

    XyoArchivistPayloadDivinerConfig fixed

--- packages/modules/diviner/src/Payload/Archivist/Config.ts
+++ packages/modules/diviner/src/Payload/Archivist/Config.ts
@@ -1,18 +1,15 @@
 import { XyoArchivist } from '@xyo-network/archivist'
 import { XyoPayload } from '@xyo-network/payload'
 
-import { XyoHuriPayloadDivinerConfig } from '../Huri'
+import { XyoDivinerConfig } from '../../Config'
 
 export type XyoArchivistPayloadDivinerConfigSchema = 'network.xyo.diviner.payload.archivist.config'
 export const XyoArchivistPayloadDivinerConfigSchema: XyoArchivistPayloadDivinerConfigSchema = 'network.xyo.diviner.payload.archivist.config'
 
-export type XyoArchivistPayloadDivinerConfig<
-  TTargetPayload extends XyoPayload = XyoPayload,
-  T extends XyoPayload = XyoPayload,
-> = XyoHuriPayloadDivinerConfig<
+export type XyoArchivistPayloadDivinerConfig<TTargetPayload extends XyoPayload = XyoPayload, T extends XyoPayload = XyoPayload> = XyoDivinerConfig<
   TTargetPayload,
-  {
+  T & {
     schema: XyoArchivistPayloadDivinerConfigSchema
     archivist: XyoArchivist
-  } & T
+  }
 >

2f3597321373f1d854bf7d62b0d4be7388e749cc
Arie Trouw <arie.trouw@xylabs.com>
2022-08-29 07:57:30 -0700

    Uniswap logic simplification

--- packages/plugins/packages/crypto/packages/market/packages/asset/src/lib/divineUniswapPrices.ts
+++ packages/plugins/packages/crypto/packages/market/packages/asset/src/lib/divineUniswapPrices.ts
@@ -1,6 +1,6 @@
 import { exists, PartialRecord } from '@xylabs/sdk-js'
 import { XyoPayloadBuilder } from '@xyo-network/payload'
-import { XyoUniswapCryptoMarketPayload } from '@xyo-network/uniswap-crypto-market-payload-plugin'
+import { XyoUniswapCryptoMarketPayload, XyoUniswapCryptoPair } from '@xyo-network/uniswap-crypto-market-payload-plugin'
 
 import { AssetInfo, Currency, Token, ValueBasis } from '../Model'
 import { XyoCryptoMarketAssetPayload } from '../Payload'
@@ -17,23 +17,29 @@ const mapUniswapToken = (symbol: string): Token | Currency => {
   return symbol.toLowerCase() as Token
 }
 
+const pairsContainingToken = (uniswapPayload: XyoUniswapCryptoMarketPayload, token: Token) => {
+  return uniswapPayload?.pairs
+    .map((p) => p.tokens)
+    .filter((p) => p.some((x) => x.symbol.toLowerCase() === token))
+    .filter(exists)
+}
+
+const tokensFromPairs = (pairs: XyoUniswapCryptoPair[]) => {
+  return pairs
+    .map((p) => p.tokens)
+    .flatMap((t) => t)
+    .map((t) => t.symbol.toLowerCase() as Token)
+}
+
 export const divineUniswapPrices = (uniswapPayload: XyoUniswapCryptoMarketPayload | undefined): XyoCryptoMarketAssetPayload => {
   let assets: PartialRecord<Token, AssetInfo | undefined> = {}
   if (uniswapPayload) {
-    const tokens: Set<Token> = new Set(
-      uniswapPayload?.pairs
-        .map((p) => p.tokens)
-        .flatMap((t) => t)
-        .map((t) => t.symbol.toLowerCase() as Token),
-    )
+    const tokens: Set<Token> = new Set(tokensFromPairs(uniswapPayload.pairs))
     assets = Object.fromEntries(
       [...tokens].map((token) => {
-        const pairsContainingToken = uniswapPayload?.pairs
-          .map((p) => p.tokens)
-          .filter((p) => p.some((x) => x.symbol.toLowerCase() === token))
-          .filter(exists)
+        const pairs = pairsContainingToken(uniswapPayload, token)
         const value: ValueBasis = Object.fromEntries(
-          pairsContainingToken
+          pairs
             .map((pair) => {
               const current = pair.filter((p) => p.symbol.toLowerCase() === token)?.[0]
               const other = pair.filter((p) => p.symbol.toLowerCase() !== token)?.[0]

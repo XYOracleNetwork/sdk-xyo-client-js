77934b2ceec642d963d0dbdc8f7e9a2ae595faf6
Arie Trouw <arie.trouw@xylabs.com>
2022-09-02 09:49:30 -0700

    initialize/shutdown queries

---
+++ packages/modules/packages/module/src/Query/Shutdown.ts
@@ -0,0 +1,8 @@
+import { XyoQueryPayload } from './Query'
+
+export type XyoModuleShutdownQueryPayloadSchema = 'network.xyo.query.module.shutdown'
+export const XyoModuleShutdownQueryPayloadSchema: XyoModuleShutdownQueryPayloadSchema = 'network.xyo.query.module.shutdown'
+
+export type XyoModuleShutdownQueryPayload = XyoQueryPayload<{
+  schema: XyoModuleShutdownQueryPayloadSchema
+}>

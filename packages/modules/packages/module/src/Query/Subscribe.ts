0c132ea45331bd0a239b9a787a00417cc76178df
Arie Trouw <arie.trouw@xylabs.com>
2022-09-02 19:54:40 -0700

    deps cleanup

--- packages/modules/packages/module/src/Query/Subscribe.ts
+++ packages/modules/packages/module/src/Query/Subscribe.ts
@@ -4,12 +4,14 @@ export type XyoModuleSubscribeQueryPayloadSchema = 'network.xyo.query.module.sub
 export const XyoModuleSubscribeQueryPayloadSchema: XyoModuleSubscribeQueryPayloadSchema = 'network.xyo.query.module.subscribe'
 
 export interface XyoModuleSubscribeFilter {
+  /** @field if specified, at least one of the schemas must be present in the boundwtness to generate a notification */
   schema?: string[]
-  address?: string[]
 }
 
+//requests notification when a boundwitness is added to the modules chain that meets the filter criteria
 export type XyoModuleSubscribeQueryPayload = XyoQueryPayload<{
   schema: XyoModuleSubscribeQueryPayloadSchema
+  /** @field The address that will receive notifications */
   address: string
   /** @field A subscribe with a null for filter is an unsubscribe */
   filter?: XyoModuleSubscribeFilter | null

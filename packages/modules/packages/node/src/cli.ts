c81669414e43294dcdf14e0b826ce780067ded29
Arie Trouw <arie.trouw@xylabs.com>
2022-09-12 16:09:32 -0700

    cli experiement

--- packages/modules/packages/node/src/cli.ts
+++ packages/modules/packages/node/src/cli.ts
@@ -1,4 +1,4 @@
-import { XyoModule } from '@xyo-network/module'
+import { XyoModule, XyoModuleResolverFunc } from '@xyo-network/module'
 import yargs from 'yargs'
 // eslint-disable-next-line import/no-internal-modules
 import { hideBin } from 'yargs/helpers'
@@ -21,11 +21,11 @@ const parseOptions = () => {
     })
 }
 
-const loadModule = async (pkg: string, name?: string): Promise<XyoModule> => {
+const loadModule = async (pkg: string, name?: string, resolver?: XyoModuleResolverFunc): Promise<XyoModule> => {
   const loadedPkg = await import(pkg)
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const ModuleConstructor: any = name ? loadedPkg[name] : loadedPkg
-  return new ModuleConstructor()
+  return new ModuleConstructor(undefined, undefined, resolver)
 }
 
 const xyo = () => {
@@ -37,16 +37,22 @@ const xyo = () => {
         return yargs
       },
       async (yargs) => {
+        console.log(`yargs: ${JSON.stringify(yargs, null, 2)}`)
         const { verbose, module } = yargs
         const modules = Array.isArray(module) ? module : [module]
         if (verbose) console.info('Starting Node')
 
         const node = new XyoMemoryNode()
 
+        const resolver: XyoModuleResolverFunc = (address: string) => {
+          console.log(`Resolving: ${address}`)
+          return node.resolve(address)
+        }
+
         await Promise.all(
           modules.map(async (module) => {
             const [pkg, name] = module.split('.')
-            const instance = await loadModule(pkg, name)
+            const instance = await loadModule(pkg, name, resolver)
             console.log(`Arg: ${instance.address}`)
             node.register(instance)
             node.attach(instance.address)

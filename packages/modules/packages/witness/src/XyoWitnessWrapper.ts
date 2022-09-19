81b0ded8e679b2eaa64a5fab795ca609dfeda92b
Arie Trouw <arie.trouw@xylabs.com>
2022-09-19 16:59:44 -0700

    node wrapper types fixed

--- packages/modules/packages/witness/src/XyoWitnessWrapper.ts
+++ packages/modules/packages/witness/src/XyoWitnessWrapper.ts
@@ -5,10 +5,6 @@ import { XyoWitnessObserveQuerySchema, XyoWitnessQuery } from './Queries'
 import { Witness } from './Witness'
 
 export class XyoWitnessWrapper extends XyoModuleWrapper implements Witness {
-  public get queries() {
-    return this.module.queries
-  }
-
   async observe(payload?: Partial<XyoPayload> | undefined): Promise<XyoPayload | null> {
     const query: XyoWitnessQuery = { payload, schema: XyoWitnessObserveQuerySchema }
     return (await this.module.query(query))[1][0]

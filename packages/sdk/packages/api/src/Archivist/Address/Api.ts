884116a54a90b7f95cba91ed8e64e1d7eb645504
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-19 13:54:52 -0500

    Interfaces for ModuleDescription response

--- packages/sdk/packages/api/src/Archivist/Address/Api.ts
+++ packages/sdk/packages/api/src/Archivist/Address/Api.ts
@@ -2,9 +2,12 @@ import { XyoBoundWitness } from '@xyo-network/boundwitness'
 
 import { XyoApiConfig } from '../../models'
 import { XyoApiSimple } from '../../Simple'
-import { XyoArchivistPayloadApi } from '../Payload'
+export interface ModuleDescription {
+  address: string
+  queries: string[]
+}
 
-export class XyoAddressApi<T extends XyoBoundWitness = XyoBoundWitness, C extends XyoApiConfig = XyoApiConfig> extends XyoArchivistPayloadApi<T, C> {
+export class XyoAddressApi<C extends XyoApiConfig = XyoApiConfig> extends XyoApiSimple<ModuleDescription[], C> {
   public address(address: string): XyoApiSimple<XyoBoundWitness[]> {
     return new XyoApiSimple<XyoBoundWitness[]>({
       ...this.config,

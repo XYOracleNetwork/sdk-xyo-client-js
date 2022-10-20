559adccaf8df052b9d1cc9befe76ca17e00ba913
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-19 21:29:04 -0500

    tests for block chain pagination

--- packages/sdk/packages/api/src/Diviner/AddressHistoryDiviner/XyoRemoteAddressHistoryDiviner.spec.ts
+++ packages/sdk/packages/api/src/Diviner/AddressHistoryDiviner/XyoRemoteAddressHistoryDiviner.spec.ts
@@ -1,5 +1,5 @@
 import { XyoAccount } from '@xyo-network/account'
-import { XyoBoundWitness, XyoBoundWitnessSchema } from '@xyo-network/boundwitness'
+import { BoundWitnessWrapper, XyoBoundWitness, XyoBoundWitnessSchema } from '@xyo-network/boundwitness'
 import { XyoDivinerWrapper } from '@xyo-network/diviner'
 import { XyoPayloadBuilder } from '@xyo-network/payload'
 
@@ -38,6 +38,34 @@ describe('XyoRemoteAddressHistoryDiviner', () => {
       result.map((bw) => {
         expect(bw.addresses).toContain(address)
       })
+    })
+  })
+  describe('with hash offset returns', () => {
+    let result: XyoBoundWitness[]
+    let hash: string
+    beforeAll(async () => {
+      let source = new XyoPayloadBuilder({ schema: AddressHistoryQuerySchema }).fields({ address }).build()
+      result = (await new XyoDivinerWrapper(diviner).divine([source])) as XyoBoundWitness[]
+      expect(result.length).toBeGreaterThan(0)
+      hash = new BoundWitnessWrapper(result?.[0]).hash
+      source = new XyoPayloadBuilder({ schema: AddressHistoryQuerySchema }).fields({ address, offset: hash }).build()
+      result = (await new XyoDivinerWrapper(diviner).divine([source])) as XyoBoundWitness[]
+      expect(result.length).toBeGreaterThan(0)
+    })
+    it('results starting at hash', () => {
+      expect(new BoundWitnessWrapper(result?.[0]).hash).toBe(hash)
+    })
+  })
+  describe('with limit returns', () => {
+    const limit = 1
+    let result: XyoBoundWitness[]
+    beforeAll(async () => {
+      const source = new XyoPayloadBuilder({ schema: AddressHistoryQuerySchema }).fields({ address, limit }).build()
+      result = (await new XyoDivinerWrapper(diviner).divine([source])) as XyoBoundWitness[]
+      expect(result.length).toBeGreaterThan(0)
+    })
+    it('number of results specified by the limit', () => {
+      expect(result.length).toBe(limit)
     })
   })
   describe('with non-existent address', () => {

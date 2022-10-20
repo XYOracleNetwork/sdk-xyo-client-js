bf2e8b7c1ae17ad3f5cdcec8e1996db22dfdb8d4
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-19 20:59:30 -0500

    Divine address from supplied query

--- packages/sdk/packages/api/src/Diviner/AddressHistoryDiviner/XyoRemoteAddressHistoryDiviner.ts
+++ packages/sdk/packages/api/src/Diviner/AddressHistoryDiviner/XyoRemoteAddressHistoryDiviner.ts
@@ -1,11 +1,11 @@
 import { assertEx } from '@xylabs/assert'
-import { XyoAccount } from '@xyo-network/account'
 import { XyoBoundWitness } from '@xyo-network/boundwitness'
-import { XyoDiviner, XyoDivinerDivineQuery, XyoDivinerDivineQuerySchema } from '@xyo-network/diviner'
-import { PayloadWrapper, XyoPayload, XyoPayloadBuilder, XyoPayloads } from '@xyo-network/payload'
+import { XyoDiviner, XyoDivinerDivineQuerySchema } from '@xyo-network/diviner'
+import { XyoPayloads } from '@xyo-network/payload'
 
 import { RemoteDivinerError } from '../RemoteDivinerError'
 import { XyoRemoteDivinerConfig } from '../XyoRemoteDivinerConfig'
+import { isAddressHistoryQueryPayload } from './AddressHistoryDiviner'
 
 /** @description Diviner Context that connects to a remote Diviner using the API */
 export class XyoRemoteAddressHistoryDiviner extends XyoDiviner<XyoRemoteDivinerConfig> {
@@ -20,18 +20,16 @@ export class XyoRemoteAddressHistoryDiviner extends XyoDiviner<XyoRemoteDivinerC
   public override async divine(payloads?: XyoPayloads): Promise<XyoBoundWitness[]> {
     if (!payloads) return []
     try {
-      // const address = payloads.find<{ address: string; schema: string }>((p) => p?.schema === 'TODO')?.address
-      const address = 'TODO'
+      const address = payloads.find(isAddressHistoryQueryPayload)?.address
       if (!address) return []
-      const bwResult = await this.api.addresses.address(address).boundWitnesses.get('tuple')
-      const [, response, error] = bwResult
-      if (error?.status >= 400) {
-        throw new RemoteDivinerError('divine', `${error.statusText} [${error.status}]`)
+      const [data, body, response] = await this.api.addresses.address(address).boundWitnesses.get('tuple')
+      if (response?.status >= 400) {
+        throw new RemoteDivinerError('divine', `${response.statusText} [${response.status}]`)
       }
-      if (response?.error?.length) {
-        throw new RemoteDivinerError('divine', response?.error)
+      if (body?.error?.length) {
+        throw new RemoteDivinerError('divine', body?.error)
       }
-      return bwResult?.[0] as XyoBoundWitness[]
+      return data || []
     } catch (ex) {
       console.error(ex)
       throw ex

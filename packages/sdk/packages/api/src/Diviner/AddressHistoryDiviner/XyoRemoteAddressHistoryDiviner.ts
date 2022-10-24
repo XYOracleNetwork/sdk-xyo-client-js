c215048be06aad36c1b4197d1a4b55c58046129d
Arie Trouw <arie.trouw@xylabs.com>
2022-10-24 14:33:17 -0700

    diviner api moved from config

--- packages/sdk/packages/api/src/Diviner/AddressHistoryDiviner/XyoRemoteAddressHistoryDiviner.ts
+++ packages/sdk/packages/api/src/Diviner/AddressHistoryDiviner/XyoRemoteAddressHistoryDiviner.ts
@@ -1,21 +1,41 @@
-import { assertEx } from '@xylabs/assert'
 import { XyoBoundWitness, XyoBoundWitnessSchema } from '@xyo-network/boundwitness'
 import { XyoDiviner, XyoDivinerDivineQuerySchema } from '@xyo-network/diviner'
 import { XyoModuleParams } from '@xyo-network/module'
 import { XyoPayloads } from '@xyo-network/payload'
 
+import { XyoArchivistApi } from '../../Archivist'
 import { RemoteDivinerError } from '../RemoteDivinerError'
 import { XyoRemoteDivinerConfig, XyoRemoteDivinerConfigSchema } from '../XyoRemoteDivinerConfig'
 import { AddressHistoryDiviner, isAddressHistoryQueryPayload } from './AddressHistoryDiviner'
 
+export type XyoRemoteAddressHistoryDivinerParams = XyoModuleParams<XyoRemoteDivinerConfig> & {
+  api?: XyoArchivistApi
+}
+
 /** @description Diviner Context that connects to a remote Diviner using the API */
 export class XyoRemoteAddressHistoryDiviner extends XyoDiviner<XyoRemoteDivinerConfig> implements AddressHistoryDiviner {
-  static override async create(params?: XyoModuleParams<XyoRemoteDivinerConfig>) {
+  protected _api?: XyoArchivistApi
+
+  static override async create(params?: XyoRemoteAddressHistoryDivinerParams) {
     return (await super.create(params)) as XyoRemoteAddressHistoryDiviner
   }
 
+  protected constructor(params?: XyoRemoteAddressHistoryDivinerParams) {
+    super(params)
+    this._api = params?.api
+  }
+
   public get api() {
-    return assertEx(this.config?.api, 'API not defined')
+    if (this._api) {
+      return this._api
+    }
+    // eslint-disable-next-line deprecation/deprecation
+    if (this.config?.api) {
+      this.logger?.warn('api specified in config but should be specified in params')
+      // eslint-disable-next-line deprecation/deprecation
+      return this.config?.api
+    }
+    throw Error('No api specified')
   }
 
   public override queries() {
--- packages/sdk/packages/api/src/Diviner/XyoRemoteDivinerConfig.ts
+++ packages/sdk/packages/api/src/Diviner/XyoRemoteDivinerConfig.ts
@@ -6,6 +6,7 @@ export type XyoRemoteDivinerConfigSchema = 'network.xyo.diviner.remote.config'
 export const XyoRemoteDivinerConfigSchema: XyoRemoteDivinerConfigSchema = 'network.xyo.diviner.remote.config'
 
 export type XyoRemoteDivinerConfig = XyoDivinerConfig & {
+  /** @deprecated use in params instead */
   api?: XyoArchivistApi
   archive?: string
   schema: XyoRemoteDivinerConfigSchema

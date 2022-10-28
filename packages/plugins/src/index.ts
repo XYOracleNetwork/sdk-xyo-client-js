67a0c37256f3099f48e4ee3ceeea82d61343abd2
Arie Trouw <arie.trouw@xylabs.com>
2022-10-28 10:13:36 -0700

    uniswap peerDeps cleanup

--- packages/plugins/src/index.ts
+++ packages/plugins/src/index.ts
@@ -3,8 +3,10 @@ export * from '@xyo-network/crypto-payload-plugins'
 export * from '@xyo-network/domain-payload-plugin'
 export * from '@xyo-network/elevation-payload-plugin'
 export * from '@xyo-network/id-payload-plugin'
+export * from '@xyo-network/location-certainty-payload-plugin'
 export * from '@xyo-network/location-payload-plugin'
 export * from '@xyo-network/module-instance-payload-plugin'
+export * from '@xyo-network/pentair-payload-plugin'
 export * from '@xyo-network/schema-payload-plugin'
 export * from '@xyo-network/system-payload-plugins'
 
@@ -13,9 +15,11 @@ import { XyoCryptoPayloadPlugins } from '@xyo-network/crypto-payload-plugins'
 import { XyoDomainPayloadPlugin } from '@xyo-network/domain-payload-plugin'
 import { XyoLocationElevationPayloadPlugin } from '@xyo-network/elevation-payload-plugin'
 import { XyoIdPayloadPlugin } from '@xyo-network/id-payload-plugin'
+import { LocationCertaintyPayloadPlugin } from '@xyo-network/location-certainty-payload-plugin'
 import { XyoLocationPayloadPlugin } from '@xyo-network/location-payload-plugin'
 import { XyoModuleInstancePayloadPlugin } from '@xyo-network/module-instance-payload-plugin'
 import { XyoPayloadPluginFunc } from '@xyo-network/payload-plugin'
+import { XyoPentairScreenlogicPayloadPlugin } from '@xyo-network/pentair-payload-plugin'
 import { XyoSchemaPayloadPlugin } from '@xyo-network/schema-payload-plugin'
 import { XyoSystemPayloadPlugins } from '@xyo-network/system-payload-plugins'
 
@@ -28,7 +32,9 @@ export const XyoPayloadPlugins: XyoPayloadPluginFunc[] = [
   XyoIdPayloadPlugin,
   XyoLocationPayloadPlugin,
   XyoModuleInstancePayloadPlugin,
+  XyoPentairScreenlogicPayloadPlugin,
   XyoSchemaPayloadPlugin,
+  LocationCertaintyPayloadPlugin,
 ]
 
 // eslint-disable-next-line import/no-default-export

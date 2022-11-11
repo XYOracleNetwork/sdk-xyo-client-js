883f178a05bcf05a22e2c2cd15ef42f243f703c2
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-10 18:06:16 -0600

    Witness Module & Wrapper test cases

--- packages/modules/packages/witness/src/XyoWitness.spec.ts
+++ packages/modules/packages/witness/src/XyoWitness.spec.ts
@@ -1,26 +1,68 @@
-import { Module, XyoModule } from '@xyo-network/module'
+import { Module, XyoModule, XyoModuleParams } from '@xyo-network/module'
 import { PayloadWrapper, XyoPayloadBuilder } from '@xyo-network/payload'
 
+import { XyoWitnessConfig, XyoWitnessConfigSchema } from './Config'
 import { Witness } from './Witness'
 import { XyoWitness } from './XyoWitness'
 import { XyoWitnessWrapper } from './XyoWitnessWrapper'
 
-test('XyoWitness', async () => {
-  const schema = 'network.xyo.debug'
-  const witness = await XyoWitness.create({
-    config: { schema: 'xyo.network.test.witness.config', targetSchema: 'xyo.network.test' },
+const targetSchema = 'network.xyo.schema.target'
+
+describe('XyoWitness', () => {
+  test('types', async () => {
+    const schema = 'network.xyo.debug'
+    const witness = await XyoWitness.create({
+      config: { schema: XyoWitnessConfigSchema, targetSchema },
+    })
+    const payload = new XyoPayloadBuilder({ schema }).build()
+    const witnessAsModule: Module = witness
+    const witnessAsWitness: Witness = witness
+    const witnessAsXyoModule: XyoModule = witness
+    const wrapper = new XyoWitnessWrapper(witnessAsModule ?? witnessAsXyoModule ?? witnessAsWitness)
+
+    const payloads = await wrapper.observe([payload])
+    expect(payloads).toBeArray()
+    expect(payloads.length).toBeGreaterThan(0)
+    const answerWrapper = new PayloadWrapper(payloads[0])
+
+    expect(answerWrapper.schema).toBe(schema)
+    expect(answerWrapper.valid).toBe(true)
   })
-  const payload = new XyoPayloadBuilder({ schema }).build()
-  const witnessAsModule: Module = witness
-  const witnessAsWitness: Witness = witness
-  const witnessAsXyoModule: XyoModule = witness
-  const wrapper = new XyoWitnessWrapper(witnessAsModule ?? witnessAsXyoModule ?? witnessAsWitness)
-
-  const payloads = await wrapper.observe([payload])
-  expect(payloads).toBeArray()
-  expect(payloads.length).toBeGreaterThan(0)
-  const answerWrapper = new PayloadWrapper(payloads[0])
-
-  expect(answerWrapper.schema).toBe(schema)
-  expect(answerWrapper.valid).toBe(true)
+  describe('observe', () => {
+    const config: XyoWitnessConfig = { schema: XyoWitnessConfigSchema, targetSchema }
+    const params: XyoModuleParams<XyoWitnessConfig> = { config }
+    describe('with no payload supplied to observe', () => {
+      describe('returns empty array', () => {
+        it('when module queried directly', async () => {
+          const witness = await XyoWitness.create(params)
+          const observation = await witness.observe()
+          expect(observation).toBeArrayOfSize(0)
+        })
+        it('when module queried with XyoWitnessWrapper', async () => {
+          const witness = await XyoWitness.create(params)
+          const wrapper = new XyoWitnessWrapper(witness)
+          const observation = await wrapper.observe()
+          expect(observation).toBeArrayOfSize(0)
+        })
+      })
+    })
+    describe('with payload supplied to observe', () => {
+      describe('returns payloads with targetSchema', () => {
+        const observed = new XyoPayloadBuilder({ schema: 'network.xyo.test' }).build()
+        it('when module queried directly', async () => {
+          const witness = await XyoWitness.create(params)
+          const observation = await witness.observe([observed])
+          expect(observation).toBeArrayOfSize(1)
+          expect(observation?.[0]?.schema).toBe(targetSchema)
+        })
+        it('when module queried with XyoWitnessWrapper', async () => {
+          const witness = await XyoWitness.create(params)
+          const wrapper = new XyoWitnessWrapper(witness)
+          const observation = await wrapper.observe([observed])
+          expect(observation).toBeArrayOfSize(1)
+          expect(observation?.[0]?.schema).toBe(targetSchema)
+        })
+      })
+    })
+  })
 })
--- packages/sdk/packages/witnesses/src/Adhoc/Witness.spec.ts
+++ packages/sdk/packages/witnesses/src/Adhoc/Witness.spec.ts
@@ -1,25 +1,44 @@
 import { XyoModuleParams } from '@xyo-network/module'
 import { XyoPayloadBuilder } from '@xyo-network/payload'
+import { XyoWitnessWrapper } from '@xyo-network/witness'
 
 import { XyoAdhocWitness, XyoAdhocWitnessConfig, XyoAdhocWitnessConfigSchema } from './Witness'
+
+const targetSchema = 'network.xyo.schema.target'
 
 describe('XyoAdhocWitness', () => {
   describe('observe', () => {
     const payload = new XyoPayloadBuilder({ schema: 'network.xyo.debug' }).build()
-    const config: XyoAdhocWitnessConfig = { payload, schema: XyoAdhocWitnessConfigSchema, targetSchema: 'network.xyo.schema.target' }
+    const config: XyoAdhocWitnessConfig = { payload, schema: XyoAdhocWitnessConfigSchema, targetSchema }
     const params: XyoModuleParams<XyoAdhocWitnessConfig> = { config }
     describe('with no payload supplied to observe', () => {
       it('uses targetSchema', async () => {
         const witness = await XyoAdhocWitness.create(params)
         const observation = await witness.observe()
+        expect(observation).toBeArrayOfSize(1)
+        expect(observation?.[0]?.schema).toBe(config.targetSchema)
+      })
+      it('uses targetSchema with XyoWitnessWrapper', async () => {
+        const witness = await XyoAdhocWitness.create(params)
+        const wrapper = new XyoWitnessWrapper(witness)
+        const observation = await wrapper.observe()
+        expect(observation).toBeArrayOfSize(1)
         expect(observation?.[0]?.schema).toBe(config.targetSchema)
       })
     })
     describe('with payload supplied to observe', () => {
+      const observed = new XyoPayloadBuilder({ schema: 'network.xyo.test' }).build()
       it('uses payload schema', async () => {
-        const observed = new XyoPayloadBuilder({ schema: 'network.xyo.test' }).build()
         const witness = await XyoAdhocWitness.create(params)
         const observation = await witness.observe([observed])
+        expect(observation).toBeArrayOfSize(1)
+        expect(observation?.[0]?.schema).toBe(observed.schema)
+      })
+      it('uses payload schema with XyoWitnessWrapper', async () => {
+        const witness = await XyoAdhocWitness.create(params)
+        const wrapper = new XyoWitnessWrapper(witness)
+        const observation = await wrapper.observe([observed])
+        expect(observation).toBeArrayOfSize(1)
         expect(observation?.[0]?.schema).toBe(observed.schema)
       })
     })

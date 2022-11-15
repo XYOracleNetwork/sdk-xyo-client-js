9fb95eca2eb111e7ca1a69f133c80e3a3196abf2
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-14 18:29:19 -0600

    Remove helpers in favor of native module functionality

--- packages/node/packages/core/lib/src/Module/getModuleDescription.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@xyo-network/module'
-import { ModuleDescription } from '@xyo-network/node-core-model'
-
-export const getModuleDescription = (module: Module): ModuleDescription => {
-  const { address, queries: getQueries } = module
-  const queries = getQueries()
-  return { address, queries }
-}

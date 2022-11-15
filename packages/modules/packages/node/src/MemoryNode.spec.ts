a4f4fd2c1f3e99982b30e162aa6962c9d3048f92
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-14 20:03:19 -0600

    Tests for module (& nested module) descriptions

--- packages/modules/packages/node/src/MemoryNode.spec.ts
+++ packages/modules/packages/node/src/MemoryNode.spec.ts
@@ -1,5 +1,5 @@
 /* eslint-disable max-statements */
-import { XyoArchivistWrapper } from '@xyo-network/archivist'
+import { XyoArchivistWrapper, XyoMemoryArchivist } from '@xyo-network/archivist'
 import {
   DivinerModule,
   XyoArchivistPayloadDiviner,
@@ -8,57 +8,96 @@ import {
   XyoHuriPayload,
   XyoHuriSchema,
 } from '@xyo-network/diviner'
-import { XyoModule, XyoModuleResolver } from '@xyo-network/module'
+import { ModuleDescription, XyoModule, XyoModuleResolver } from '@xyo-network/module'
 import { PayloadWrapper, XyoPayload, XyoPayloadBuilder, XyoPayloadSchema } from '@xyo-network/sdk'
 
 import { MemoryNode } from './MemoryNode'
 import { NodeModule } from './NodeModule'
 
-test('Create Node', async () => {
-  const XyoMemoryArchivist = (await import('@xyo-network/archivist')).XyoMemoryArchivist
-  const node: NodeModule = await MemoryNode.create()
-  const archivist = await XyoMemoryArchivist.create()
-  const diviner: XyoModule = await XyoArchivistPayloadDiviner.create({
-    config: { archivist: archivist.address, schema: XyoArchivistPayloadDivinerConfigSchema },
-    resolver: new XyoModuleResolver().add(archivist),
-  })
-  await node.register(archivist)
-  node.attach(archivist.address)
-  await node.register(diviner)
-  node.attach(diviner.address)
-  expect((await node.registered()).length).toBe(2)
-  expect((await node.attached()).length).toBe(2)
-  const foundArchivist = (await node.resolve({ address: [archivist.address] })).shift()
-  expect(foundArchivist).toBeDefined()
-  expect(foundArchivist?.address).toBe(archivist.address)
-  const testPayload = new XyoPayloadBuilder<XyoPayload<{ schema: XyoPayloadSchema; test: boolean }>>({ schema: XyoPayloadSchema })
-    .fields({ test: true })
-    .build()
+describe('MemoryNode', () => {
+  describe('create', () => {
+    it('Creates MemoryNode', async () => {
+      const XyoMemoryArchivist = (await import('@xyo-network/archivist')).XyoMemoryArchivist
+      const node: NodeModule = await MemoryNode.create()
+      const archivist = await XyoMemoryArchivist.create()
+      const diviner: XyoModule = await XyoArchivistPayloadDiviner.create({
+        config: { archivist: archivist.address, schema: XyoArchivistPayloadDivinerConfigSchema },
+        resolver: new XyoModuleResolver().add(archivist),
+      })
+      await node.register(archivist)
+      node.attach(archivist.address)
+      await node.register(diviner)
+      node.attach(diviner.address)
+      expect((await node.registered()).length).toBe(2)
+      expect((await node.attached()).length).toBe(2)
+      const foundArchivist = (await node.resolve({ address: [archivist.address] })).shift()
+      expect(foundArchivist).toBeDefined()
+      expect(foundArchivist?.address).toBe(archivist.address)
+      const testPayload = new XyoPayloadBuilder<XyoPayload<{ schema: XyoPayloadSchema; test: boolean }>>({ schema: XyoPayloadSchema })
+        .fields({ test: true })
+        .build()
 
-  const foundArchivistWrapper = foundArchivist ? new XyoArchivistWrapper(foundArchivist) : undefined
-  await foundArchivistWrapper?.insert([testPayload])
+      const foundArchivistWrapper = foundArchivist ? new XyoArchivistWrapper(foundArchivist) : undefined
+      await foundArchivistWrapper?.insert([testPayload])
 
-  /*const subscribeQuery: XyoModuleSubscribeQuery = { payloads: [testPayload], schema: XyoModuleSubscribeQuerySchema }
+      /*const subscribeQuery: XyoModuleSubscribeQuery = { payloads: [testPayload], schema: XyoModuleSubscribeQuerySchema }
   await foundArchivist?.query(subscribeQuery)*/
 
-  const payloads = await foundArchivistWrapper?.all()
-  expect(payloads?.length).toBe(1)
-
-  if (payloads && payloads[0]) {
-    const huri = new PayloadWrapper(payloads[0]).hash
-    const huriPayload: XyoHuriPayload = { huri: [huri], schema: XyoHuriSchema }
-    const module = (await node.resolve({ address: [diviner.address] })).shift() as DivinerModule
-    const foundDiviner = module ? new XyoDivinerWrapper(module) : null
-    expect(foundDiviner).toBeDefined()
-    if (foundDiviner) {
-      const foundDivinerWrapper = new XyoDivinerWrapper(foundDiviner)
-      const payloads = await foundDivinerWrapper.divine([huriPayload])
-      console.log(`payloads: ${JSON.stringify(payloads, null, 2)}`)
+      const payloads = await foundArchivistWrapper?.all()
       expect(payloads?.length).toBe(1)
-      expect(payloads[0]).toBeDefined()
-      if (payloads?.length === 1 && payloads[0]) {
-        expect(new PayloadWrapper(payloads[0]).hash).toBe(huri)
+
+      if (payloads && payloads[0]) {
+        const huri = new PayloadWrapper(payloads[0]).hash
+        const huriPayload: XyoHuriPayload = { huri: [huri], schema: XyoHuriSchema }
+        const module = (await node.resolve({ address: [diviner.address] })).shift() as DivinerModule
+        const foundDiviner = module ? new XyoDivinerWrapper(module) : null
+        expect(foundDiviner).toBeDefined()
+        if (foundDiviner) {
+          const foundDivinerWrapper = new XyoDivinerWrapper(foundDiviner)
+          const payloads = await foundDivinerWrapper.divine([huriPayload])
+          // console.log(`payloads: ${JSON.stringify(payloads, null, 2)}`)
+          expect(payloads?.length).toBe(1)
+          expect(payloads[0]).toBeDefined()
+          if (payloads?.length === 1 && payloads[0]) {
+            expect(new PayloadWrapper(payloads[0]).hash).toBe(huri)
+          }
+        }
       }
+    })
+  })
+  describe('description', () => {
+    let node: MemoryNode
+    const validateModuleDescription = (description: ModuleDescription) => {
+      expect(description).toBeObject()
+      expect(description.address).toBeString()
+      expect(description.queries).toBeArray()
+      description.queries.map((query) => {
+        expect(query).toBeString()
+      })
     }
-  }
+    beforeEach(async () => {
+      node = await MemoryNode.create()
+    })
+    it('describes a node without children', async () => {
+      const description = await node.description()
+      validateModuleDescription(description)
+      expect(description.children).toBeArrayOfSize(0)
+    })
+    it('describes a node with children', async () => {
+      const archivists = await Promise.all([
+        await XyoMemoryArchivist.create(),
+        await XyoMemoryArchivist.create(),
+        await XyoMemoryArchivist.create(),
+        await XyoMemoryArchivist.create(),
+      ])
+      archivists.map((archivist) => {
+        node.register(archivist)
+        node.attach(archivist.address)
+      })
+      const description = await node.description()
+      validateModuleDescription(description)
+      expect(description.children).toBeArrayOfSize(4)
+      description.children?.map(validateModuleDescription)
+    })
+  })
 })

82a7c2fafd5d074d755a91bae03934af0f8cc5a3
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-16 17:26:04 -0600

    Use identity functions in diviner filters

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/divineGas.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/divineGas.ts
@@ -1,12 +1,11 @@
-import { XyoEthereumGasEtherchainV1Schema, XyoEthereumGasEtherchainV2Schema } from '@xyo-network/etherchain-gas-ethereum-blockchain-payload-plugins'
-import { XyoEthereumGasEtherscanSchema } from '@xyo-network/etherscan-ethereum-gas-payload-plugin'
 import { XyoPayload, XyoPayloads } from '@xyo-network/payload'
 
+import { isXyoEthereumGasEtherchainV1Payload, isXyoEthereumGasEtherchainV2Payload, isXyoEthereumGasEtherscanPayload } from './identities'
 import { transformGasFromEtherscan } from './transforms'
 
 export const divineGas = (payloads: XyoPayloads): XyoPayload => {
-  const etherChainV1 = payloads.filter((p) => p.schema === XyoEthereumGasEtherchainV1Schema)
-  const etherChainV2 = payloads.filter((p) => p.schema === XyoEthereumGasEtherchainV2Schema)
-  const etherscan = payloads.filter((p) => p.schema === XyoEthereumGasEtherscanSchema).map(transformGasFromEtherscan)
+  const etherChainV1 = payloads.filter(isXyoEthereumGasEtherchainV1Payload)
+  const etherChainV2 = payloads.filter(isXyoEthereumGasEtherchainV2Payload)
+  const etherscan = payloads.filter(isXyoEthereumGasEtherscanPayload).map(transformGasFromEtherscan)
   throw new Error('Not Implemented')
 }

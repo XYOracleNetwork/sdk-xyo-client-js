a91ddd3ffe575565479199e5f737f810a73f977e
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-16 12:39:51 -0600

    Test case matrix

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/divineGas.spec.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/divineGas.spec.ts
@@ -1,9 +1,47 @@
+import { sampleEtherchainGasV1, sampleEtherchainGasV2, sampleEtherscanGas } from '../test'
 import { divineGas } from './divineGas'
 
-describe('divineGas', () => {
-  it('divines gas', () => {
-    const result = divineGas()
-    expect(result).toBeObject()
-    expect(result.timestamp).toBeNumber()
+describe.skip('divineGas', () => {
+  describe('with no payloads supplied', () => {
+    it('divines gas', () => {
+      const result = divineGas([])
+      expect(result).toBeObject()
+      expect(result.timestamp).toBeNumber()
+    })
+  })
+  describe('with sparse payloads supplied', () => {
+    it.each([
+      [sampleEtherchainGasV1],
+      [sampleEtherchainGasV2],
+      [sampleEtherscanGas],
+      [sampleEtherchainGasV1, sampleEtherchainGasV2],
+      [sampleEtherchainGasV2, sampleEtherscanGas],
+      [sampleEtherchainGasV1, sampleEtherscanGas],
+    ])('divines gas', (...payloads) => {
+      const result = divineGas(payloads)
+      expect(result).toBeObject()
+      expect(result.timestamp).toBeNumber()
+    })
+  })
+  describe('with one of each supported payload supplied', () => {
+    it('divines gas', () => {
+      const result = divineGas([sampleEtherchainGasV1, sampleEtherchainGasV2, sampleEtherscanGas])
+      expect(result).toBeObject()
+      expect(result.timestamp).toBeNumber()
+    })
+  })
+  describe('with multiple of each supported payload supplied', () => {
+    it('divines gas', () => {
+      const result = divineGas([
+        sampleEtherchainGasV1,
+        sampleEtherchainGasV1,
+        sampleEtherchainGasV2,
+        sampleEtherchainGasV2,
+        sampleEtherscanGas,
+        sampleEtherscanGas,
+      ])
+      expect(result).toBeObject()
+      expect(result.timestamp).toBeNumber()
+    })
   })
 })

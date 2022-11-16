af2dcd61b0f9d5cc5c2671733c445bf40676826c
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-16 12:25:25 -0600

    Prep for divining gas from multiple sources

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/divineGas.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/divineGas.ts
@@ -1,6 +1,10 @@
-import { XyoPayload } from '@xyo-network/payload'
+import { XyoEthereumGasEtherchainV1Schema, XyoEthereumGasEtherchainV2Schema } from '@xyo-network/etherchain-gas-ethereum-blockchain-payload-plugins'
+import { XyoEthereumGasEtherscanSchema } from '@xyo-network/etherscan-ethereum-gas-payload-plugin'
+import { XyoPayload, XyoPayloads } from '@xyo-network/payload'
 
-export const divineGas = (): // TODO: Props
-XyoPayload => {
+export const divineGas = (payloads: XyoPayloads): XyoPayload => {
+  const etherChainV1 = payloads.filter((p) => p.schema === XyoEthereumGasEtherchainV1Schema)
+  const etherChainV2 = payloads.filter((p) => p.schema === XyoEthereumGasEtherchainV2Schema)
+  const etherscan = payloads.filter((p) => p.schema === XyoEthereumGasEtherscanSchema)
   throw new Error('Not Implemented')
 }

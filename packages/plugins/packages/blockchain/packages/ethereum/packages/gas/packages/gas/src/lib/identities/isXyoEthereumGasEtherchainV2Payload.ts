92f4b43ad5bea2bccf192357133dd23b5becb142
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-16 16:57:06 -0600

    isXyoEthereumGasEtherchainV2Payload identity function

---
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/identities/isXyoEthereumGasEtherchainV2Payload.ts
@@ -0,0 +1,6 @@
+import { XyoEthereumGasEtherchainV2Payload, XyoEthereumGasEtherchainV2Schema } from '@xyo-network/etherchain-gas-ethereum-blockchain-payload-plugins'
+import { XyoPayload } from '@xyo-network/payload'
+
+export const isXyoEthereumGasEtherchainV2Payload = (payload?: XyoPayload | null): payload is XyoEthereumGasEtherchainV2Payload => {
+  return payload?.schema === XyoEthereumGasEtherchainV2Schema
+}

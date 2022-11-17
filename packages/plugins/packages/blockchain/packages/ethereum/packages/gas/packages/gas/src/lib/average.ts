475604c84f97e0484ca4f1fd98617d8e390e20a5
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-17 09:11:12 -0600

    Average gas prices helper method

---
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/average.ts
@@ -0,0 +1,40 @@
+import { exists } from '@xylabs/exists'
+
+import { TransactionCosts } from '../Model'
+
+export const average = (...input: (TransactionCosts | undefined)[]): TransactionCosts => {
+  // Get all the assets represented
+  const filtered = input.filter(exists)
+  const count = filtered.length
+  const sum: TransactionCosts = filtered.reduce((a, b) => {
+    return {
+      baseFee: {
+        medium: a.baseFee.medium + b.baseFee.medium,
+      },
+      gas: {
+        high: a.gas.high + b.gas.high,
+        low: a.gas.low + b.gas.low,
+        medium: a.gas.medium + b.gas.low,
+        veryHigh: a.gas.veryHigh + b.gas.veryHigh,
+      },
+      priorityFee: {
+        medium: a.priorityFee.medium + b.priorityFee.medium,
+      },
+    }
+  })
+  const average: TransactionCosts = {
+    baseFee: {
+      medium: sum.baseFee.medium / count,
+    },
+    gas: {
+      high: sum.gas.high / count,
+      low: sum.gas.low / count,
+      medium: sum.gas.medium / count,
+      veryHigh: sum.gas.veryHigh / count,
+    },
+    priorityFee: {
+      medium: sum.priorityFee.medium / count,
+    },
+  }
+  return average
+}

168e1b741f0b1bc1cd141ded78b182559103239f
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-16 19:04:25 -0600

    Symmetry in gas estimate API calls

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/etherscan/src/lib/getGasFromEtherscan.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/etherscan/src/lib/getGasFromEtherscan.ts
@@ -3,8 +3,6 @@ import axios from 'axios'
 import { EthereumGasEtherscanResponse } from '../Payload'
 
 export const getGasFromEtherscan = async (apiKey: string): Promise<EthereumGasEtherscanResponse> => {
-  const gasPrices = (
-    await axios.get<EthereumGasEtherscanResponse>(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${apiKey}`)
-  ).data
-  return gasPrices
+  const url = `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${apiKey}`
+  return (await axios.get<EthereumGasEtherscanResponse>(url)).data
 }

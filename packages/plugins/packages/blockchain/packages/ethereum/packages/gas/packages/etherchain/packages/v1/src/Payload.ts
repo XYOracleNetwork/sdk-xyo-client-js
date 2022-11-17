89341fe34182725b50a8f6543d6e53d9031876f9
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-16 18:55:58 -0600

    Use internal interface to construct payload

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/etherchain/packages/v1/src/Payload.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/etherchain/packages/v1/src/Payload.ts
@@ -2,13 +2,17 @@ import { XyoPayload } from '@xyo-network/payload'
 
 import { XyoEthereumGasEtherchainV1Schema } from './Schema'
 
-export type XyoEthereumGasEtherchainV1Payload = XyoPayload<{
+export interface EthereumGasEtherchainV1Response {
   currentBaseFee: number
   fast: number
   fastest: number
   recommendedBaseFee: number
   safeLow: number
-  schema: XyoEthereumGasEtherchainV1Schema
   standard: number
-  timestamp: number
-}>
+}
+
+export type XyoEthereumGasEtherchainV1Payload = XyoPayload<
+  EthereumGasEtherchainV1Response & {
+    schema: XyoEthereumGasEtherchainV1Schema
+  }
+>

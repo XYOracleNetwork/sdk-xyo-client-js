966b7aa32bd91b579e865858a160ca15ca818130
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-17 09:50:29 -0600

    Diviner round trip & tests

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/divineGas.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/divineGas.ts
@@ -1,11 +1,19 @@
-import { XyoPayload, XyoPayloads } from '@xyo-network/payload'
+import { XyoPayloadBuilder, XyoPayloads } from '@xyo-network/payload'
 
+import { TransactionCosts } from '../Model'
+import { XyoEthereumGasPayload } from '../Payload'
+import { XyoEthereumGasSchema } from '../Schema'
+import { average } from './average'
 import { isXyoEthereumGasEtherchainV1Payload, isXyoEthereumGasEtherchainV2Payload, isXyoEthereumGasEtherscanPayload } from './identities'
-import { transformGasFromEtherscan } from './transforms'
+import { transformGasFromEtherchainV1, transformGasFromEtherchainV2, transformGasFromEtherscan } from './transforms'
 
-export const divineGas = (payloads: XyoPayloads): XyoPayload => {
-  const etherChainV1 = payloads.filter(isXyoEthereumGasEtherchainV1Payload)
-  const etherChainV2 = payloads.filter(isXyoEthereumGasEtherchainV2Payload)
+export const divineGas = (payloads: XyoPayloads): XyoEthereumGasPayload => {
+  const etherChainV1 = payloads.filter(isXyoEthereumGasEtherchainV1Payload).map(transformGasFromEtherchainV1)
+  const etherChainV2 = payloads.filter(isXyoEthereumGasEtherchainV2Payload).map(transformGasFromEtherchainV2)
   const etherscan = payloads.filter(isXyoEthereumGasEtherscanPayload).map(transformGasFromEtherscan)
-  throw new Error('Not Implemented')
+  const transactionCosts: TransactionCosts[] = [...etherChainV1, ...etherChainV2, ...etherscan]
+  const avg = average(transactionCosts)
+  const timestamp = Date.now()
+  const payload = new XyoPayloadBuilder<XyoEthereumGasPayload>({ schema: XyoEthereumGasSchema }).fields({ ...avg, timestamp }).build()
+  return payload
 }

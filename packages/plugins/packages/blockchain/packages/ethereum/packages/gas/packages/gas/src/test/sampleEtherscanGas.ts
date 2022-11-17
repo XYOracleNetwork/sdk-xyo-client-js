a1f35dca2e457b99d05558446329eaa51f63e08e
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-17 09:49:55 -0600

    Format test data

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/test/sampleEtherscanGas.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/test/sampleEtherscanGas.ts
@@ -3,14 +3,14 @@ import { XyoEthereumGasEtherscanPayload, XyoEthereumGasEtherscanSchema } from '@
 export const sampleEtherscanGas: XyoEthereumGasEtherscanPayload = {
   message: 'OK',
   result: {
-    FastGasPrice: '13',
-    LastBlock: '15986476',
-    ProposeGasPrice: '13',
-    SafeGasPrice: '12',
-    gasUsedRatio: '0.345005466666667,0.391415466666667,0.424558733333333,0.513428133333333,0.428615366666667',
-    suggestBaseFee: '11.744544475',
+    FastGasPrice: '30',
+    LastBlock: '15990556',
+    ProposeGasPrice: '29',
+    SafeGasPrice: '28',
+    gasUsedRatio: '0.416266666666667,0.5672862,0.540979033333333,0.342410966666667,0.389071233333333',
+    suggestBaseFee: '27.616709247',
   },
   schema: XyoEthereumGasEtherscanSchema,
   status: '1',
-  timestamp: 1668648728013,
+  timestamp: 1668697958837,
 }

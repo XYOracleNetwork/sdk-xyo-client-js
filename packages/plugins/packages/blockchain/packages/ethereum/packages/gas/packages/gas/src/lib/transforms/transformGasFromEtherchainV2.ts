87b3af63d3a20b062577ca7366750d5fedd75e26
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-17 14:17:14 -0600

    Use correct units for v2 response

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/transforms/transformGasFromEtherchainV2.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/transforms/transformGasFromEtherchainV2.ts
@@ -6,21 +6,21 @@ import { MinPriorityFee } from './PriorityFeeConstants'
 
 const getGasRange = (payload: XyoEthereumGasEtherchainV2Payload): GasRange => {
   const { slow, standard, fast, rapid } = payload.data
-  const low = parseUnits(slow.toString(), 'gwei').toNumber()
-  const medium = parseUnits(standard.toString(), 'gwei').toNumber()
-  const high = parseUnits(fast.toString(), 'gwei').toNumber()
-  const veryHigh = parseUnits(rapid.toString(), 'gwei').toNumber()
+  const low = parseUnits(slow.toString(), 'wei').toNumber()
+  const medium = parseUnits(standard.toString(), 'wei').toNumber()
+  const high = parseUnits(fast.toString(), 'wei').toNumber()
+  const veryHigh = parseUnits(rapid.toString(), 'wei').toNumber()
   return { high, low, medium, veryHigh }
 }
 
 const getBaseFeeRange = (payload: XyoEthereumGasEtherchainV2Payload): BaseFeeRange => {
-  const medium = parseUnits('0', 'gwei').toNumber()
+  const medium = parseUnits('0', 'wei').toNumber()
   return { medium }
 }
 
 const getPriorityFeeRange = (payload: XyoEthereumGasEtherchainV2Payload): PriorityFeeRange => {
   const low = MinPriorityFee
-  const medium = Math.max(parseUnits('0', 'gwei').toNumber(), low)
+  const medium = Math.max(parseUnits('0', 'wei').toNumber(), low)
   return { low, medium }
 }
 

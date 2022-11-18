f3c334df24119a5f48e4612efd2ce9335c11c9d9
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-17 22:17:31 -0600

    API documentation

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/etherscan/src/Payload.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/etherscan/src/Payload.ts
@@ -2,14 +2,35 @@ import { XyoPayload } from '@xyo-network/payload'
 
 import { XyoEthereumGasEtherscanSchema } from './Schema'
 
+/**
+ * https://docs.etherscan.io/api-endpoints/gas-tracker#get-gas-oracle
+ */
 export interface EthereumGasEtherscanResponse {
   message: string
   result: {
+    /**
+     * Priority Fee
+     */
     FastGasPrice: string
+    /**
+     * Number of last block
+     */
     LastBlock: string
+    /**
+     * Priority Fee
+     */
     ProposeGasPrice: string
+    /**
+     * Priority Fee
+     */
     SafeGasPrice: string
+    /**
+     * Estimate of how busy the network is
+     */
     gasUsedRatio: string
+    /**
+     * The baseFee of the next pending block
+     */
     suggestBaseFee: string
   }
   status: string

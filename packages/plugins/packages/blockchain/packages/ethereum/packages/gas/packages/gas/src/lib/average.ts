2da25dbf7eba208d87d845acfbff667c2dc751e5
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-17 21:20:05 -0600

    WIP towards fully sparse data handling

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/average.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/average.ts
@@ -1,6 +1,28 @@
 import { exists } from '@xylabs/exists'
 
-import { FeeData } from '../Model'
+import { BaseFee, FeeData, FeePerGas, PriorityFeePerGas } from '../Model'
+
+type PropertyArray<Type> = {
+  [Property in keyof Type]: Array<number>
+}
+
+interface FeeDataPropertyArray {
+  baseFee: PropertyArray<BaseFee>
+  feePerGas: PropertyArray<FeePerGas>
+  priorityFeePerGas: PropertyArray<PriorityFeePerGas>
+}
+
+interface PartialFeedData {
+  baseFee: Partial<BaseFee>
+  feePerGas: Partial<FeePerGas>
+  priorityFeePerGas: Partial<PriorityFeePerGas>
+}
+
+const listAverage = (x?: number[]) => (x?.length ? x.reduce((a, b) => a + b) / x.length : undefined)
+
+const averageProperty = (data: FeeData[], propertyExpression: (x: FeeData) => number | undefined) => {
+  return listAverage(data.map(propertyExpression).filter(exists))
+}
 
 export const average = (input?: (FeeData | undefined)[]): FeeData => {
   // Get all the assets represented
@@ -9,6 +31,8 @@ export const average = (input?: (FeeData | undefined)[]): FeeData => {
   if (count < 1) {
     throw new Error('Unable to compute average on empty list')
   }
+  // TODO: We should sum per property and divide by existent properties
+  // to allow for the handling of missing/undefined values
   const sum: FeeData = filtered.reduce((a, b) => {
     return {
       baseFee: {
@@ -26,6 +50,74 @@ export const average = (input?: (FeeData | undefined)[]): FeeData => {
       },
     }
   }, filtered[0])
+  const feeDataList: FeeDataPropertyArray = {
+    baseFee: {
+      medium: filtered.map((x) => x.baseFee.medium).filter(exists),
+    },
+    feePerGas: {
+      high: filtered.map((x) => x.feePerGas.high).filter(exists),
+      low: filtered.map((x) => x.feePerGas.low).filter(exists),
+      medium: filtered.map((x) => x.feePerGas.medium).filter(exists),
+      veryHigh: filtered.map((x) => x.feePerGas.veryHigh).filter(exists),
+    },
+    priorityFeePerGas: {
+      high: filtered.map((x) => x.priorityFeePerGas.high).filter(exists),
+      low: filtered.map((x) => x.priorityFeePerGas.low).filter(exists),
+      medium: filtered.map((x) => x.priorityFeePerGas.medium).filter(exists),
+      veryHigh: filtered.map((x) => x.priorityFeePerGas.veryHigh).filter(exists),
+    },
+  }
+  const avg: PartialFeedData = {
+    baseFee: {
+      medium: listAverage(feeDataList.baseFee.medium),
+    },
+    feePerGas: {
+      high: listAverage(feeDataList.feePerGas.high),
+      low: listAverage(feeDataList.feePerGas.low),
+      medium: listAverage(feeDataList.feePerGas.medium),
+      veryHigh: listAverage(feeDataList.feePerGas.veryHigh),
+    },
+    priorityFeePerGas: {
+      high: listAverage(feeDataList.priorityFeePerGas.high),
+      low: listAverage(feeDataList.priorityFeePerGas.low),
+      medium: listAverage(feeDataList.priorityFeePerGas.medium),
+      veryHigh: listAverage(feeDataList.priorityFeePerGas.veryHigh),
+    },
+  }
+  const otherAvg: PartialFeedData = {
+    baseFee: {
+      medium: listAverage(filtered.map((x) => x.baseFee.medium).filter(exists)),
+    },
+    feePerGas: {
+      high: listAverage(filtered.map((x) => x.feePerGas.high).filter(exists)),
+      low: listAverage(filtered.map((x) => x.feePerGas.low).filter(exists)),
+      medium: listAverage(filtered.map((x) => x.feePerGas.medium).filter(exists)),
+      veryHigh: listAverage(filtered.map((x) => x.feePerGas.veryHigh).filter(exists)),
+    },
+    priorityFeePerGas: {
+      high: listAverage(filtered.map((x) => x.priorityFeePerGas.high).filter(exists)),
+      low: listAverage(filtered.map((x) => x.priorityFeePerGas.low).filter(exists)),
+      medium: listAverage(filtered.map((x) => x.priorityFeePerGas.medium).filter(exists)),
+      veryHigh: listAverage(filtered.map((x) => x.priorityFeePerGas.veryHigh).filter(exists)),
+    },
+  }
+  const otherOtherAvg: PartialFeedData = {
+    baseFee: {
+      medium: averageProperty(filtered, (x) => x.baseFee.medium),
+    },
+    feePerGas: {
+      high: averageProperty(filtered, (x) => x.feePerGas.high),
+      low: averageProperty(filtered, (x) => x.feePerGas.low),
+      medium: averageProperty(filtered, (x) => x.feePerGas.medium),
+      veryHigh: averageProperty(filtered, (x) => x.feePerGas.veryHigh),
+    },
+    priorityFeePerGas: {
+      high: averageProperty(filtered, (x) => x.priorityFeePerGas.high),
+      low: averageProperty(filtered, (x) => x.priorityFeePerGas.low),
+      medium: averageProperty(filtered, (x) => x.priorityFeePerGas.medium),
+      veryHigh: averageProperty(filtered, (x) => x.priorityFeePerGas.veryHigh),
+    },
+  }
   const average: FeeData = {
     baseFee: {
       medium: sum.baseFee.medium / count,

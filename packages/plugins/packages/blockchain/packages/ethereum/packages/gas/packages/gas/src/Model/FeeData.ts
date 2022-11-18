5d5c47e7e8521c1c43c211e0b22e5cf3febe1b9d
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-17 19:15:20 -0600

    Add documentation

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/Model/FeeData.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/Model/FeeData.ts
@@ -1,9 +1,34 @@
+/* eslint-disable @typescript-eslint/member-ordering */
 import { BaseFee } from './BaseFee'
 import { FeePerGas } from './FeePerGas'
 import { PriorityFeePerGas } from './PriorityFeePerGas'
 
 export interface FeeData {
+  // Legacy
+  gasPrice?: string
+
+  // EIP-1559
+  // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1559.md
+  /**
+   * There is a base fee per gas in protocol, which can move up or
+   * down each block according to a formula which is a function of
+   * gas used in parent block and gas target (block gas limit divided
+   * by elasticity multiplier) of parent block. The algorithm results
+   * in the base fee per gas increasing when blocks are above the gas
+   * target, and decreasing when blocks are below the gas target. The
+   * base fee per gas is burned.
+   */
   baseFee: BaseFee
+  /**
+   * Transactions also specify the maximum fee per gas they are
+   * willing to pay total (aka: max fee), which covers both the
+   * priority fee and the block's network fee per gas (aka: base fee)
+   */
   feePerGas: FeePerGas
+  /**
+   * Transactions specify the maximum fee per gas they are willing to
+   * give to miners to incentivize them to include their transaction
+   * (aka: priority fee).
+   */
   priorityFeePerGas: PriorityFeePerGas
 }

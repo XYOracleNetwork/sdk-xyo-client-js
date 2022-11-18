486abe7b13fb84e79800c8870afaba4de9d4637b
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-18 00:37:49 -0600

    Documentation

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/average.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/average.ts
@@ -2,17 +2,33 @@ import { exists } from '@xylabs/exists'
 
 import { FeeData } from '../Model'
 
+/**
+ * Averages an array of numbers
+ * @param x Averages an array of numbers
+ * @returns The average of the array of numbers or undefined if the
+ * array is empty or undefined
+ */
 const listAverage = (x?: number[]) => (x?.length ? x.reduce((a, b) => a + b) / x.length : undefined)
 
+/**
+ * Averages the property indicates by the supplied property
+ * expression across the array of FeeData
+ * @param data The array of data to average
+ * @param propertyExpression The expression for the property to
+ * average
+ * @returns The average of the supplied property across the array
+ */
 const averageProperty = (data: FeeData[], propertyExpression: (x: FeeData) => number | undefined) => {
   return listAverage(data.map(propertyExpression).filter(exists))
 }
 
+/**
+ * Averages the supplied FeeData
+ * @param input An array of FeeData or undefined
+ * @returns The average of the supplied FeeData
+ */
 export const average = (input?: (FeeData | undefined)[]): FeeData => {
   const data = input?.filter(exists) || []
-  // if (data.length < 1) {
-  //   throw new Error('Unable to compute average on empty list')
-  // }
   const averaged: FeeData = {
     baseFee: averageProperty(data, (x) => x.baseFee),
     feePerGas: {

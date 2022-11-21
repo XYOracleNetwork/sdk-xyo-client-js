cbbce3b1e9cfe2f25d08af881f17bb75f1e51801
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-21 17:37:19 -0600

    Add Authorization Header to HTTP Requests

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/blocknative/src/lib/getGasFromBlocknative.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/blocknative/src/lib/getGasFromBlocknative.ts
@@ -2,7 +2,11 @@ import axios from 'axios'
 
 import { EthereumGasBlocknativeResponse } from '../Payload'
 
+const url = 'https://api.blocknative.com/gasprices/blockprices'
+
+const Authorization = '9d3e23c3-e31d-4f9c-9d7c-c579cb75d226'
+const config = { headers: { Authorization } }
+
 export const getGasFromBlocknative = async (): Promise<EthereumGasBlocknativeResponse> => {
-  const url = 'https://api.blocknative.com/gasprices/blockprices'
-  return (await axios.get<EthereumGasBlocknativeResponse>(url)).data
+  return (await axios.get<EthereumGasBlocknativeResponse>(url, config)).data
 }

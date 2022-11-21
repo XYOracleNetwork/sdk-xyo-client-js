2ed6b3d9d507bc1f5db8fa867cbc56cd1c58ce68
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-21 14:33:58 -0600

    Strongly type API response

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/blocknative/src/Payload.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/blocknative/src/Payload.ts
@@ -2,35 +2,51 @@ import { XyoPayload } from '@xyo-network/payload'
 
 import { XyoEthereumGasBlocknativeSchema } from './Schema'
 
+export type WithConfidence<T> = T & {
+  confidence: number
+}
+export interface BaseFee {
+  baseFee: number
+}
+
+export interface EstimatedPrice {
+  maxFeePerGas: number
+  maxPriorityFeePerGas: number
+  price: number
+}
+
+export interface BlockPrice {
+  baseFeePerGas: number
+  blockNumber: number
+  estimatedPrices: WithConfidence<EstimatedPrice>[]
+  estimatedTransactionCount: number
+}
+
 export interface EthereumGasBlocknativeResponse {
-  message: string
-  result: {
-    /**
-     * Priority Fee
-     */
-    FastGasPrice: string
-    /**
-     * Number of last block
-     */
-    LastBlock: string
-    /**
-     * Priority Fee
-     */
-    ProposeGasPrice: string
-    /**
-     * Priority Fee
-     */
-    SafeGasPrice: string
-    /**
-     * Estimate of how busy the network is
-     */
-    gasUsedRatio: string
-    /**
-     * The baseFee of the next pending block
-     */
-    suggestBaseFee: string
-  }
-  status: string
+  blockPrices: BlockPrice[]
+  currentBlockNumber: number
+  estimatedBaseFees: [
+    {
+      'pending+1': WithConfidence<BaseFee>[]
+    },
+    {
+      'pending+2': WithConfidence<BaseFee>[]
+    },
+    {
+      'pending+3': WithConfidence<BaseFee>[]
+    },
+    {
+      'pending+4': WithConfidence<BaseFee>[]
+    },
+    {
+      'pending+5': WithConfidence<BaseFee>[]
+    },
+  ]
+  maxPrice: number
+  msSinceLastBlock: number
+  network: 'main'
+  system: 'ethereum'
+  unit: 'gwei'
 }
 
 export type XyoEthereumGasBlocknativePayload = XyoPayload<

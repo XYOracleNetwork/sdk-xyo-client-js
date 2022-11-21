42eb164f21c84642861d831c647f8cd253835eb9
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-21 17:40:01 -0600

    Assert additional Witness fields in Witness tests

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/blocknative/src/Witness.spec.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/blocknative/src/Witness.spec.ts
@@ -12,17 +12,8 @@ describe('XyoEthereumGasBlocknativeWitness', () => {
       },
     })
     const [actual] = await sut.observe()
-    expect(actual.result).toBeObject()
-    expect(actual.result.FastGasPrice).toBeString()
-    expect(actual.result.gasUsedRatio).toBeString()
-    expect(actual.result.LastBlock).toBeString()
-    expect(actual.result.ProposeGasPrice).toBeString()
-    expect(actual.result.SafeGasPrice).toBeString()
-    expect(actual.result.suggestBaseFee).toBeString()
-
     expect(actual.timestamp).toBeNumber()
     expect(actual.schema).toBe(XyoEthereumGasBlocknativeSchema)
-
     const answerWrapper = new PayloadWrapper(actual)
     expect(answerWrapper.valid).toBe(true)
   })

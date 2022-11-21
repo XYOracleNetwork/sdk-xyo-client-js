3e331ac874827e9952052bb8e382c525286185eb
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-21 17:38:10 -0600

    Unit test actual API calls

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/blocknative/src/lib/getGasFromBlocknative.spec.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/blocknative/src/lib/getGasFromBlocknative.spec.ts
@@ -1,115 +1,38 @@
+import { PendingBlockNumber } from '../Payload'
 import { getGasFromBlocknative } from './getGasFromBlocknative'
-
-const sampleResponse = {
-  blockPrices: [
-    {
-      baseFeePerGas: 12.189397666,
-      blockNumber: 16020718,
-      estimatedPrices: [
-        {
-          confidence: 99,
-          maxFeePerGas: 20.59,
-          maxPriorityFeePerGas: 1.09,
-          price: 13,
-        },
-        {
-          confidence: 95,
-          maxFeePerGas: 19.95,
-          maxPriorityFeePerGas: 0.45,
-          price: 12,
-        },
-        {
-          confidence: 90,
-          maxFeePerGas: 19.73,
-          maxPriorityFeePerGas: 0.23,
-          price: 12,
-        },
-        {
-          confidence: 80,
-          maxFeePerGas: 19.67,
-          maxPriorityFeePerGas: 0.17,
-          price: 12,
-        },
-        {
-          confidence: 70,
-          maxFeePerGas: 19.64,
-          maxPriorityFeePerGas: 0.14,
-          price: 12,
-        },
-      ],
-      estimatedTransactionCount: 156,
-    },
-  ],
-  currentBlockNumber: 16020717,
-  estimatedBaseFees: [
-    {
-      'pending+1': [
-        {
-          baseFee: 13.72,
-          confidence: 99,
-        },
-      ],
-    },
-    {
-      'pending+2': [
-        {
-          baseFee: 15.43,
-          confidence: 99,
-        },
-      ],
-    },
-    {
-      'pending+3': [
-        {
-          baseFee: 17.36,
-          confidence: 99,
-        },
-      ],
-    },
-    {
-      'pending+4': [
-        {
-          baseFee: 18.67,
-          confidence: 99,
-        },
-      ],
-    },
-    {
-      'pending+5': [
-        {
-          baseFee: 19.5,
-          confidence: 99,
-        },
-      ],
-    },
-  ],
-  maxPrice: 24,
-  msSinceLastBlock: 7012,
-  network: 'main',
-  system: 'ethereum',
-  unit: 'gwei',
-}
 
 describe('getGasFromBlocknative', () => {
   test('returns prices', async () => {
-    // const gas = await getGasFromBlocknative()
-    const gas = await Promise.resolve(sampleResponse)
-    expect(gas).toBeObject()
-
-    expect(gas?.blockPrices).toBeArrayOfSize(1)
-    gas.blockPrices.map((blockPrice) => {
+    const result = await getGasFromBlocknative()
+    expect(result).toBeObject()
+    expect(result?.blockPrices).toBeArrayOfSize(1)
+    result.blockPrices.map((blockPrice) => {
       expect(blockPrice.baseFeePerGas).toBeNumber()
       expect(blockPrice.blockNumber).toBeNumber()
       expect(blockPrice.estimatedPrices).toBeArrayOfSize(5)
-      blockPrice.estimatedPrices.map((estimatedPrice) => {})
+      blockPrice.estimatedPrices.map((estimatedPrice) => {
+        expect(estimatedPrice?.confidence).toBeNumber()
+        expect(estimatedPrice?.maxFeePerGas).toBeNumber()
+        expect(estimatedPrice?.maxPriorityFeePerGas).toBeNumber()
+        expect(estimatedPrice?.price).toBeNumber()
+      })
       expect(blockPrice.estimatedTransactionCount).toBeNumber()
     })
-    expect(gas?.currentBlockNumber).toBeNumber()
-    expect(gas?.maxPrice).toBeNumber()
-    expect(gas?.msSinceLastBlock).toBeNumber()
-
-    expect(gas?.network).toEqual('main')
-    expect(gas?.system).toEqual('ethereum')
-    expect(gas?.unit).toEqual('gwei')
+    expect(result?.estimatedBaseFees).toBeArrayOfSize(5)
+    for (let index = 0; index < result?.estimatedBaseFees.length; index++) {
+      const blockNumber = `pending+${index + 1}` as PendingBlockNumber
+      const pendingBlockInfo = result.estimatedBaseFees?.[index]?.[blockNumber]
+      expect(pendingBlockInfo).toBeArrayOfSize(1)
+      const pendingBlock = pendingBlockInfo?.[0]
+      expect(pendingBlock).toBeObject()
+      expect(pendingBlock?.baseFee).toBeNumber()
+      expect(pendingBlock?.confidence).toBeNumber()
+    }
+    expect(result?.currentBlockNumber).toBeNumber()
+    expect(result?.maxPrice).toBeNumber()
+    expect(result?.msSinceLastBlock).toBeNumber()
+    expect(result?.network).toEqual('main')
+    expect(result?.system).toEqual('ethereum')
+    expect(result?.unit).toEqual('gwei')
   })
 })

8d4b777a1f1286df006ccd155d32cffb21259f9f
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-22 06:27:58 -0600

    Remove dual witnesses in favor of single witness

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/ethgasstation/packages/v1/src/Schema.ts
+++
@@ -1,6 +0,0 @@
-export type XyoEthereumGasEthgasstationV1WitnessConfigSchema = 'network.xyo.blockchain.ethereum.gas.ethgasstation.v1.witness.config'
-export const XyoEthereumGasEthgasstationV1WitnessConfigSchema: XyoEthereumGasEthgasstationV1WitnessConfigSchema =
-  'network.xyo.blockchain.ethereum.gas.ethgasstation.v1.witness.config'
-
-export type XyoEthereumGasEthgasstationV1Schema = 'network.xyo.blockchain.ethereum.gas.ethgasstation.v1'
-export const XyoEthereumGasEthgasstationV1Schema: XyoEthereumGasEthgasstationV1Schema = 'network.xyo.blockchain.ethereum.gas.ethgasstation.v1'
--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/ethgasstation/packages/v2/src/Schema.ts
+++
@@ -1,6 +0,0 @@
-export type XyoEthereumGasEthgasstationV2WitnessConfigSchema = 'network.xyo.blockchain.ethereum.gas.ethgasstation.v2.witness.config'
-export const XyoEthereumGasEthgasstationV2WitnessConfigSchema: XyoEthereumGasEthgasstationV2WitnessConfigSchema =
-  'network.xyo.blockchain.ethereum.gas.ethgasstation.v2.witness.config'
-
-export type XyoEthereumGasEthgasstationV2Schema = 'network.xyo.blockchain.ethereum.gas.ethgasstation.v2'
-export const XyoEthereumGasEthgasstationV2Schema: XyoEthereumGasEthgasstationV2Schema = 'network.xyo.blockchain.ethereum.gas.ethgasstation.v2'

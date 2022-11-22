f7ad5af25dce1a0bcaa8823b0e1b5aeb905b6bad
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-22 08:05:11 -0600

    Use numeric value

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/ethers/src/lib/getGasFromEthers.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/ethers/src/lib/getGasFromEthers.ts
@@ -1,20 +1,19 @@
 import { Provider } from '@ethersproject/providers'
-import { parseUnits } from '@ethersproject/units'
 
 import { EthereumGasEthersResponse } from '../Payload'
 
-const convertToGwei = (value: { toString(): string }) => {
-  return parseUnits(value.toString(), 'gwei').toNumber()
+const formatFeeDataValue = (value: { toNumber(): number }) => {
+  return value.toNumber()
 }
 
 export const getGasFromEthers = async (provider: Provider): Promise<EthereumGasEthersResponse> => {
   // https://docs.ethers.io/v5/api/providers/provider/#Provider-getFeeData
   const feeData = await provider.getFeeData()
-  const feeDataInGwei: EthereumGasEthersResponse = {
-    gasPrice: feeData?.gasPrice ? convertToGwei(feeData.gasPrice) : null,
-    lastBaseFeePerGas: feeData?.lastBaseFeePerGas ? convertToGwei(feeData.lastBaseFeePerGas) : null,
-    maxFeePerGas: feeData?.maxFeePerGas ? convertToGwei(feeData.maxFeePerGas) : null,
-    maxPriorityFeePerGas: feeData?.maxPriorityFeePerGas ? convertToGwei(feeData.maxPriorityFeePerGas) : null,
+  const formattedFeeData: EthereumGasEthersResponse = {
+    gasPrice: feeData?.gasPrice ? formatFeeDataValue(feeData.gasPrice) : null,
+    lastBaseFeePerGas: feeData?.lastBaseFeePerGas ? formatFeeDataValue(feeData.lastBaseFeePerGas) : null,
+    maxFeePerGas: feeData?.maxFeePerGas ? formatFeeDataValue(feeData.maxFeePerGas) : null,
+    maxPriorityFeePerGas: feeData?.maxPriorityFeePerGas ? formatFeeDataValue(feeData.maxPriorityFeePerGas) : null,
   }
-  return feeDataInGwei
+  return formattedFeeData
 }

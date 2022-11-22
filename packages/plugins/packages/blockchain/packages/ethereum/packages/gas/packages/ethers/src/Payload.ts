ceef4215ab8679e04de4edfdcb706ae23d79ee26
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-22 07:42:27 -0600

    Match response to SDK output

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/ethers/src/Payload.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/ethers/src/Payload.ts
@@ -6,34 +6,10 @@ import { XyoEthereumGasEthersSchema } from './Schema'
  * https://docs.ethers.io/api-endpoints/gas-tracker#get-gas-oracle
  */
 export interface EthereumGasEthersResponse {
-  message: string
-  result: {
-    /**
-     * Priority Fee
-     */
-    FastGasPrice: string
-    /**
-     * Number of last block
-     */
-    LastBlock: string
-    /**
-     * Priority Fee
-     */
-    ProposeGasPrice: string
-    /**
-     * Priority Fee
-     */
-    SafeGasPrice: string
-    /**
-     * Estimate of how busy the network is
-     */
-    gasUsedRatio: string
-    /**
-     * The baseFee of the next pending block
-     */
-    suggestBaseFee: string
-  }
-  status: string
+  gasPrice: null | number
+  lastBaseFeePerGas: null | number
+  maxFeePerGas: null | number
+  maxPriorityFeePerGas: null | number
 }
 
 export type XyoEthereumGasEthersPayload = XyoPayload<

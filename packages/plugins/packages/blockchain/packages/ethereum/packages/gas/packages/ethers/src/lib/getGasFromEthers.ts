07d0d38d627d7fbf6e45005e1a0b9a72a4f9fbf7
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-22 08:07:13 -0600

    Simplify numeric normalization

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/ethers/src/lib/getGasFromEthers.ts
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/ethers/src/lib/getGasFromEthers.ts
@@ -2,18 +2,18 @@ import { Provider } from '@ethersproject/providers'
 
 import { EthereumGasEthersResponse } from '../Payload'
 
-const formatFeeDataValue = (value: { toNumber(): number }) => {
-  return value.toNumber()
+const formatFeeDataValue = (value: { toNumber(): number } | null): number | null => {
+  return value ? value.toNumber() : null
 }
 
 export const getGasFromEthers = async (provider: Provider): Promise<EthereumGasEthersResponse> => {
   // https://docs.ethers.io/v5/api/providers/provider/#Provider-getFeeData
   const feeData = await provider.getFeeData()
   const formattedFeeData: EthereumGasEthersResponse = {
-    gasPrice: feeData?.gasPrice ? formatFeeDataValue(feeData.gasPrice) : null,
-    lastBaseFeePerGas: feeData?.lastBaseFeePerGas ? formatFeeDataValue(feeData.lastBaseFeePerGas) : null,
-    maxFeePerGas: feeData?.maxFeePerGas ? formatFeeDataValue(feeData.maxFeePerGas) : null,
-    maxPriorityFeePerGas: feeData?.maxPriorityFeePerGas ? formatFeeDataValue(feeData.maxPriorityFeePerGas) : null,
+    gasPrice: formatFeeDataValue(feeData.gasPrice),
+    lastBaseFeePerGas: formatFeeDataValue(feeData.lastBaseFeePerGas),
+    maxFeePerGas: formatFeeDataValue(feeData.maxFeePerGas),
+    maxPriorityFeePerGas: formatFeeDataValue(feeData.maxPriorityFeePerGas),
   }
   return formattedFeeData
 }

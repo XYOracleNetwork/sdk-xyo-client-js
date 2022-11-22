29f2af24f65755b0f268a86ceadd5d3727541bf7
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-22 17:11:10 -0600

    Normalize Ethers results

---
+++ packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/gas/src/lib/transforms/transformGasFromEthers.ts
@@ -0,0 +1,27 @@
+import { XyoEthereumGasEthersPayload } from '@xyo-network/ethers-ethereum-gas-payload-plugin'
+
+import { FeeData, FeePerGas, PriorityFeePerGas } from '../../Model'
+
+const getBaseFee = (payload: XyoEthereumGasEthersPayload): number | undefined => {
+  const { lastBaseFeePerGas } = payload
+  return lastBaseFeePerGas ? lastBaseFeePerGas / 1_000_000_000 : undefined
+}
+
+const getFeePerGas = (payload: XyoEthereumGasEthersPayload): Partial<FeePerGas> => {
+  const { maxFeePerGas } = payload
+  const high = maxFeePerGas ? maxFeePerGas / 1_000_000_000 : undefined
+  return { high }
+}
+
+const getPriorityFeePerGas = (payload: XyoEthereumGasEthersPayload): Partial<PriorityFeePerGas> => {
+  const { maxPriorityFeePerGas } = payload
+  const high = maxPriorityFeePerGas ? maxPriorityFeePerGas / 1_000_000_000 : undefined
+  return { high }
+}
+
+export const transformGasFromEthers = (payload: XyoEthereumGasEthersPayload): FeeData => {
+  const baseFee = getBaseFee(payload)
+  const feePerGas = getFeePerGas(payload)
+  const priorityFeePerGas = getPriorityFeePerGas(payload)
+  return { baseFee, feePerGas, priorityFeePerGas }
+}

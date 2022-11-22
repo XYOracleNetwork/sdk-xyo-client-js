f6d483d170e4f7a087e2e0d12385c704883403bb
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-22 06:27:58 -0600

    Remove dual witnesses in favor of single witness

--- packages/plugins/packages/blockchain/packages/ethereum/packages/gas/packages/ethgasstation/packages/v1/src/lib/getV1GasFromEtherchain.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-import { EthereumGasEthgasstationV1Response } from '../Payload'
-
-const url = 'https://www.ethgasstation.org/api/gasPriceOracle'
-
-export const getV1GasFromEthgasstation = async (): Promise<EthereumGasEthgasstationV1Response> => {
-  return (await axios.get<EthereumGasEthgasstationV1Response>(url)).data
-}

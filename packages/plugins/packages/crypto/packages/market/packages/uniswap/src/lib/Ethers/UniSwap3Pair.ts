c9da8c4e8cb30f8b925615bda58a376521dd9064
Arie Trouw <arie.trouw@xylabs.com>
2022-11-22 16:23:25 -0800

    Code Climate Cleanup

--- packages/plugins/packages/crypto/packages/market/packages/uniswap/src/lib/Ethers/UniSwap3Pair.ts
+++ packages/plugins/packages/crypto/packages/market/packages/uniswap/src/lib/Ethers/UniSwap3Pair.ts
@@ -10,6 +10,13 @@ import { IUniswapV3Pool, IUniswapV3Pool__factory } from '@xyo-network/uniswap-ty
 import { logErrors, logErrorsAsync } from '../logErrors'
 import { EthersUniswap3PoolSlot0Wrapper } from './Uniswap3PoolSlot0Wrapper'
 
+//null is used as 'in-progress'
+const waitNotNull = async (closure: () => unknown) => {
+  while (closure() === null) {
+    await delay(10)
+  }
+}
+
 export class EthersUniSwap3Pair {
   protected address: string
   protected provider: Provider
@@ -17,10 +24,8 @@ export class EthersUniSwap3Pair {
   private _pool?: Pool | null
   private _poolContract?: IUniswapV3Pool
   private _slot0?: EthersUniswap3PoolSlot0Wrapper | null
-  private _token0?: Token | null
-  private _token0Contract?: IERC20Metadata | null
-  private _token1?: Token | null
-  private _token1Contract?: IERC20Metadata | null
+  private _tokenContracts: (IERC20Metadata | null | undefined)[] = [undefined, undefined]
+  private _tokens: (Token | null | undefined)[] = [undefined, undefined]
 
   constructor(address: string, provider: Provider) {
     this.address = address
@@ -29,16 +34,14 @@ export class EthersUniSwap3Pair {
 
   public async pool(): Promise<Pool> {
     return await logErrorsAsync(async () => {
-      while (this._pool === null) {
-        await delay(10)
-      }
+      await waitNotNull(() => this._pool)
       this._pool = this._pool || null
       const slot0 = await this.slot0()
       this._pool =
         this._pool ??
         new Pool(
-          await this.token0(),
-          await this.token1(),
+          await this.token(0),
+          await this.token(1),
           slot0.feeProtocol,
           slot0.sqrtPriceX96.toHexString(),
           (await this.poolContract().liquidity()).toHexString(),
@@ -57,18 +60,16 @@ export class EthersUniSwap3Pair {
 
   public async price() {
     return await logErrorsAsync(async () => {
+      const tokenIndexes: (0 | 1)[] = [0, 1]
       const pool = await this.pool()
-      const token0 = await this.token0()
-      const token1 = await this.token1()
-      const token0Price = parseFloat(pool.priceOf(await this.token0()).toSignificant())
-      const token1Price = parseFloat(pool.priceOf(token1).toSignificant())
-      const token0Symbol = assertEx(token0.symbol, 'Token0 Missing Symbols').toLowerCase()
-      const token1Symbol = assertEx(token1.symbol, 'Token1 Missing Symbols').toLowerCase()
+      const tokens = await Promise.all(tokenIndexes.map((value) => this.token(value)))
+      const tokenContracts = await Promise.all(tokenIndexes.map((value) => this.tokenContract(value)))
+      const tokenPrices = tokens.map((token) => parseFloat(pool.priceOf(token).toSignificant()))
+      const tokenSymbols = tokens.map((token, index) => assertEx(token.symbol, `Token[${index}] Missing Symbols`).toLowerCase())
       const result = {
-        tokens: [
-          { address: (await this.token0Contract()).address, symbol: token0Symbol, value: token0Price },
-          { address: (await this.token1Contract()).address, symbol: token1Symbol, value: token1Price },
-        ],
+        tokens: tokenIndexes.map((value) => {
+          return { address: tokenContracts[value].address, symbol: tokenSymbols[value], value: tokenPrices[value] }
+        }),
       }
       return result
     })
@@ -76,72 +77,33 @@ export class EthersUniSwap3Pair {
 
   public async slot0(): Promise<EthersUniswap3PoolSlot0Wrapper> {
     return await logErrorsAsync(async () => {
-      while (this._slot0 === null) {
-        await delay(10)
-      }
+      await waitNotNull(() => this._slot0)
       this._slot0 = this._slot0 || null
       this._slot0 = this._slot0 ?? new EthersUniswap3PoolSlot0Wrapper(await this.poolContract().slot0())
       return assertEx(this._slot0)
     })
   }
 
-  public async token0(): Promise<Token> {
+  public async token(index: 0 | 1): Promise<Token> {
     return await logErrorsAsync(async () => {
-      while (this._token0 === null) {
-        await delay(10)
-      }
-      this._token0 = this._token0 || null
-      this._token0 =
-        this._token0 ??
-        new Token(
-          ChainId.MAINNET,
-          (await this.token0Contract()).address,
-          await (await this.token0Contract()).decimals(),
-          await (await this.token0Contract()).symbol(),
-          await (await this.token0Contract()).name(),
-        )
-      return assertEx(this._token0)
+      await waitNotNull(() => this._tokens[index])
+      this._tokens[index] = this._tokens[index] || null
+      const tokenContract = await this.tokenContract(index)
+      this._tokens[index] =
+        this._tokens[index] ??
+        new Token(ChainId.MAINNET, tokenContract.address, await tokenContract.decimals(), await tokenContract.symbol(), await tokenContract.name())
+      return assertEx(this._tokens[index])
     })
   }
 
-  public async token0Contract(): Promise<IERC20Metadata> {
+  public async tokenContract(index: 0 | 1): Promise<IERC20Metadata> {
     return await logErrorsAsync(async () => {
-      while (this._token0Contract === null) {
-        await delay(10)
-      }
-      this._token0Contract = this._token0Contract || null
-      this._token0Contract = this._token0Contract ?? IERC20Metadata__factory.connect(await this.poolContract().token0(), this.provider)
-      return assertEx(this._token0Contract)
-    })
-  }
-
-  public async token1(): Promise<Token> {
-    return await logErrorsAsync(async () => {
-      while (this._token1 === null) {
-        await delay(10)
-      }
-      this._token1 = this._token1 || null
-      this._token1 =
-        this._token1 ??
-        new Token(
-          ChainId.MAINNET,
-          (await this.token1Contract()).address,
-          await (await this.token1Contract()).decimals(),
-          await (await this.token1Contract()).symbol(),
-          await (await this.token1Contract()).name(),
-        )
-      return assertEx(this._token1)
-    })
-  }
-
-  public async token1Contract(): Promise<IERC20Metadata> {
-    return await logErrorsAsync(async () => {
-      while (this._token1Contract === null) {
-        await delay(10)
-      }
-      this._token1Contract = this._token1Contract || null
-      this._token1Contract = this._token1Contract ?? IERC20Metadata__factory.connect(await this.poolContract().token1(), this.provider)
-      return assertEx(this._token1Contract)
+      await waitNotNull(() => this._tokenContracts[index])
+      this._tokenContracts[index] = this._tokenContracts[index] || null
+      this._tokenContracts[index] =
+        this._tokenContracts[index] ??
+        IERC20Metadata__factory.connect(await (index === 0 ? this.poolContract().token0() : this.poolContract().token1()), this.provider)
+      return assertEx(this._tokenContracts[index])
     })
   }
 }

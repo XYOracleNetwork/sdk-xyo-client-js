e437f952134d3807e92e020f681f4884d75d9b58
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-28 14:53:11 -0600

    Assert diviner wrapper response parts

--- packages/modules/packages/diviner/src/DivinerWrapper.spec.ts
+++ packages/modules/packages/diviner/src/DivinerWrapper.spec.ts
@@ -3,12 +3,12 @@ import { XyoPayloadBuilder, XyoPayloads } from '@xyo-network/payload'
 import { Promisable } from '@xyo-network/promise'
 
 import { AbstractDiviner } from './AbstractDiviner'
-import { DivinerConfig } from './Config'
+import { DivinerConfig, XyoDivinerConfigSchema } from './Config'
 import { DivinerWrapper } from './DivinerWrapper'
 import { XyoDivinerDivineQuerySchema } from './Queries'
 
 class IdentityDiviner extends AbstractDiviner {
-  static override configSchema = 'network.xyo.test'
+  static override configSchema = XyoDivinerConfigSchema
   static override targetSchema = 'network.xyo.test'
 
   static override async create(params?: XyoModuleParams<DivinerConfig>) {
@@ -26,14 +26,18 @@ class IdentityDiviner extends AbstractDiviner {
 
 describe('DivinerWrapper', () => {
   describe('divine', () => {
-    it('calls methods on mock', () => {
-      // TODO:
-    })
     it('returns divined result', async () => {
+      const schema = 'network.xyo.debug'
       const diviner = await IdentityDiviner.create()
       const sut = new DivinerWrapper(diviner)
-      const payloads = [new XyoPayloadBuilder({ schema: 'network.xyo.test' }).build()]
+      const payloads = [new XyoPayloadBuilder({ schema }).build()]
       const result = await sut.divine(payloads)
+      expect(result).toBeArrayOfSize(payloads.length + 1)
+      const [answer, query] = result
+      expect(answer).toBeObject()
+      expect(answer.schema).toBe(schema)
+      expect(query).toBeObject()
+      expect(query.schema).toBe(XyoDivinerDivineQuerySchema)
     })
   })
 })

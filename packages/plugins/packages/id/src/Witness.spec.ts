d1672e4265575a3d02075a541f36caf1b844f307
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-29 14:03:45 -0600

    Tests for no config supplied cases

--- packages/plugins/packages/id/src/Witness.spec.ts
+++ packages/plugins/packages/id/src/Witness.spec.ts
@@ -1,13 +1,14 @@
 import { PayloadWrapper } from '@xyo-network/payload'
 
+import { XyoIdPayload } from './Payload'
 import { XyoIdSchema } from './Schema'
 import { XyoIdWitness, XyoIdWitnessConfigSchema } from './Witness'
 
 describe('XyoIdWitness', () => {
   describe('observe', () => {
+    const inlineSalt = 'inline salt'
     describe('with config', () => {
       const configSalt = 'config salt'
-      const inlineSalt = 'inline salt'
       const config = {
         salt: configSalt,
         schema: XyoIdWitnessConfigSchema,
@@ -16,36 +17,63 @@ describe('XyoIdWitness', () => {
       describe('with payloads supplied to observe', () => {
         it('without salt uses config salt', async () => {
           const witness = await XyoIdWitness.create({ config })
-          const [observation] = await witness.observe([{ schema: XyoIdSchema }])
-          expect(observation.schema).toBe(XyoIdSchema)
+          const observations = await witness.observe([{ schema: XyoIdSchema }])
+          validateObservationShape(observations)
+          const [observation] = observations
           expect(observation.salt).toBe(witness.config.salt)
-          expect(new PayloadWrapper(observation).valid).toBe(true)
         })
         it('with salt uses inline salt', async () => {
           const witness = await XyoIdWitness.create({ config })
-          const [observation] = await witness.observe([{ salt: inlineSalt }])
-          expect(observation.schema).toBe(XyoIdSchema)
+          const observations = await witness.observe([{ salt: inlineSalt }])
+          validateObservationShape(observations)
+          const [observation] = observations
           expect(observation.salt).toBe(inlineSalt)
-          expect(new PayloadWrapper(observation).valid).toBe(true)
         })
       })
       describe('with no payloads supplied to observe', () => {
-        it('uses inline salt', async () => {
+        it('uses config salt', async () => {
           const witness = await XyoIdWitness.create({ config })
-          const [observation] = await witness.observe()
-          expect(observation.schema).toBe(XyoIdSchema)
+          const observations = await witness.observe()
+          validateObservationShape(observations)
+          const [observation] = observations
           expect(observation.salt).toBe(witness.config.salt)
-          expect(new PayloadWrapper(observation).valid).toBe(true)
         })
       })
     })
     describe('with no config', () => {
-      test('returns result', async () => {
-        const witness = await XyoIdWitness.create()
-        const [observation] = await witness.observe([{ salt: 'test' }])
-        expect(observation.schema).toBe(XyoIdSchema)
-        expect(new PayloadWrapper(observation).valid).toBe(true)
+      describe('with payloads supplied to observe', () => {
+        it('without salt uses random numeric string', async () => {
+          const witness = await XyoIdWitness.create()
+          const observations = await witness.observe([{ schema: XyoIdSchema }])
+          validateObservationShape(observations)
+          const [observation] = observations
+          expect(parseInt(observation.salt)).toBeInteger()
+        })
+        it('with salt uses inline salt', async () => {
+          const witness = await XyoIdWitness.create()
+          const observations = await witness.observe([{ salt: inlineSalt }])
+          validateObservationShape(observations)
+          const [observation] = observations
+          expect(observation.salt).toBe(inlineSalt)
+        })
+      })
+      describe('with no payloads supplied to observe', () => {
+        it('uses random numeric string', async () => {
+          const witness = await XyoIdWitness.create()
+          const observations = await witness.observe()
+          validateObservationShape(observations)
+          const [observation] = observations
+          expect(parseInt(observation.salt)).toBeInteger()
+        })
       })
     })
   })
 })
+
+const validateObservationShape = (observations: XyoIdPayload[]) => {
+  expect(observations).toBeArrayOfSize(1)
+  const [observation] = observations
+  expect(observation.salt).toBeString()
+  expect(observation.schema).toBe(XyoIdSchema)
+  expect(new PayloadWrapper(observation).valid).toBe(true)
+}

773643c620f4e2335634e5284651ec2db0bcb1bf
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-29 13:38:20 -0600

    Additional tests for cases when config present

--- packages/plugins/packages/id/src/Witness.spec.ts
+++ packages/plugins/packages/id/src/Witness.spec.ts
@@ -4,22 +4,48 @@ import { XyoIdSchema } from './Schema'
 import { XyoIdWitness, XyoIdWitnessConfigSchema } from './Witness'
 
 describe('XyoIdWitness', () => {
-  test('observe', async () => {
-    const witness = await XyoIdWitness.create({
-      config: {
-        salt: 'test',
+  describe('observe', () => {
+    describe('with config', () => {
+      const configSalt = 'config salt'
+      const inlineSalt = 'inline salt'
+      const config = {
+        salt: configSalt,
         schema: XyoIdWitnessConfigSchema,
         targetSchema: XyoIdSchema,
-      },
+      }
+      describe('with payloads supplied to observe', () => {
+        it('without salt uses config salt', async () => {
+          const witness = await XyoIdWitness.create({ config })
+          const [observation] = await witness.observe([{ schema: XyoIdSchema }])
+          expect(observation.schema).toBe(XyoIdSchema)
+          expect(observation.salt).toBe(witness.config.salt)
+          expect(new PayloadWrapper(observation).valid).toBe(true)
+        })
+        it('with salt uses inline salt', async () => {
+          const witness = await XyoIdWitness.create({ config })
+          const [observation] = await witness.observe([{ salt: inlineSalt }])
+          expect(observation.schema).toBe(XyoIdSchema)
+          expect(observation.salt).toBe(inlineSalt)
+          expect(new PayloadWrapper(observation).valid).toBe(true)
+        })
+      })
+      describe('with no payloads supplied to observe', () => {
+        it('uses inline salt', async () => {
+          const witness = await XyoIdWitness.create({ config })
+          const [observation] = await witness.observe()
+          expect(observation.schema).toBe(XyoIdSchema)
+          expect(observation.salt).toBe(witness.config.salt)
+          expect(new PayloadWrapper(observation).valid).toBe(true)
+        })
+      })
     })
-    const [observation] = await witness.observe([{ salt: 'test' }])
-    expect(observation.schema).toBe('network.xyo.id')
-    expect(new PayloadWrapper(observation).valid).toBe(true)
-  })
-  test('observe [no config]', async () => {
-    const witness = await XyoIdWitness.create()
-    const [observation] = await witness.observe([{ salt: 'test' }])
-    expect(observation.schema).toBe('network.xyo.id')
-    expect(new PayloadWrapper(observation).valid).toBe(true)
+    describe('with no config', () => {
+      test('returns result', async () => {
+        const witness = await XyoIdWitness.create()
+        const [observation] = await witness.observe([{ salt: 'test' }])
+        expect(observation.schema).toBe(XyoIdSchema)
+        expect(new PayloadWrapper(observation).valid).toBe(true)
+      })
+    })
   })
 })

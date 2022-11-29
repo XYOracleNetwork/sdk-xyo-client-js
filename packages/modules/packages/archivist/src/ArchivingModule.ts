4e606630f59c14b74cfd41516a8b699bc52db547
Arie Trouw <arie.trouw@xylabs.com>
2022-11-29 14:02:26 -0800

    Panel updated and test updated

--- packages/modules/packages/archivist/src/ArchivingModule.ts
+++ packages/modules/packages/archivist/src/ArchivingModule.ts
@@ -1,4 +1,5 @@
 import { XyoAccount } from '@xyo-network/account'
+import { XyoBoundWitness } from '@xyo-network/boundwitness'
 import { EmptyObject } from '@xyo-network/core'
 import { Module, ModuleQueryResult, XyoModule, XyoModuleConfig } from '@xyo-network/module'
 import { XyoPayload } from '@xyo-network/payload'
@@ -17,18 +18,24 @@ export class ArchivingModule<TConfig extends ArchivingModuleConfig = ArchivingMo
   protected bindResult(payloads: XyoPayload[], account?: XyoAccount): PromiseEx<ModuleQueryResult, XyoAccount> {
     const promise = new PromiseEx<ModuleQueryResult, XyoAccount>(async (resolve) => {
       const result = this.bindResultInternal(payloads, account)
-      const archivists = (await this.resolveArchivists()) ?? []
-      await Promise.all(
-        archivists.map((archivist) => {
-          return archivist.insert(payloads)
-        }),
-      )
+      await this.storeToArchivists([result[0], ...result[1]])
       resolve?.(result)
     }, account)
     return promise
   }
 
   protected async resolveArchivists() {
-    return (await this.resolver?.resolve({ address: this.config.archivists ?? [] }))?.map((archivist) => new ArchivistWrapper(archivist))
+    return (await this.resolver?.resolve({ address: this.config.archivists ?? [] }))?.map((archivist) => new ArchivistWrapper(archivist)) ?? []
+  }
+
+  protected async storeToArchivists(payloads: XyoPayload[]): Promise<XyoBoundWitness[]> {
+    const archivists = await this.resolveArchivists()
+    return (
+      await Promise.all(
+        archivists.map((archivist) => {
+          return archivist.insert(payloads)
+        }),
+      )
+    ).map(([bw]) => bw)
   }
 }

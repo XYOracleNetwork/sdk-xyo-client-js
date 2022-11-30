8762946828e6517a18aa2d51077308c64276fad0
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-30 16:20:32 -0600

    Tests for archive creation

--- packages/sdk/packages/api/src/Api/Archive/Api.spec.ts
+++ packages/sdk/packages/api/src/Api/Archive/Api.spec.ts
@@ -1,26 +1,22 @@
-import { XyoApiError } from '@xyo-network/api-models'
-
-import { getApi, getRandomArchiveName } from '../ApiUtil.spec'
+import { getApi, getRandomArchiveName, getTokenForNewUser } from '../ApiUtil.spec'
 
 describe('XyoArchivistArchiveApi', () => {
   const api = getApi()
-  describe('get', function () {
-    let archive = ''
-    beforeEach(() => {
-      archive = getRandomArchiveName()
+  const archive = getRandomArchiveName()
+  beforeEach(async () => {
+    const token = await getTokenForNewUser(api)
+    api.config.jwtToken = token
+  })
+  describe('put', () => {
+    it('creates the archive', async () => {
+      const response = await api.archives.archive(archive).put()
+      expect(response?.archive).toEqual(archive)
     })
-    describe('get', function () {
-      it('gets the archive', async () => {
-        await api.archives.archive(archive).get()
-        const response = await api.archives.archive(archive).get()
-        expect(response?.archive).toBe(archive)
-      })
-    })
-    describe('put', function () {
-      it('creates the archive', async () => {
-        const response = await api.archives.archive(archive).put()
-        expect(response?.archive).toEqual(archive)
-      })
+  })
+  describe('get', () => {
+    it('gets the archive', async () => {
+      const response = await api.archives.archive(archive).get()
+      expect(response?.archive).toBe(archive)
     })
   })
 })

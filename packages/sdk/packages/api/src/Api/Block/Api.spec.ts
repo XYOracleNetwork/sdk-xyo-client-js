76f6665fc1c194672f56428f90f65ab9ace82ea3
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-30 15:30:21 -0600

    Update block test

--- packages/sdk/packages/api/src/Api/Block/Api.spec.ts
+++ packages/sdk/packages/api/src/Api/Block/Api.spec.ts
@@ -1,5 +1,4 @@
-import { XyoApiError } from '@xyo-network/api-models'
-import { BoundWitnessBuilder, XyoBoundWitness } from '@xyo-network/boundwitness'
+import { BoundWitnessBuilder } from '@xyo-network/boundwitness'
 
 import { testPayload } from '../../Test'
 import { getApi, getNewArchive } from '../ApiUtil.spec'
@@ -9,46 +8,50 @@ describe('XyoArchivistArchiveBlockApi', () => {
   describe('post', function () {
     it.each([true, false])('posts a single bound witness', async (inlinePayloads) => {
       const builder = new BoundWitnessBuilder({ inlinePayloads }).payload(testPayload)
-      const [boundWitness] = builder.build()
-      const response = await api.archives.archive().block.post([boundWitness])
-      expect(response?.length).toEqual(1)
+      const [bw] = builder.build()
+      const blocks = [bw]
+      const response = await api.archives.archive().block.post(blocks)
+      expect(response?.length).toEqual(blocks.length)
     })
     it.each([true, false])('posts multiple bound witnesses', async (inlinePayloads) => {
       const builder = new BoundWitnessBuilder({ inlinePayloads }).payload(testPayload)
-      const [json] = builder.build()
-      const boundWitnesses: XyoBoundWitness[] = [json, json]
-      //TODO: We are casting the result here since the server has not yet been updated to return the actual saved data
-      const response = await api.archives.archive().block.post(boundWitnesses)
-      expect(response?.length).toEqual(2)
+      const [bw] = builder.build()
+      const blocks = [bw, bw]
+      const response = await api.archives.archive().block.post(blocks)
+      expect(response?.length).toEqual(blocks.length)
     })
   })
   describe('stats', () => {
+    const [bw] = new BoundWitnessBuilder().build()
+    const blocks = [bw]
+    let archive = ''
+    beforeEach(async () => {
+      archive = await getNewArchive(api)
+    })
     it('returns stats for boundwitness', async () => {
-      const stats = await api.archives.archive().block.stats.get()
-      expect(stats?.count).toBeGreaterThan(0)
+      await api.archives.archive(archive).block.post(blocks)
+      const stats = await api.archives.archive(archive).block.stats.get()
+      expect(stats?.count).toBeGreaterThanOrEqual(blocks.length)
     })
   })
   describe('find', () => {
-    describe('findBoundWitnessesBefore', function () {
-      it('returns bound witnesses from before the timestamp', async () => {
-        const [boundWitness] = new BoundWitnessBuilder().build()
-        await api.archives.archive().block.post([boundWitness])
-        const timestamp = Date.now() + 10000
-        // eslint-disable-next-line deprecation/deprecation
-        const response = await api.archives.archive('temp').block.findBefore(timestamp)
-        expect(response?.length).toBe(1)
-      })
+    const [bw] = new BoundWitnessBuilder().build()
+    const blocks = [bw]
+    let archive = ''
+    beforeEach(async () => {
+      archive = await getNewArchive(api)
     })
-
-    describe('findBoundWitnessesAfter', function () {
-      it('returns bound witnesses from before the timestamp', async () => {
-        const archive = await getNewArchive(api)
-        const [boundWitness] = new BoundWitnessBuilder().build()
-        await api.archives.archive(archive).block.post([boundWitness])
-        const timestamp = Date.now() - 10000
-        const response = await api.archives.archive(archive).block.find({ order: 'asc', timestamp })
-        expect(response?.length).toBe(1)
-      })
+    it('returns bound witnesses from before the timestamp', async () => {
+      const timestamp = Date.now() - 10000
+      await api.archives.archive(archive).block.post(blocks)
+      const response = await api.archives.archive(archive).block.find({ order: 'asc', timestamp })
+      expect(response?.length).toBe(blocks.length)
+    })
+    it('returns bound witnesses from after the timestamp', async () => {
+      await api.archives.archive(archive).block.post(blocks)
+      const timestamp = Date.now() + 10000
+      const response = await api.archives.archive(archive).block.find({ order: 'desc', timestamp })
+      expect(response?.length).toBe(blocks.length)
     })
   })
 })

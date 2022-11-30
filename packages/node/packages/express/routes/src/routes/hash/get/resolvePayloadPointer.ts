f74bc19a5440814f54117f91bf28b072dc75c575
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-30 07:04:47 -0600

    Dynamically obtain archivists from node

--- packages/node/packages/express/routes/src/routes/hash/get/resolvePayloadPointer.ts
+++ packages/node/packages/express/routes/src/routes/hash/get/resolvePayloadPointer.ts
@@ -1,5 +1,7 @@
+import { assertEx } from '@xylabs/assert'
 import { requestAccessibleArchives } from '@xyo-network/express-node-lib'
-import { PayloadPointerPayload } from '@xyo-network/node-core-model'
+import { BoundWitnessesArchivist, PayloadArchivist, PayloadPointerPayload } from '@xyo-network/node-core-model'
+import { TYPES } from '@xyo-network/node-core-types'
 import { XyoPayload } from '@xyo-network/payload'
 import { Request } from 'express'
 
@@ -7,10 +9,14 @@ import { combineRules } from './combineRules'
 import { findPayload } from './findPayload'
 
 export const resolvePayloadPointer = async (req: Request, pointer: PayloadPointerPayload): Promise<XyoPayload | undefined> => {
-  const { boundWitnessArchivist, payloadArchivist } = req.app
+  const { node } = req.app
   const searchCriteria = combineRules(pointer.reference)
   const accessibleArchives = await requestAccessibleArchives(req, searchCriteria.archives)
   if (!accessibleArchives.length) return undefined
   searchCriteria.archives = accessibleArchives
+  const payloadArchivists = await node.tryResolve({ name: [assertEx(TYPES.PayloadArchivist.description)] })
+  const payloadArchivist = assertEx(payloadArchivists[0]) as PayloadArchivist
+  const boundWitnessArchivists = await node.tryResolve({ name: [assertEx(TYPES.BoundWitnessArchivist.description)] })
+  const boundWitnessArchivist = assertEx(boundWitnessArchivists[0]) as BoundWitnessesArchivist
   return findPayload(boundWitnessArchivist, payloadArchivist, searchCriteria)
 }

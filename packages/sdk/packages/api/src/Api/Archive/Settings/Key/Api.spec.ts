fb4f52580865047198535201f50582f6de6f651d
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-30 16:20:01 -0600

    Tests for API key endpoint

--- packages/sdk/packages/api/src/Api/Archive/Settings/Key/Api.spec.ts
+++ packages/sdk/packages/api/src/Api/Archive/Settings/Key/Api.spec.ts
@@ -1,30 +1,35 @@
-import { XyoApiError } from '@xyo-network/api-models'
+import { assertEx } from '@xylabs/assert'
 
-import { getApi, getRandomArchiveName } from '../../../ApiUtil.spec'
+import { XyoArchiveKey } from '../../../../models'
+import { XyoArchivistApi } from '../../../Api'
+import { getApi, getRandomArchiveName, getTokenForNewUser } from '../../../ApiUtil.spec'
 
 describe('XyoArchivistArchiveSettingsKeyApi', () => {
+  let api: XyoArchivistApi
   let archive = ''
-  beforeAll(() => {
+  beforeEach(async () => {
     archive = getRandomArchiveName()
+    api = getApi()
+    const token = await getTokenForNewUser(api)
+    api.config.jwtToken = token
+    await api.archives.archive(archive).put()
   })
-  describe('post', function () {
+  describe('post', () => {
     it('Creates an archive key', async () => {
-      const api = getApi()
-      const archiveApi = api.archives.archive(archive)
-      await archiveApi.put()
-      const response = await archiveApi.settings.key.post()
-      expect(response?.keys.length).toBe(1)
+      const response = await api.archives.archive(archive).settings.key.post()
+      expect(response).toBeObject()
     })
   })
-  describe('get', function () {
+  describe('get', () => {
+    let key: XyoArchiveKey
+    beforeEach(async () => {
+      const response = await api.archives.archive(archive).settings.key.post()
+      key = response as unknown as XyoArchiveKey
+    })
     it('Returns the keys for the archive', async () => {
-      const api = getApi()
-      const archiveApi = api.archives.archive(archive)
-      await archiveApi.put()
-      const key = await archiveApi.settings.key.post()
-      const response = await archiveApi.settings.key.get()
-      expect(response?.length).toEqual(1)
-      expect(response?.[0]).toEqual(key)
+      const response = await api.archives.archive(archive).settings.key.get()
+      expect(response).toBeArrayOfSize(1)
+      expect(response).toContainEqual(key)
     })
   })
 })

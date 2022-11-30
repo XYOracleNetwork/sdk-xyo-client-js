712fc490899180d1505365d52c06090ec6df191c
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-30 13:32:57 -0600

    Remove try/catch from tests

--- packages/sdk/packages/api/src/Api/Archive/Api.spec.ts
+++ packages/sdk/packages/api/src/Api/Archive/Api.spec.ts
@@ -11,25 +11,15 @@ describe('XyoArchivistArchiveApi', () => {
     })
     describe('get', function () {
       it('gets the archive', async () => {
-        try {
-          await api.archives.archive(archive).get()
-          const response = await api.archives.archive(archive).get()
-          expect(response?.archive).toBe(archive)
-        } catch (ex) {
-          const error = ex as XyoApiError
-          expect(error === undefined)
-        }
+        await api.archives.archive(archive).get()
+        const response = await api.archives.archive(archive).get()
+        expect(response?.archive).toBe(archive)
       })
     })
     describe('put', function () {
       it('creates the archive', async () => {
-        try {
-          const response = await api.archives.archive(archive).put()
-          expect(response?.archive).toEqual(archive)
-        } catch (ex) {
-          const error = ex as XyoApiError
-          expect(error === undefined)
-        }
+        const response = await api.archives.archive(archive).put()
+        expect(response?.archive).toEqual(archive)
       })
     })
   })
--- packages/sdk/packages/api/src/Api/Block/Api.spec.ts
+++ packages/sdk/packages/api/src/Api/Block/Api.spec.ts
@@ -10,70 +10,44 @@ describe('XyoArchivistArchiveBlockApi', () => {
     it.each([true, false])('posts a single bound witness', async (inlinePayloads) => {
       const builder = new BoundWitnessBuilder({ inlinePayloads }).payload(testPayload)
       const [boundWitness] = builder.build()
-      try {
-        const response = await api.archives.archive().block.post([boundWitness])
-        expect(response?.length).toEqual(1)
-      } catch (ex) {
-        const error = ex as XyoApiError
-        expect(error === undefined)
-      }
+      const response = await api.archives.archive().block.post([boundWitness])
+      expect(response?.length).toEqual(1)
     })
     it.each([true, false])('posts multiple bound witnesses', async (inlinePayloads) => {
       const builder = new BoundWitnessBuilder({ inlinePayloads }).payload(testPayload)
       const [json] = builder.build()
       const boundWitnesses: XyoBoundWitness[] = [json, json]
       //TODO: We are casting the result here since the server has not yet been updated to return the actual saved data
-      try {
-        const response = await api.archives.archive().block.post(boundWitnesses)
-        expect(response?.length).toEqual(2)
-      } catch (ex) {
-        const error = ex as XyoApiError
-        expect(error === undefined)
-      }
+      const response = await api.archives.archive().block.post(boundWitnesses)
+      expect(response?.length).toEqual(2)
     })
   })
   describe('stats', () => {
     it('returns stats for boundwitness', async () => {
-      try {
-        const stats = await api.archives.archive().block.stats.get()
-        expect(stats?.count).toBeGreaterThan(0)
-      } catch (ex) {
-        const error = ex as XyoApiError
-        console.log(JSON.stringify(error.response?.data, null, 2))
-        expect(error === undefined)
-      }
+      const stats = await api.archives.archive().block.stats.get()
+      expect(stats?.count).toBeGreaterThan(0)
     })
   })
   describe('find', () => {
     describe('findBoundWitnessesBefore', function () {
       it('returns bound witnesses from before the timestamp', async () => {
-        try {
-          const [boundWitness] = new BoundWitnessBuilder().build()
-          await api.archives.archive().block.post([boundWitness])
-          const timestamp = Date.now() + 10000
-          // eslint-disable-next-line deprecation/deprecation
-          const response = await api.archives.archive('temp').block.findBefore(timestamp)
-          expect(response?.length).toBe(1)
-        } catch (ex) {
-          const error = ex as XyoApiError
-          expect(error === undefined)
-        }
+        const [boundWitness] = new BoundWitnessBuilder().build()
+        await api.archives.archive().block.post([boundWitness])
+        const timestamp = Date.now() + 10000
+        // eslint-disable-next-line deprecation/deprecation
+        const response = await api.archives.archive('temp').block.findBefore(timestamp)
+        expect(response?.length).toBe(1)
       })
     })
 
     describe('findBoundWitnessesAfter', function () {
       it('returns bound witnesses from before the timestamp', async () => {
-        try {
-          const archive = await getNewArchive(api)
-          const [boundWitness] = new BoundWitnessBuilder().build()
-          await api.archives.archive(archive).block.post([boundWitness])
-          const timestamp = Date.now() - 10000
-          const response = await api.archives.archive(archive).block.find({ order: 'asc', timestamp })
-          expect(response?.length).toBe(1)
-        } catch (ex) {
-          const error = ex as XyoApiError
-          expect(error === undefined)
-        }
+        const archive = await getNewArchive(api)
+        const [boundWitness] = new BoundWitnessBuilder().build()
+        await api.archives.archive(archive).block.post([boundWitness])
+        const timestamp = Date.now() - 10000
+        const response = await api.archives.archive(archive).block.find({ order: 'asc', timestamp })
+        expect(response?.length).toBe(1)
       })
     })
   })

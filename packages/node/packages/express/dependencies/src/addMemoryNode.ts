62e276f58790aeec5f9b2ad7d633930487f5f5d1
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-30 06:42:58 -0600

    Register types to node by symbol name

--- packages/node/packages/express/dependencies/src/addMemoryNode.ts
+++ packages/node/packages/express/dependencies/src/addMemoryNode.ts
@@ -1,8 +1,30 @@
-import { AbstractNode, MemoryNode, NodeConfigSchema, XyoModule } from '@xyo-network/modules'
+import { MemoryNode, NodeConfigSchema, XyoModule } from '@xyo-network/modules'
 import { TYPES } from '@xyo-network/node-core-types'
 import { Container } from 'inversify'
 
 const config = { schema: NodeConfigSchema }
+
+const archivists = [
+  // TYPES.ArchiveArchivist,
+  // TYPES.ArchiveBoundWitnessArchivistFactory,
+  // TYPES.ArchiveKeyArchivist,
+  // TYPES.ArchivePayloadArchivistFactory,
+  // TYPES.ArchivePermissionsArchivistFactory,
+  TYPES.BoundWitnessArchivist,
+  TYPES.PayloadArchivist,
+  // TYPES.UserArchivist,
+  TYPES.WitnessedPayloadArchivist,
+]
+
+const diviners = [
+  TYPES.AddressHistoryDiviner,
+  TYPES.BoundWitnessDiviner,
+  TYPES.BoundWitnessStatsDiviner,
+  TYPES.ModuleAddressDiviner,
+  TYPES.PayloadDiviner,
+  TYPES.PayloadStatsDiviner,
+  TYPES.SchemaStatsDiviner,
+]
 
 export const addMemoryNode = async (container: Container) => {
   const node = await MemoryNode.create({ config: config })
@@ -11,5 +33,25 @@ export const addMemoryNode = async (container: Container) => {
     node.register(mod)
     node.attach(mod.address)
   })
-  container.bind<AbstractNode>(TYPES.Node).toConstantValue(node)
+  container.bind<MemoryNode>(TYPES.Node).toConstantValue(node)
+  await addArchivists(container, node)
+  await addDiviners(container, node)
+}
+
+const addArchivists = async (container: Container, node: MemoryNode) => {
+  await Promise.all(
+    archivists.map(async (type) => {
+      const mod = await container.getAsync<XyoModule>(type)
+      node.attach(mod.address, type.description)
+    }),
+  )
+}
+
+const addDiviners = async (container: Container, node: MemoryNode) => {
+  await Promise.all(
+    diviners.map(async (type) => {
+      const mod = await container.getAsync<XyoModule>(type)
+      node.attach(mod.address, type.description)
+    }),
+  )
 }

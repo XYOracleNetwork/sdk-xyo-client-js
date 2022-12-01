b5e6a678c798d6474e15f13999872b6b3911989f
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-30 19:24:57 -0600

    Remove unused constants

--- packages/node/packages/core/model/src/Diviner/PayloadDiviner.ts
+++ packages/node/packages/core/model/src/Diviner/PayloadDiviner.ts
@@ -4,17 +4,11 @@ import { XyoPayload } from '@xyo-network/payload'
 import { ArchiveQueryPayload } from './ArchiveQueryPayload'
 import { XyoPayloadDivinerPredicate } from './XyoPayloadDivinerPredicate'
 
-export type PayloadSchema = 'network.xyo.diviner.payload.'
-export const PayloadSchema: PayloadSchema = 'network.xyo.diviner.payload.'
-
 export type PayloadQuerySchema = 'network.xyo.diviner.payload.query'
 export const PayloadQuerySchema: PayloadQuerySchema = 'network.xyo.diviner.payload.query'
 
 export type PayloadConfigSchema = 'network.xyo.diviner.payload.config'
 export const PayloadConfigSchema: PayloadConfigSchema = 'network.xyo.diviner.payload.config'
-
-export type PayloadPayload = XyoPayload<{ schema: PayloadSchema }>
-export const isPayloadPayload = (x?: XyoPayload | null): x is PayloadPayload => x?.schema === PayloadSchema
 
 export type PayloadQueryPayload = ArchiveQueryPayload<{ schema: PayloadQuerySchema } & XyoPayloadDivinerPredicate>
 export const isPayloadQueryPayload = (x?: XyoPayload | null): x is PayloadQueryPayload => x?.schema === PayloadQuerySchema

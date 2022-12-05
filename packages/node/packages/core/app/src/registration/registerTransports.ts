4a6ec436e04c89fc86094c9704298467ad347efd
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-05 15:42:26 -0600

    Rename to match interface

--- packages/node/packages/core/app/src/registration/registerTransports.ts
+++ packages/node/packages/core/app/src/registration/registerTransports.ts
@@ -2,6 +2,6 @@ import { AbstractNode } from '@xyo-network/modules'
 
 import { NodeRegistrationFunction } from '../model'
 
-export const addTransports: NodeRegistrationFunction = async (_node: AbstractNode) => {
+export const registerTransports: NodeRegistrationFunction = async (_node: AbstractNode) => {
   // Add Express, File, etc.
 }

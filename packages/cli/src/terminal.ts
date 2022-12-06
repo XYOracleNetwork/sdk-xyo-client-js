1c3e8000da6e0d8efad11144cb56d34faae1d266
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-06 14:40:55 -0600

    Print sized image on CLI start

--- packages/cli/src/terminal.ts
+++ packages/cli/src/terminal.ts
@@ -55,8 +55,9 @@ const getCommand = (node: Node): Promise<boolean> => {
 }
 
 export const startTerminal = async (node: MemoryNode) => {
+  const shrink = { height: 30, width: 82 }
+  await terminal.drawImage('./packages/cli/src/xyo_logo_full_white.png', { shrink })
   let running = true
-  await terminal.drawImage('./packages/cli/src/xyo_logo_full_white.png', { shrink: { height: 10, width: 10 } })
   while (running) {
     running = await getCommand(node)
   }

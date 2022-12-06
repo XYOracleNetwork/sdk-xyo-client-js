3b93d08049b12f5f934b5da96294e08fd9489c5f
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-06 10:18:36 -0600

    Add express transport to node

--- packages/node/packages/core/app/src/configuration/transport/providers/expressHttp.ts
+++ packages/node/packages/core/app/src/configuration/transport/providers/expressHttp.ts
@@ -1,7 +1,9 @@
+import { getApp, server } from '@xyo-network/express-node-server'
 import { AbstractNode } from '@xyo-network/modules'
 
 import { NodeConfigurationFunction } from '../../../model'
 
-export const configureExpressHttpTransport: NodeConfigurationFunction = (_node: AbstractNode) => {
+export const configureExpressHttpTransport: NodeConfigurationFunction = async (_node: AbstractNode) => {
   // TODO: Configure express server handing it the Node
+  await server()
 }

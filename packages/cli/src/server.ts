133f92d51ba5878727fd70b27ed0c480ff9de526
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-06 14:20:39 -0600

    Launch Node via Nodemon

--- packages/cli/src/server.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv'
-config()
-import { tryParseInt } from '@xylabs/sdk-api-express-ecs'
-import { server } from '@xyo-network/express-node-server'
-
-void server(tryParseInt(process.env.APP_PORT))

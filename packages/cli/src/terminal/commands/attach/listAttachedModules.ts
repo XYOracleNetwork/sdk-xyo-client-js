a45440fffe19995756f09161383a91519474ccaf
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-06 18:36:56 -0600

    listAttachedModules formatting

--- packages/cli/src/terminal/commands/attach/listAttachedModules.ts
+++ packages/cli/src/terminal/commands/attach/listAttachedModules.ts
@@ -1,10 +1,15 @@
 import { MemoryNode } from '@xyo-network/node'
 import { terminal } from 'terminal-kit'
 
+import { newline } from '../../../lib'
+
 export const listAttachedModules = async (node: MemoryNode) => {
-  terminal.yellow('\nList Attached Modules\n')
+  newline()
+  terminal.yellow('List Attached Modules')
+  newline()
   const mods = await node.attachedModules()
   mods.forEach((mod) => {
     terminal(`0x${mod.address}`)
+    newline()
   })
 }

ce2774094c18ddef5478d2b95c6211a311938aae
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-07 08:33:12 -0600

    Documentation and notes

--- packages/cli/src/lib/daemon/daemonize.ts
+++ packages/cli/src/lib/daemon/daemonize.ts
@@ -1,8 +1,18 @@
 import { spawn } from 'child_process'
 import { openSync } from 'fs'
 
+// TODO: Separate files?
+// TODO: Create if not exists but only open in append mode
+export const logFile = '/tmp/xyo.log'
+
+// TODO: Write pid to file as singleton to ensure only running
+// one copy of node
+/**
+ * The file to use to ensure singleton process
+ */
+export const pidFile = '/tmp/xyo.pid'
+
 export const daemonizeNode = (): number | undefined => {
-  const logFile = '/tmp/xyo.log'
   const out = openSync(logFile, 'a+')
   const err = openSync(logFile, 'a+')
   const bin = 'tail'
@@ -15,3 +25,6 @@ export const daemonizeNode = (): number | undefined => {
   daemon.unref()
   return daemon.pid
 }
+
+// TODO: Restart process
+// TODO: If already running do something smart

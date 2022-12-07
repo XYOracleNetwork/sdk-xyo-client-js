3420d9efff82361814db7a696c3e7c974da832f8
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-07 08:20:08 -0600

    Return pid from daemonized process

--- packages/cli/src/lib/daemon/daemonize.ts
+++ packages/cli/src/lib/daemon/daemonize.ts
@@ -1,12 +1,17 @@
 import { spawn } from 'child_process'
+import { openSync } from 'fs'
 
-export const daemonizeNode = () => {
+export const daemonizeNode = (): number | undefined => {
+  const logFile = '/tmp/xyo.log'
+  const out = openSync(logFile, 'a+')
+  const err = openSync(logFile, 'a+')
   const bin = 'tail'
   const args: ReadonlyArray<string> = ['-f', 'package.json']
   const daemon = spawn(bin, args, {
     detached: true,
     env: process.env,
-    stdio: ['ignore', 'ignore', 'ignore'],
+    stdio: ['ignore', out, err],
   })
   daemon.unref()
+  return daemon.pid
 }

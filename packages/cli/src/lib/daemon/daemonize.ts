31a26c6fe034d9de2e7fa8ecf2a122d8ac46330c
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-07 08:01:53 -0600

    Daemonize node on start

--- packages/cli/src/lib/daemon/daemonize.ts
+++ packages/cli/src/lib/daemon/daemonize.ts
@@ -1,17 +1,12 @@
 import { spawn } from 'child_process'
-import { open } from 'fs/promises'
 
-export const daemonizeNode = async () => {
-  const logFile = '/tmp/xyo.log'
-  const fdOut = await open(logFile, 'a+')
-  const fdErr = await open(logFile, 'a+')
-  const bin = 'while true; do sleep 1 && date; done;'
-  const args: ReadonlyArray<string> = []
-
+export const daemonizeNode = () => {
+  const bin = 'tail'
+  const args: ReadonlyArray<string> = ['-f', 'package.json']
   const daemon = spawn(bin, args, {
     detached: true,
     env: process.env,
-    stdio: ['ignore', fdOut.fd, fdErr.fd],
+    stdio: ['ignore', 'ignore', 'ignore'],
   })
   daemon.unref()
 }

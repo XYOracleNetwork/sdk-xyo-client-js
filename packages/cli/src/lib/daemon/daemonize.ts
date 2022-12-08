ec22375b44f8868a5885619d2f6716120b0ac088
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-07 19:14:55 -0600

    Use separate files for stdout/stderr

--- packages/cli/src/lib/daemon/daemonize.ts
+++ packages/cli/src/lib/daemon/daemonize.ts
@@ -1,20 +1,29 @@
 import { spawn } from 'child_process'
 import { openSync } from 'fs'
+import { tmpdir } from 'os'
+import { join } from 'path'
 
-// TODO: Separate files?
-// TODO: Create if not exists but only open in append mode
-export const logFile = '/tmp/xyo.log'
+/**
+ * File used for process stdout
+ */
+export const outFile = join(tmpdir(), 'xyo.stdout.txt')
+
+/**
+ * File used for process stderr
+ */
+export const errFile = join(tmpdir(), 'xyo.stderr.txt')
 
 // TODO: Write pid to file as singleton to ensure only running
 // one copy of node
 /**
  * The file to use to ensure singleton process
  */
-export const pidFile = '/tmp/xyo.pid'
+export const pidFile = join(tmpdir(), 'xyo.pid')
 
 export const daemonizeNode = (): number | undefined => {
-  const out = openSync(logFile, 'a+')
-  const err = openSync(logFile, 'a+')
+  // TODO: Create if not exists but only open in append mode
+  const out = openSync(outFile, 'a+')
+  const err = openSync(errFile, 'a+')
   const bin = 'tail'
   const args: ReadonlyArray<string> = ['-f', 'package.json']
   const daemon = spawn(bin, args, {

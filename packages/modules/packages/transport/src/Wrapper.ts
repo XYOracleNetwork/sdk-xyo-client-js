ed7feb4898238175f6027437eaadc3731a72f861
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-08 17:24:50 -0600

    Extend ModuleWrapper

--- packages/modules/packages/transport/src/Wrapper.ts
+++ packages/modules/packages/transport/src/Wrapper.ts
@@ -1,20 +1,6 @@
 import { XyoArchivistApi } from '@xyo-network/api'
-import { ModuleQueryResult, ModuleWrapper, XyoModuleParams, XyoQueryBoundWitness } from '@xyo-network/module'
-import { AbstractNode } from '@xyo-network/node'
-import { XyoPayload } from '@xyo-network/payload'
+import { Module, ModuleWrapper } from '@xyo-network/module'
 
-import { XyoRemoteModuleConfig } from './Config'
-
-export type XyoRemoteAddressHistoryDivinerParams = XyoModuleParams<XyoRemoteModuleConfig> & {
-  api?: XyoArchivistApi
-}
-
-export abstract class RemoteModuleWrapper<T extends AbstractNode = AbstractNode> extends ModuleWrapper<T> {
-  // constructor(protected readonly node: AbstractNode) {}
-  // TODO: How to handle sub address paths
-  // TODO: How to X
-  override async query<T extends XyoQueryBoundWitness = XyoQueryBoundWitness>(query: T, payloads?: XyoPayload[]): Promise<ModuleQueryResult> {
-    await Promise.resolve()
-    throw new Error('TODO')
-  }
+export class RemoteModuleWrapper<TModule extends Module = Module> extends ModuleWrapper<TModule> {
+  // TODO: Discover
 }

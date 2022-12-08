54a809168542ef19af5d480a88bb62687f401266
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-08 09:22:55 -0600

    Run the Node using Node.js as a child process

--- packages/cli/src/lib/process/start.ts
+++ packages/cli/src/lib/process/start.ts
@@ -1,9 +1,15 @@
 import { MemoryNode } from '@xyo-network/node'
 import { getNode } from '@xyo-network/node-app'
 import { spawn } from 'child_process'
+import { join } from 'path'
 
 import { getErrFileDescriptor, getOutFileDescriptor } from './logs'
 import { setPid } from './pid'
+
+/**
+ * The path to the script to run the Node
+ */
+const runNodeScriptPath = join(__dirname, '..', '..', '..', 'runNode.js')
 
 /**
  * Runs the XYO Node process
@@ -11,7 +17,7 @@ import { setPid } from './pid'
  * @param args The arguments to pass to the process
  * @returns The process ID of the Node
  */
-export const start = async (bin = 'tail', args: ReadonlyArray<string> = ['-f', 'package.json'], daemonize = false): Promise<MemoryNode> => {
+export const start = async (bin = 'node', args: ReadonlyArray<string> = [runNodeScriptPath], daemonize = false): Promise<MemoryNode> => {
   // NOTE: Sync FD here because async warns about closing
   // when we background process as daemon
   const out = getOutFileDescriptor()

56d5faccb9b3393d4983ebe0a5bb8117a8981c9b
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-08 08:45:05 -0600

    Start node once via process

--- packages/cli/src/terminal/startTerminal.ts
+++ packages/cli/src/terminal/startTerminal.ts
@@ -1,13 +1,12 @@
 import { MemoryNode } from '@xyo-network/node'
 
-import { printLine, start } from '../lib'
+import { printLine } from '../lib'
 import { getCommand } from './getCommand'
 import { printLogo } from './printLogo'
 import { stopTerminal } from './stopTerminal'
 
 export const startTerminal = async (node: MemoryNode) => {
   await printLogo()
-  await start()
   printLine('XYO Node Running', 'green')
   let running = true
   while (running) {

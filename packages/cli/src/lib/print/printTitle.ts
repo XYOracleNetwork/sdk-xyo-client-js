3b91dbdd56048e36318b42e245fb1394efd83ef7
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-07 22:24:47 -0600

    Add newline before each title

--- packages/cli/src/lib/print/printTitle.ts
+++ packages/cli/src/lib/print/printTitle.ts
@@ -1,5 +1,7 @@
+import { newline } from './newline'
 import { printLine } from './printLine'
 
 export const printTitle = (text?: string | undefined) => {
+  newline()
   printLine(text, 'yellow')
 }

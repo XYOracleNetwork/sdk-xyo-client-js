c15e4135d6bd0cf1092694ec3b1c83b587e48149
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-07 21:37:43 -0600

    Move constants into module

--- packages/cli/src/lib/process/files.ts
+++ packages/cli/src/lib/process/files.ts
@@ -10,10 +10,3 @@ export const outFile = join(tmpdir(), 'xyo.stdout.txt')
  * File used for process stderr
  */
 export const errFile = join(tmpdir(), 'xyo.stderr.txt')
-
-// TODO: Write pid to file as singleton to ensure only running
-// one copy of node
-/**
- * The file to use to ensure singleton process
- */
-export const pidFile = join(tmpdir(), 'xyo.pid')

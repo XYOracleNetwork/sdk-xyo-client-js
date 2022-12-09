83614ed68a372999930fdb36788ce86819a0e63c
Arie Trouw <arie.trouw@xylabs.com>
2022-12-09 12:28:47 -0800

    build fixed

--- packages/plugins/packages/payload/packages/domain/src/Plugin.ts
+++ packages/plugins/packages/payload/packages/domain/src/Plugin.ts
@@ -3,13 +3,11 @@ import { createXyoPayloadPlugin } from '@xyo-network/payload-plugin'
 
 import { XyoDomainPayload } from './Payload'
 import { XyoDomainSchema } from './Schema'
-import jsonSchema from './schema.json'
 import { domainConfigTemplate } from './Template'
 import { XyoDomainPayloadWrapper } from './Wrapper'
 
 export const DomainPayloadPlugin = () =>
   createXyoPayloadPlugin<XyoDomainPayload>({
-    jsonSchema,
     schema: XyoDomainSchema,
     template: domainConfigTemplate,
     wrap: (payload: XyoPayload) => new XyoDomainPayloadWrapper(payload as XyoDomainPayload),

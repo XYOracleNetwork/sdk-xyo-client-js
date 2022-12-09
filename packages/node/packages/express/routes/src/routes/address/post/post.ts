1aa69eeade54fabfcd5740f7139ea3977d6b2396
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-08 21:47:04 -0600

    POST query to address

---
+++ packages/node/packages/express/routes/src/routes/address/post/post.ts
@@ -0,0 +1,24 @@
+import { asyncHandler } from '@xylabs/sdk-api-express-ecs'
+import { ModuleQueryResult } from '@xyo-network/module'
+import { trimAddressPrefix } from '@xyo-network/node-core-lib'
+import { RequestHandler } from 'express'
+
+import { AddressPathParams } from '../AddressPathParams'
+
+const handler: RequestHandler<AddressPathParams, ModuleQueryResult> = async (req, res, next) => {
+  const { address } = req.params
+  const { node } = req.app
+  if (address) {
+    const normalizedAddress = trimAddressPrefix(address).toLowerCase()
+    const modules = await node.tryResolve({ address: [normalizedAddress] })
+    if (modules.length) {
+      const mod = modules[0]
+      const queryResult = await mod.query(req.body)
+      res.json(queryResult)
+      return
+    }
+  }
+  next('route')
+}
+
+export const postAddress = asyncHandler(handler)

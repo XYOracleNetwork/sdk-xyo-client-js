88163b8b8cfa126cbc74194cdc912d8d2e22ac23
Arie Trouw <arie.trouw@xylabs.com>
2022-12-09 12:26:52 -0800

    schema import fixed

--- packages/plugins/packages/payload/packages/domain/src/Plugin.ts
+++ packages/plugins/packages/payload/packages/domain/src/Plugin.ts
@@ -3,12 +3,13 @@ import { createXyoPayloadPlugin } from '@xyo-network/payload-plugin'
 
 import { XyoDomainPayload } from './Payload'
 import { XyoDomainSchema } from './Schema'
+import jsonSchema from './schema.json'
 import { domainConfigTemplate } from './Template'
 import { XyoDomainPayloadWrapper } from './Wrapper'
 
 export const DomainPayloadPlugin = () =>
   createXyoPayloadPlugin<XyoDomainPayload>({
-    jsonSchema: require('./schema.json'),
+    jsonSchema,
     schema: XyoDomainSchema,
     template: domainConfigTemplate,
     wrap: (payload: XyoPayload) => new XyoDomainPayloadWrapper(payload as XyoDomainPayload),

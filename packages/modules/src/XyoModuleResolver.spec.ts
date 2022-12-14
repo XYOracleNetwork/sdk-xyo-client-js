362aee02a72a65a6f419ac2744836f99334dd5df
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-14 13:53:13 -0600

    move repetitive code to beforeAll function

--- packages/modules/src/XyoModuleResolver.spec.ts
+++ packages/modules/src/XyoModuleResolver.spec.ts
@@ -5,36 +5,25 @@ import { XyoModule, XyoModuleResolver } from '@xyo-network/module'
 import { XyoWitnessObserveQuerySchema } from '@xyo-network/witness'
 
 describe('XyoModuleResolver', () => {
-  test('simple by address', async () => {
-    const archivist: XyoModule = await MemoryArchivist.create()
-    const witness = await IdWitness.create({ config: { salt: 'test', schema: IdWitnessConfigSchema } })
-
-    const resolver = new XyoModuleResolver()
+  let archivist: XyoModule
+  let witness: XyoModule
+  let resolver: XyoModuleResolver
+  beforeAll(async () => {
+    archivist = await MemoryArchivist.create()
+    witness = await IdWitness.create({ config: { salt: 'test', schema: IdWitnessConfigSchema } })
+    resolver = new XyoModuleResolver()
     resolver.add(archivist)
     resolver.add(witness)
-
+  })
+  test('simple by address', async () => {
     expect((await resolver.resolve({ address: [archivist.address] })).length).toBe(1)
     expect((await resolver.resolve({ address: [witness.address] })).length).toBe(1)
   })
   test('simple by config', async () => {
-    const archivist: XyoModule = await MemoryArchivist.create()
-    const witness = await IdWitness.create({ config: { salt: 'test', schema: IdWitnessConfigSchema } })
-
-    const resolver = new XyoModuleResolver()
-    resolver.add(archivist)
-    resolver.add(witness)
-
     expect((await resolver.resolve({ config: [MemoryArchivist.configSchema] })).length).toBe(1)
     expect((await resolver.resolve({ config: [IdWitness.configSchema] })).length).toBe(1)
   })
   test('simple by query', async () => {
-    const archivist: XyoModule = await MemoryArchivist.create()
-    const witness = await IdWitness.create({ config: { salt: 'test', schema: IdWitnessConfigSchema } })
-
-    const resolver = new XyoModuleResolver()
-    resolver.add(archivist)
-    resolver.add(witness)
-
     expect((await resolver.resolve({ query: [[XyoArchivistGetQuerySchema, XyoWitnessObserveQuerySchema]] })).length).toBe(0)
     expect((await resolver.resolve({ query: [[XyoArchivistGetQuerySchema]] })).length).toBe(1)
     expect((await resolver.resolve({ query: [[XyoWitnessObserveQuerySchema]] })).length).toBe(1)

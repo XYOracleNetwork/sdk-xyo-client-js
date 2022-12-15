16e1aa4d79b97e656e61beb1d337f0342ef7d130
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-14 18:33:34 -0600

    Document

--- packages/cli/src/lib/print/setTerminalTitle.ts
+++ packages/cli/src/lib/print/setTerminalTitle.ts
@@ -1,8 +1,12 @@
 import { terminal } from 'terminal-kit'
 
-export const setTerminalTitle = (title = 'XYO Node') => {
-  terminal.appName = 'APP Name'
-  // NOTE: String-building here because octal escape
+/**
+ * Sets the title of the terminal
+ * @param title The title, defaults to 'XYO'
+ */
+export const setTerminalTitle = (title = 'XYO') => {
+  // NOTE: String-building here because escape
   // sequences are not allowed in template strings
+  // in strict mode
   terminal('\x1B]0;' + title + '\x07')
 }

2d95ed2b99f9b3eef0412c0fa5fa2104a93220e5
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-15 11:53:18 -0600

    Simplify type names

--- packages/modules/src/XyoModuleResolver.spec.ts
+++ packages/modules/src/XyoModuleResolver.spec.ts
@@ -1,17 +1,17 @@
 /* eslint-disable import/no-internal-modules */
-import { MemoryArchivist, XyoArchivistGetQuerySchema } from '@xyo-network/archivist'
+import { ArchivistGetQuerySchema, MemoryArchivist } from '@xyo-network/archivist'
 import { IdWitness, IdWitnessConfigSchema } from '@xyo-network/id-plugin'
-import { AbstractModule, AbstractModuleResolver } from '@xyo-network/module'
+import { Module, SimpleModuleResolver } from '@xyo-network/module'
 import { XyoWitnessObserveQuerySchema } from '@xyo-network/witness'
 
 describe('XyoModuleResolver', () => {
-  let archivist: XyoModule
-  let witness: XyoModule
-  let resolver: XyoModuleResolver
+  let archivist: Module
+  let witness: Module
+  let resolver: SimpleModuleResolver
   beforeAll(async () => {
     archivist = await MemoryArchivist.create()
     witness = await IdWitness.create({ config: { salt: 'test', schema: IdWitnessConfigSchema } })
-    resolver = new XyoModuleResolver()
+    resolver = new SimpleModuleResolver()
     resolver.add(archivist)
     resolver.add(witness)
   })
@@ -20,14 +20,13 @@ describe('XyoModuleResolver', () => {
     expect((await resolver.resolve({ address: [witness.address] })).length).toBe(1)
   })
   test('simple by config', async () => {
-
     expect((await resolver.resolve({ config: [MemoryArchivist.configSchema] })).length).toBe(1)
     expect((await resolver.resolve({ config: [IdWitness.configSchema] })).length).toBe(1)
   })
   test('simple by query', async () => {
-    expect((await resolver.resolve({ query: [[XyoArchivistGetQuerySchema, XyoWitnessObserveQuerySchema]] })).length).toBe(0)
-    expect((await resolver.resolve({ query: [[XyoArchivistGetQuerySchema]] })).length).toBe(1)
+    expect((await resolver.resolve({ query: [[ArchivistGetQuerySchema, XyoWitnessObserveQuerySchema]] })).length).toBe(0)
+    expect((await resolver.resolve({ query: [[ArchivistGetQuerySchema]] })).length).toBe(1)
     expect((await resolver.resolve({ query: [[XyoWitnessObserveQuerySchema]] })).length).toBe(1)
-    expect((await resolver.resolve({ query: [[XyoArchivistGetQuerySchema], [XyoWitnessObserveQuerySchema]] })).length).toBe(2)
+    expect((await resolver.resolve({ query: [[ArchivistGetQuerySchema], [XyoWitnessObserveQuerySchema]] })).length).toBe(2)
   })
 })

6ff9ea4b7d788fdfa197b9b7cb56c0bf82d1d10a
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-14 23:29:52 -0600

    Limit terminal log size

--- packages/cli/src/lib/print/printFile.ts
+++ packages/cli/src/lib/print/printFile.ts
@@ -3,7 +3,7 @@ import { ScreenBuffer, terminal, TextBuffer } from 'terminal-kit'
 type LoadFunction = (x: string) => Promise<void>
 
 export const printFile = async (path: string) => {
-  const sb = new ScreenBuffer({ dst: terminal })
+  const sb = new ScreenBuffer({ dst: terminal, height: 25 })
   const text = new TextBuffer({ dst: sb })
   await (text.load as LoadFunction)(path)
   text.draw()

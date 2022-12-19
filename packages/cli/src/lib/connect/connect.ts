d79a38793e418fc7e52e73aaad11e968a4ac2009
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-19 14:16:12 -0600

    Hoist constant

--- packages/cli/src/lib/connect/connect.ts
+++ packages/cli/src/lib/connect/connect.ts
@@ -12,11 +12,11 @@ const nodeConnectionErrorMsg = 'Error connecting to Node'
 export const connect = async (attempts = 10, interval = 500) => {
   // TODO: Configurable via config or dynamically determined
   const apiDomain = process.env.API_DOMAIN || 'http://localhost:8080'
+  const apiConfig = { apiDomain }
   printLine(`Connecting to Node at: ${apiDomain}`)
   let count = 0
   do {
     try {
-      const apiConfig = { apiDomain }
       const node = await HttpProxyModule.create({ apiConfig, config })
       printLine(`Connected to Node at: ${apiDomain}`)
       printLine(`Node Address: 0x${node.address}`)

5c81961e5bcbcbb22b8dca463ef4f92ddc526218
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-20 07:16:49 -0600

    Save account

--- packages/cli/src/lib/wallet/saveMnemonic.ts
+++ packages/cli/src/lib/wallet/saveMnemonic.ts
@@ -2,7 +2,9 @@ import { writeFile } from 'fs/promises'
 import { homedir } from 'os'
 import { join } from 'path'
 
-export const saveMnemonic = (mnemonic: string): Promise<void> => {
-  // TODO: Save
-  return Promise.reject()
+const fileName = 'xyo.seed'
+
+export const saveMnemonic = async (mnemonic: string): Promise<void> => {
+  const file = join(homedir(), fileName)
+  await writeFile(file, mnemonic, { encoding: 'utf-8' })
 }

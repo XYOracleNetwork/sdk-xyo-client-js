9c1cd9f297263f603b3327a46911f4c6ab2f9493
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-21 09:02:06 -0600

    Assert diviner queries on wrapper creation

--- packages/modules/packages/diviner/src/DivinerWrapper.ts
+++ packages/modules/packages/diviner/src/DivinerWrapper.ts
@@ -1,10 +1,15 @@
-import { ModuleWrapper } from '@xyo-network/module'
+import { assertEx } from '@xylabs/assert'
+import { Module, ModuleWrapper } from '@xyo-network/module'
 import { PayloadWrapper, XyoPayload } from '@xyo-network/payload'
 
 import { Diviner } from './Diviner'
 import { XyoDivinerDivineQuery, XyoDivinerDivineQuerySchema } from './Queries'
 
 export class DivinerWrapper extends ModuleWrapper implements Diviner {
+  constructor(module: Module) {
+    super(module)
+    assertEx(module.queries().includes(XyoDivinerDivineQuerySchema))
+  }
   divine(payloads?: XyoPayload[]): Promise<XyoPayload[]> {
     const queryPayload = PayloadWrapper.parse<XyoDivinerDivineQuery>({ schema: XyoDivinerDivineQuerySchema })
     return this.sendQuery(queryPayload, payloads)

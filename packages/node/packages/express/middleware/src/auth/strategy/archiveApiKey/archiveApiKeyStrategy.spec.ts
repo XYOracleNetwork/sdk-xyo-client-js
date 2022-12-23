b4b10341aaf160f7879d75f3fcdefc766846c00c
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-23 07:58:36 -0600

    Tests for auth

---
+++ packages/node/packages/express/middleware/src/auth/strategy/archiveApiKey/archiveApiKeyStrategy.spec.ts
@@ -0,0 +1,41 @@
+import { assertEx } from '@xylabs/assert'
+import { claimArchive, createArchiveKey, getTokenForUnitTestUser, request } from '@xyo-network/express-node-test'
+import { StatusCodes } from 'http-status-codes'
+
+describe('archiveApiKeyStrategy', () => {
+  let token = ''
+  let archive = ''
+  let key = ''
+  beforeAll(async () => {
+    token = await getTokenForUnitTestUser()
+    archive = (await claimArchive(token)).archive
+    const createKeyResponse = await createArchiveKey(token, archive)
+    expect(createKeyResponse).toBeTruthy()
+    expect(createKeyResponse).toBeArrayOfSize(1)
+    expect(createKeyResponse?.[0]?.key).toBeString()
+    key = assertEx(createKeyResponse?.[0]).key
+  })
+  describe('for archive with key', () => {
+    it('allows API access using API key', async () => {
+      await (await request()).get(`/archive/${archive}/settings/key`).set('x-api-key', key).expect(StatusCodes.OK)
+    })
+  })
+  describe('for archive without key', () => {
+    it('disallows API access using API key', async () => {
+      const archive = (await claimArchive(token)).archive
+      await (await request()).get(`/archive/${archive}/settings/key`).set('x-api-key', key).expect(StatusCodes.UNAUTHORIZED)
+    })
+  })
+  describe('for routes that allow unauthenticated', () => {
+    it('allows access without API key', async () => {
+      await (await request()).get('/archive').expect(StatusCodes.OK)
+    })
+    it('allows access by valid API key', async () => {
+      await (await request()).get('/archive').set('x-api-key', key).expect(StatusCodes.OK)
+    })
+    it('disallows access by invalid API key', async () => {
+      const key = 'foo'
+      await (await request()).get('/archive').set('x-api-key', key).expect(StatusCodes.UNAUTHORIZED)
+    })
+  })
+})

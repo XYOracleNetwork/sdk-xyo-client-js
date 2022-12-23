f1aa27e160a67fab42370cc34be02b5a6a6381d4
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-22 20:00:07 -0600

    tests for module remote node

--- packages/modules/packages/http/src/RemoteNode/RemoteNode.spec.ts
+++ packages/modules/packages/http/src/RemoteNode/RemoteNode.spec.ts
@@ -1,4 +1,8 @@
+import { assertEx } from '@xylabs/assert'
+import { XyoArchivistApi } from '@xyo-network/api'
 import { XyoApiConfig } from '@xyo-network/api-models'
+import { ModuleWrapper } from '@xyo-network/module'
+import { Module } from '@xyo-network/module-model'
 import { NodeConfig, NodeConfigSchema } from '@xyo-network/node'
 
 import { RemoteNode } from './RemoteNode'
@@ -10,8 +14,38 @@ const name = 'PayloadDiviner'
 describe('RemoteNode', () => {
   describe('create', () => {
     it('creates module', async () => {
-      const mod = await RemoteNode.create({ apiConfig, config })
-      expect(mod).toBeTruthy()
+      const sut = await RemoteNode.create({ apiConfig, config })
+      expect(sut).toBeTruthy()
+    })
+  })
+  describe('tryResolve', () => {
+    const validateModuleResolutionResponse = async (mods: Module[]) => {
+      expect(mods).toBeArray()
+      expect(mods.length).toBe(1)
+      const mod = assertEx(mods.pop())
+      const wrapped = new ModuleWrapper(mod)
+      const response = await wrapped.discover()
+      expect(response).toBeTruthy()
+    }
+    let sut: RemoteNode
+    beforeAll(async () => {
+      sut = await RemoteNode.create({ apiConfig, config })
+      expect(sut).toBeTruthy()
+    })
+    it('resolves by name', async () => {
+      const mods = await sut.tryResolve({ name: [name] })
+      expect(mods)
+      await validateModuleResolutionResponse(mods)
+    })
+    it('resolves by address', async () => {
+      const api = new XyoArchivistApi(apiConfig)
+      const response = await api.get()
+      expect(response).toBeTruthy()
+      expect(response?.address).toBeString()
+      const address = assertEx(response?.address)
+      const mods = await sut.tryResolve({ address: [address] })
+      expect(mods)
+      await validateModuleResolutionResponse(mods)
     })
   })
 })

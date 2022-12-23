8e438da72a03d36ba38de15061ff3dddedb38eb3
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-22 20:29:27 -0600

    Completely implemented interface

--- packages/modules/packages/http/src/RemoteNode/RemoteNode.ts
+++ packages/modules/packages/http/src/RemoteNode/RemoteNode.ts
@@ -1,8 +1,27 @@
 import { XyoApiConfig } from '@xyo-network/api-models'
 import { ModuleParams } from '@xyo-network/module'
-import { Module, ModuleFilter } from '@xyo-network/module-model'
-import { AbstractNode, NodeConfig } from '@xyo-network/node'
+import {
+  AbstractModuleConfigSchema,
+  AbstractModuleDiscoverQuerySchema,
+  AbstractModuleSubscribeQuerySchema,
+  Module,
+  ModuleDescription,
+  ModuleFilter,
+  ModuleQueryResult,
+  XyoQueryBoundWitness,
+} from '@xyo-network/module-model'
+import {
+  NodeConfig,
+  NodeModule,
+  XyoNodeAttachedQuerySchema,
+  XyoNodeAttachQuerySchema,
+  XyoNodeDetachQuerySchema,
+  XyoNodeRegisteredQuerySchema,
+} from '@xyo-network/node'
+import { PayloadFields, SchemaFields, XyoPayload } from '@xyo-network/payload-model'
+import { Promisable } from '@xyo-network/promise'
 
+import { HttpProxyModule } from '../HttpProxyModule'
 import { RemoteModuleResolver } from '../RemoteModuleResolver'
 
 export interface RemoteNodeModuleParams<TConfig extends NodeConfig = NodeConfig> extends ModuleParams<TConfig> {
@@ -10,24 +29,55 @@ export interface RemoteNodeModuleParams<TConfig extends NodeConfig = NodeConfig>
   apiConfig: XyoApiConfig
 }
 
-// TODO: Don't inherit from AbstractNode but rather implement NodeModule
-// TODO: Create remote module for node to issue queries against
-export class RemoteNode<TConfig extends NodeConfig = NodeConfig> extends AbstractNode<TConfig> {
+export class RemoteNode<TConfig extends NodeConfig = NodeConfig> implements NodeModule {
   protected readonly apiConfig: XyoApiConfig
+  protected readonly internalResolver: RemoteModuleResolver
 
-  constructor(params: RemoteNodeModuleParams<TConfig>) {
-    super(params, new RemoteModuleResolver(params.apiConfig))
+  constructor(params: RemoteNodeModuleParams<TConfig>, protected readonly module: HttpProxyModule) {
     this.apiConfig = params.apiConfig
+    this.internalResolver = new RemoteModuleResolver(params.apiConfig)
+  }
+  get address(): string {
+    return this.module.address
+  }
+  get config() {
+    return this.module.config
   }
 
-  static override async create(params: RemoteNodeModuleParams<NodeConfig>): Promise<RemoteNode> {
-    return (await super.create(params)) as RemoteNode
+  get isModuleResolver(): boolean {
+    return true
+  }
+
+  static async create(params: RemoteNodeModuleParams<NodeConfig>): Promise<RemoteNode> {
+    const module = await HttpProxyModule.create({ apiConfig: params.apiConfig, config: { schema: AbstractModuleConfigSchema } })
+    return new this(params, module)
   }
 
   attach(_address: string): void {
     throw new Error('Method not implemented.')
   }
+  attached(): Promise<string[]> {
+    throw new Error('Method not implemented.')
+  }
+  description(): Promise<ModuleDescription> {
+    return this.module.description()
+  }
   detach(_address: string): void {
+    throw new Error('Method not implemented.')
+  }
+  queries(): string[] {
+    return this.module.queries()
+  }
+  query<T extends XyoQueryBoundWitness = XyoQueryBoundWitness>(query: T, payloads?: XyoPayload[]): Promise<ModuleQueryResult> {
+    return this.module.query(query, payloads)
+  }
+  public queryable(schema: string, _addresses?: string[] | undefined) {
+    return this.module.queryable(schema)
+  }
+  register(_module: Module): void {
+    throw new Error('Method not implemented.')
+  }
+  registered(): string[] {
     throw new Error('Method not implemented.')
   }
   async resolve(filter?: ModuleFilter): Promise<Module[]> {

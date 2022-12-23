c34edf2df5b3cfdadc12f16a6278797dc2f304f3
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-23 08:40:31 -0600

    Cache resolved modules

--- packages/modules/packages/http/src/RemoteModuleResolver/RemoteModuleResolver.ts
+++ packages/modules/packages/http/src/RemoteModuleResolver/RemoteModuleResolver.ts
@@ -1,9 +1,12 @@
+import { exists } from '@xylabs/exists'
 import { XyoApiConfig } from '@xyo-network/api-models'
 import { AbstractModuleConfigSchema, Module, ModuleFilter, ModuleResolver } from '@xyo-network/module-model'
 
 import { HttpProxyModule } from '../HttpProxyModule'
 
 export class RemoteModuleResolver implements ModuleResolver {
+  private modules: Record<string, HttpProxyModule> = {}
+
   // TODO: Allow optional ctor param for supplying address for nested Nodes
   // protected readonly address?: string,
   constructor(protected readonly apiConfig: XyoApiConfig) {}
@@ -18,15 +21,12 @@ export class RemoteModuleResolver implements ModuleResolver {
   async resolve(filter?: ModuleFilter): Promise<Module[]> {
     const addresses = filter?.address
     const names = filter?.name
-    // TODO: These should be AND not OR filtering
     // TODO: Handle filter?.config
     // TODO: Handle filter?.query
-    const byAddress =
-      addresses?.map((address) => HttpProxyModule.create({ address, apiConfig: this.apiConfig, config: { schema: AbstractModuleConfigSchema } })) ||
-      []
-    const byName =
-      names?.map((name) => HttpProxyModule.create({ apiConfig: this.apiConfig, config: { schema: AbstractModuleConfigSchema }, name })) || []
-    const modules = await Promise.all([...byAddress, ...byName])
+    // TODO: Should these be AND not OR filtering, can we do it in memory afterwards?
+    const byAddress = addresses?.map(this.resolveByAddress) || []
+    const byName = names?.map(this.resolveByName) || []
+    const modules = (await Promise.all([...byAddress, ...byName])).filter(exists)
     return modules
   }
 
@@ -37,5 +37,21 @@ export class RemoteModuleResolver implements ModuleResolver {
     } catch {
       return Promise.resolve([] as Module[])
     }
+  }
+
+  private async resolveByAddress(address: string): Promise<HttpProxyModule> {
+    const cached = this.modules[address]
+    if (cached) return cached
+    const mod = await HttpProxyModule.create({ address, apiConfig: this.apiConfig, config: { schema: AbstractModuleConfigSchema } })
+    this.modules[address] = mod
+    return mod
+  }
+  private async resolveByName(name: string): Promise<HttpProxyModule> {
+    const cached = this.modules[name]
+    if (cached) return cached
+    const mod = await HttpProxyModule.create({ apiConfig: this.apiConfig, config: { schema: AbstractModuleConfigSchema }, name })
+    this.modules[name] = mod
+    this.modules[mod.address] = mod
+    return mod
   }
 }

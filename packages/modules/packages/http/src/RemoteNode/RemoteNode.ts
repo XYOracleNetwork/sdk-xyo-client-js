e05961e0d1cfff790b35790aed69b3a77420f390
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-12-22 21:06:42 -0600

    Inherit from AbstractNode

--- packages/modules/packages/http/src/RemoteNode/RemoteNode.ts
+++ packages/modules/packages/http/src/RemoteNode/RemoteNode.ts
@@ -2,24 +2,14 @@ import { XyoApiConfig } from '@xyo-network/api-models'
 import { ModuleParams } from '@xyo-network/module'
 import {
   AbstractModuleConfigSchema,
-  AbstractModuleDiscoverQuerySchema,
-  AbstractModuleSubscribeQuerySchema,
   Module,
   ModuleDescription,
   ModuleFilter,
   ModuleQueryResult,
   XyoQueryBoundWitness,
 } from '@xyo-network/module-model'
-import {
-  NodeConfig,
-  NodeModule,
-  XyoNodeAttachedQuerySchema,
-  XyoNodeAttachQuerySchema,
-  XyoNodeDetachQuerySchema,
-  XyoNodeRegisteredQuerySchema,
-} from '@xyo-network/node'
-import { PayloadFields, SchemaFields, XyoPayload } from '@xyo-network/payload-model'
-import { Promisable } from '@xyo-network/promise'
+import { AbstractNode, NodeConfig, NodeConfigSchema } from '@xyo-network/node'
+import { XyoPayload } from '@xyo-network/payload-model'
 
 import { HttpProxyModule } from '../HttpProxyModule'
 import { RemoteModuleResolver } from '../RemoteModuleResolver'
@@ -29,61 +19,58 @@ export interface RemoteNodeModuleParams<TConfig extends NodeConfig = NodeConfig>
   apiConfig: XyoApiConfig
 }
 
-export class RemoteNode<TConfig extends NodeConfig = NodeConfig> implements NodeModule {
+export class RemoteNode<TConfig extends NodeConfig = NodeConfig> extends AbstractNode {
+  static readonly configSchema = NodeConfigSchema
+
   protected readonly apiConfig: XyoApiConfig
   protected readonly internalResolver: RemoteModuleResolver
 
   constructor(params: RemoteNodeModuleParams<TConfig>, protected readonly module: HttpProxyModule) {
+    super(params)
     this.apiConfig = params.apiConfig
     this.internalResolver = new RemoteModuleResolver(params.apiConfig)
   }
-  get address(): string {
+
+  override get address(): string {
     return this.module.address
   }
-  get config() {
-    return this.module.config
-  }
 
-  get isModuleResolver(): boolean {
-    return true
-  }
-
-  static async create(params: RemoteNodeModuleParams<NodeConfig>): Promise<RemoteNode> {
+  static override async create(params: RemoteNodeModuleParams<NodeConfig>): Promise<RemoteNode> {
     const module = await HttpProxyModule.create({ apiConfig: params.apiConfig, config: { schema: AbstractModuleConfigSchema } })
     return new this(params, module)
   }
 
-  attach(_address: string): void {
+  override attach(_address: string): void {
     throw new Error('Method not implemented.')
   }
-  attached(): Promise<string[]> {
+  override attached(): Promise<string[]> {
     throw new Error('Method not implemented.')
   }
-  description(): Promise<ModuleDescription> {
+  override description(): Promise<ModuleDescription> {
     return this.module.description()
   }
-  detach(_address: string): void {
+  override detach(_address: string): void {
     throw new Error('Method not implemented.')
   }
-  queries(): string[] {
+  override queries(): string[] {
     return this.module.queries()
   }
-  query<T extends XyoQueryBoundWitness = XyoQueryBoundWitness>(query: T, payloads?: XyoPayload[]): Promise<ModuleQueryResult> {
+  override query<T extends XyoQueryBoundWitness = XyoQueryBoundWitness>(query: T, payloads?: XyoPayload[]): Promise<ModuleQueryResult> {
     return this.module.query(query, payloads)
   }
-  public queryable(schema: string, _addresses?: string[] | undefined) {
+  override queryable(schema: string, _addresses?: string[] | undefined) {
     return this.module.queryable(schema)
   }
-  register(_module: Module): void {
+  override register(_module: Module): void {
     throw new Error('Method not implemented.')
   }
-  registered(): string[] {
+  override registered(): string[] {
     throw new Error('Method not implemented.')
   }
-  async resolve(filter?: ModuleFilter): Promise<Module[]> {
-    return await this.internalResolver.resolve(filter)
+  override resolve(filter?: ModuleFilter): Promise<Module[]> {
+    return this.internalResolver.resolve(filter)
   }
-  async tryResolve(filter?: ModuleFilter): Promise<Module[]> {
-    return await this.internalResolver.tryResolve(filter)
+  override tryResolve(filter?: ModuleFilter): Promise<Module[]> {
+    return this.internalResolver.tryResolve(filter)
   }
 }

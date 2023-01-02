0ac83a52a337b892092a21b508d4519f6dec4fc7
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-01-02 16:17:43 -0600

    Return exact type of interface

--- packages/modules/packages/module/packages/model/src/model/ModuleRepository.ts
+++ packages/modules/packages/module/packages/model/src/model/ModuleRepository.ts
@@ -2,10 +2,10 @@ import { Module } from './Module'
 import { ModuleResolver } from './ModuleResolver'
 
 export interface ModuleRepository<TModule extends Module = Module> extends ModuleResolver<TModule> {
-  add(module: TModule, name?: string): ModuleRepository<TModule>
-  add(module: TModule[], name?: string[]): ModuleRepository<TModule>
-  add(module: TModule | TModule[], name?: string | string[]): ModuleRepository<TModule>
+  add(module: TModule, name?: string): this
+  add(module: TModule[], name?: string[]): this
+  add(module: TModule | TModule[], name?: string | string[]): this
 
-  remove(name: string | string[]): ModuleRepository<TModule>
-  remove(address: string | string[]): ModuleRepository<TModule>
+  remove(name: string | string[]): this
+  remove(address: string | string[]): this
 }

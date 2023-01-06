2b8b17f4e56870e50a91870376fc360ab47d41af
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-01-06 08:01:04 -0600

    Event emitting placeholder

--- packages/modules/packages/module/src/Resolver/ResolverEventEmitter.ts
+++ packages/modules/packages/module/src/Resolver/ResolverEventEmitter.ts
@@ -6,12 +6,20 @@ export class ResolverEventEmitter<T extends ModuleResolver = ModuleResolver> imp
   get isModuleResolver(): boolean {
     return true
   }
+
   async resolve(filter?: ModuleFilter): Promise<Module[]> {
     const modules = await this.resolver.resolve(filter)
+    await Promise.allSettled(modules.map((mod) => this.onModuleResolved(mod, filter)))
     return modules
   }
+
   async tryResolve(filter?: ModuleFilter): Promise<Module[]> {
     const modules = await this.resolver.tryResolve(filter)
+    await Promise.allSettled(modules.map((mod) => this.onModuleResolved(mod, filter)))
     return modules
+  }
+
+  protected onModuleResolved(_mod: Module, _filter?: ModuleFilter): Promise<void> {
+    return Promise.resolve()
   }
 }

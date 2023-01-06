892586e27a40ceab573eba029b641d7ecb8df708
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-01-05 19:10:09 -0600

    Tests for module addition

--- packages/modules/packages/module/src/Resolver/CompositeModuleResolver.spec.ts
+++ packages/modules/packages/module/src/Resolver/CompositeModuleResolver.spec.ts
@@ -18,15 +18,9 @@ describe('CompositeModuleResolver', () => {
     let resolvers: ModuleRepository[]
     let sut: CompositeModuleResolver
     beforeEach(() => {
-      moduleA = mock<Module>({
-        address: 'A',
-      })
-      moduleB = mock<Module>({
-        address: 'B',
-      })
-      moduleC = mock<Module>({
-        address: 'C',
-      })
+      moduleA = mock<Module>({ address: 'b0e75b722e6cb03bbae3f488ed1e5a82bd7c381a' })
+      moduleB = mock<Module>({ address: 'b0e75b722e6cb03bbae3f488ed1e5a82bd7c381b' })
+      moduleC = mock<Module>({ address: 'b0e75b722e6cb03bbae3f488ed1e5a82bd7c381c' })
       resolverA = new SimpleModuleResolver()
       resolverA.add(moduleA, moduleAName)
       resolverA.add(moduleC, moduleCName)
@@ -36,17 +30,59 @@ describe('CompositeModuleResolver', () => {
       resolvers = [resolverA, resolverB]
       sut = new CompositeModuleResolver(resolvers)
     })
-    it('resolves module in first resolver', async () => {
-      const result = await sut.tryResolve({ name: [moduleAName] })
-      expect(result).toBeArrayOfSize(1)
+    describe('add', () => {
+      it('adds module to resolvers', async () => {
+        const address = 'b0e75b722e6cb03bbae3f488ed1e5a82bd7c381d'
+        const name = 'mod'
+        const mod = mock<Module>({ address })
+        expect(sut.add(mod, name)).toBeTruthy()
+        expect(await sut.tryResolve({ address: [address] })).toBeArrayOfSize(1)
+        expect(await sut.tryResolve({ name: [name] })).toBeArrayOfSize(1)
+        expect(await resolverA.tryResolve({ address: [address] })).toBeArrayOfSize(1)
+        expect(await resolverA.tryResolve({ name: [name] })).toBeArrayOfSize(1)
+        expect(await resolverB.tryResolve({ address: [address] })).toBeArrayOfSize(1)
+        expect(await resolverB.tryResolve({ name: [name] })).toBeArrayOfSize(1)
+      })
     })
-    it('resolves module in second resolver', async () => {
-      const result = await sut.tryResolve({ name: [moduleBName] })
-      expect(result).toBeArrayOfSize(1)
+    describe.skip('remove', () => {
+      it('removes module from resolvers', async () => {
+        const address = moduleC.address
+        const name = moduleCName
+        expect(await sut.tryResolve({ address: [address] })).toBeArrayOfSize(0)
+        expect(await sut.tryResolve({ name: [name] })).toBeArrayOfSize(0)
+        expect(await resolverA.tryResolve({ address: [address] })).toBeArrayOfSize(0)
+        expect(await resolverA.tryResolve({ name: [name] })).toBeArrayOfSize(0)
+        expect(await resolverB.tryResolve({ address: [address] })).toBeArrayOfSize(0)
+        expect(await resolverB.tryResolve({ name: [name] })).toBeArrayOfSize(0)
+      })
     })
-    it('resolves module in both resolvers', async () => {
-      const result = await sut.tryResolve({ name: [moduleCName] })
-      expect(result).toBeArrayOfSize(1)
+    describe('resolve', () => {
+      it('resolves module in first resolver', async () => {
+        const result = await sut.resolve({ name: [moduleAName] })
+        expect(result).toBeArrayOfSize(1)
+      })
+      it('resolves module in second resolver', async () => {
+        const result = await sut.resolve({ name: [moduleBName] })
+        expect(result).toBeArrayOfSize(1)
+      })
+      it('resolves module in both resolvers', async () => {
+        const result = await sut.resolve({ name: [moduleCName] })
+        expect(result).toBeArrayOfSize(1)
+      })
+    })
+    describe('tryResolve', () => {
+      it('resolves module in first resolver', async () => {
+        const result = await sut.tryResolve({ name: [moduleAName] })
+        expect(result).toBeArrayOfSize(1)
+      })
+      it('resolves module in second resolver', async () => {
+        const result = await sut.tryResolve({ name: [moduleBName] })
+        expect(result).toBeArrayOfSize(1)
+      })
+      it('resolves module in both resolvers', async () => {
+        const result = await sut.tryResolve({ name: [moduleCName] })
+        expect(result).toBeArrayOfSize(1)
+      })
     })
   })
 })

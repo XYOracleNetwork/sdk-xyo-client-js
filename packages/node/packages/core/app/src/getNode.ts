d71a8ad53bd52800736f45207fdaae7305c620b7
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-01-07 18:24:21 -0600

    autoAttachExternallyResolved

--- packages/node/packages/core/app/src/getNode.ts
+++ packages/node/packages/core/app/src/getNode.ts
@@ -1,12 +1,15 @@
 import { Account } from '@xyo-network/account'
 import { DynamicModuleResolver } from '@xyo-network/modules'
-import { MemoryNode, NodeConfigSchema } from '@xyo-network/node'
+import { MemoryNode, MemoryNodeParams, NodeConfigSchema } from '@xyo-network/node'
 
 import { configureEnvironment, configureTransports } from './configuration'
 
+const autoAttachExternallyResolved = true
+const config = { schema: NodeConfigSchema }
+
 export const getNode = async (account = Account.random()): Promise<MemoryNode> => {
   const resolver = new DynamicModuleResolver()
-  const params = { account, config: { schema: NodeConfigSchema }, resolver }
+  const params: MemoryNodeParams = { account, autoAttachExternallyResolved, config, resolver }
   const node = await MemoryNode.create(params)
   await configureEnvironment(node)
   await configureTransports(node)

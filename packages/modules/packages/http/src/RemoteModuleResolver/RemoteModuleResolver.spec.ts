c6abf95e10ff7e721a81e176c246ce36bf88230f
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-01-07 23:51:31 -0600

    Add helpers for archivist name generation

--- packages/modules/packages/http/src/RemoteModuleResolver/RemoteModuleResolver.spec.ts
+++ packages/modules/packages/http/src/RemoteModuleResolver/RemoteModuleResolver.spec.ts
@@ -5,6 +5,7 @@ import { ModuleWrapper } from '@xyo-network/module'
 import { Module } from '@xyo-network/module-model'
 import { MemoryNode, NodeConfigSchema } from '@xyo-network/node'
 
+import { getBoundWitnessArchivistName, getPayloadArchivistName } from '../util'
 import { RemoteModuleResolver } from './RemoteModuleResolver'
 
 const apiConfig: XyoApiConfig = { apiDomain: process.env.API_DOMAIN || 'http://localhost:8080' }
@@ -56,11 +57,13 @@ describe('RemoteModuleResolver', () => {
       await validateModuleResolutionResponse(mods)
     })
     it('resolves BoundWitness archives', async () => {
-      const mods = await node.tryResolve({ name: ['temp[boundwitness]'] })
+      const name = getBoundWitnessArchivistName('temp')
+      const mods = await node.tryResolve({ name: [name] })
       await validateModuleResolutionResponse(mods)
     })
     it('resolves Payload archives', async () => {
-      const mods = await node.tryResolve({ name: ['temp[payload]'] })
+      const name = getPayloadArchivistName('temp')
+      const mods = await node.tryResolve({ name: [name] })
       await validateModuleResolutionResponse(mods)
     })
   })

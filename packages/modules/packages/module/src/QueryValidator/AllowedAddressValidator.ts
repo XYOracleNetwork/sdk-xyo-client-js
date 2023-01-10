d22e54c3320420dc69ba20775e471ea2a3390a27
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-01-10 12:18:18 -0600

    Expose getter

--- packages/modules/packages/module/src/QueryValidator/AllowedAddressValidator.ts
+++ packages/modules/packages/module/src/QueryValidator/AllowedAddressValidator.ts
@@ -6,25 +6,30 @@ import { Queryable, QueryValidator } from './QueryValidator'
 type SortedPipedAddressesString = string
 
 export class AllowedAddressValidator implements QueryValidator {
-  protected allowedAddressSets: Record<SchemaString, SortedPipedAddressesString[]> = {}
-  protected disallowedAddresses: Record<SchemaString, AddressString[]> = {}
+  protected _allowedAddressSets: Record<SchemaString, SortedPipedAddressesString[]> = {}
+  protected _disallowedAddresses: Record<SchemaString, AddressString[]> = {}
   constructor(mod: AbstractModule) {
     if (mod.config.security?.allowed) {
       Object.entries(mod.config.security?.allowed).forEach(([schema, addressesList]) => {
-        this.allowedAddressSets[schema] = addressesList.map((addresses) => addresses.sort().join('|'))
+        this._allowedAddressSets[schema] = addressesList.map((addresses) => addresses.sort().join('|'))
       })
     }
-    this.disallowedAddresses = mod.config.security?.disallowed || {}
+    this._disallowedAddresses = mod.config.security?.disallowed || {}
   }
+
+  public get allowedAddressSets(): Record<SchemaString, SortedPipedAddressesString[]> {
+    return this._allowedAddressSets
+  }
+
   queryable: Queryable = (query, _mod) => {
     const schema = query.query.schema
     const addresses = query.addresses
     return addresses ? this.queryAllowed(schema, addresses) ?? !this.queryDisallowed(schema, addresses) ?? true : true
   }
   protected queryAllowed = (schema: SchemaString, addresses: SortedPipedAddressesString[]) => {
-    return this.allowedAddressSets?.[schema]?.includes(addresses.sort().join('|'))
+    return this._allowedAddressSets?.[schema]?.includes(addresses.sort().join('|'))
   }
   protected queryDisallowed = (schema: SchemaString, addresses: string[]) => {
-    return addresses.reduce((previousValue, address) => previousValue || this.disallowedAddresses?.[schema]?.includes(address), false)
+    return addresses.reduce((previousValue, address) => previousValue || this._disallowedAddresses?.[schema]?.includes(address), false)
   }
 }

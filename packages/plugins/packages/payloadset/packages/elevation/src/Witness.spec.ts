5e95782beb47048aa42fa98f2b7ff374eaa41955
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-01-10 12:16:46 -0600

    Simply test logic

--- packages/plugins/packages/payloadset/packages/elevation/src/Witness.spec.ts
+++ packages/plugins/packages/payloadset/packages/elevation/src/Witness.spec.ts
@@ -6,49 +6,34 @@ import { Quadkey } from '@xyo-network/quadkey'
 
 import { ElevationWitness, ElevationWitnessConfigSchema } from './Witness'
 
+const locations = [
+  { quadkey: assertEx(Quadkey.fromLngLat({ lat: 32, lng: 117 }, 24)?.base10String), schema: LocationSchema },
+  { quadkey: assertEx(Quadkey.fromLngLat({ lat: 31, lng: 116 }, 24)?.base10String), schema: LocationSchema },
+  { quadkey: assertEx(Quadkey.fromLngLat({ lat: 33, lng: 118 }, 24)?.base10String), schema: LocationSchema },
+]
+
 describe('ElevationWitness', () => {
   test('Witnessing via Observe', async () => {
     const witness = await ElevationWitness.create()
-
-    const result = (await witness.observe([
-      { quadkey: assertEx(Quadkey.fromLngLat({ lat: 32, lng: 117 }, 24)).base10String, schema: LocationSchema },
-      { quadkey: assertEx(Quadkey.fromLngLat({ lat: 31, lng: 116 }, 24)).base10String, schema: LocationSchema },
-      { quadkey: assertEx(Quadkey.fromLngLat({ lat: 33, lng: 118 }, 24)).base10String, schema: LocationSchema },
-    ])) as ElevationPayload[]
-
-    expect(result[0].elevation).toBeDefined()
-    expect(result[1].elevation).toBeDefined()
-    expect(result[2].elevation).toBeDefined()
-    expect(result[0].elevation === result[1].elevation).toBe(false)
-    expect(result[0].elevation === result[2].elevation).toBe(false)
-    expect(result[1].elevation === result[2].elevation).toBe(false)
-    expect(new PayloadWrapper(result[0]).valid).toBe(true)
-    expect(new PayloadWrapper(result[1]).valid).toBe(true)
-    expect(new PayloadWrapper(result[2]).valid).toBe(true)
+    const result = (await witness.observe(locations)) as ElevationPayload[]
+    validateResult(result)
   })
 
   test('Witnessing via Config', async () => {
-    const witness = await ElevationWitness.create({
-      config: {
-        locations: [
-          { quadkey: assertEx(Quadkey.fromLngLat({ lat: 32, lng: 117 }, 24)?.base10String), schema: LocationSchema },
-          { quadkey: assertEx(Quadkey.fromLngLat({ lat: 31, lng: 116 }, 24)?.base10String), schema: LocationSchema },
-          { quadkey: assertEx(Quadkey.fromLngLat({ lat: 33, lng: 118 }, 24)?.base10String), schema: LocationSchema },
-        ],
-        schema: ElevationWitnessConfigSchema,
-      },
-    })
-
+    const witness = await ElevationWitness.create({ config: { locations, schema: ElevationWitnessConfigSchema } })
     const result = (await witness.observe()) as ElevationPayload[]
-
-    expect(result[0].elevation).toBeDefined()
-    expect(result[1].elevation).toBeDefined()
-    expect(result[2].elevation).toBeDefined()
-    expect(result[0].elevation === result[1].elevation).toBe(false)
-    expect(result[0].elevation === result[2].elevation).toBe(false)
-    expect(result[1].elevation === result[2].elevation).toBe(false)
-    expect(new PayloadWrapper(result[0]).valid).toBe(true)
-    expect(new PayloadWrapper(result[1]).valid).toBe(true)
-    expect(new PayloadWrapper(result[2]).valid).toBe(true)
+    validateResult(result)
   })
 })
+
+const validateResult = (result: ElevationPayload[]) => {
+  expect(result[0].elevation).toBeDefined()
+  expect(result[1].elevation).toBeDefined()
+  expect(result[2].elevation).toBeDefined()
+  expect(result[0].elevation === result[1].elevation).toBe(false)
+  expect(result[0].elevation === result[2].elevation).toBe(false)
+  expect(result[1].elevation === result[2].elevation).toBe(false)
+  expect(new PayloadWrapper(result[0]).valid).toBe(true)
+  expect(new PayloadWrapper(result[1]).valid).toBe(true)
+  expect(new PayloadWrapper(result[2]).valid).toBe(true)
+}

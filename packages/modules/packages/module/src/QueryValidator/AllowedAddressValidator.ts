cab2c3bb1debe4629213b66d3b1f573fa4fbe194
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-01-10 09:49:42 -0600

    Optimize

--- packages/modules/packages/module/src/QueryValidator/AllowedAddressValidator.ts
+++ packages/modules/packages/module/src/QueryValidator/AllowedAddressValidator.ts
@@ -1,22 +1,25 @@
-import { SchemaString } from '@xyo-network/module-model'
+import { AddressString, SchemaString } from '@xyo-network/module-model'
 
 import { Queryable } from './QueryValidator'
+
+type SortedPipedAddressesString = string
 
 export const allowedAddressValidator: Queryable = (query, mod) => {
   const schema = query.query.schema
   const addresses = query.addresses
-  const allowedAddressSets: Record<SchemaString, string[]> = {}
+  const allowedAddressSets: Record<SchemaString, SortedPipedAddressesString[]> = {}
+  const disallowedAddresses: Record<SchemaString, AddressString[]> = mod.config.security?.disallowed || {}
+
   if (mod.config.security?.allowed) {
     Object.entries(mod.config.security?.allowed).forEach(([schema, addressesList]) => {
       allowedAddressSets[schema] = addressesList.map((addresses) => addresses.sort().join('|'))
     })
   }
-  const queryAllowed = (schema: SchemaString, addresses: string[]) => {
+  const queryAllowed = (schema: SchemaString, addresses: SortedPipedAddressesString[]) => {
     return allowedAddressSets?.[schema]?.includes(addresses.sort().join('|'))
   }
 
   const queryDisallowed = (schema: SchemaString, addresses: string[]) => {
-    const disallowedAddresses = mod.config.security?.disallowed || {}
     return addresses.reduce((previousValue, address) => previousValue || disallowedAddresses?.[schema]?.includes(address), false)
   }
   return addresses ? queryAllowed(schema, addresses) ?? !queryDisallowed(schema, addresses) ?? true : true

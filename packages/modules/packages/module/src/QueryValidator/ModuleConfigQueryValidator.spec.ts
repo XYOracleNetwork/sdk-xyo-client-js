8641cb8f440aca71e53b86333f0b9cbe1956bacf
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-01-18 13:36:25 -0600

    Scaffold tests

---
+++ packages/modules/packages/module/src/QueryValidator/ModuleConfigQueryValidator.spec.ts
@@ -0,0 +1,43 @@
+describe('ModuleConfigQueryValidator', () => {
+  describe('queryable', () => {
+    describe('allowed', () => {
+      it('allows schema from allowed address', () => {
+        // TODO
+      })
+      it('allows schema from allowed addresses', () => {
+        // TODO
+      })
+      it('allows schema from allowed addresses set', () => {
+        // TODO
+      })
+      it('disallows schema from non-allowed address', () => {
+        // TODO
+      })
+      it('disallows schema from non-allowed addresses', () => {
+        // TODO
+      })
+      it('disallows non-allowed schema from allowed address', () => {
+        // TODO
+      })
+    })
+    describe('disallowed', () => {
+      it('allows schema from non-disallowed address', () => {
+        // TODO
+      })
+      it('allows schema from non-disallowed addresses', () => {
+        // TODO
+      })
+      it('disallows schema from disallowed address', () => {
+        // TODO
+      })
+      it('disallows schema from disallowed addresses', () => {
+        // TODO
+      })
+    })
+    describe('allowed & disallowed', () => {
+      it('disallowed takes precedence', () => {
+        // TODO
+      })
+    })
+  })
+})

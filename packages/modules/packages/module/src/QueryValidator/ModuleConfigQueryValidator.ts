dc29311fbb532d07699d54b48f60f1b5a4c0d839
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-01-18 15:53:25 -0600

    Update naming/optimize edge cases

--- packages/modules/packages/module/src/QueryValidator/ModuleConfigQueryValidator.ts
+++ packages/modules/packages/module/src/QueryValidator/ModuleConfigQueryValidator.ts
@@ -1,4 +1,5 @@
 import { AbstractModuleConfig, AbstractModuleQuery, AddressString, CosigningAddressSet, SchemaString } from '@xyo-network/module-model'
+import { add } from 'lodash'
 
 import { QueryBoundWitnessWrapper } from '../Query'
 import { Queryable, QueryValidator } from './QueryValidator'
@@ -6,38 +7,50 @@ import { Queryable, QueryValidator } from './QueryValidator'
 export type SortedPipedAddressesString = string
 
 export class ModuleConfigQueryValidator<TConfig extends AbstractModuleConfig = AbstractModuleConfig> implements QueryValidator {
-  protected _allowedAddressSets: Record<SchemaString, SortedPipedAddressesString[]> = {}
-  protected _disallowedAddresses: Record<SchemaString, AddressString[]> = {}
+  protected allowedAddressSets: Record<SchemaString, SortedPipedAddressesString[]> = {}
+  protected disallowedAddresses: Record<SchemaString, AddressString[]> = {}
+  protected readonly hasRules: boolean
+
   constructor(config?: TConfig) {
     if (config?.security?.allowed) {
       Object.entries(config.security?.allowed).forEach(([schema, addressesList]) => {
-        this._allowedAddressSets[schema] = addressesList.map(toAddressesString)
+        this.allowedAddressSets[schema] = addressesList.map(toAddressesString)
       })
     }
-    this._disallowedAddresses = config?.security?.disallowed || {}
+    // TODO: ToLowerCase
+    this.disallowedAddresses = config?.security?.disallowed || {}
+    this.hasRules = Object.keys(this.allowedAddressSets).length > 0 || Object.keys(this.allowedAddressSets).length > 0
   }
 
   queryable: Queryable = (query, payloads) => {
+    if (!this.hasRules) return true
+    const addresses = query.addresses
+    if (!addresses.length) return false
     const wrapper = QueryBoundWitnessWrapper.parseQuery<AbstractModuleQuery>(query, payloads)
     const schema = wrapper.query.schema
-    const addresses = query.addresses
-    return addresses ? this.queryAllowed(schema, addresses) ?? !this.queryDisallowed(schema, addresses) ?? true : true
+    return this.queryAllowed(schema, addresses) && !this.queryDisallowed(schema, addresses)
   }
+
   protected queryAllowed = (schema: SchemaString, addresses: string[]) => {
     // All cosigners must sign
     if (addresses.length > 1) {
       const signatories = toAddressesString(addresses)
-      const validCosigners = this._allowedAddressSets?.[schema]?.includes(signatories)
+      const validCosigners = this.allowedAddressSets?.[schema]?.includes(signatories)
       if (validCosigners) return true
     }
     // OR all signers have to be allowed individually
-    return addresses.every((address) => this._allowedAddressSets?.[schema]?.includes(address))
+    return addresses.every((address) => this.allowedAddressSets?.[schema]?.includes(address) || false)
   }
   protected queryDisallowed = (schema: SchemaString, addresses: string[]) => {
-    return addresses.reduce((previousValue, address) => previousValue || this._disallowedAddresses?.[schema]?.includes(address), false)
+    return addresses.reduce((previousValue, address) => previousValue || this.disallowedAddresses?.[schema]?.includes(address), false)
   }
 }
 
 const toAddressesString = (addresses: string | CosigningAddressSet): SortedPipedAddressesString => {
-  return Array.isArray(addresses) ? addresses.sort().join('|') : addresses
+  return Array.isArray(addresses)
+    ? addresses
+        .sort()
+        .map((address) => address.toLowerCase())
+        .join('|')
+    : addresses.toLowerCase()
 }

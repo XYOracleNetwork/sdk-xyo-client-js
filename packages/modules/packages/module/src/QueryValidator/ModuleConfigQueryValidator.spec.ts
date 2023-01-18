0b5c02b4b32f449e7c162f08091bf55ec9164739
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-01-18 14:11:48 -0600

    Positive assertion tests

--- packages/modules/packages/module/src/QueryValidator/ModuleConfigQueryValidator.spec.ts
+++ packages/modules/packages/module/src/QueryValidator/ModuleConfigQueryValidator.spec.ts
@@ -1,19 +1,64 @@
+import { Account } from '@xyo-network/account'
+import {
+  AbstractModuleConfig,
+  AbstractModuleConfigSchema,
+  AbstractModuleDiscoverQuerySchema,
+  AddressString,
+  CosigningAddressSet,
+  SchemaString,
+} from '@xyo-network/module-model'
+import { XyoPayloadBuilder } from '@xyo-network/payload-builder'
+
+import { QueryBoundWitnessBuilder } from '../Query'
+import { ModuleConfigQueryValidator } from './ModuleConfigQueryValidator'
+
+const schema = AbstractModuleDiscoverQuerySchema
+
 describe('ModuleConfigQueryValidator', () => {
+  const allowed1 = Account.random()
+  const allowed2 = Account.random()
+  const allowedCosigner1 = Account.random()
+  const allowedCosigner2 = Account.random()
+  const disallowed1 = Account.random()
+  const disallowed2 = Account.random()
+  const allowed: Record<SchemaString, (AddressString | CosigningAddressSet)[]> = {}
+  allowed[AbstractModuleDiscoverQuerySchema] = [
+    allowed1.addressValue.hex,
+    allowed2.addressValue.hex,
+    [allowedCosigner1.addressValue.hex, allowedCosigner2.addressValue.hex],
+  ]
+  const disallowed: Record<SchemaString, AddressString[]> = {}
+  disallowed[AbstractModuleDiscoverQuerySchema] = [disallowed1.addressValue.hex, disallowed2.addressValue.hex]
+  const config: AbstractModuleConfig = { schema: AbstractModuleConfigSchema, security: { allowed } }
+  const sut = new ModuleConfigQueryValidator(config)
   describe('queryable', () => {
     describe('allowed', () => {
       it('allows schema from allowed address', () => {
-        // TODO
+        const queryPayload = new XyoPayloadBuilder({ schema }).build()
+        const query = new QueryBoundWitnessBuilder({ inlinePayloads: true }).witness(allowed1).query(queryPayload).build()
+        expect(sut.queryable(query[0], query[1])).toBeTrue()
       })
       it('allows schema from allowed addresses', () => {
-        // TODO
+        const queryPayload = new XyoPayloadBuilder({ schema }).build()
+        const query = new QueryBoundWitnessBuilder({ inlinePayloads: true }).witness(allowed1).witness(allowed2).query(queryPayload).build()
+        expect(sut.queryable(query[0], query[1])).toBeTrue()
       })
       it('allows schema from allowed addresses set', () => {
-        // TODO
+        const queryPayload = new XyoPayloadBuilder({ schema }).build()
+        const query = new QueryBoundWitnessBuilder({ inlinePayloads: true })
+          .witness(allowedCosigner1)
+          .witness(allowedCosigner2)
+          .query(queryPayload)
+          .build()
+        expect(sut.queryable(query[0], query[1])).toBeTrue()
       })
       it('disallows schema from non-allowed address', () => {
         // TODO
       })
       it('disallows schema from non-allowed addresses', () => {
+        // TODO
+      })
+      it('disallows schema from no address', () => {
         // TODO
       })
       it('disallows non-allowed schema from allowed address', () => {

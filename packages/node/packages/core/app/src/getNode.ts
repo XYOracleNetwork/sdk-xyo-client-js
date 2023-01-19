ff6dc7c6753f56c3c172a29182173147e6082442
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-01-19 17:27:15 -0600

    Dynamic module resolver for JIT Archivists

--- packages/node/packages/core/app/src/getNode.ts
+++ packages/node/packages/core/app/src/getNode.ts
@@ -1,12 +1,15 @@
 import { Account } from '@xyo-network/account'
+import { DynamicModuleResolver } from '@xyo-network/modules'
 import { MemoryNode, MemoryNodeParams, NodeConfigSchema } from '@xyo-network/node'
 
 import { configureEnvironment, configureTransports } from './configuration'
 
 const config = { schema: NodeConfigSchema }
 
+const resolver = new DynamicModuleResolver()
+
 export const getNode = async (account = Account.random()): Promise<MemoryNode> => {
-  const params: MemoryNodeParams = { account, config }
+  const params: MemoryNodeParams = { account, config, resolver }
   const node = await MemoryNode.create(params)
   await configureEnvironment(node)
   await configureTransports(node)

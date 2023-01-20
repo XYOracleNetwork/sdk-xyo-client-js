98f60d181a3109c006689225edf1c51525f4aa7a
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-01-19 20:13:07 -0600

    Deprecate Archives routes in SDK

--- packages/sdk/packages/api/src/Api/Archives/Api.ts
+++ packages/sdk/packages/api/src/Api/Archives/Api.ts
@@ -3,15 +3,13 @@ import { XyoApiSimple } from '../../Simple'
 import { XyoArchivistArchiveApi } from '../Archive'
 
 export class XyoArchivistArchivesApi extends XyoApiSimple<XyoArchive[]> {
+  /**
+   * @deprecated Use module API
+   */
   public archive(archive = 'temp') {
     return new XyoArchivistArchiveApi({
       ...this.config,
       root: `${this.root}${archive}/`,
     })
-  }
-
-  /** @deprecated: use archive(string) instead */
-  public select(archive = 'temp') {
-    return this.archive(archive)
   }
 }

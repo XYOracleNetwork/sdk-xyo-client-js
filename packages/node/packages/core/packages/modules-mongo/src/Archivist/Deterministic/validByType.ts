a2c3cb766b5705ae4689e6f5d4186f4e456a6f6a
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-01-26 13:36:50 -0600

    Add missing import

--- packages/node/packages/core/packages/modules-mongo/src/Archivist/Deterministic/validByType.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Archivist/Deterministic/validByType.ts
@@ -1,5 +1,6 @@
 import { XyoBoundWitnessSchema } from '@xyo-network/boundwitness-model'
 import { BoundWitnessWrapper } from '@xyo-network/boundwitness-wrapper'
+import { XyoPayload } from '@xyo-network/payload-model'
 import { PayloadWrapper } from '@xyo-network/payload-wrapper'
 
 export const validByType = (results: [BoundWitnessWrapper[], PayloadWrapper[]] = [[], []], value?: XyoPayload) => {

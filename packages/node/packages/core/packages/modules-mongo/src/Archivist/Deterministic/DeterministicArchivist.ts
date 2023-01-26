bc61cdd8c9487892d7d8b44f048f448dda8fa9a1
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-01-26 09:30:51 -0600

    Add ctor/create

--- packages/node/packages/core/packages/modules-mongo/src/Archivist/Deterministic/DeterministicArchivist.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Archivist/Deterministic/DeterministicArchivist.ts
@@ -1,9 +1,35 @@
-import { AbstractArchivist } from '@xyo-network/archivist'
+import { assertEx } from '@xylabs/assert'
+import { Account } from '@xyo-network/account'
+import { AbstractArchivist, ArchivistConfig } from '@xyo-network/archivist'
 import { XyoBoundWitness } from '@xyo-network/boundwitness-model'
+import { ModuleParams } from '@xyo-network/module'
 import { XyoPayload } from '@xyo-network/payload-model'
 import { PromisableArray } from '@xyo-network/promise'
+import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 
-export class DeterministicArchivist extends AbstractArchivist {
+import { COLLECTIONS } from '../../collections'
+import { getBaseMongoSdk } from '../../Mongo'
+
+export interface MongoDBDeterministicArchivistParams<TConfig extends ArchivistConfig = ArchivistConfig> extends ModuleParams<TConfig> {
+  boundWitnesses: BaseMongoSdk<XyoBoundWitness>
+  payloads: BaseMongoSdk<XyoPayload>
+}
+
+export class MongoDBDeterministicArchivist<TConfig extends ArchivistConfig = ArchivistConfig> extends AbstractArchivist {
+  protected readonly boundWitnesses: BaseMongoSdk<XyoBoundWitness>
+  protected readonly payloads: BaseMongoSdk<XyoPayload>
+
+  public constructor(params: MongoDBDeterministicArchivistParams<TConfig>) {
+    super(params)
+    this.account = params?.account || new Account({ phrase: assertEx(process.env.ACCOUNT_SEED) })
+    this.boundWitnesses = params?.boundWitnesses || getBaseMongoSdk<XyoBoundWitness>(COLLECTIONS.BoundWitnesses)
+    this.payloads = params?.payloads || getBaseMongoSdk<XyoPayload>(COLLECTIONS.Payloads)
+  }
+
+  static override async create(params?: Partial<MongoDBDeterministicArchivistParams>): Promise<MongoDBDeterministicArchivist> {
+    return (await super.create(params)) as MongoDBDeterministicArchivist
+  }
+
   get(hashes: string[]): PromisableArray<XyoPayload> {
     throw new Error('Method not implemented.')
   }

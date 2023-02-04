70d0f48577fb9a94c1ea3ec940d19c71b9c5a8f2
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-04 12:03:31 -0600

    Register handlers when running under TS

--- packages/cli/src/command/commands/config.ts
+++ packages/cli/src/command/commands/config.ts
@@ -1,15 +1,17 @@
 import { ArgumentsCamelCase, Argv, CommandBuilder, CommandModule } from 'yargs'
 
+import { opts } from '../requireDirectoryOptions'
+
 // eslint-disable-next-line @typescript-eslint/ban-types
 type Arguments = {}
 
 export const aliases: ReadonlyArray<string> = []
 export const builder: CommandBuilder = (yargs: Argv) =>
-  yargs.usage('Usage: $0 config <command> [Options]').commandDir('./config').demandCommand().version(false)
+  yargs.usage('Usage: $0 config <command> [Options]').commandDir('./config', opts).demandCommand().version(false)
 export const command = 'config <command> [Options]'
 export const deprecated = false
 export const describe = 'XYO config commands'
-export const handler = function (_argv: ArgumentsCamelCase<Arguments>) {
+export const handler = (_argv: ArgumentsCamelCase<Arguments>) => {
   // do something with argv.
   console.log('handler')
 }
--- packages/cli/src/command/commands/config/show.ts
+++ packages/cli/src/command/commands/config/show.ts
@@ -1,23 +1,26 @@
-import { ArgumentsCamelCase, CommandBuilder, CommandModule } from 'yargs'
+import { json } from 'stream/consumers'
+import { ArgumentsCamelCase, CommandBuilder, CommandModule, Options } from 'yargs'
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type Arguments = {}
 
 export const aliases: ReadonlyArray<string> = []
 export const builder: CommandBuilder = {
-  banana: {
-    default: 'cool',
-  },
-  batman: {
-    default: 'sad',
-  },
+  outFile: {
+    description: 'The file path to output the config to',
+    type: 'string',
+  } as Options,
+  output: {
+    choices: ['json'], // TODO: YAML
+    default: json,
+  } as Options,
 }
 export const command = 'show'
 export const deprecated = false
 export const describe = 'display config'
-export const handler = function (_argv: ArgumentsCamelCase<Arguments>) {
+export const handler = (_argv: ArgumentsCamelCase<Arguments>) => {
   // do something with argv.
-  console.log('handler')
+  console.log(_argv)
 }
 
 const mod: CommandModule = {

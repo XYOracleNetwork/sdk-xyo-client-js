6225b0c9e7b13a3b3c42f9e2a895cf62ca0f1b8a
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-04 08:02:53 -0600

    Sample CLI versioning technique

--- packages/cli/src/xyo.ts
+++ packages/cli/src/xyo.ts
@@ -2,9 +2,13 @@ import yargs from 'yargs'
 // eslint-disable-next-line import/no-internal-modules
 import { hideBin } from 'yargs/helpers'
 
+// import * as pack from '../package.json'
+
 void yargs(hideBin(process.argv))
+  .help()
   .commandDir('./command', {
     extensions: process.env.NODE_ENV === 'development' ? ['js', 'ts'] : ['js'],
   })
+  // .version(pack.version)
   .demandCommand(1)
   .parse()

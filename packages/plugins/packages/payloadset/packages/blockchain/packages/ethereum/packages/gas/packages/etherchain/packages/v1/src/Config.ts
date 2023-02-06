defa02ec6d42f454cad17b5757c2000b0d591c1d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-06 16:13:49 -0600

    Remove Etherchain V1

--- packages/plugins/packages/payloadset/packages/blockchain/packages/ethereum/packages/gas/packages/etherchain/packages/v1/src/Config.ts
+++
@@ -1,7 +0,0 @@
-import { XyoWitnessConfig } from '@xyo-network/witness'
-
-import { XyoEthereumGasEtherchainV1WitnessConfigSchema } from './Schema'
-
-export type XyoEthereumGasEtherchainV1WitnessConfig = XyoWitnessConfig<{
-  schema: XyoEthereumGasEtherchainV1WitnessConfigSchema
-}>

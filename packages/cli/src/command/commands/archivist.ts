93e9963d764571d39ce26f10f3ac29adcadc74b8
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-06 06:54:04 -0600

    Find all archivists if no subcommand specified

--- packages/cli/src/command/commands/archivist.ts
+++ packages/cli/src/command/commands/archivist.ts
@@ -1,28 +1,31 @@
+import { EmptyObject } from '@xyo-network/core'
+import { ArchivistGetQuerySchema } from '@xyo-network/modules'
 import { parse } from 'path'
 import { ArgumentsCamelCase, Argv, CommandBuilder, CommandModule } from 'yargs'
 
+import { BaseArguments } from '../BaseArguments'
 import { opts } from '../requireDirectoryOptions'
-import { getModule } from './util'
+import { getNode } from '../util'
 
 export const aliases: ReadonlyArray<string> = []
 export const builder: CommandBuilder = (yargs: Argv) =>
-  yargs.usage('Usage: $0 archivist <query> <address> [Options]').commandDir(parse(__filename).name, opts).demandCommand().version(false)
+  yargs.usage('Usage: $0 archivist <query> <address> [Options]').commandDir(parse(__filename).name, opts).version(false)
 export const command = 'archivist'
 export const deprecated = false
 export const describe = 'Issue queries against an XYO archivist'
-export const handler = async (argv: ArgumentsCamelCase<Arguments>) => {
-  const { hashes, verbose } = argv
+export const handler = async (argv: ArgumentsCamelCase<BaseArguments>) => {
+  const { verbose } = argv
   try {
-    const archivist = await getModule(argv)
-    const result = await archivist.get(hashes)
-    console.log(result)
+    const node = await getNode(argv)
+    const result = ((await node.description()) ?? {})?.children?.filter((mod) => mod.queries.includes(ArchivistGetQuerySchema))
+    console.log(JSON.stringify(result))
   } catch (error) {
     if (verbose) console.error(error)
-    throw new Error('Error querying archivist')
+    throw new Error('Error querying for archivists')
   }
 }
 
-const mod: CommandModule = {
+const mod: CommandModule<EmptyObject, BaseArguments> = {
   aliases,
   command,
   deprecated,

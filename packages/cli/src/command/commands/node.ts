b3acdc52138824b56e6c4a9a602117640158f96c
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-06 06:57:31 -0600

    Node describe on root command

--- packages/cli/src/command/commands/node.ts
+++ packages/cli/src/command/commands/node.ts
@@ -1,23 +1,30 @@
+import { EmptyObject } from '@xyo-network/core'
 import { parse } from 'path'
 import { ArgumentsCamelCase, Argv, CommandBuilder, CommandModule } from 'yargs'
 
+import { BaseArguments } from '../BaseArguments'
 import { opts } from '../requireDirectoryOptions'
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-type Arguments = {}
+import { getNode } from '../util'
 
 export const aliases: ReadonlyArray<string> = []
 export const builder: CommandBuilder = (yargs: Argv) =>
-  yargs.usage('Usage: $0 node <command> [Options]').commandDir(parse(__filename).name, opts).demandCommand().version(false)
+  yargs.usage('Usage: $0 node <command> [Options]').commandDir(parse(__filename).name, opts).version(false)
 export const command = 'node'
 export const deprecated = false
 export const describe = 'Issue queries against an XYO Node'
-export const handler = function (argv: ArgumentsCamelCase<Arguments>) {
-  console.log(command)
-  console.log(argv)
+export const handler = async (argv: ArgumentsCamelCase<BaseArguments>) => {
+  const { verbose } = argv
+  try {
+    const node = await getNode(argv)
+    const result = (await node.description()) ?? {}
+    console.log(JSON.stringify(result))
+  } catch (error) {
+    if (verbose) console.error(error)
+    throw new Error('Error querying for archivists')
+  }
 }
 
-const mod: CommandModule = {
+const mod: CommandModule<EmptyObject, BaseArguments> = {
   aliases,
   command,
   deprecated,

b92074d53ee7f36dbaf9a63e0c2e36b7275bac11
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-09 13:48:42 -0600

    Handle QueryBW separately

--- packages/node/packages/core/packages/modules-mongo/src/Archivist/Deterministic/validByType.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Archivist/Deterministic/validByType.ts
@@ -1,5 +1,7 @@
 import { XyoBoundWitnessSchema } from '@xyo-network/boundwitness-model'
 import { BoundWitnessWrapper } from '@xyo-network/boundwitness-wrapper'
+import { QueryBoundWitnessWrapper } from '@xyo-network/module'
+import { XyoQueryBoundWitnessSchema } from '@xyo-network/module-model'
 import { XyoPayload } from '@xyo-network/payload-model'
 import { PayloadWrapper } from '@xyo-network/payload-wrapper'
 
@@ -7,9 +9,16 @@ export const validByType = (results: [BoundWitnessWrapper[], PayloadWrapper[]] =
   const payload = PayloadWrapper.parse(value)
   if (payload.valid) {
     if (payload?.schema.startsWith(XyoBoundWitnessSchema)) {
-      const bw = BoundWitnessWrapper.parse(payload.payload)
-      if (bw.valid) {
+      const p = payload.payload
+      if (payload.schema === XyoQueryBoundWitnessSchema) {
+        // TODO: Validate errors
+        const bw = QueryBoundWitnessWrapper.parse(p)
         results[0].push(bw)
+      } else {
+        const bw = BoundWitnessWrapper.parse(p)
+        if (bw.valid) {
+          results[0].push(bw)
+        }
       }
     } else {
       results[1].push(payload)

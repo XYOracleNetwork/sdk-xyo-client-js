5be12ef4f96138705a485d571605e398ac57352c
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-14 11:56:17 -0600

    Use archivist to retrieve HURI

--- packages/node/packages/express/packages/routes/src/routes/hash/get/getBlockForRequest.ts
+++ packages/node/packages/express/packages/routes/src/routes/hash/get/getBlockForRequest.ts
@@ -1,37 +1,15 @@
-import { exists } from '@xylabs/exists'
 import { ArchivistWrapper } from '@xyo-network/archivist-wrapper'
-import { XyoBoundWitnessSchema } from '@xyo-network/boundwitness-model'
-import { requestCanAccessArchive, resolveBySymbol } from '@xyo-network/express-node-lib'
-import { PayloadPointerPayload, payloadPointerSchema, XyoPayloadFilterPredicate, XyoPayloadWithMeta } from '@xyo-network/node-core-model'
-import { TYPES } from '@xyo-network/node-core-types'
+import { PayloadPointerPayload, payloadPointerSchema } from '@xyo-network/node-core-model'
 import { XyoPayload } from '@xyo-network/payload-model'
 import { Request } from 'express'
 
 import { resolvePayloadPointer } from './resolvePayloadPointer'
 
-const findByHash = async (req: Request, hash: string) => {
-  const { node } = req.app
-  const payloadFilter: XyoPayloadFilterPredicate = { hash }
-  const payloadArchivist = await resolveBySymbol(node, TYPES.PayloadArchivist)
-  const payloadWrapper = new ArchivistWrapper(payloadArchivist)
-  const payloads = (await payloadWrapper.find(payloadFilter)).filter(exists)
-
-  if (payloads.length) return payloads
-
-  const boundWitnessArchivist = await resolveBySymbol(node, TYPES.BoundWitnessArchivist)
-  const boundWitnessWrapper = new ArchivistWrapper(boundWitnessArchivist)
-  return (await boundWitnessWrapper.find({ ...payloadFilter, schema: XyoBoundWitnessSchema })).filter(exists)
-}
-
 export const getBlockForRequest = async (req: Request, hash: string): Promise<XyoPayload | undefined> => {
-  for (const block of await findByHash(req, hash)) {
-    const blockWithMeta = block as XyoPayloadWithMeta
-    if (!blockWithMeta?._archive) {
-      continue
-    }
-    if (await requestCanAccessArchive(req, blockWithMeta._archive)) {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      return block.schema === payloadPointerSchema ? await resolvePayloadPointer(req, block as any as PayloadPointerPayload) : block
-    }
+  const { archivist } = req.app
+  const block = (await new ArchivistWrapper(archivist).get([hash])).pop()
+  if (block) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return block.schema === payloadPointerSchema ? await resolvePayloadPointer(req, block as any as PayloadPointerPayload) : block
   }
 }

ab5e740c63ae2a211d44cee35ad3ba940813c488
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-18 07:52:27 -0600

    Remove unused test vars

--- packages/modules/src/spec/VariousNodeConfigurations.spec.ts
+++ packages/modules/src/spec/VariousNodeConfigurations.spec.ts
@@ -2,56 +2,45 @@ import { MemoryArchivist, MemoryArchivistConfigSchema } from '@xyo-network/archi
 import { MemoryAddressHistoryDiviner, MemoryAddressHistoryDivinerConfigSchema } from '@xyo-network/diviner'
 import { IdWitness, IdWitnessConfigSchema } from '@xyo-network/id-plugin'
 import { AbstractModule } from '@xyo-network/module'
-import { MemoryNode, NodeConfigSchema, NodeWrapper } from '@xyo-network/node'
+import { MemoryNode, NodeConfigSchema } from '@xyo-network/node'
 
 describe('MultiNodeConfiguration', () => {
   let primaryArchivist: AbstractModule
   let primaryNode: MemoryNode
-  let primaryNodeWrapper: NodeWrapper
 
   let leftArchivist: AbstractModule
   let leftDiviner: AbstractModule
   let leftNode: MemoryNode
-  let leftNodeWrapper: NodeWrapper
 
   let rightNode: MemoryNode
-  let rightNodeWrapper: NodeWrapper
   let rightArchivist: AbstractModule
   let rightWitness: AbstractModule
 
   beforeAll(async () => {
     primaryNode = await MemoryNode.create({ config: { name: 'primaryNode', schema: NodeConfigSchema } })
-    primaryNodeWrapper = new NodeWrapper(primaryNode)
     primaryArchivist = await MemoryArchivist.create({ config: { name: 'primaryArchivist', schema: MemoryArchivistConfigSchema } })
     await primaryNode.register(primaryArchivist).attach(primaryArchivist.address, true)
 
     rightNode = await MemoryNode.create({ config: { name: 'rightNode', schema: NodeConfigSchema } })
-    rightNodeWrapper = new NodeWrapper(rightNode)
     rightArchivist = await MemoryArchivist.create()
     rightWitness = await IdWitness.create({ config: { name: 'rightWitness', salt: 'test', schema: IdWitnessConfigSchema } })
-    rightNode.register(rightArchivist)
-    await rightNodeWrapper.attach(rightArchivist.address, true)
-    rightNode.register(rightWitness)
-    await rightNodeWrapper.attach(rightWitness.address, true)
+    await rightNode.register(rightArchivist).attach(rightArchivist.address, true)
+    await rightNode.register(rightWitness).attach(rightWitness.address, true)
 
     leftNode = await MemoryNode.create({ config: { name: 'leftNode', schema: NodeConfigSchema } })
-    leftNodeWrapper = new NodeWrapper(leftNode)
     leftArchivist = await MemoryArchivist.create({ config: { name: 'leftArchivist', schema: MemoryArchivistConfigSchema } })
     leftDiviner = await MemoryAddressHistoryDiviner.create({
       config: { address: leftNode.address, name: 'leftDiviner', schema: MemoryAddressHistoryDivinerConfigSchema },
     })
-    leftNode.register(leftArchivist)
-    await leftNodeWrapper.attach(leftArchivist.address, true)
-    leftNode.register(leftDiviner)
-    await leftNodeWrapper.attach(leftDiviner.address, true)
+    await leftNode.register(leftArchivist).attach(leftArchivist.address, true)
+    await leftNode.register(leftDiviner).attach(leftDiviner.address, true)
 
     primaryNode.register(leftNode)
     primaryNode.register(rightNode)
   })
   test('leftNode', async () => {
-    await primaryNodeWrapper.attach(leftNode.address, true)
+    await primaryNode.attach(leftNode.address, true)
     primaryNode.detach(rightNode.address)
-
     expect((await primaryNode.resolve({ address: [primaryArchivist.address] })).length).toBe(1)
     expect((await primaryNode.resolve({ name: ['primaryArchivist'] })).length).toBe(1)
 
@@ -75,7 +64,7 @@ describe('MultiNodeConfiguration', () => {
   })
 
   test('rightNode', async () => {
-    await primaryNodeWrapper.attach(rightNode.address, true)
+    await primaryNode.attach(rightNode.address, true)
     primaryNode.detach(leftNode.address)
     expect((await primaryNode.resolve({ address: [primaryArchivist.address] })).length).toBe(1)
     expect((await leftNode.resolve({ address: [leftDiviner.address] })).length).toBe(1)

cc03db78e7458e63f655b1d8392d16d806af1f1b
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-22 13:01:23 -0600

    Add redirects to default module

--- packages/node/packages/express/packages/routes/src/addRoutes/addTempNodeRoutes.ts
+++ packages/node/packages/express/packages/routes/src/addRoutes/addTempNodeRoutes.ts
@@ -1,11 +1,15 @@
 import { allowAnonymous } from '@xyo-network/express-node-middleware'
 import { Express } from 'express'
 
-import { getAddress, getNode, postAddress, postNodeModule } from '../routes'
+import { getAddress, postAddress } from '../routes'
 
 export const addTempNodeRoutes = (app: Express) => {
-  app.get('/node', allowAnonymous, getNode)
-  app.post('/node', allowAnonymous, postNodeModule)
+  // TODO: Allow other default modules to be mounted at root
+  const defaultModule = app.node
+  const address = defaultModule.address
+  const defaultModuleEndpoint = `/node/${address}`
+  app.get('/node', allowAnonymous, (_req, res) => res.redirect(defaultModuleEndpoint))
+  app.post('/node', allowAnonymous, (_req, res) => res.redirect(defaultModuleEndpoint))
   app.get('/node/:address', allowAnonymous, getAddress)
   app.post('/node/:address', allowAnonymous, postAddress)
 }

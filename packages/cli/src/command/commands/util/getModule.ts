ec1f7bc5fb62d58de03d67020ba8e833f19c3c6c
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-24 11:55:30 -0600

    Hoist constants

--- packages/cli/src/command/commands/util/getModule.ts
+++ packages/cli/src/command/commands/util/getModule.ts
@@ -6,11 +6,14 @@ import { printError } from '../../../lib'
 import { getApiConfig } from '../../util'
 import { ModuleArguments } from '../ModuleArguments'
 
+const schema = HttpBridgeConfigSchema
+const security = { allowAnonymous: true }
+
 export const getModule = async (args: ModuleArguments): Promise<ModuleWrapper> => {
   const { address, verbose } = args
   try {
     const apiConfig = await getApiConfig(args)
-    const node = await HttpBridge.create({ config: { nodeUri: `${apiConfig.apiDomain}/node`, schema: HttpBridgeConfigSchema } })
+    const node = await HttpBridge.create({ config: { nodeUri: `${apiConfig.apiDomain}/node`, schema, security } })
     const resolved = await node.resolve({ address: [address] })
     const mod = assertEx(resolved.pop(), `Failed to load module [${address}]`)
     return new ModuleWrapper(mod)

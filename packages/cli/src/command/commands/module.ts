d7bd7e1a888fca176db6a8951029ef622c1d5362
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-24 04:14:53 -0600

    Add CLI commands for module queries

--- packages/cli/src/command/commands/module.ts
+++ packages/cli/src/command/commands/module.ts
@@ -1,32 +1,31 @@
 import { EmptyObject } from '@xyo-network/core'
-import { ArchivistGetQuerySchema } from '@xyo-network/modules'
 import { parse } from 'path'
 import { ArgumentsCamelCase, Argv, CommandBuilder, CommandModule } from 'yargs'
 
 import { printError, printLine } from '../../lib'
-import { BaseArguments } from '../BaseArguments'
 import { opts } from '../requireDirectoryOptions'
-import { getNode } from '../util'
+import { ModuleArguments } from './ModuleArguments'
+import { getModule } from './util'
 
 export const aliases: ReadonlyArray<string> = []
 export const builder: CommandBuilder = (yargs: Argv) =>
-  yargs.usage('Usage: $0 archivist <query> <address> [Options]').commandDir(parse(__filename).name, opts)
-export const command = 'archivist'
+  yargs.usage('Usage: $0 module <query> <address> [Options]').commandDir(parse(__filename).name, opts)
+export const command = 'module <address>'
 export const deprecated = false
-export const describe = 'Issue queries against an XYO archivist'
-export const handler = async (argv: ArgumentsCamelCase<BaseArguments>) => {
+export const describe = 'Issue queries against an XYO Module'
+export const handler = async (argv: ArgumentsCamelCase<ModuleArguments>) => {
   const { verbose } = argv
   try {
-    const node = await getNode(argv)
-    const archivists = node.resolve({ query: [[ArchivistGetQuerySchema]] })
-    printLine(JSON.stringify(archivists))
+    const module = await getModule(argv)
+    const result = (await module.describe()) ?? {}
+    printLine(JSON.stringify(result))
   } catch (error) {
     if (verbose) printError(JSON.stringify(error))
     throw new Error('Error querying for archivists')
   }
 }
 
-const mod: CommandModule<EmptyObject, BaseArguments> = {
+const mod: CommandModule<EmptyObject, ModuleArguments> = {
   aliases,
   command,
   deprecated,
--- packages/cli/src/command/commands/util/getModule.ts
+++ packages/cli/src/command/commands/util/getModule.ts
@@ -1,17 +1,19 @@
 import { assertEx } from '@xylabs/assert'
 import { HttpBridge, HttpBridgeConfigSchema } from '@xyo-network/http-bridge'
-import { Module, ModuleWrapper } from '@xyo-network/modules'
+import { ModuleWrapper } from '@xyo-network/modules'
 
 import { printError } from '../../../lib'
 import { getApiConfig } from '../../util'
 import { ModuleArguments } from '../ModuleArguments'
 
-export const getModule = async (args: ModuleArguments): Promise<Module> => {
+export const getModule = async (args: ModuleArguments): Promise<ModuleWrapper> => {
   const { address, verbose } = args
   try {
     const apiConfig = await getApiConfig(args)
     const node = await HttpBridge.create({ config: { nodeUri: `${apiConfig.apiDomain}/node`, schema: HttpBridgeConfigSchema } })
-    return new ModuleWrapper(assertEx((await node.resolve({ address: [address] })).pop(), `Failed to load module [${address}]`))
+    const resolved = await node.resolve({ address: [address] })
+    const mod = assertEx(resolved.pop(), `Failed to load module [${address}]`)
+    return new ModuleWrapper(mod)
   } catch (error) {
     if (verbose) printError(JSON.stringify(error))
     throw new Error(`Unable to connect to XYO Module at ${address}`)

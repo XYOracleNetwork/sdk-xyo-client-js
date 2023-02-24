07fb2c557dcf096ed4abd35a2c813051f7b6f46c
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-24 12:45:42 -0600

    Child describe queries

---
+++ packages/cli/src/command/commands/node/describe/children.ts
@@ -0,0 +1,43 @@
+import { EmptyObject } from '@xyo-network/core'
+import { ModuleWrapper } from '@xyo-network/module'
+import { CommandBuilder, CommandModule } from 'yargs'
+
+import { printError, printLine } from '../../../../lib'
+import { BaseArguments } from '../../../BaseArguments'
+import { getNode } from '../../../util'
+
+export const aliases: ReadonlyArray<string> = []
+export const builder: CommandBuilder = {}
+export const command = 'children'
+export const deprecated = false
+export const describe = 'Display the supported queries for the XYO Node'
+export const handler = async (argv: BaseArguments) => {
+  const { verbose } = argv
+  try {
+    const node = await getNode(argv)
+    const description = await node.describe()
+    const childAddresses = description?.children || []
+    const children = await Promise.all(childAddresses?.map((child) => node.resolve({ address: [child] })))
+    const childDescriptions = await Promise.all(
+      children
+        .flat()
+        .map((child) => new ModuleWrapper(child))
+        .map((mod) => mod.describe()),
+    )
+    printLine(JSON.stringify(childDescriptions))
+  } catch (error) {
+    if (verbose) printError(JSON.stringify(error))
+    throw new Error('Error querying for archivists')
+  }
+}
+
+const mod: CommandModule<EmptyObject, BaseArguments> = {
+  aliases,
+  command,
+  deprecated,
+  describe,
+  handler,
+}
+
+// eslint-disable-next-line import/no-default-export
+export default mod

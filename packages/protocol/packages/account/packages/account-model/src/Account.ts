1e821d9c0b7f1f90cbfd039403713ef88f813a20
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-26 15:42:24 -0600

    Implement account static/instance interfaces

--- packages/protocol/packages/account/packages/account-model/src/Account.ts
+++ packages/protocol/packages/account/packages/account-model/src/Account.ts
@@ -21,18 +21,18 @@ export type InitializationConfig = PhraseInitializationConfig | PrivateKeyInitia
 
 export type AccountConfig = InitializationConfig & AccountOptions
 
+export interface AccountModel extends KeyPairModel {
+  get addressValue(): AddressValueModel
+  get previousHash(): string
+  sign(hash: Uint8Array | string): Uint8Array
+  verify(msg: Uint8Array | string, signature: Uint8Array | string): boolean
+}
+
 export interface AccountModelStatic {
+  new (opts?: AccountConfig): AccountModel
   fromMnemonic(mnemonic: string, path?: string): AccountModel
   fromPhrase(phrase: string): AccountModel
   fromPrivateKey(key: Uint8Array | string): AccountModel
   isXyoWallet(value: unknown): boolean
   random(): AccountModel
-}
-
-export interface AccountModel extends KeyPairModel {
-  new (opts?: AccountConfig): this
-  get addressValue(): AddressValueModel
-  get previousHash(): string
-  sign(hash: Uint8Array | string): Uint8Array
-  verify(msg: Uint8Array | string, signature: Uint8Array | string): boolean
 }

266b841f68bbbdbb899610b64e2808d8189f4ed2
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-26 14:06:58 -0600

    Account Model Package

---
+++ packages/protocol/packages/account/packages/account-model/src/Account.ts
@@ -0,0 +1,39 @@
+import { DataLike } from '@xyo-network/core'
+
+import { AddressValue, KeyPair } from './Key'
+
+export const ethMessagePrefix = '\x19Ethereum Signed Message:\n'
+
+interface PhraseInitializationConfig {
+  phrase: string
+}
+interface PrivateKeyInitializationConfig {
+  privateKey: DataLike
+}
+interface MnemonicInitializationConfig {
+  mnemonic: string
+  path?: string
+}
+interface AccountOptions {
+  previousHash?: Uint8Array | string
+}
+
+export type InitializationConfig = PhraseInitializationConfig | PrivateKeyInitializationConfig | MnemonicInitializationConfig
+
+export type AccountConfig = InitializationConfig & AccountOptions
+
+export interface AccountModelStatic {
+  fromMnemonic(mnemonic: string, path?: string): AccountModel
+  fromPhrase(phrase: string): AccountModel
+  fromPrivateKey(key: Uint8Array | string): AccountModel
+  isXyoWallet(value: unknown): boolean
+  random(): AccountModel
+}
+
+export interface AccountModel extends KeyPair {
+  new (opts?: AccountConfig): this
+  get addressValue(): AddressValue
+  get previousHash(): string
+  sign(hash: Uint8Array | string): Uint8Array
+  verify(msg: Uint8Array | string, signature: Uint8Array | string): boolean
+}
---
+++ packages/protocol/packages/account/packages/account-model/src/Key/index.ts
@@ -0,0 +1,4 @@
+export * from './AddressValue'
+export * from './KeyPair'
+export * from './PrivateKey'
+export * from './PublicKey'

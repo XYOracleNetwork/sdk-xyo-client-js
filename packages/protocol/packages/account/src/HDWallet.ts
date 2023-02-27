d951afba1a7cf9ff48d5dc700616c1624cbbcff6
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-27 16:44:35 -0600

    Add derivation functions for child paths

--- packages/protocol/packages/account/src/HDWallet.ts
+++ packages/protocol/packages/account/src/HDWallet.ts
@@ -1,6 +1,15 @@
 import { HDNode } from '@ethersproject/hdnode'
 import { staticImplements } from '@xylabs/static-implements'
+import { AccountInstance } from '@xyo-network/account-model'
 import { WalletInstance, WalletStatic } from '@xyo-network/wallet-model'
 
+import { HDAccount } from './HDAccount'
+
 @staticImplements<WalletStatic>()
-export class HDWallet extends HDNode implements WalletInstance {}
+export class HDWallet extends HDNode implements WalletInstance {
+  protected readonly _account = new HDAccount(this)
+
+  account(path?: string): AccountInstance {
+    return path ? new HDAccount(this.derivePath(path)) : this._account
+  }
+}

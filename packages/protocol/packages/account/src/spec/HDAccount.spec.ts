32f9012f7eaaf1aa33ae77b3032d192f70b7caac
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-27 16:22:16 -0600

    Create Node once

--- packages/protocol/packages/account/src/spec/HDAccount.spec.ts
+++ packages/protocol/packages/account/src/spec/HDAccount.spec.ts
@@ -5,17 +5,16 @@ import { HDAccount } from '../HDAccount'
 describe('HDAccount', () => {
   const hash = '6c21ca7cddc2822ea635a96ba1aa36bf08d0f57ffb2989870659a263e9f8ee3a'
   const mnemonic = 'later puppy sound rebuild rebuild noise ozone amazing hope broccoli crystal grief'
+  const node = HDNode.fromMnemonic(mnemonic)
 
   describe('constructor', () => {
     it('can be created from HDNode', () => {
-      const node = HDNode.fromMnemonic(mnemonic)
       const sut = new HDAccount(node)
       expect(sut).toBeDefined()
     })
   })
   describe('sign', () => {
     it('signs hashes', () => {
-      const node = HDNode.fromMnemonic(mnemonic)
       const sut = new HDAccount(node)
       expect(sut).toBeDefined()
       const signature = sut.sign(hash)
@@ -26,13 +25,13 @@ describe('HDAccount', () => {
   describe('previousHash', () => {
     describe('when nothing signed before', () => {
       it('returns undefined', () => {
-        const sut = new HDAccount(HDNode.fromMnemonic(mnemonic))
+        const sut = new HDAccount(node)
         expect(sut.previousHash?.hex).toBe(undefined)
       })
     })
     describe('when something signed before', () => {
       it('returns last signed value', () => {
-        const sut = new HDAccount(HDNode.fromMnemonic(mnemonic))
+        const sut = new HDAccount(node)
         sut.sign(hash)
         expect(sut.previousHash?.hex).toBe(hash)
       })

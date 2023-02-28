be0bc22438093525a935b5db2ba2dadf2b3fce15
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-28 15:09:49 -0600

    Test interoperability

--- packages/protocol/packages/account/src/spec/HDWallet.spec.ts
+++ packages/protocol/packages/account/src/spec/HDWallet.spec.ts
@@ -10,9 +10,9 @@ describe('HDWallet', () => {
   })
   describe('deriveAccount', () => {
     const paths = ['m/0/4', "m/44'/0'/0'", "m/44'/60'/0'/0/0", "m/44'/60'/0'/0/1", "m/49'/0'/0'", "m/84'/0'/0'", "m/84'/0'/0'/0"]
-    it.each(paths)('works repeatably', (path: string) => {
+    it.each(paths)('works repeatably & interoperably', (path: string) => {
       const sutA = HDWallet.fromMnemonic(mnemonic)
-      const sutB = HDWallet.fromMnemonic(mnemonic)
+      const sutB = HDWallet.fromExtendedKey(sutA.extendedKey)
       const accountA = sutA.deriveAccount(path)
       const accountB = sutB.deriveAccount(path)
       expect(accountA.addressValue.hex).toBe(accountB.addressValue.hex)

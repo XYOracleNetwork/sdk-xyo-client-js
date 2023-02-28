1e0727d6c870d8063bbd785e2103694b272821b7
Arie Trouw <arie.trouw@xylabs.com>
2023-02-27 20:11:40 -0800

    cast

--- packages/cli/src/command/util/getNode.ts
+++ packages/cli/src/command/util/getNode.ts
@@ -1,7 +1,7 @@
 import { assertEx } from '@xylabs/assert'
 import { AxiosJson } from '@xyo-network/axios'
 import { HttpBridge, HttpBridgeConfigSchema } from '@xyo-network/http-bridge'
-import { NodeWrapper } from '@xyo-network/modules'
+import { NodeModule, NodeWrapper } from '@xyo-network/modules'
 
 import { printError } from '../../lib'
 import { BaseArguments } from '../BaseArguments'
@@ -17,7 +17,7 @@ export const getNode = async (args: BaseArguments): Promise<NodeWrapper> => {
     const axios = new AxiosJson()
     const bridge = await HttpBridge.create({ axios, config: { nodeUri, schema, security } })
     const node = assertEx((await bridge.resolve({ address: [bridge.rootAddress] }))?.pop(), 'Failed to resolve rootNode')
-    return NodeWrapper.wrap(node)
+    return NodeWrapper.wrap(node as NodeModule)
   } catch (error) {
     if (verbose) printError(JSON.stringify(error))
     throw new Error('Unable to connect to XYO Node')

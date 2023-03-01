67ce06a5bbc81edce4bb24ce6185bf9d08a5074c
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-01 08:40:54 -0600

    More distinct schema names

--- packages/modules/packages/diviner/src/AddressSpace/AddressSpaceDiviner.ts
+++ packages/modules/packages/diviner/src/AddressSpace/AddressSpaceDiviner.ts
@@ -3,11 +3,11 @@ import { PayloadFindFilter, XyoPayload } from '@xyo-network/payload-model'
 
 import { AbstractDiviner } from '../AbstractDiviner'
 
-export type AddressSpaceSchema = 'network.xyo.diviner.address'
-export const AddressSpaceSchema: AddressSpaceSchema = 'network.xyo.diviner.address'
+export type AddressSpaceSchema = 'network.xyo.diviner.address.space'
+export const AddressSpaceSchema: AddressSpaceSchema = 'network.xyo.diviner.address.space'
 
-export type AddressSpaceQuerySchema = 'network.xyo.diviner.address.query'
-export const AddressSpaceQuerySchema: AddressSpaceQuerySchema = 'network.xyo.diviner.address.query'
+export type AddressSpaceQuerySchema = 'network.xyo.diviner.address.space.query'
+export const AddressSpaceQuerySchema: AddressSpaceQuerySchema = 'network.xyo.diviner.address.space.query'
 
 export type AddressSpacePayload = XyoPayload<{ schema: AddressSpaceSchema }>
 export const isAddressSpacePayload = (x?: XyoPayload | null): x is AddressSpacePayload => x?.schema === AddressSpaceSchema

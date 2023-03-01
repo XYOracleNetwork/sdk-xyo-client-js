6194547a677b8b3e91a06dcf2a0d381711ddd067
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-01 07:40:57 -0600

    Memory Address Space Diviner

--- packages/modules/packages/diviner/src/AddressSpace/MemoryAddressSpaceDiviner.ts
+++ packages/modules/packages/diviner/src/AddressSpace/MemoryAddressSpaceDiviner.ts
@@ -1,12 +1,12 @@
 import { assertEx } from '@xylabs/assert'
+import { AddressSchema } from '@xyo-network/address-payload-plugin'
 import { ArchivistGetQuerySchema } from '@xyo-network/archivist'
 import { ArchivistWrapper } from '@xyo-network/archivist-wrapper'
 import { XyoBoundWitness, XyoBoundWitnessSchema } from '@xyo-network/boundwitness-model'
-import { BoundWitnessWrapper } from '@xyo-network/boundwitness-wrapper'
 import { DivinerConfig } from '@xyo-network/diviner-model'
 import { ModuleParams } from '@xyo-network/module'
+import { XyoPayloadBuilder } from '@xyo-network/payload-builder'
 import { XyoPayload } from '@xyo-network/payload-model'
-import { PayloadWrapper } from '@xyo-network/payload-wrapper'
 
 import { AbstractDiviner, DivinerParams } from '../AbstractDiviner'
 import { AddressSpaceDiviner } from './AddressSpaceDiviner'
@@ -22,9 +22,9 @@ export type MemoryAddressSpaceDivinerConfig = DivinerConfig<
   }
 >
 
-// This diviner returns the most recent boundwitness signed by the address that can be found
-// if multiple broken chains are found, all the heads are returned
-
+/**
+ * This Diviner returns the list of all addresses encountered for the reachable archivists
+ */
 export class MemoryAddressSpaceDiviner extends AbstractDiviner<DivinerParams<MemoryAddressSpaceDivinerConfig>> implements AddressSpaceDiviner {
   static override configSchema = MemoryAddressSpaceDivinerConfigSchema
 
@@ -44,46 +44,7 @@ export class MemoryAddressSpaceDiviner extends AbstractDiviner<DivinerParams<Mem
         }),
       )
     ).flat()
-
-    const bwRecords = this.buildWrapperRecords(bwLists)
-
-    const chains = Object.values(this.buildAddressChains(this.config.address, bwRecords))
-
-    console.log(`Chains: ${JSON.stringify(chains, null, 2)}`)
-
-    //return the heads of each chain (get the last bw on each chain)
-    return chains.map((chain) => assertEx(PayloadWrapper.unwrap(chain.shift())))
-  }
-
-  private buildAddressChains(address: string, bwRecords: Record<string, BoundWitnessWrapper>): Record<string, BoundWitnessWrapper[]> {
-    const arrayedResult = Object.entries(bwRecords).reduce<Record<string, BoundWitnessWrapper[]>>((prev, [key, value]) => {
-      prev[key] = [value]
-      return prev
-    }, {})
-    return Object.entries(bwRecords).reduce<Record<string, BoundWitnessWrapper[]>>((prev, [key, value]) => {
-      //check if key is still there (may have been deleted as prevHash)
-      if (prev[key]) {
-        const previousHash = value.prev(address)
-        if (previousHash) {
-          //if we have the previousHash, move this bw to its chain
-          if (prev[previousHash]) {
-            prev[key].push(...prev[previousHash])
-            delete prev[previousHash]
-          }
-        }
-      }
-      return prev
-    }, arrayedResult)
-  }
-
-  //build object with hashes as keys and wrappers as values
-  private buildWrapperRecords(lists: XyoBoundWitness[]) {
-    return lists
-      .filter((bw) => bw.addresses.includes(this.config.address))
-      .reduce<Record<string, BoundWitnessWrapper>>((bwRecords, bw) => {
-        const wrapper = new BoundWitnessWrapper(bw)
-        bwRecords[wrapper.hash] = wrapper
-        return bwRecords
-      }, {})
+    const addresses = new Set<string>(bwLists.map((bw) => bw.addresses).flat())
+    return [...addresses].map((address) => new XyoPayloadBuilder({ schema: AddressSchema }).fields({ address }).build())
   }
 }

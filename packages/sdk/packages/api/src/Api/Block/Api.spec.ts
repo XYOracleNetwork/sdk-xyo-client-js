3f0bfdc40006deef6f7e9e6a720db6d1b044a7d6
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-02 15:24:29 -0600

    Remove Block API SDK method tests

--- packages/sdk/packages/api/src/Api/Block/Api.spec.ts
+++ packages/sdk/packages/api/src/Api/Block/Api.spec.ts
@@ -1,7 +1,7 @@
 import { BoundWitnessBuilder } from '@xyo-network/boundwitness-builder'
 
 import { testPayload } from '../../Test'
-import { getApi, getNewArchive } from '../ApiUtil.spec'
+import { getApi } from '../ApiUtil.spec'
 
 describe('XyoArchivistArchiveBlockApi', () => {
   const api = getApi()
@@ -19,39 +19,6 @@ describe('XyoArchivistArchiveBlockApi', () => {
       const blocks = [bw, bw]
       const response = await api.archives.archive().block.post(blocks)
       expect(response?.length).toEqual(blocks.length)
-    })
-  })
-  describe('stats', () => {
-    const [bw] = new BoundWitnessBuilder().build()
-    const blocks = [bw]
-    let archive = ''
-    beforeEach(async () => {
-      archive = await getNewArchive(api)
-    })
-    it('returns stats for boundwitness', async () => {
-      await api.archives.archive(archive).block.post(blocks)
-      const stats = await api.archives.archive(archive).block.stats.get()
-      expect(stats?.count).toBeGreaterThanOrEqual(blocks.length)
-    })
-  })
-  describe('find', () => {
-    const [bw] = new BoundWitnessBuilder().build()
-    const blocks = [bw]
-    let archive = ''
-    beforeEach(async () => {
-      archive = await getNewArchive(api)
-    })
-    it('returns bound witnesses from before the timestamp', async () => {
-      const timestamp = Date.now() - 10000
-      await api.archives.archive(archive).block.post(blocks)
-      const response = await api.archives.archive(archive).block.find({ order: 'asc', timestamp })
-      expect(response?.length).toBe(blocks.length)
-    })
-    it('returns bound witnesses from after the timestamp', async () => {
-      await api.archives.archive(archive).block.post(blocks)
-      const timestamp = Date.now() + 10000
-      const response = await api.archives.archive(archive).block.find({ order: 'desc', timestamp })
-      expect(response?.length).toBe(blocks.length)
     })
   })
 })

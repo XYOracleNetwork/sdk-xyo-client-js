3f1082bd7ef02b57b03e098e3329e84bc03635d7
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-02 16:20:44 -0600

    Remove legacy archivist Block SDK methods

--- packages/sdk/packages/api/src/Api/Archive/Api.ts
+++ packages/sdk/packages/api/src/Api/Archive/Api.ts
@@ -1,24 +1,9 @@
 import { XyoArchive } from '../../models'
 import { XyoApiSimple } from '../../Simple'
-import { XyoArchivistArchiveBlockApi } from '../Block'
 import { XyoArchivistArchiveSettingsApi } from './Settings'
 
 export class XyoArchivistArchiveApi extends XyoApiSimple<XyoArchive> {
-  private _block?: XyoArchivistArchiveBlockApi
   private _settings?: XyoArchivistArchiveSettingsApi
-
-  /**
-   * @deprecated Use module API
-   */
-  get block(): XyoArchivistArchiveBlockApi {
-    this._block =
-      this._block ??
-      new XyoArchivistArchiveBlockApi({
-        ...this.config,
-        root: `${this.root}block/`,
-      })
-    return this._block
-  }
 
   get settings(): XyoArchivistArchiveSettingsApi {
     this._settings =
--- packages/sdk/packages/api/src/Api/Block/Api.ts
+++
@@ -1,8 +0,0 @@
-import { XyoBoundWitness } from '@xyo-network/boundwitness-model'
-
-import { XyoApiSimple } from '../../Simple'
-
-export class XyoArchivistArchiveBlockApi<
-  T extends XyoBoundWitness[] = XyoBoundWitness[],
-  C extends XyoBoundWitness[] = XyoBoundWitness[],
-> extends XyoApiSimple<T, C> {}

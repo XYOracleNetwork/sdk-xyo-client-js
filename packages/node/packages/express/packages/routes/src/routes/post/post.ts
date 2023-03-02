5b6092e65538b7ce785ac604c93a348941efec09
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-02 16:47:38 -0600

    Refactor temp Node API in favor of Module Query

--- packages/node/packages/express/packages/routes/src/routes/post/post.ts
+++ packages/node/packages/express/packages/routes/src/routes/post/post.ts
@@ -1,23 +1,22 @@
 import { asyncHandler, NoReqParams } from '@xylabs/sdk-api-express-ecs'
-import { ModuleQueryResult, XyoQueryBoundWitness } from '@xyo-network/modules'
+import { ModuleQueryResult, XyoQueryBoundWitness } from '@xyo-network/module-model'
 import { XyoPayload } from '@xyo-network/payload-model'
+// import { setRawResponseFormat } from '@xyo-network/express-node-middleware'
 import { RequestHandler } from 'express'
 import { StatusCodes } from 'http-status-codes'
 
 export type PostNodeRequestBody = [XyoQueryBoundWitness, undefined | XyoPayload[]]
 
 const handler: RequestHandler<NoReqParams, ModuleQueryResult, PostNodeRequestBody> = async (req, res) => {
+  // setRawResponseFormat(res)
   const { node } = req.app
   const [bw, payloads] = Array.isArray(req.body) ? req.body : []
-  if (!bw) {
-    res.sendStatus(StatusCodes.BAD_REQUEST)
+  if (bw) {
+    const queryResult = await node.query(bw, payloads)
+    res.json(queryResult)
     return
   }
-  // TODO: Get actual query config
-  // const queryConfig = await getQueryConfig(mod, req, bw, payloads)
-  const queryConfig = undefined
-  const queryResult = await node.query(bw, payloads, queryConfig)
-  res.json(queryResult)
+  res.sendStatus(StatusCodes.BAD_REQUEST)
   return
 }
 

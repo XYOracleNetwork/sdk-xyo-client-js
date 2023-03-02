37a9e792b8100937d3e48ee8007816eceb56ba03
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-02 17:48:36 -0600

    Remove legacy auth strategies

--- packages/node/packages/express/packages/middleware/src/auth/strategy/archiveApiKey/index.ts
+++
@@ -1,6 +0,0 @@
-import passport from 'passport'
-
-export const archiveApiKeyStrategyName = 'archiveApiKey'
-export const archiveApiKeyStrategy = passport.authenticate(archiveApiKeyStrategyName, { session: false })
-
-export * from './archiveApiKeyStrategy'

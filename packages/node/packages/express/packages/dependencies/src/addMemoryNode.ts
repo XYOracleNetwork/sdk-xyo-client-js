78f5847ae1e1a8293c98a114dab2e0d9227c5788
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-02 08:41:40 -0600

    Remove dynamic archivists

--- packages/node/packages/express/packages/dependencies/src/addMemoryNode.ts
+++ packages/node/packages/express/packages/dependencies/src/addMemoryNode.ts
@@ -1,25 +1,11 @@
-import { exists } from '@xylabs/exists'
-import { fulfilled } from '@xylabs/promise'
-import { AbstractModule, AddressModuleFilter, DynamicModuleResolver, MemoryNode, NameModuleFilter, NodeConfigSchema } from '@xyo-network/modules'
-import { archivistRegex, ArchivistRegexResult } from '@xyo-network/node-core-lib'
-import { ArchiveArchivist, ArchiveBoundWitnessArchivistFactory, ArchivePayloadArchivistFactory } from '@xyo-network/node-core-model'
+import { AbstractModule, MemoryNode, NodeConfigSchema } from '@xyo-network/modules'
 import { TYPES } from '@xyo-network/node-core-types'
 import { Container } from 'inversify'
 
 const config = { schema: NodeConfigSchema }
 
 // TODO: Grab from actual type lists (which are not yet exported)
-const archivists = [
-  // TYPES.ArchiveArchivist,
-  // TYPES.ArchiveBoundWitnessArchivistFactory,
-  // TYPES.ArchivePayloadArchivistFactory,
-  // TYPES.ArchivePermissionsArchivistFactory,
-  TYPES.Archivist,
-  TYPES.BoundWitnessArchivist,
-  TYPES.PayloadArchivist,
-  // TYPES.UserArchivist,
-  // TYPES.WitnessedPayloadArchivist,
-]
+const archivists = [TYPES.Archivist, TYPES.BoundWitnessArchivist, TYPES.PayloadArchivist]
 
 const diviners = [
   TYPES.AddressHistoryDiviner,
@@ -37,7 +23,6 @@ export const addMemoryNode = async (container: Container, memoryNode?: MemoryNod
   container.bind<MemoryNode>(TYPES.Node).toConstantValue(node)
   await addDependenciesToNodeByType(container, node, archivists)
   await addDependenciesToNodeByType(container, node, diviners)
-  //  addDynamicArchivists(container, node)
 }
 
 const addDependenciesToNodeByType = async (container: Container, node: MemoryNode, types: symbol[]) => {
@@ -50,41 +35,4 @@ const addDependenciesToNodeByType = async (container: Container, node: MemoryNod
       }
     }),
   )
-}
-
-const addDynamicArchivists = (container: Container, node: MemoryNode) => {
-  const { downResolver } = node
-  if (downResolver) {
-    if ((downResolver as DynamicModuleResolver)?.resolveImplementation) {
-      const dynamicResolver = downResolver as DynamicModuleResolver
-      const archives = container.get<ArchiveArchivist>(TYPES.ArchiveArchivist)
-      const archiveBoundWitnessArchivistFactory = container.get<ArchiveBoundWitnessArchivistFactory>(TYPES.ArchiveBoundWitnessArchivistFactory)
-      const archivePayloadsArchivistFactory = container.get<ArchivePayloadArchivistFactory>(TYPES.ArchivePayloadArchivistFactory)
-      dynamicResolver.resolveImplementation = async (filter) => {
-        if (!filter) return []
-        const filters: string[] = []
-        if ((filter as AddressModuleFilter)?.address) filters.push(...(filter as AddressModuleFilter).address)
-        if ((filter as NameModuleFilter)?.name) filters.push(...(filter as NameModuleFilter).name)
-        const archivistFilters = filters.map((filter) => archivistRegex.exec(filter)?.groups as ArchivistRegexResult).filter(exists)
-        if (archivistFilters.length) {
-          const potentialArchives = await Promise.allSettled(archivistFilters.map((filter) => archives.find({ archive: filter.archive })))
-          const existingArchives = potentialArchives
-            .filter(fulfilled)
-            .map((v) => v.value)
-            .flat()
-            .map((archive) => archive.archive)
-          const modules = archivistFilters
-            .filter((filter) => existingArchives.includes(filter.archive))
-            .map((filter) => {
-              // TODO: Get this string from module alongside name builder helpers
-              return filter.type === 'boundwitness'
-                ? archiveBoundWitnessArchivistFactory(filter.archive)
-                : archivePayloadsArchivistFactory(filter.archive)
-            })
-          return await Promise.all(modules)
-        }
-        return []
-      }
-    }
-  }
 }

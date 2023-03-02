1776a78279716ebe7d115cbfb68d68e54580dbce
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-02 17:10:24 -0600

    Remove legacy query model

--- packages/node/packages/core/packages/model/src/Query/QueryHandler.ts
+++
@@ -1,9 +0,0 @@
-import { XyoPayload } from '@xyo-network/payload-model'
-
-import { Handler } from '../Domain'
-import { Optional } from '../Optional'
-import { Query } from './Query'
-
-export interface QueryHandler<T extends Query, R extends XyoPayload = XyoPayload> extends Handler<T, Optional<R>> {
-  handle(query: T): Promise<Optional<R>>
-}

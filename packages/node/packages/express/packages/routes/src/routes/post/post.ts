84f5ed4517bb0571e92343598853f0e1f51a1655
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-02 16:39:44 -0600

    Refactor legacy Node API to use Node Module Query

--- packages/node/packages/express/packages/routes/src/routes/post/post.ts
+++ packages/node/packages/express/packages/routes/src/routes/post/post.ts
@@ -1,16 +1,24 @@
-import { asyncHandler } from '@xylabs/sdk-api-express-ecs'
+import { asyncHandler, NoReqParams } from '@xylabs/sdk-api-express-ecs'
+import { ModuleQueryResult, XyoQueryBoundWitness } from '@xyo-network/modules'
+import { XyoPayload } from '@xyo-network/payload-model'
+import { RequestHandler } from 'express'
 import { StatusCodes } from 'http-status-codes'
 
-import { formatRequest } from './formatRequest'
-import { PostNodeRequestHandler } from './PostNodeRequestHandler'
-import { queueQueries } from './queueQueries'
+export type PostNodeRequestBody = [XyoQueryBoundWitness, undefined | XyoPayload[]]
 
-const handler: PostNodeRequestHandler = async (req, res) => {
-  const boundWitnesses = formatRequest(req)
-  // TODO: Validate protocol only here: new XyoBoundWitnessWrapper(bw).validator.all()
-  const queued = queueQueries(boundWitnesses, req)
-  const result: string[][] = await Promise.all(queued.map(async (x) => await Promise.all(x)))
-  res.status(StatusCodes.ACCEPTED).json(result)
+const handler: RequestHandler<NoReqParams, ModuleQueryResult, PostNodeRequestBody> = async (req, res) => {
+  const { node } = req.app
+  const [bw, payloads] = Array.isArray(req.body) ? req.body : []
+  if (!bw) {
+    res.sendStatus(StatusCodes.BAD_REQUEST)
+    return
+  }
+  // TODO: Get actual query config
+  // const queryConfig = await getQueryConfig(mod, req, bw, payloads)
+  const queryConfig = undefined
+  const queryResult = await node.query(bw, payloads, queryConfig)
+  res.json(queryResult)
+  return
 }
 
 export const postNode = asyncHandler(handler)

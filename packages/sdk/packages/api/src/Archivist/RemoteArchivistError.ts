cdefb4ab675738dd30b5297a6d3b4130c7a1cc5d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-02 14:40:56 -0600

    Remove Remote Archivist in favor of Module API

--- packages/sdk/packages/api/src/Archivist/RemoteArchivistError.ts
+++
@@ -1,9 +0,0 @@
-export class RemoteArchivistError extends Error {
-  isRemoteArchivistError = true
-  constructor(action: string, error: Error['cause'], message?: string) {
-    super(`Remote Archivist [${action}] failed${message ? ` (${message})` : ''}`, { cause: error })
-  }
-  static isRemoteArchivistError(error: unknown): RemoteArchivistError | undefined {
-    return (error as RemoteArchivistError).isRemoteArchivistError ? (error as RemoteArchivistError) : undefined
-  }
-}
--- packages/sdk/packages/api/src/Archivist/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './RemoteArchivistError'
-export * from './XyoRemoteArchivist'
-export * from './XyoRemoteArchivistConfig'

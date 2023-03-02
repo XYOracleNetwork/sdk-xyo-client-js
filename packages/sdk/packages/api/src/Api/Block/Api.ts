9671e28d4720697beec67f0f88fd050632f3a39a
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-02 15:48:43 -0600

    Refactor API shape

--- packages/sdk/packages/api/src/Api/Block/Api.ts
+++ packages/sdk/packages/api/src/Api/Block/Api.ts
@@ -1,10 +1,8 @@
-import { XyoApiConfig } from '@xyo-network/api-models'
 import { XyoBoundWitness } from '@xyo-network/boundwitness-model'
 
 import { XyoApiSimple } from '../../Simple'
-import { WithArchive } from '../../WithArchive'
 
 export class XyoArchivistArchiveBlockApi<
-  T extends XyoBoundWitness = XyoBoundWitness,
-  C extends WithArchive<XyoApiConfig> = WithArchive<XyoApiConfig>,
+  T extends XyoBoundWitness[] = XyoBoundWitness[],
+  C extends XyoBoundWitness[] = XyoBoundWitness[],
 > extends XyoApiSimple<T, C> {}

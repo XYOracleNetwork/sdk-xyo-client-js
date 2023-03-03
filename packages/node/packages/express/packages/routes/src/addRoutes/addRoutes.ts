ccd6ebd3c88178efadca9d10444fe1c109cbed29
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-02 19:02:28 -0600

    Remove legacy block routes

--- packages/node/packages/express/packages/routes/src/addRoutes/addRoutes.ts
+++ packages/node/packages/express/packages/routes/src/addRoutes/addRoutes.ts
@@ -1,14 +1,12 @@
 import { Express } from 'express'
 
 import { addAddressRoutes } from './addAddressRoutes'
-import { addBlockRoutes } from './addBlockRoutes'
 import { addDomainRoutes } from './addDomainRoutes'
 import { addNodeRoutes } from './addNodeRoutes'
 import { addTempNodeRoutes } from './addTempNodeRoutes'
 
 export const addRoutes = (app: Express): Express => {
   addAddressRoutes(app)
-  addBlockRoutes(app)
   addDomainRoutes(app)
   addTempNodeRoutes(app)
   // This needs to be the last true route handler since it is

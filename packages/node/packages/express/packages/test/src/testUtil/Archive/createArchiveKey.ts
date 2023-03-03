78980cd39e60f08a7bc59b8cf0612e974dc48884
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-02 19:19:58 -0600

    Remove archive test helpers

--- packages/node/packages/express/packages/test/src/testUtil/Archive/createArchiveKey.ts
+++
@@ -1,9 +0,0 @@
-import { XyoArchiveKey } from '@xyo-network/api'
-import { StatusCodes } from 'http-status-codes'
-
-import { request } from '../Server'
-
-export const createArchiveKey = async (token: string, archive: string, expectedStatus: StatusCodes = StatusCodes.OK): Promise<XyoArchiveKey[]> => {
-  const response = await (await request()).post(`/archive/${archive}/settings/key`).auth(token, { type: 'bearer' }).expect(expectedStatus)
-  return response.body.data
-}

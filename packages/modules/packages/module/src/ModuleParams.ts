ec40a3b8b04ff23fb186e6eff1787d0e55e3ec85
Arie Trouw <arie.trouw@xylabs.com>
2023-03-07 14:54:51 -0800

    WIP

--- packages/modules/packages/module/src/ModuleParams.ts
+++ packages/modules/packages/module/src/ModuleParams.ts
@@ -1,29 +1,35 @@
 import { AccountInstance } from '@xyo-network/account-model'
 import { AnyObject, BaseParams } from '@xyo-network/core'
-import { ModuleConfig } from '@xyo-network/module-model'
+import { ModuleConfig, ModuleQueriedEventArgs } from '@xyo-network/module-model'
 import { XyoWalletBase } from '@xyo-network/wallet'
 import Emittery from 'emittery'
+
+export type EventName = PropertyKey
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type EventData = Record<EventName, any>
 
 export type WithAdditional<T, TAdditional extends AnyObject | undefined = undefined> = TAdditional extends AnyObject ? T & TAdditional : T
 
 export type BaseEmitterParams<
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  TEmittery extends Emittery<any> = Emittery<any>,
+  TEventData extends EventData = EventData,
   TAdditionalParams extends AnyObject | undefined = undefined,
 > = WithAdditional<
   BaseParams<{
-    emittery: TEmittery
+    emittery: Emittery<TEventData>
   }>,
   TAdditionalParams
 >
 
 export type BasicModuleParams<
   TConfig extends ModuleConfig = ModuleConfig,
-  TEmittery extends Emittery = Emittery,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  TEventData extends EventData = EventData,
   TAdditionalParams extends AnyObject | undefined = undefined,
 > = WithAdditional<
   BaseEmitterParams<
-    TEmittery,
+    TEventData,
     {
       config: TConfig
     }
@@ -33,11 +39,12 @@ export type BasicModuleParams<
 
 export type AccountModuleParams<
   TConfig extends ModuleConfig = ModuleConfig,
-  TEmittery extends Emittery = Emittery,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  TEventData extends EventData = EventData,
   TAdditionalParams extends AnyObject | undefined = undefined,
 > = BasicModuleParams<
   TConfig,
-  TEmittery,
+  TEventData,
   WithAdditional<
     {
       account: AccountInstance
@@ -48,11 +55,12 @@ export type AccountModuleParams<
 
 export type WalletModuleParams<
   TConfig extends ModuleConfig = ModuleConfig,
-  TEmittery extends Emittery = Emittery,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  TEventData extends EventData = EventData,
   TAdditionalParams extends AnyObject | undefined = undefined,
 > = BasicModuleParams<
   TConfig,
-  TEmittery,
+  TEventData,
   WithAdditional<
     {
       accountDerivationPath: string
@@ -62,11 +70,13 @@ export type WalletModuleParams<
   >
 >
 
+export type ModuleEventData = EventData & { moduleQueried: ModuleQueriedEventArgs }
+
 export type ModuleParams<
   TConfig extends ModuleConfig = ModuleConfig,
-  TEmittery extends Emittery = Emittery,
+  TEventData extends ModuleEventData = ModuleEventData,
   TAdditionalParams extends AnyObject | undefined = undefined,
 > =
-  | AccountModuleParams<TConfig, TEmittery, TAdditionalParams>
-  | WalletModuleParams<TConfig, TEmittery, TAdditionalParams>
-  | BasicModuleParams<TConfig, TEmittery, TAdditionalParams>
+  | AccountModuleParams<TConfig, TEventData, TAdditionalParams>
+  | WalletModuleParams<TConfig, TEventData, TAdditionalParams>
+  | BasicModuleParams<TConfig, TEventData, TAdditionalParams>

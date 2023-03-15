5a5b0664445679556e3d340b52237a0fe9221175
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-15 14:14:51 -0500

    Strongly type global setup

--- packages/node/packages/express/packages/test/src/globalSetup.ts
+++ packages/node/packages/express/packages/test/src/globalSetup.ts
@@ -2,13 +2,14 @@ import { config } from 'dotenv'
 config()
 import { PayloadValidator } from '@xyo-network/payload-validator'
 import { XyoSchemaNameValidator } from '@xyo-network/schema-name-validator'
+import { Config } from 'jest'
 
 /**
  * Jest global setup method run before
  * any tests are run
  * https://jestjs.io/docs/configuration#globalsetup-string
  */
-module.exports = async () => {
-  await Promise.resolve()
+module.exports = async (_globalConfig: Config, _projectConfig: Config) => {
   PayloadValidator.setSchemaNameValidatorFactory((schema) => new XyoSchemaNameValidator(schema))
+  await Promise.resolve()
 }

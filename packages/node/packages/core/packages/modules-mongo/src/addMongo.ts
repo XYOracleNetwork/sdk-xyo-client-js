d6744d0c4a55520b697981af095358eed63fddc9
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-15 18:34:51 -0500

    Use sync container module for archivist

--- packages/node/packages/core/packages/modules-mongo/src/addMongo.ts
+++ packages/node/packages/core/packages/modules-mongo/src/addMongo.ts
@@ -1,13 +1,13 @@
 import { Container } from 'inversify'
 
 import { ArchivistContainerModule } from './Archivist'
-import { getDivinerContainerModule } from './Diviner'
+import { DivinerContainerModule } from './Diviner'
 import { JobQueueContainerModule } from './JobQueue'
 import { ManagerContainerModule } from './Manager'
 
 export const addMongo = async (container: Container) => {
-  container.load(ArchivistContainerModule)
-  container.load(await getDivinerContainerModule(container))
+  await container.loadAsync(ArchivistContainerModule)
+  container.load(DivinerContainerModule)
   container.load(ManagerContainerModule)
   container.load(JobQueueContainerModule)
 }

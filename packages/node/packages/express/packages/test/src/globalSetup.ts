8f8410ffbe7e26301bf08b32955ee89e3d849055
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-15 15:19:21 -0500

    Start memory mongo before all tests

--- packages/node/packages/express/packages/test/src/globalSetup.ts
+++ packages/node/packages/express/packages/test/src/globalSetup.ts
@@ -3,6 +3,12 @@ config()
 import { PayloadValidator } from '@xyo-network/payload-validator'
 import { XyoSchemaNameValidator } from '@xyo-network/schema-name-validator'
 import { Config } from 'jest'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+
+declare global {
+  // eslint-disable-next-line no-var
+  var mongo: MongoMemoryServer
+}
 
 /**
  * Jest global setup method run before
@@ -11,5 +17,10 @@ import { Config } from 'jest'
  */
 module.exports = async (_globalConfig: Config, _projectConfig: Config) => {
   PayloadValidator.setSchemaNameValidatorFactory((schema) => new XyoSchemaNameValidator(schema))
+  const mongo = new MongoMemoryServer()
+  await mongo.start()
+  globalThis.mongo
+  const uri = mongo.getUri()
+  process.env.MONGO_CONNECTION_STRING = uri
   await Promise.resolve()
 }

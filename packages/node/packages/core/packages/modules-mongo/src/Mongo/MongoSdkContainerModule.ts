314971810d76dab5f6dbba4b6b4f01a746be0a75
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-15 23:01:26 -0500

    Add Mongo SDKs to DI

--- packages/node/packages/core/packages/modules-mongo/src/Mongo/MongoSdkContainerModule.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Mongo/MongoSdkContainerModule.ts
@@ -1,7 +1,15 @@
+import { User, XyoBoundWitnessWithMeta, XyoPayloadWithMeta } from '@xyo-network/node-core-model'
+import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { ContainerModule, interfaces } from 'inversify'
 
+import { COLLECTIONS } from '../collections'
+import { MONGO_TYPES } from '../mongoTypes'
+import { getBaseMongoSdk } from './getBaseMongoSdk'
+
 export const MongoSdkContainerModule = new ContainerModule((bind: interfaces.Bind) => {
-  // bind(MongoDBDeterministicArchivist).toDynamicValue(getMongoDBDeterministicArchivist).inSingletonScope()
-  // bind<AbstractArchivist>(TYPES.Archivist).toDynamicValue(getMongoDBDeterministicArchivist).inSingletonScope()
-  // bind<AbstractModule>(TYPES.Module).toDynamicValue(getMongoDBDeterministicArchivist).inSingletonScope()
+  bind<BaseMongoSdk<XyoBoundWitnessWithMeta>>(MONGO_TYPES.BoundWitnessSdk).toConstantValue(
+    getBaseMongoSdk<XyoBoundWitnessWithMeta>(COLLECTIONS.BoundWitnesses),
+  )
+  bind<BaseMongoSdk<XyoPayloadWithMeta>>(MONGO_TYPES.PayloadSdk).toConstantValue(getBaseMongoSdk<XyoPayloadWithMeta>(COLLECTIONS.Payloads))
+  bind<BaseMongoSdk<User>>(MONGO_TYPES.PayloadSdk).toConstantValue(getBaseMongoSdk<User>(COLLECTIONS.Users))
 })

56665029bcfe8cda59dc3a2def0e602c4c4062d6
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-15 20:20:21 -0500

    Add BWs for DB tests

--- packages/node/packages/core/packages/modules-mongo/src/Diviner/AddressSpace/spec/MongoDBAddressSpaceDiviner.spec.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Diviner/AddressSpace/spec/MongoDBAddressSpaceDiviner.spec.ts
@@ -1,7 +1,9 @@
 import { Account } from '@xyo-network/account'
 import { AddressPayload, AddressSchema } from '@xyo-network/address-payload-plugin'
+import { BoundWitnessBuilder } from '@xyo-network/boundwitness-builder'
 import { AddressSpaceQueryPayload, AddressSpaceQuerySchema, XyoArchivistPayloadDivinerConfigSchema } from '@xyo-network/diviner'
-import { XyoBoundWitnessMeta, XyoBoundWitnessWithMeta, XyoPayloadWithMeta } from '@xyo-network/node-core-model'
+import { XyoBoundWitnessWithMeta } from '@xyo-network/node-core-model'
+import { XyoPayloadBuilder } from '@xyo-network/payload-builder'
 import { PayloadWrapper } from '@xyo-network/payload-wrapper'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { mock } from 'jest-mock-extended'
@@ -11,7 +13,8 @@ import { MongoDBAddressSpaceDiviner } from '../MongoDBAddressSpaceDiviner'
 
 describe('MongoDBAddressSpaceDiviner', () => {
   const phrase = 'temp'
-  const address = new Account({ phrase }).addressValue.hex
+  const account = new Account({ phrase })
+  const address = account.addressValue.hex
   const logger = mock<Console>()
   const boundWitnessSdk = new BaseMongoSdk<XyoBoundWitnessWithMeta>({
     collection: COLLECTIONS.BoundWitnesses,
@@ -20,10 +23,15 @@ describe('MongoDBAddressSpaceDiviner', () => {
   let sut: MongoDBAddressSpaceDiviner
   beforeAll(async () => {
     sut = await MongoDBAddressSpaceDiviner.create({
+      account,
       boundWitnessSdk,
       config: { schema: XyoArchivistPayloadDivinerConfigSchema },
       logger,
     })
+    // TODO: Insert via archivist
+    const payload = new XyoPayloadBuilder({ schema: 'network.xyo.test' }).build()
+    const bw = new BoundWitnessBuilder().payload(payload).witness(account).build()[0]
+    await boundWitnessSdk.insertOne(bw as unknown as XyoBoundWitnessWithMeta)
   })
   describe('divine', () => {
     describe('with valid query', () => {
--- packages/node/packages/core/packages/modules-mongo/src/Diviner/BoundWitness/spec/MongoDBBoundWitnessDiviner.spec.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Diviner/BoundWitness/spec/MongoDBBoundWitnessDiviner.spec.ts
@@ -1,3 +1,4 @@
+import { Account } from '@xyo-network/account'
 import { BoundWitnessBuilder } from '@xyo-network/boundwitness-builder'
 import { XyoBoundWitnessSchema } from '@xyo-network/boundwitness-model'
 import { XyoArchivistPayloadDivinerConfigSchema } from '@xyo-network/diviner'
@@ -7,6 +8,7 @@ import {
   XyoBoundWitnessWithMeta,
   XyoBoundWitnessWithPartialMeta,
 } from '@xyo-network/node-core-model'
+import { XyoPayloadBuilder } from '@xyo-network/payload-builder'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { mock } from 'jest-mock-extended'
 
@@ -14,6 +16,8 @@ import { COLLECTIONS } from '../../../collections'
 import { MongoDBBoundWitnessDiviner } from '../MongoDBBoundWitnessDiviner'
 
 describe('MongoDBBoundWitnessDiviner', () => {
+  const phrase = 'temp'
+  const account = new Account({ phrase })
   const logger = mock<Console>()
   const boundWitnessSdk = new BaseMongoSdk<XyoBoundWitnessWithMeta>({
     collection: COLLECTIONS.BoundWitnesses,
@@ -22,12 +26,14 @@ describe('MongoDBBoundWitnessDiviner', () => {
   let sut: MongoDBBoundWitnessDiviner
   beforeAll(async () => {
     sut = await MongoDBBoundWitnessDiviner.create({
+      account,
       boundWitnessSdk,
       config: { schema: XyoArchivistPayloadDivinerConfigSchema },
       logger,
     })
     // TODO: Insert via archivist
-    const bw = new BoundWitnessBuilder().build()[0]
+    const payload = new XyoPayloadBuilder({ schema: 'network.xyo.test' }).build()
+    const bw = new BoundWitnessBuilder().payload(payload).witness(account).build()[0]
     await boundWitnessSdk.insertOne(bw as unknown as XyoBoundWitnessWithMeta)
   })
   describe('divine', () => {

e55f4e81b7442a6097560ec2aad61fb72ef789f7
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-15 20:21:02 -0500

    Create test replica set using expected DB collections

--- packages/node/packages/express/packages/test/src/globalSetup.ts
+++ packages/node/packages/express/packages/test/src/globalSetup.ts
@@ -11,6 +11,8 @@ declare global {
   var mongo: MongoMemoryReplSet
 }
 
+const database = process.env.MONGO_DATABASE || 'archivist'
+
 /**
  * Jest global setup method runs before any tests are run
  * https://jestjs.io/docs/configuration#globalsetup-string
@@ -21,9 +23,14 @@ module.exports = async (_globalConfig: Config, _projectConfig: Config) => {
   // https://nodkz.github.io/mongodb-memory-server/docs/guides/quick-start-guide/#replicaset
   // This will create an new instance of "MongoMemoryReplSet" and automatically start all Servers
   // To use Transactions, the "storageEngine" needs to be changed to `wiredTiger`
-  const mongo = await MongoMemoryReplSet.create({ replSet: { count: 3, storageEngine: 'wiredTiger' } }) // This will create an ReplSet with 4 members and storage-engine "wiredTiger"
+  const mongo = await MongoMemoryReplSet.create({
+    instanceOpts: [],
+    replSet: { count: 3, storageEngine: 'wiredTiger' },
+  }) // This will create an ReplSet with 4 members and storage-engine "wiredTiger"
   globalThis.mongo = mongo
   const uri = mongo.getUri()
-  process.env.MONGO_CONNECTION_STRING = uri
+  const mongoConnectionString = uri.split('/').slice(0, -1).concat(database).join('/') + uri.split('/').slice(-1)
+  // Recreate connection string to ReplicaSet adding default DB in the proper place
+  process.env.MONGO_CONNECTION_STRING = mongoConnectionString
   await Promise.resolve()
 }

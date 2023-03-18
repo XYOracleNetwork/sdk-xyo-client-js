048e132258970c34a40cbb641db79cae4218f052
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-17 23:11:31 -0500

    Bridged Discover test

--- packages/node/packages/express/packages/test/src/routes/name/Archivist.spec.ts
+++ packages/node/packages/express/packages/test/src/routes/name/Archivist.spec.ts
@@ -1,22 +1,21 @@
-import assertEx from '@xylabs/assert'
+import { assertEx } from '@xylabs/assert'
 import { Account } from '@xyo-network/account'
 import { AxiosJson } from '@xyo-network/axios'
 import { BoundWitnessBuilder } from '@xyo-network/boundwitness-builder'
 import { uuid } from '@xyo-network/core'
 import { getApp } from '@xyo-network/express-node-server'
 import { HttpBridge, HttpBridgeConfigSchema, XyoHttpBridgeParams } from '@xyo-network/http-bridge'
-import { ArchivistGetQuerySchema, ArchivistInsertQuerySchema } from '@xyo-network/modules'
+import { ArchivistGetQuerySchema, ArchivistInsertQuerySchema, ArchivistWrapper } from '@xyo-network/modules'
 import { PayloadBuilder } from '@xyo-network/payload-builder'
 import { Payload, XyoPayload } from '@xyo-network/payload-model'
-import { AddressPayload, AddressSchema } from '@xyo-network/plugins'
 import { QueryPayload, QuerySchema } from '@xyo-network/query-payload-plugin'
 import { Express } from 'express'
 import { StatusCodes } from 'http-status-codes'
-import { join } from 'path'
 import supertest, { SuperTest, Test } from 'supertest'
 
 import { request } from '../../testUtil'
 
+const name = ['Archivist']
 const path = '/Archivist'
 const security = { allowAnonymous: true }
 
@@ -28,41 +27,26 @@ describe('/Archivist', () => {
   beforeAll(async () => {
     app = await getApp()
     req = supertest(app)
-    const serverUri = req.get('/').url
-    const discover = await req.get('/')
-    const address = assertEx((discover.body.data as XyoPayload[]).find<AddressPayload>((p): p is AddressPayload => p.schema === AddressSchema))
-    const nodeUri = `/${address.address}`
+    const baseURL = req.get('/').url
+    // const discover = await req.get('/')
+    // const address = assertEx((discover.body.data as XyoPayload[]).find<AddressPayload>((p): p is AddressPayload => p.schema === AddressSchema))
+    const nodeUri = '/node'
     const params: XyoHttpBridgeParams = {
-      axios: new AxiosJson({
-        baseURL: serverUri,
-      }),
+      axios: new AxiosJson({ baseURL }),
       config: { nodeUri, schema: HttpBridgeConfigSchema, security },
     }
     bridge = await HttpBridge.create(params)
-    const other = req.get('/')
-    const foo = ''
   })
-  describe('GET', () => {
+  describe('Discover', () => {
     it('issues Discover query', async () => {
-      const response = await req.get(path).redirects(1).expect(StatusCodes.OK)
-      expect(response.body.data).toBeArray()
-      const data: Payload[] = response.body.data
-      expect(data).toBeArray()
-      validateDiscoverResponseContainsQuerySchemas(data, [ArchivistGetQuerySchema, ArchivistInsertQuerySchema])
-    })
-  })
-  describe('POST', () => {
-    describe('ArchivistInsertQuerySchema', () => {
-      describe('with BoundWitness', () => {
-        it('inserts BoundWitness', async () => {
-          const payload = new PayloadBuilder({ schema: 'network.xyo.debug' }).fields({ nonce: uuid() }).build()
-          const boundWitness = new BoundWitnessBuilder().payload(payload).witness(account).build()
-          const response = await (await request()).post(path).send().redirects(1).expect(StatusCodes.OK)
-          expect(response.body.data).toBeArray()
-          const data: Payload[] = response.body.data
-          expect(data).toBeArray()
-        })
-      })
+      const modules = await bridge.downResolver.resolve({ name })
+      expect(modules).toBeArrayOfSize(1)
+      const mod = modules.pop()
+      expect(mod).toBeTruthy()
+      const archivist = ArchivistWrapper.wrap(mod)
+      const discover = await archivist.discover()
+      expect(discover).toBeArray()
+      validateDiscoverResponseContainsQuerySchemas(discover, [ArchivistGetQuerySchema, ArchivistInsertQuerySchema])
     })
   })
 })

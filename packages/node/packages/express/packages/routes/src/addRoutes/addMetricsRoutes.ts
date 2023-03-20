542c3752ad1dda2a2f4bd0ec1a534400142e6e40
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-20 16:40:57 -0500

    Add metrics docker compose

--- packages/node/packages/express/packages/routes/src/addRoutes/addMetricsRoutes.ts
+++ packages/node/packages/express/packages/routes/src/addRoutes/addMetricsRoutes.ts
@@ -1,4 +1,4 @@
-import { adminApiKeyStrategy } from '@xyo-network/express-node-middleware'
+import { adminApiKeyStrategy, allowAnonymous, isDevelopment } from '@xyo-network/express-node-middleware'
 import { Express } from 'express'
 
 import { getMetrics } from '../routes'
@@ -6,7 +6,7 @@ import { getMetrics } from '../routes'
 export const addMetricsRoutes = (app: Express) => {
   app.get(
     '/metrics',
-    adminApiKeyStrategy,
+    isDevelopment() ? allowAnonymous : adminApiKeyStrategy,
     getMetrics /* #swagger.tags = ['Node'] */,
     /* #swagger.summary = 'Gets modules on the Node' */
   )

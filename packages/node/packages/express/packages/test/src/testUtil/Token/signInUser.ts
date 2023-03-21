747aeedafb226651b23be1183d066ec6384f75bb
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-21 10:40:25 -0500

    Remove legacy request pattern

--- packages/node/packages/express/packages/test/src/testUtil/Token/signInUser.ts
+++ packages/node/packages/express/packages/test/src/testUtil/Token/signInUser.ts
@@ -2,18 +2,19 @@ import { Wallet } from '@ethersproject/wallet'
 import { StatusCodes } from 'http-status-codes'
 
 import { TestWeb3User } from '../Model'
-import { request } from '../Server'
+import { getRequestClient } from '../Server'
 
 export const signInUser = async (user: TestWeb3User): Promise<string> => {
-  const challengeResponse = await (await request()).post(`/account/${user.address}/challenge`).send(user).expect(StatusCodes.OK)
-  const { state } = challengeResponse.body.data
+  const client = getRequestClient()
+  const challengeResponse = await client.post(`/account/${user.address}/challenge`, user)
+  expect(challengeResponse.status).toBe(StatusCodes.OK)
+  const { state } = challengeResponse.data
   const wallet = new Wallet(user.privateKey)
   const signature = await wallet.signMessage(state)
-  const verifyBody = {
-    address: wallet.address,
-    message: state,
-    signature,
-  }
-  const tokenResponse = await (await request()).post(`/account/${wallet.address}/verify`).send(verifyBody).expect(StatusCodes.OK)
-  return tokenResponse.body.data.token
+  const verifyBody = { address: wallet.address, message: state, signature }
+  const tokenResponse = await client.post(`/account/${wallet.address}/verify`, verifyBody)
+  expect(tokenResponse.status).toBe(StatusCodes.OK)
+  const { token } = tokenResponse.data
+  expect(token).toBeString()
+  return token
 }

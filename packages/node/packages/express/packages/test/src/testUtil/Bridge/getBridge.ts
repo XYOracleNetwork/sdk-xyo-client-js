d852f7db358e5f02c0d6c329babf747f911e4c8d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-21 10:22:41 -0500

    Funnel test clients through helper method

--- packages/node/packages/express/packages/test/src/testUtil/Bridge/getBridge.ts
+++ packages/node/packages/express/packages/test/src/testUtil/Bridge/getBridge.ts
@@ -1,17 +1,17 @@
-import { AxiosJson } from '@xyo-network/axios'
 import { HttpBridge, HttpBridgeConfigSchema, XyoHttpBridgeParams } from '@xyo-network/http-bridge'
+
+import { getRequestClient } from '../Server'
 
 let bridge: HttpBridge
 
 export const getBridge = async (): Promise<HttpBridge> => {
   if (bridge) return bridge
-  const baseURL = process.env.API_DOMAIN
-  const axios = new AxiosJson({ baseURL })
+  const client = getRequestClient()
   const nodeUri = '/node'
   const schema = HttpBridgeConfigSchema
   const security = { allowAnonymous: true }
   const config = { nodeUri, schema, security }
-  const params: XyoHttpBridgeParams = { axios, config }
+  const params: XyoHttpBridgeParams = { axios: client, config }
   bridge = await HttpBridge.create(params)
   return bridge
 }
--- packages/node/packages/express/packages/test/src/testUtil/Payload/getExistingPayload.ts
+++ packages/node/packages/express/packages/test/src/testUtil/Payload/getExistingPayload.ts
@@ -1,9 +1,9 @@
-import { AxiosJson } from '@xyo-network/axios'
 import { Payload } from '@xyo-network/payload-model'
 
+import { getRequestClient } from '../Server'
+
 export const getExistingPayloadByHash = async <T extends Payload = Payload>(hash: string): Promise<T> => {
-  const baseURL = process.env.API_DOMAIN
-  const axios = new AxiosJson({ baseURL })
-  const response = await axios.get<T>(`/${hash}`)
+  const client = getRequestClient()
+  const response = await client.get<T>(`/${hash}`)
   return response.data
 }

78c1fc3fc6aac6233f1d885062d4fa74b074a32f
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-21 09:45:04 -0500

    Update Archivist tests to use test helpers

--- packages/node/packages/express/packages/test/src/routes/name/Archivist.spec.ts
+++ packages/node/packages/express/packages/test/src/routes/name/Archivist.spec.ts
@@ -1,21 +1,14 @@
-import { uuid } from '@xyo-network/core'
 import { ArchivistGetQuerySchema, ArchivistInsertQuerySchema, ArchivistWrapper } from '@xyo-network/modules'
-import { PayloadBuilder } from '@xyo-network/payload-builder'
 import { PayloadWrapper } from '@xyo-network/payload-wrapper'
 
-import { getBridge, validateDiscoverResponseContainsQuerySchemas } from '../../testUtil'
+import { getArchivist, getNewPayload, validateDiscoverResponseContainsQuerySchemas } from '../../testUtil'
 
 const moduleName = 'Archivist'
 
 describe(`/${moduleName}`, () => {
   let archivist: ArchivistWrapper
   beforeAll(async () => {
-    const name = [moduleName]
-    const modules = await (await getBridge()).downResolver.resolve({ name })
-    expect(modules).toBeArrayOfSize(1)
-    const mod = modules.pop()
-    expect(mod).toBeTruthy()
-    archivist = ArchivistWrapper.wrap(mod)
+    archivist = await getArchivist()
   })
   describe('ModuleDiscoverQuerySchema', () => {
     it('issues query', async () => {
@@ -26,7 +19,7 @@ describe(`/${moduleName}`, () => {
   })
   describe('ArchivistInsertQuerySchema', () => {
     it('issues query', async () => {
-      const payload = new PayloadBuilder({ schema: 'network.xyo.debug' }).fields({ nonce: uuid() }).build()
+      const payload = getNewPayload()
       const hash = PayloadWrapper.parse(payload).hash
       const response = await archivist.insert([payload])
       expect(response).toBeArray()
@@ -38,7 +31,7 @@ describe(`/${moduleName}`, () => {
     })
   })
   describe('ArchivistGetQuerySchema', () => {
-    const payload = new PayloadBuilder({ schema: 'network.xyo.debug' }).fields({ nonce: uuid() }).build()
+    const payload = getNewPayload()
     beforeAll(async () => {
       const result = await archivist.insert([payload])
       expect(result).toBeTruthy()

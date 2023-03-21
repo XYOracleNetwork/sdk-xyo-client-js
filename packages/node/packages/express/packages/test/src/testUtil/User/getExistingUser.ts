ab7f7ccf14be85e2a7c7c9b89ac99efea4906d61
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-21 10:36:04 -0500

    Remove legacy request pattern

--- packages/node/packages/express/packages/test/src/testUtil/User/getExistingUser.ts
+++ packages/node/packages/express/packages/test/src/testUtil/User/getExistingUser.ts
@@ -1,12 +1,17 @@
 import { StatusCodes } from 'http-status-codes'
 
 import { TestWeb3User } from '../Model'
-import { request } from '../Server'
+import { getRequestClient } from '../Server'
 import { getNewUser } from './getNewUser'
 
 export const getExistingUser = async (expectedStatus: StatusCodes = StatusCodes.CREATED): Promise<TestWeb3User> => {
   const apiKey = process.env.API_KEY as string
   const user = getNewUser()
-  await (await request()).post('/user/signup').set('x-api-key', apiKey).send({ address: user.address }).expect(expectedStatus)
+  const client = getRequestClient()
+  const path = '/user/signup'
+  const data = { address: user.address }
+  const config = { headers: { 'x-api-key': apiKey } }
+  const response = await client.post(path, data, config)
+  expect(response.status).toBe(expectedStatus)
   return user
 }

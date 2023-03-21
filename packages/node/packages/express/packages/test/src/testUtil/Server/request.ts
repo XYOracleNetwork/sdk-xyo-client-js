ee3d6c2f7b8ed72a1bd74fad12f019d03d2d1ac7
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-21 09:54:14 -0500

    Deprecate legacy testing server

--- packages/node/packages/express/packages/test/src/testUtil/Server/request.ts
+++ packages/node/packages/express/packages/test/src/testUtil/Server/request.ts
@@ -4,6 +4,11 @@ import supertest, { SuperTest, Test } from 'supertest'
 
 let app: Express | undefined
 
+/**
+ *
+ * @returns
+ * @deprecated Use global server via Node helpers instead
+ */
 export async function request(): Promise<SuperTest<Test>> {
   if (!app) app = await getApp()
   return supertest(app)

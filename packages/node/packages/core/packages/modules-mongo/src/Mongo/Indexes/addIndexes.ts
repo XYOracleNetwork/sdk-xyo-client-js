0a41bc4f7ee423d30ee21341ec769a9dab8c8d14
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-21 18:17:47 -0500

    Index creation by collection

--- packages/node/packages/core/packages/modules-mongo/src/Mongo/Indexes/addIndexes.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Mongo/Indexes/addIndexes.ts
@@ -1,5 +1,11 @@
-import { Db } from 'mongodb'
+import { Db, IndexDescription } from 'mongodb'
+
+const indexes: Record<string, IndexDescription[]> = {}
 
 export const addIndexes = async (db: Db) => {
-  await Promise.resolve()
+  const collections = Object.keys(indexes)
+  for (const collection of collections) {
+    const indexSpecs = indexes.collection
+    await db.collection(collection).createIndexes(indexSpecs)
+  }
 }

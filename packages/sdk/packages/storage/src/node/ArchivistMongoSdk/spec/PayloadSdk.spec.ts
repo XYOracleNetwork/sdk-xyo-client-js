2f7813b112d5b3475018b9dd77ef5dc2644c6cfa
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-22 07:22:56 -0500

    Simplify test setup

--- packages/sdk/packages/storage/src/node/ArchivistMongoSdk/spec/PayloadSdk.spec.ts
+++ packages/sdk/packages/storage/src/node/ArchivistMongoSdk/spec/PayloadSdk.spec.ts
@@ -1,5 +1,4 @@
 import { assertEx } from '@xylabs/assert'
-import { delay } from '@xylabs/delay'
 import { describeIf } from '@xylabs/jest-helpers'
 import { uuid } from '@xyo-network/core'
 import { PayloadBuilder } from '@xyo-network/payload-builder'
@@ -28,48 +27,37 @@ const getMongoSdk = (archive: string) => {
   )
 }
 
-const getPayloads = async (number = 5): Promise<Payload[]> => {
-  return await Promise.all(
-    new Array(number).fill(0).map(async (_) => {
-      await delay(2)
-      return new PayloadBuilder({ schema }).fields({ prop: uuid() }).build(true)
-    }),
-  )
+const getPayloads = (number = 5): Payload[] => {
+  return new Array(number).fill(0).map((_val, idx) => {
+    const payload = new PayloadBuilder({ schema }).fields({ prop: uuid() }).build(true)
+    payload._timestamp = idx + 1
+    return payload
+  })
 }
 
 describeIf(process.env.MONGO_CONNECTION_STRING)('XyoArchivistPayloadMongoSdk', () => {
   const numPayloads = 20
   const limit = 10
-  let sdk: XyoArchivistPayloadMongoSdk
-  let payloads: PayloadWithPartialMeta[] = []
+  const payloads: PayloadWithPartialMeta[] = getPayloads(numPayloads)
+  const sdk: XyoArchivistPayloadMongoSdk = getMongoSdk('test')
   beforeAll(async () => {
-    sdk = getMongoSdk('test')
-    payloads = await getPayloads(numPayloads)
     for (let payload = 0; payload < payloads.length; payload++) {
       await sdk.insert(payloads[payload])
     }
   })
   describe('findAfter', () => {
-    let payload: PayloadWithPartialMeta | undefined
-    let hash = ''
-    let timestamp = 0
-    beforeAll(async () => {
-      hash = PayloadWrapper.hash(payloads[0])
-      expect(hash).toBeTruthy()
-      payload = (await sdk.findByHash(hash))[0]
-      expect(payload).toBeDefined()
-      timestamp = payload?._timestamp || 0
-      expect(timestamp).toBeTruthy()
-    })
+    const payload: PayloadWithPartialMeta = payloads[0]
+    const firstPayloadHash = PayloadWrapper.hash(payload)
+    const timestamp = assertEx(payload._timestamp)
     it('finds all records after the specified timestamp', async () => {
-      const actual = await sdk.findAfter(0, limit)
+      const actual = await sdk.findAfter(timestamp, limit)
       expect(actual.length).toBe(limit)
       expect(actual).toBeSortedBy('_timestamp', { descending: false })
-      const hashes = actual?.map?.((bw) => bw._hash)
-      expect(hashes).not.toContain(hash)
+      const hashes = actual?.map?.((p) => p._hash)
+      expect(hashes).not.toContain(firstPayloadHash)
     })
     it('uses an index to perform the query', async () => {
-      const plan = await sdk.findAfterPlan(0, limit)
+      const plan = await sdk.findAfterPlan(timestamp, limit)
       expect(plan?.queryPlanner?.winningPlan?.inputStage?.inputStage?.stage).toBe('IXSCAN')
       expect(plan?.executionStats?.nReturned).toBeLessThanOrEqual(limit)
       expect(plan?.executionStats?.totalDocsExamined).toBeLessThanOrEqual(limit)
@@ -77,17 +65,9 @@ describeIf(process.env.MONGO_CONNECTION_STRING)('XyoArchivistPayloadMongoSdk', (
     })
   })
   describe('findBefore', () => {
-    let payload: PayloadWithPartialMeta | undefined
-    let hash = ''
-    let timestamp = 0
-    beforeAll(async () => {
-      hash = payloads[payloads.length - 1]?._hash || ''
-      expect(hash).toBeTruthy()
-      payload = (await sdk.findByHash(hash))[0]
-      expect(payload).toBeDefined()
-      timestamp = payload?._timestamp || 0
-      expect(timestamp).toBeTruthy()
-    })
+    const payload: PayloadWithPartialMeta = payloads[payloads.length - 1]
+    const hash = PayloadWrapper.hash(payload)
+    const timestamp = assertEx(payload._timestamp)
     it('finds all records before the specified timestamp', async () => {
       const actual = await sdk.findBefore(timestamp, limit)
       expect(actual.length).toBe(limit)
@@ -113,17 +93,9 @@ describeIf(process.env.MONGO_CONNECTION_STRING)('XyoArchivistPayloadMongoSdk', (
     })
   })
   describe('findByHash', () => {
-    let payload: PayloadWithPartialMeta | undefined
-    let hash = ''
-    let timestamp = 0
-    beforeAll(async () => {
-      hash = payloads[Math.floor(Math.random() * payloads.length)]?._hash || ''
-      expect(hash).toBeTruthy()
-      payload = (await sdk.findByHash(hash))[0]
-      expect(payload).toBeDefined()
-      timestamp = payload?._timestamp || 0
-      expect(timestamp).toBeTruthy()
-    })
+    const payload = payloads[Math.floor(Math.random() * payloads.length)]
+    const hash = PayloadWrapper.hash(payload)
+    const timestamp = assertEx(payload._timestamp)
     it('uses an index to perform the query by hash', async () => {
       const plan = await sdk.findByHashPlan(hash)
       expect(plan?.queryPlanner?.winningPlan?.inputStage?.stage).toBe('IXSCAN')

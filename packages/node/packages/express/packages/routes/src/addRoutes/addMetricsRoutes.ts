e83919fe1ca20d184352b9fd9922501187aef7e6
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-22 11:02:39 -0500

    Add health check endpoint

--- packages/node/packages/express/packages/routes/src/addRoutes/addMetricsRoutes.ts
+++ packages/node/packages/express/packages/routes/src/addRoutes/addMetricsRoutes.ts
@@ -5,6 +5,20 @@ import { getMetrics } from '../routes'
 
 export const addMetricsRoutes = (app: Express) => {
   app.get(
+    '/healthz',
+    allowAnonymous,
+    (req, res) => {
+      const data = {
+        date: new Date(),
+        message: 'Ok',
+        uptime: process.uptime(),
+      }
+      res.status(200).send(data)
+    },
+    /* #swagger.tags = ['Health'] */
+    /* #swagger.summary = 'Used for quick health check' */
+  )
+  app.get(
     '/metrics',
     isDevelopment() ? allowAnonymous : adminApiKeyStrategy,
     getMetrics /* #swagger.tags = ['Node'] */,

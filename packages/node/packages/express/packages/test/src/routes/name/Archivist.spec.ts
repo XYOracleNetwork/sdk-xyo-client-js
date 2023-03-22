eaf4d564dddea23fe96ff7775903161c78991ff5
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-22 14:44:26 -0500

    Diversify BWs in test data

--- packages/node/packages/express/packages/test/src/routes/name/Archivist.spec.ts
+++ packages/node/packages/express/packages/test/src/routes/name/Archivist.spec.ts
@@ -14,13 +14,13 @@ describe(`/${moduleName}`, () => {
   const payloadA: PayloadWrapper = PayloadWrapper.parse(getNewPayload())
   const boundWitnessA: BoundWitnessWrapper = BoundWitnessWrapper.parse(getNewBoundWitness(account, [payloadA])[0])
   const payloadB: PayloadWrapper = PayloadWrapper.parse(getNewPayload())
-  const boundWitnessB: BoundWitnessWrapper = BoundWitnessWrapper.parse(getNewBoundWitness(account, [payloadA])[0])
+  const boundWitnessB: BoundWitnessWrapper = BoundWitnessWrapper.parse(getNewBoundWitness(account, [payloadB])[0])
   const payloadC: PayloadWrapper = PayloadWrapper.parse(getNewPayload())
-  const boundWitnessC: BoundWitnessWrapper = BoundWitnessWrapper.parse(getNewBoundWitness(account, [payloadA])[0])
+  const boundWitnessC: BoundWitnessWrapper = BoundWitnessWrapper.parse(getNewBoundWitness(account, [payloadC])[0])
   const payloadD: PayloadWrapper = PayloadWrapper.parse(getNewPayload())
-  const boundWitnessD: BoundWitnessWrapper = BoundWitnessWrapper.parse(getNewBoundWitness(account, [payloadA])[0])
+  const boundWitnessD: BoundWitnessWrapper = BoundWitnessWrapper.parse(getNewBoundWitness(account, [payloadD])[0])
   const payloadE: PayloadWrapper = PayloadWrapper.parse(getNewPayload())
-  const boundWitnessE: BoundWitnessWrapper = BoundWitnessWrapper.parse(getNewBoundWitness(account, [payloadA])[0])
+  const boundWitnessE: BoundWitnessWrapper = BoundWitnessWrapper.parse(getNewBoundWitness(account, [payloadE])[0])
 
   const cases: [string, PayloadWrapperBase[]][] = [
     ['Payload', [payloadA]],

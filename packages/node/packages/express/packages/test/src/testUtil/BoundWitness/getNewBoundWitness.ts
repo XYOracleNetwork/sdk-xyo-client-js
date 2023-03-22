eb88e0cc105ed5217251c743dd20d6382766062f
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-22 15:07:59 -0500

    Support multiple signers

--- packages/node/packages/express/packages/test/src/testUtil/BoundWitness/getNewBoundWitness.ts
+++ packages/node/packages/express/packages/test/src/testUtil/BoundWitness/getNewBoundWitness.ts
@@ -4,10 +4,10 @@ import { Payload } from '@xyo-network/payload-model'
 import { unitTestSigningAccount } from '../Account'
 import { getNewPayloads } from '../Payload'
 
-export const getNewBoundWitness = (account = unitTestSigningAccount, payloads: Payload[] = getNewPayloads(1)) => {
-  return new BoundWitnessBuilder().payloads(payloads).witness(account).build()
+export const getNewBoundWitness = (signers = [unitTestSigningAccount], payloads: Payload[] = getNewPayloads(1)) => {
+  return new BoundWitnessBuilder().payloads(payloads).witnesses(signers).build()
 }
 
-export const getNewBoundWitnesses = (account = unitTestSigningAccount, numBoundWitnesses = 1, numPayloads = 1) => {
-  return new Array(numBoundWitnesses).fill(0).map(() => getNewBoundWitness(account, getNewPayloads(numPayloads)))
+export const getNewBoundWitnesses = (signers = [unitTestSigningAccount], numBoundWitnesses = 1, numPayloads = 1) => {
+  return new Array(numBoundWitnesses).fill(0).map(() => getNewBoundWitness(signers, getNewPayloads(numPayloads)))
 }

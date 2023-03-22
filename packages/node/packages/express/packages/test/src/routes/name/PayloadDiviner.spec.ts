fa9ec4b1e2b4e5c2461aad5bd242234cd5f50534
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-22 15:56:19 -0500

    Multi-schema divine tests

--- packages/node/packages/express/packages/test/src/routes/name/PayloadDiviner.spec.ts
+++ packages/node/packages/express/packages/test/src/routes/name/PayloadDiviner.spec.ts
@@ -109,20 +109,42 @@ describe(`/${moduleName}`, () => {
     describe('schema', () => {
       describe('with single schema', () => {
         const schema = getTestSchemaName()
-        it('divines Payload by schema', async () => {
-          const payloadBase = getNewPayload()
-          payloadBase.schema = schema
-          const payload: PayloadWrapper = PayloadWrapper.parse(payloadBase)
+        const schemas = [schema]
+        const payloadBase = getNewPayload()
+        payloadBase.schema = schema
+        const payload: PayloadWrapper = PayloadWrapper.parse(payloadBase)
+        const payloads = [payload]
+        beforeAll(async () => {
           await archivist.insert([payload.payload])
-
-          const query: PayloadQueryPayload = {
-            schema: PayloadQuerySchema,
-            schemas: [schema],
-          }
+        })
+        it('divines Payload by schema', async () => {
+          const query: PayloadQueryPayload = { schema: PayloadQuerySchema, schemas }
           const response = await diviner.divine([query])
-          expect(response).toBeArrayOfSize(1)
+          expect(response).toBeArrayOfSize(payloads.length)
           const responseHashes = response.map((p) => PayloadWrapper.hash(p))
-          expect(responseHashes).toContain(payload.hash)
+          expect(responseHashes).toContainAllValues(payloads.map((p) => p.hash))
+        })
+      })
+      describe('with multiple schemas', () => {
+        const schemaA = getTestSchemaName()
+        const schemaB = getTestSchemaName()
+        const schemas = [schemaA, schemaB]
+        const payloadBaseA = getNewPayload()
+        payloadBaseA.schema = schemaA
+        const payloadA: PayloadWrapper = PayloadWrapper.parse(payloadBaseA)
+        const payloadBaseB = getNewPayload()
+        payloadBaseB.schema = schemaA
+        const payloadB: PayloadWrapper = PayloadWrapper.parse(payloadBaseB)
+        const payloads = [payloadA, payloadB]
+        beforeAll(async () => {
+          await archivist.insert([payloadA.payload, payloadB.payload])
+        })
+        it('divines Payload by schema', async () => {
+          const query: PayloadQueryPayload = { schema: PayloadQuerySchema, schemas }
+          const response = await diviner.divine([query])
+          expect(response).toBeArrayOfSize(payloads.length)
+          const responseHashes = response.map((p) => PayloadWrapper.hash(p))
+          expect(responseHashes).toContainAllValues(payloads.map((p) => p.hash))
         })
       })
     })

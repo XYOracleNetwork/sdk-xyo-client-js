fa8703f465cfc1406034c7a562cc53586e3049b7
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-22 11:26:32 -0500

    Move route handler from inline to module

--- packages/node/packages/express/packages/routes/src/addRoutes/addMetricsRoutes.ts
+++ packages/node/packages/express/packages/routes/src/addRoutes/addMetricsRoutes.ts
@@ -1,19 +1,12 @@
 import { adminApiKeyStrategy, allowAnonymous, isDevelopment } from '@xyo-network/express-node-middleware'
 import { Express } from 'express'
-import { ReasonPhrases } from 'http-status-codes'
 
-import { getMetrics } from '../routes'
-const message = ReasonPhrases.OK
+import { getHealthz, getMetrics } from '../routes'
 export const addMetricsRoutes = (app: Express) => {
   app.get(
     '/healthz',
     allowAnonymous,
-    (req, res) => {
-      const date = new Date()
-      const { cpuUsage, memoryUsage, uptime } = process
-      const data = { cpuUsage, date, memoryUsage, message, uptime }
-      res.status(200).send(data)
-    },
+    getHealthz,
     /* #swagger.tags = ['Health'] */
     /* #swagger.summary = 'Used for quick health check' */
   )
@@ -22,5 +15,13 @@ export const addMetricsRoutes = (app: Express) => {
     isDevelopment() ? allowAnonymous : adminApiKeyStrategy,
     getMetrics /* #swagger.tags = ['Node'] */,
     /* #swagger.summary = 'Gets modules on the Node' */
+  )
+  app.get(
+    '/ready',
+    allowAnonymous,
+    // TODO: Custom endpoint as readiness !== health
+    getHealthz,
+    /* #swagger.tags = ['Health'] */
+    /* #swagger.summary = 'Used for readiness check' */
   )
 }

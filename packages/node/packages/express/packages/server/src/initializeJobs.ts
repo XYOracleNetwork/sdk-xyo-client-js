413456afd0758af5a5e331bcafe70f14c77db4bc
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-24 02:55:13 -0500

    Disable background jobs for beta

--- packages/node/packages/express/packages/server/src/initializeJobs.ts
+++ packages/node/packages/express/packages/server/src/initializeJobs.ts
@@ -4,7 +4,7 @@ import { JobQueue } from '@xyo-network/node-core-model'
 import { TYPES } from '@xyo-network/node-core-types'
 import { Job, JobProvider } from '@xyo-network/shared'
 
-const runJobs = true
+const runJobs = false
 
 export const initializeJobs = async () => {
   const jobs = runJobs ? (await container.getAllAsync<JobProvider>(TYPES.JobProvider)).flatMap((provider) => provider?.jobs).filter(exists) : []

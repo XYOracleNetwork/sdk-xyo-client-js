19c04b0e2e81168f4b0ea1ca4f15e0fc99789f76
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-23 22:52:40 -0500

    Fix linting issues

--- packages/modules/packages/bridge/packages/http/src/HttpBridgeConfig.ts
+++ packages/modules/packages/bridge/packages/http/src/HttpBridgeConfig.ts
@@ -1,6 +1,5 @@
 import { BridgeConfig } from '@xyo-network/bridge-model'
 import { AnyObject } from '@xyo-network/core'
-import { Url } from 'url'
 
 export type HttpBridgeConfigSchema = 'network.xyo.bridge.http.config'
 export const HttpBridgeConfigSchema: HttpBridgeConfigSchema = 'network.xyo.bridge.http.config'

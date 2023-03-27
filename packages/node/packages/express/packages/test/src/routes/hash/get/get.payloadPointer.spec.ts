7670f995d8a79a59515cc4da082d7c90cf75be9f
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-27 05:53:20 -0500

    Calculate necessary values locally

--- packages/node/packages/express/packages/test/src/routes/hash/get/get.payloadPointer.spec.ts
+++ packages/node/packages/express/packages/test/src/routes/hash/get/get.payloadPointer.spec.ts
@@ -15,15 +15,7 @@ import { Payload } from '@xyo-network/payload-model'
 import { PayloadWrapper } from '@xyo-network/payload-wrapper'
 import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 
-import {
-  getHash,
-  getNewBlock,
-  getNewBoundWitness,
-  insertBlock,
-  insertPayload,
-  otherUnitTestSigningAccount,
-  unitTestSigningAccount,
-} from '../../../testUtil'
+import { getHash, getNewBoundWitness, insertBlock, insertPayload, otherUnitTestSigningAccount, unitTestSigningAccount } from '../../../testUtil'
 
 const createPayloadPointer = async (
   addresses: string[][] = [],
@@ -51,11 +43,10 @@ const createPayloadPointer = async (
   reference.push([timestampRule])
 
   const pointer = new PayloadBuilder<PayloadPointerPayload>({ schema: PayloadPointerSchema }).fields({ reference }).build()
-  const pointerResponse = await insertBlock(getNewBlock(pointer), unitTestSigningAccount)
+  const pointerResponse = await insertPayload(pointer)
   expect(pointerResponse.length).toBe(2)
-  const pointerHash = pointerResponse[0].payload_hashes[0]
-  expect(pointerHash).toBeString()
-  return pointerHash
+  expect(pointerResponse.map((bw) => bw.payload_schemas.includes(PayloadPointerSchema)).some((x) => x)).toBeTrue()
+  return PayloadWrapper.hash(pointer)
 }
 
 describe('/:hash', () => {
@@ -74,7 +65,7 @@ describe('/:hash', () => {
       const payloadResponse = await insertPayload(payloads, account)
       expect(payloadResponse.length).toBe(2)
       // Create pointer to reference data
-      pointerHash = await createPayloadPointer([], [[payload.schema]])
+      pointerHash = await createPayloadPointer([[account.addressValue.hex]], [[payload.schema]])
     })
     it('a single payload matching the pointer criteria', async () => {
       const response = await getHash(pointerHash)
@@ -97,14 +88,14 @@ describe('/:hash', () => {
   describe('with rules for', () => {
     describe('address', () => {
       describe('single address', () => {
-        it.only('returns payload signed by address', async () => {
+        it('returns payload signed by address', async () => {
           const account = Account.random()
           const data = getNewBoundWitness([account])
           const payload = data[1][0]
-          const blockResponse = await insertBlock(block, account)
-          expect(blockResponse.length).toBe(2)
           const payloadResponse = await insertPayload(payload, account)
           expect(payloadResponse.length).toBe(2)
+          const blockResponse = await insertBlock(block, account)
+          expect(blockResponse.length).toBe(2)
           const pointerHash = await createPayloadPointer([[account.addressValue.hex]], [[payload.schema]])
           const result = await getHash(pointerHash)
           expect(result).toBeTruthy()
@@ -133,7 +124,7 @@ describe('/:hash', () => {
       })
       it('no matching address', async () => {
         const account = Account.random()
-        const pointerHash = await createPayloadPointer([[account.addressValue.hex]])
+        const pointerHash = await createPayloadPointer([[account.addressValue.hex]], [[payload.schema]])
         const result = await getHash(pointerHash)
         expect(result).toBeObject()
         const error = result as unknown as { errors: { detail: string; status: string }[] }

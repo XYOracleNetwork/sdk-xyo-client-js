4e7ed06e23b86a0de638ddb3405dfe5ed02660f8
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-27 08:38:31 -0500

    Use inline polyfill

--- packages/node/packages/express/packages/routes/src/routes/hash/get/findPayload.ts
+++ packages/node/packages/express/packages/routes/src/routes/hash/get/findPayload.ts
@@ -15,6 +15,23 @@ import { PayloadWrapper } from '@xyo-network/payload-wrapper'
 
 const limit = 1
 
+// https://stackoverflow.com/a/53187807
+/**
+ * Returns the index of the last element in the array where predicate is true, and -1
+ * otherwise.
+ * @param array The source array to search in
+ * @param predicate find calls predicate once for each element of the array, in descending
+ * order, until it finds one where predicate returns true. If such an element is found,
+ * findLastIndex immediately returns that element index. Otherwise, findLastIndex returns -1.
+ */
+export function findLastIndex<T>(array: Array<T>, predicate: (value: T, index: number, obj: T[]) => boolean): number {
+  let l = array.length
+  while (l--) {
+    if (predicate(array[l], l, array)) return l
+  }
+  return -1
+}
+
 const createBoundWitnessFilterFromSearchCriteria = (searchCriteria: PayloadSearchCriteria): Payload[] => {
   const { addresses, direction, schemas, timestamp } = searchCriteria
   const order = direction === 'asc' ? 'asc' : 'desc'
@@ -47,8 +64,10 @@ export const findPayload = async (
       const { schemas, direction } = searchCriteria
       let payloadIndex = direction === 'asc' ? 0 : bw.payloadHashes.length - 1
       if (schemas) {
-        const sorted = direction === 'asc' ? bw.payloadSchemas : bw.payloadSchemas.reverse()
-        payloadIndex = sorted.findIndex((schema) => schemas.includes(schema))
+        payloadIndex =
+          direction === 'asc'
+            ? bw.payloadSchemas.findIndex((schema) => schemas.includes(schema))
+            : findLastIndex(bw.payloadSchemas, (schema) => schemas.includes(schema))
       }
       const hash = bw.payloadHashes[payloadIndex]
       const payloads = ArchivistWrapper.wrap(archivist)

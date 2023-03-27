88d777b878922ad3880ec3231396d730e2014da4
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-27 16:42:17 -0500

    Refactor Gas Price Automation

--- packages/automation-witness-server/src/Jobs/EthereumGas/getTask.ts
+++ packages/automation-witness-server/src/Jobs/EthereumGas/getTask.ts
@@ -5,25 +5,24 @@ import { XyoEthereumGasSchema } from '@xyo-network/gas-price-payload-plugin'
 import { Task } from '@xyo-network/shared'
 
 import { getDiviner } from './getDiviner'
-import { getDivinerResultPanel } from './getDivinerResultPanel'
-import { getWitnessPanel } from './getWitnessPanel'
+import { reportDivinerResult } from './reportDivinerResult'
+import { reportGasPrices } from './reportGasPrices'
 
 export const getTask = (): Task => {
   const logger = getDefaultLogger()
   const task: Task = async () => {
     try {
       logger.log('Witnessing Ethereum Gas Prices')
-      const witnessPanel = await getWitnessPanel()
-      const payloads = await witnessPanel.report()
+      const payloads = await reportGasPrices()
       logger.log('Witnessed Ethereum Gas Prices')
       logger.log('Divining Aggregated Gas Price')
       const diviner = await getDiviner()
-      const result = (await new DivinerWrapper({ module: diviner }).divine(payloads)).find((p) => p.schema === XyoEthereumGasSchema)
+      const results = await new DivinerWrapper({ module: diviner }).divine(payloads)
+      const result = results.find((p) => p.schema === XyoEthereumGasSchema)
       const answer = assertEx(result, 'Empty XyoEthereumGasPayload response from diviner')
       logger.log('Divined Aggregated Gas Price')
       logger.log('Witnessing Aggregated Gas Price')
-      const divinerResultPanel = await getDivinerResultPanel(answer)
-      await divinerResultPanel.report()
+      await reportDivinerResult(answer)
       logger.log('Witnessed Aggregated Gas Price')
     } catch (error) {
       logger.error(error)

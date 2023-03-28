dee4227c79331c4493ee306e67d978a97b953436
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-28 12:06:04 -0500

    SetIterator tests

--- packages/node/packages/core/packages/modules-mongo/src/Util/SetIterator/spec/SetIterator.spec.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Util/SetIterator/spec/SetIterator.spec.ts
@@ -1,76 +1,65 @@
 import { SetIterator } from '../SetIterator'
 
 describe('SetIterator', () => {
-  const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  const additional = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+  const values = [0, 1]
+  const additional = [2, 3]
   describe('addValues', () => {
     it('Adds values in bulk', () => {
       const iterator = new SetIterator(values)
       iterator.addValues(additional)
     })
     it('Supports addition while iterating', () => {
-      const batchSize = values.length / 2
       const iterator = new SetIterator(values)
 
       const result1 = iterator.next()
-      expect(result1.value).toEqual(values.slice(0, batchSize))
+      expect(result1.value).toEqual(values[0])
       expect(result1.done).toBe(false)
 
       iterator.addValues(additional)
 
       const result2 = iterator.next()
-      expect(result2.value).toEqual(values.slice(batchSize))
+      expect(result2.value).toEqual(values[1])
       expect(result2.done).toBe(false)
 
       iterator.addValues(additional)
 
       const result3 = iterator.next()
-      expect(result3.value).toEqual(additional.slice(0, batchSize))
+      expect(result3.value).toEqual(additional[0])
       expect(result3.done).toBe(false)
 
       const result4 = iterator.next()
-      expect(result4.value).toEqual(additional.slice(batchSize))
+      expect(result4.value).toEqual(additional[1])
       expect(result4.done).toBe(false)
+
+      const result5 = iterator.next()
+      expect(result5.value).toEqual(values[0])
+      expect(result5.done).toBe(false)
+
+      const result6 = iterator.next()
+      expect(result6.value).toEqual(values[1])
+      expect(result6.done).toBe(false)
     })
   })
   describe('next', () => {
-    it.each([1, 3, 5, 10])('Gets values in bulk according to the batch size', (batchSize) => {
-      const iterator = new SetIterator(values)
-      for (let i = 0; i < 20; i++) {
-        const result = iterator.next()
-        expect(result.value.length).toBeLessThanOrEqual(batchSize)
-        expect(result.done).toBe(false)
-      }
-    })
     it('Overflows back to the beginning', () => {
       const iterator = new SetIterator(values)
-      iterator.addValues(additional)
-
-      const result1 = iterator.next()
-      expect(result1.value).toEqual(values)
-      expect(result1.done).toBe(false)
-
-      const result2 = iterator.next()
-      expect(result2.value).toEqual(additional)
-      expect(result2.done).toBe(false)
-
-      const result3 = iterator.next()
-      expect(result3.value).toEqual(values)
-      expect(result3.done).toBe(false)
-
-      const result4 = iterator.next()
-      expect(result4.value).toEqual(additional)
-      expect(result4.done).toBe(false)
+      for (let i = 0; i < values.length; i++) {
+        const result = iterator.next()
+        expect(result.value).toEqual(values[i])
+        expect(result.done).toBe(false)
+      }
+      const result = iterator.next()
+      expect(result.value).toEqual(values[0])
+      expect(result.done).toBe(false)
     })
     it('Handles empty values', () => {
       const iterator = new SetIterator([])
-
       const result1 = iterator.next()
-      expect(result1.value).toEqual([])
+      expect(result1.value).toBeUndefined()
       expect(result1.done).toBe(true)
     })
     it('Works with for-of loop', () => {
-      const maxIterations = 5
+      const maxIterations = values.length * 3
       const iterator = new SetIterator(values)
       let iterations = 0
       for (const batch of iterator) {

2b12dd9d74e90632fa8c7caf7e10ab76a87e7577
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-28 04:30:58 -0500

    Implement basic set operations

--- packages/node/packages/core/packages/modules-mongo/src/Util/WorkingSetCache/WorkingSetCache.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Util/WorkingSetCache/WorkingSetCache.ts
@@ -1,11 +1,24 @@
+const union = <TKey>(a: Set<TKey>, b: Set<TKey>): Set<TKey> => {
+  return new Set([...a, ...b])
+}
+const intersection = <TKey>(a: Set<TKey>, b: Set<TKey>): Set<TKey> => {
+  return new Set(Array.from(a).filter((x) => b.has(x)))
+}
+const difference = <TKey>(a: Set<TKey>, b: Set<TKey>): Set<TKey> => {
+  return new Set(Array.from(a).filter((x) => !b.has(x)))
+}
+
 export class WorkingSetCache<TKey> {
   private readonly done: Set<TKey> = new Set<TKey>()
   private readonly todo: Set<TKey> = new Set<TKey>()
 
   addItems(values: Array<TKey>): void {
+    // Add everything to todo that is not already in done
     throw new Error('')
   }
   getBatch(count: number): Array<TKey> {
+    // Take count from todo
+    // If todo < count then move all of done back over to todo (roll over)
     throw new Error('')
   }
 }

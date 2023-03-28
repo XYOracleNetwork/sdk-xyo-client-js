226feaffa3fedcf034e07aa4a6e74d90b5497cc4
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-28 05:39:15 -0500

    Sample implementation

--- packages/node/packages/core/packages/modules-mongo/src/Util/WorkingSetCache/WorkingSetCache.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Util/WorkingSetCache/WorkingSetCache.ts
@@ -8,17 +8,37 @@ const difference = <TKey>(a: Set<TKey>, b: Set<TKey>): Set<TKey> => {
   return new Set(Array.from(a).filter((x) => !b.has(x)))
 }
 
-export class WorkingSetCache<TKey> {
-  private readonly done: Set<TKey> = new Set<TKey>()
-  private readonly todo: Set<TKey> = new Set<TKey>()
+export class WorkingSetCache<TKey> implements Iterator<TKey> {
+  protected done: Set<TKey> = new Set<TKey>()
+  protected todo: Set<TKey> = new Set<TKey>()
 
-  addItems(values: Array<TKey>): void {
+  addItems(values: Iterable<TKey> | ReadonlyArray<TKey> | null | undefined): void {
     // Add everything to todo that is not already in done
-    throw new Error('')
+    const incoming = new Set(values)
+    this.todo = new Set(difference(this.done, incoming))
   }
   getBatch(count: number): Array<TKey> {
     // Take count from todo
-    // If todo < count then move all of done back over to todo (roll over)
-    throw new Error('')
+    const todo = Array.from(this.todo)
+    const batch = todo.slice(0, count)
+    const remaining = todo.slice(count)
+    // Simulate rollover by moving all of done back to todo
+    if (remaining.length < 1) {
+      this.todo = new Set(this.done)
+      this.done = new Set()
+    } else {
+      this.todo = new Set(remaining)
+      this.done = union(this.done, new Set(batch))
+    }
+    return batch
+  }
+  next(...args: [] | [undefined]): IteratorResult<TKey, any> {
+    throw new Error('Method not implemented.')
+  }
+  return?(value?: any): IteratorResult<TKey, any> {
+    throw new Error('Method not implemented.')
+  }
+  throw?(e?: any): IteratorResult<TKey, any> {
+    throw new Error('Method not implemented.')
   }
 }

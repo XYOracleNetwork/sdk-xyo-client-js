e497d47e8b357a584db54ccc8c7cad9e4e0dae9f
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-27 21:45:57 -0500

    Use native types over caches

--- packages/node/packages/core/packages/modules-mongo/src/Util/WorkingSetCache/WorkingSetCache.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Util/WorkingSetCache/WorkingSetCache.ts
@@ -1,4 +1,7 @@
 export class WorkingSetCache<TKey> {
+  private readonly done: Set<TKey> = new Set<TKey>()
+  private readonly todo: Set<TKey> = new Set<TKey>()
+
   addItems(values: Array<TKey>): void {
     throw new Error('')
   }

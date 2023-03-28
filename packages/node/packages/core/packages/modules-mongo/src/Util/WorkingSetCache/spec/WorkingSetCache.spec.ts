3364ac1d58f53bf74ff70a5490d6391fe5f030a6
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-28 05:47:01 -0500

    Tests for WorkingSet

---
+++ packages/node/packages/core/packages/modules-mongo/src/Util/WorkingSetCache/spec/WorkingSetCache.spec.ts
@@ -0,0 +1,18 @@
+describe('WorkingSetCache', () => {
+  describe('addItems', () => {
+    it('Adds items in bulk', () => {
+      // TODO
+    })
+    it('Removes duplicates items in bulk', () => {
+      // TODO
+    })
+  })
+  describe('getBatch', () => {
+    it('Gets items in bulk', () => {
+      // TODO
+    })
+    it('Overflows back to the beginning', () => {
+      // TODO
+    })
+  })
+})

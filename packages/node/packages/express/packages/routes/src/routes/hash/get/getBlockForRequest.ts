68b60c7c2877b5e194bdbc28bb916f8893d5ea16
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-29 15:00:04 -0500

    Remove archivist from globals & get from Node

--- packages/node/packages/express/packages/routes/src/routes/hash/get/getBlockForRequest.ts
+++ packages/node/packages/express/packages/routes/src/routes/hash/get/getBlockForRequest.ts
@@ -5,9 +5,16 @@ import { Request } from 'express'
 
 import { resolvePayloadPointer } from './resolvePayloadPointer'
 
+let archivist: ArchivistWrapper
+
 export const getBlockForRequest = async (req: Request, hash: string): Promise<Payload | undefined> => {
-  const { archivist } = req.app
-  const block = (await ArchivistWrapper.wrap(archivist).get([hash])).pop()
+  if (!archivist) {
+    const { node } = req.app
+    const modules = await node.downResolver.resolve({ name: ['Archivist'] })
+    const module = modules.pop()
+    archivist = ArchivistWrapper.wrap(module)
+  }
+  const block = (await archivist.get([hash])).pop()
   if (block) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return block.schema === PayloadPointerSchema ? await resolvePayloadPointer(req, block as any as PayloadPointerPayload) : block

dfbd2c9672842493e6f9f02078be3fbffe5669c4
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-03-29 12:37:56 -0500

    Use Account static methods for generation from mnemonic

--- packages/automation-witness-server/src/Account/getAccount.ts
+++ packages/automation-witness-server/src/Account/getAccount.ts
@@ -1,9 +1,7 @@
 import { assertEx } from '@xylabs/assert'
 import { Account } from '@xyo-network/account'
 
-import { fromMnemonic } from './HdWallet'
-
 export const getAccount = (path?: string): Account => {
   const mnemonic = assertEx(process.env.MNEMONIC, 'Missing mnemonic for wallet creation')
-  return fromMnemonic(mnemonic, path)
+  return Account.fromMnemonic(mnemonic, path)
 }

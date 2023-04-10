6d597530cac2cfdd455ea4bc413bccf9576ac4f7
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-10 11:05:56 -0500

    Ensure ESLint doesn't reorder our indexes from how we want

--- packages/node/packages/core/packages/modules-mongo/src/Mongo/Indexes/Specifications/Archives/UX_archive.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Mongo/Indexes/Specifications/Archives/UX_archive.ts
@@ -1,6 +1,7 @@
 import { IndexDescription } from 'mongodb'
 
 export const UX_archive: IndexDescription = {
+  // eslint-disable-next-line sort-keys-fix/sort-keys-fix
   key: { archive: 1 },
   name: 'archives.UX_archive',
   unique: true,
--- packages/node/packages/core/packages/modules-mongo/src/Mongo/Indexes/Specifications/ArchivistStats/UX_address.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Mongo/Indexes/Specifications/ArchivistStats/UX_address.ts
@@ -1,6 +1,7 @@
 import { IndexDescription } from 'mongodb'
 
 export const UX_address: IndexDescription = {
+  // eslint-disable-next-line sort-keys-fix/sort-keys-fix
   key: { address: 1 },
   name: 'archivist_stats.UX_address',
   unique: true,

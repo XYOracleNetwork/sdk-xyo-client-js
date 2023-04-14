73cf4a388abaf0b586e4ec6ad6715cc93913ad93
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-13 19:33:11 -0500

    Add previous hash module CLI command

---
+++ packages/cli/src/command/commands/module/previousHash.ts
@@ -0,0 +1,35 @@
+import { EmptyObject } from '@xyo-network/core'
+import { Argv, CommandBuilder, CommandModule } from 'yargs'
+
+import { printError, printLine } from '../../../lib'
+import { ModuleArguments } from '../ModuleArguments'
+import { getModule } from '../util'
+
+export const aliases: ReadonlyArray<string> = []
+export const builder: CommandBuilder = (yargs: Argv) =>
+  yargs.usage('Usage: $0 module describe <address>').positional('address', { demandOption: true, type: 'string' })
+export const command = 'previousHash <address>'
+export const deprecated = false
+export const describe = 'Issue a PreviousHash Query against the XYO Module'
+export const handler = async (argv: ModuleArguments) => {
+  const { verbose } = argv
+  try {
+    const mod = await getModule(argv)
+    const result = await mod.previousHash()
+    printLine(JSON.stringify(result))
+  } catch (error) {
+    if (verbose) printError(JSON.stringify(error))
+    throw new Error('Error Querying Module')
+  }
+}
+
+const mod: CommandModule<EmptyObject, ModuleArguments> = {
+  aliases,
+  command,
+  deprecated,
+  describe,
+  handler,
+}
+
+// eslint-disable-next-line import/no-default-export
+export default mod

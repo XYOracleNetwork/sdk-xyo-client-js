7de3204d80f421763d47c861a54c4cf14733b414
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-16 15:47:44 -0500

    Simplify method complexity

--- packages/cli/src/command/BaseArguments.ts
+++ packages/cli/src/command/BaseArguments.ts
@@ -5,7 +5,7 @@ export type OutputType = 'json' | 'raw'
 export type BaseArguments = ArgumentsCamelCase<{
   config?: string
   help: boolean
-  network?: string
+  network: string
   output: OutputType
   verbose: boolean
   version: boolean
--- packages/cli/src/command/util/getBridgeConfig.ts
+++ packages/cli/src/command/util/getBridgeConfig.ts
@@ -10,13 +10,13 @@ const security = { allowAnonymous: true }
 export const getBridgeConfig = async (args: BaseArguments): Promise<HttpBridgeConfig> => {
   const { network, verbose } = args
   try {
+    let nodeUrl = process.env.API_DOMAIN || 'http://localhost:8080'
     if (network) {
       const slug = network.toLowerCase()
       const known = knownNodes().find((node) => node.slug === slug)
-      if (known) return { nodeUrl: known.uri, schema, security }
+      if (known) nodeUrl = known.uri
     }
     await Promise.resolve('TODO: Might need to obtain from config')
-    const nodeUrl = process.env.API_DOMAIN || 'http://localhost:8080'
     return { nodeUrl, schema, security }
   } catch (error) {
     if (verbose) printError(JSON.stringify(error))

c5d552b57fa4e4584268a10809b7d25bbba7b2fa
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-16 17:17:18 -0500

    Export Factory types

--- packages/node/packages/core/packages/model/src/Module/ModuleFactory.ts
+++ packages/node/packages/core/packages/model/src/Module/ModuleFactory.ts
@@ -1,3 +1,15 @@
-import { AbstractModule, ModuleConfig } from '@xyo-network/module'
+import { AbstractModule, ModuleConfig, ModuleParams } from '@xyo-network/module'
 
-export type ModuleFactory = (config: ModuleConfig) => AbstractModule | Promise<AbstractModule>
+export type ParamsModuleFactory<T extends AbstractModule = AbstractModule, TParams extends ModuleParams = ModuleParams> = (
+  params: TParams,
+) => T | Promise<T>
+
+export type ConfigModuleFactory<T extends AbstractModule = AbstractModule, TConfig extends ModuleConfig = ModuleConfig> = (
+  config: TConfig,
+) => T | Promise<T>
+
+export type ParamsConfigModuleFactory<
+  T extends AbstractModule = AbstractModule,
+  TParams extends ModuleParams = ModuleParams,
+  TConfig extends ModuleConfig = ModuleConfig,
+> = (params: TParams) => ConfigModuleFactory<T, TConfig> | Promise<ConfigModuleFactory<T, TConfig>>

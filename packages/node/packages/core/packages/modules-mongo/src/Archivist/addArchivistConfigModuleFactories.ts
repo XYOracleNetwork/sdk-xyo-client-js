79bfccc1475b20f2d623d93348c944696967c372
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-18 12:04:34 -0500

    Prefer inferred types

--- packages/node/packages/core/packages/modules-mongo/src/Archivist/addArchivistConfigModuleFactories.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Archivist/addArchivistConfigModuleFactories.ts
@@ -1,7 +1,7 @@
 import { Account } from '@xyo-network/account'
-import { AbstractArchivist, ArchivistConfig } from '@xyo-network/archivist'
+import { ArchivistConfig } from '@xyo-network/archivist'
 import { AnyConfigSchema } from '@xyo-network/module'
-import { BoundWitnessWithMeta, ConfigModuleFactory, ConfigModuleFactoryDictionary, PayloadWithMeta } from '@xyo-network/node-core-model'
+import { BoundWitnessWithMeta, ConfigModuleFactoryDictionary, PayloadWithMeta } from '@xyo-network/node-core-model'
 import { TYPES, WALLET_PATHS } from '@xyo-network/node-core-types'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { Container } from 'inversify'
@@ -9,19 +9,19 @@ import { Container } from 'inversify'
 import { MONGO_TYPES } from '../mongoTypes'
 import { MongoDBDeterministicArchivist } from './Deterministic'
 
-const getMongoDBArchivistFactory = (container: Container): ConfigModuleFactory<AbstractArchivist> => {
+const getMongoDBArchivistFactory = (container: Container) => {
   const mnemonic = container.get<string>(TYPES.AccountMnemonic)
   const account = Account.fromMnemonic(mnemonic, WALLET_PATHS.Archivists.Archivist)
   const boundWitnessSdk: BaseMongoSdk<BoundWitnessWithMeta> = container.get<BaseMongoSdk<BoundWitnessWithMeta>>(MONGO_TYPES.BoundWitnessSdk)
   const payloadSdk: BaseMongoSdk<PayloadWithMeta> = container.get<BaseMongoSdk<PayloadWithMeta>>(MONGO_TYPES.PayloadSdk)
-  const archivistFactory = async (config: AnyConfigSchema<ArchivistConfig>) =>
+  const factory = async (config: AnyConfigSchema<ArchivistConfig>) =>
     await MongoDBDeterministicArchivist.create({
       account,
       boundWitnessSdk,
       config: { ...config, name: TYPES.Archivist.description, schema: MongoDBDeterministicArchivist.configSchema },
       payloadSdk,
     })
-  return archivistFactory
+  return factory
 }
 
 export const addArchivistConfigModuleFactories = (container: Container) => {

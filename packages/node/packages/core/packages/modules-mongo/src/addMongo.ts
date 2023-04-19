9637acd820f64fc32274f434a5f0d1decbaa9bee
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-18 21:11:03 -0500

    Refactor Memory vs Mongo modules

--- packages/node/packages/core/packages/modules-mongo/src/addMongo.ts
+++ packages/node/packages/core/packages/modules-mongo/src/addMongo.ts
@@ -5,7 +5,7 @@ import { addDivinerConfigModuleFactories } from './Diviner'
 import { JobQueueContainerModule } from './JobQueue'
 import { initializeDatabase } from './Mongo'
 
-export const addMongo = async (container: Container) => {
+export const addMongoModules = async (container: Container) => {
   await initializeDatabase()
   addArchivistConfigModuleFactories(container)
   addDivinerConfigModuleFactories(container)

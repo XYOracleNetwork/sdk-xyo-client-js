c5667c150c799e7eb3d54a12ee7e6199e1eb4c36
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-19 06:23:45 -0500

    Rename schema to match types

--- packages/node/packages/core/packages/model/src/Diviner/BoundWitnessStatsDiviner.ts
+++ packages/node/packages/core/packages/model/src/Diviner/BoundWitnessStatsDiviner.ts
@@ -5,14 +5,14 @@ import { Payload } from '@xyo-network/payload-model'
 import { ArchivistPayloadStatsDivinerConfig } from './PayloadStatsDiviner'
 import { StatsPayload } from './StatsPayload'
 
-export type BoundWitnessStatsSchema = 'network.xyo.archivist.boundwitness.stats'
-export const BoundWitnessStatsSchema: BoundWitnessStatsSchema = 'network.xyo.archivist.boundwitness.stats'
+export type BoundWitnessStatsSchema = 'network.xyo.diviner.boundwitness.stats'
+export const BoundWitnessStatsSchema: BoundWitnessStatsSchema = 'network.xyo.diviner.boundwitness.stats'
 
-export type BoundWitnessStatsQuerySchema = 'network.xyo.archivist.boundwitness.stats.query'
-export const BoundWitnessStatsQuerySchema: BoundWitnessStatsQuerySchema = 'network.xyo.archivist.boundwitness.stats.query'
+export type BoundWitnessStatsQuerySchema = 'network.xyo.diviner.boundwitness.stats.query'
+export const BoundWitnessStatsQuerySchema: BoundWitnessStatsQuerySchema = 'network.xyo.diviner.boundwitness.stats.query'
 
-export type BoundWitnessStatsConfigSchema = 'network.xyo.archivist.boundwitness.stats.config'
-export const BoundWitnessStatsConfigSchema: BoundWitnessStatsConfigSchema = 'network.xyo.archivist.boundwitness.stats.config'
+export type BoundWitnessStatsConfigSchema = 'network.xyo.diviner.boundwitness.stats.config'
+export const BoundWitnessStatsConfigSchema: BoundWitnessStatsConfigSchema = 'network.xyo.diviner.boundwitness.stats.config'
 
 export type BoundWitnessStatsDivinerConfig<
   S extends string = BoundWitnessStatsConfigSchema,

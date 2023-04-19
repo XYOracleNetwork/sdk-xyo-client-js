290d9643a4088933ed05f8924aa7ed02c93475d1
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-18 20:42:57 -0500

    Remove legacy account API from SDK

--- packages/sdk/packages/api/src/Api/Api.ts
+++ packages/sdk/packages/api/src/Api/Api.ts
@@ -1,33 +1,10 @@
-import { AddressValue } from '@xyo-network/account'
 import { XyoApiConfig } from '@xyo-network/api-models'
-import { DataLike } from '@xyo-network/core'
 import { Huri } from '@xyo-network/huri'
 import { Payload } from '@xyo-network/payload-model'
 
 import { XyoApiSimple } from '../Simple'
-import { XyoUserApi } from '../User'
-import { XyoAccountApi } from './Account'
 
 export class XyoArchivistApi<C extends XyoApiConfig = XyoApiConfig> extends XyoApiSimple<Payload[], C> {
-  private _user?: XyoUserApi
-
-  get user(): XyoUserApi {
-    this._user =
-      this._user ??
-      new XyoUserApi({
-        ...this.config,
-        root: `${this.root}user/`,
-      })
-    return this._user
-  }
-
-  account(address: DataLike) {
-    return new XyoAccountApi({
-      ...this.config,
-      root: `${this.root}wallet/${new AddressValue(address).hex}/`,
-    })
-  }
-
   huri(huri: Huri | string) {
     const huriObj = typeof huri === 'string' ? new Huri(huri) : huri
     return new XyoApiSimple<Payload>({

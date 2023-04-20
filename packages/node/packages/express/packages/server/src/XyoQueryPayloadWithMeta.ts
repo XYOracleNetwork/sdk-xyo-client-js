3f55edd0ffcaf30643ea415dce6ac5ae67a6f146
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-19 21:32:49 -0500

    Remove unused type

--- packages/node/packages/express/packages/server/src/XyoQueryPayloadWithMeta.ts
+++
@@ -1,5 +0,0 @@
-import { Query } from '@xyo-network/module-model'
-import { PayloadWithMeta } from '@xyo-network/node-core-model'
-import { Payload } from '@xyo-network/payload-model'
-
-export type QueryPayloadWithMeta<T extends Payload = Payload> = PayloadWithMeta<Query<T & { _queryId?: string }>>

c645fcd3b964fb295dd5acce5b759ccf048c244f
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-20 11:12:36 -0500

    Add Autoregressive Integrated Moving Average forecasting

---
+++ packages/node/packages/core/packages/forecasting/packages/arima/src/ForecastingMethod/arima.ts
@@ -0,0 +1,16 @@
+import { ForecastingMethod } from '@xyo-network/diviner'
+import { XyoPayload } from '@xyo-network/payload-model'
+import { ARIMA } from 'arima'
+
+export const method: ForecastingMethod = (payloads: XyoPayload[]) => {
+  // Input array of 10 data points
+  const dataPoints: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+  // Create an ARIMA model with a differencing order of 1
+  const arima = new ARIMA({ d: 1, data: dataPoints, p: 1, q: 1 })
+
+  // Forecast the next value
+  const nextValue = arima.predict(1)
+
+  return []
+}

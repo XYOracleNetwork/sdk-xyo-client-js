02938d3c5715f101829356470fbd9948193c9806
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-19 22:24:24 -0500

    Start Job Queue after Module Instantiation

--- packages/node/packages/core/packages/modules-mongo/src/addMongo.ts
+++ packages/node/packages/core/packages/modules-mongo/src/addMongo.ts
@@ -1,3 +1,5 @@
+import { JobQueue } from '@xyo-network/node-core-model'
+import { TYPES } from '@xyo-network/node-core-types'
 import { Container } from 'inversify'
 
 import { addArchivistConfigModuleFactories } from './Archivist'
@@ -7,7 +9,9 @@ import { initializeDatabase } from './Mongo'
 
 export const addMongoModules = async (container: Container) => {
   await initializeDatabase()
+  container.load(JobQueueContainerModule)
   addArchivistConfigModuleFactories(container)
   addDivinerConfigModuleFactories(container)
-  container.load(JobQueueContainerModule)
+  const jobQueue = container.get<JobQueue>(TYPES.JobQueue)
+  await jobQueue.start()
 }

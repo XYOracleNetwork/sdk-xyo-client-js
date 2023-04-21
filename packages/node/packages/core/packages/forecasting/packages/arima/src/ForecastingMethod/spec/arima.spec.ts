973063dafd02201611555a014984bd320239eaff
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-20 21:04:20 -0500

    Add forecasting tests

--- packages/node/packages/core/packages/forecasting/packages/arima/src/ForecastingMethod/spec/arima.spec.ts
+++ packages/node/packages/core/packages/forecasting/packages/arima/src/ForecastingMethod/spec/arima.spec.ts
@@ -1,1 +1,18 @@
-import { forecastingMethod } from '../arimaForecasting'
+import { Payload } from '@xyo-network/payload-model'
+
+import { arimaForecasting } from '../arimaForecasting'
+
+const transformer = (payload: Payload) => {
+  return (payload as Payload<{ data: number }>)?.data || NaN
+}
+
+describe('arimaForecasting', () => {
+  it('should forecast', () => {
+    const dataPoints: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const payloads = dataPoints.map((data) => {
+      return { data, schema: 'network.xyo.test' }
+    })
+    const result = arimaForecasting(payloads, transformer)
+    expect(result).toBeTruthy()
+  })
+})

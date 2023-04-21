e071b48e68206f157e8dd4549e2c83c6d635d7cf
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-20 23:19:52 -0500

    Support single value transform per payload

--- packages/node/packages/core/packages/forecasting/packages/arima/src/arima/configurable/configurableArima.ts
+++ packages/node/packages/core/packages/forecasting/packages/arima/src/arima/configurable/configurableArima.ts
@@ -5,26 +5,24 @@ import ARIMA, { ARIMAOptions } from 'arima'
 
 export const commonOpts: ARIMAOptions = { verbose: false }
 
-export type PredictionPayload = Payload<{ data: number }>
+const schema = 'network.xyo.diviner.prediction'
+
+export type PredictionPayload = Payload<{ error?: number; value: number }>
 
 export const configurableArima = (opts: ARIMAOptions = commonOpts, predictionSteps = 10): ForecastingMethod => {
-  return (payloads: Payload[], transformers: PayloadValueTransformer[]): Payload[] => {
+  return (payloads: Payload[], transformer: PayloadValueTransformer): Payload[] => {
     // If there's no input, there's no prediction
-    if (payloads.length === 0 || transformers.length === 0) return []
+    if (payloads.length === 0) return []
     // Transform all the values
-    const values = payloads.map((payload) => transformers.map((transformer) => transformer(payload)))
-    // Convert the parallel transformed values into serial sequences to be used as training sets
-    const trainingSets = Array(transformers.length)
-      .fill(0)
-      .map((_, index) => index)
-      .map((index) => values.map((value) => value[index]))
-    // Train a model on each training set
-    const models = trainingSets.map((trainingSet) => {
-      return new ARIMA({ ...commonOpts, ...opts }).train(trainingSet)
-    })
+    const values = payloads.map(transformer)
+    // Train a model on the training set
+    const model = new ARIMA({ ...commonOpts, ...opts }).train(values)
     // Use the trained model to predict the next N values
-    const predictions = models.map((model) => model.predict(predictionSteps))
+    const predictions = model.predict(predictionSteps)
     // Convert the predictions into payloads
-    return predictions.map((prediction) => PayloadWrapper.parse({ data: prediction, schema: 'network.xyo.test' }).payload)
+    return predictions.map((prediction) => {
+      const [value, error] = prediction
+      return PayloadWrapper.parse({ error, schema, value }).payload
+    })
   }
 }

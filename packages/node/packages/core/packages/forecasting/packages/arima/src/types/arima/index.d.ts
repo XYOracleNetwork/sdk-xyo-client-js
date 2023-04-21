c661d7314220f4371fa660726d1a487627c7ccd3
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-20 21:27:12 -0500

    update ctor opts definition

--- packages/node/packages/core/packages/forecasting/packages/arima/src/types/arima/index.d.ts
+++ packages/node/packages/core/packages/forecasting/packages/arima/src/types/arima/index.d.ts
@@ -1,9 +1,29 @@
+/* eslint-disable @typescript-eslint/member-ordering */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+declare interface ARIMAOptions {
+  auto?: boolean // automatic ARIMA (default: false)
+  // params for ARIMA (default: p: 1, d: 0, q: 1)
+  p?: number
+  d?: number
+  q?: number
+  // seasonal params (default: 0s). Setting them to non-zero values makes the ARIMA model seasonal
+  P?: number
+  D?: number
+  Q?: number
+  s?: number
+  method?: number // ARIMA method (default: 0)
+  optimizer?: number // optimization method (default: 6)
+  transpose?: boolean //transpose exogenous array when fitting SARIMAX (default: false)
+  verbose?: boolean // verbose output (default: true)
+}
+
 declare module 'arima' {
+  export { ARIMAOptions }
+
   // eslint-disable-next-line import/no-default-export
   export default class ARIMA {
-    constructor(options: any)
+    constructor(options: ARIMAOptions)
     predict(steps: number): number[]
   }
 }

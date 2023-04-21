134272c9bee1250832981881232800154518461c
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-20 21:27:37 -0500

    use training set annotation over ctor injection

--- packages/node/packages/core/packages/forecasting/packages/arima/src/ForecastingMethod/arimaForecasting.ts
+++ packages/node/packages/core/packages/forecasting/packages/arima/src/ForecastingMethod/arimaForecasting.ts
@@ -1,14 +1,15 @@
 import { ForecastingMethod, PayloadValueTransformer } from '@xyo-network/diviner'
 import { Payload } from '@xyo-network/payload-model'
 import { PayloadWrapper } from '@xyo-network/payload-wrapper'
-import { ARIMA } from 'arima'
+import ARIMA, { ARIMAOptions } from 'arima'
+
+const opts: ARIMAOptions = { d: 1, p: 1, q: 1, verbose: false }
 
 export const arimaForecasting: ForecastingMethod = (payloads: Payload[], transformers: PayloadValueTransformer[]) => {
   const values = payloads.map((payload) => transformers.map((transformer) => transformer(payload)))
   if (!values.length) return []
-  const models = values.map((dataPoints) => {
-    // Create an ARIMA model with a differencing order of 1
-    return new ARIMA({ d: 1, data: dataPoints, p: 1, q: 1 })
+  const models = values.map((trainingSet) => {
+    return new ARIMA(opts).train(trainingSet)
   })
   const predictions = models.map((model) => model.predict(1))
   return predictions.map((prediction) => PayloadWrapper.parse({ data: prediction, schema: 'network.xyo.test' }))

581ffaf6dcb20d76649b0436ba90d94e6e981166
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-20 23:05:12 -0500

    Comments and test data

--- packages/node/packages/core/packages/forecasting/packages/arima/src/arima/configurable/configurableArima.ts
+++ packages/node/packages/core/packages/forecasting/packages/arima/src/arima/configurable/configurableArima.ts
@@ -5,18 +5,26 @@ import ARIMA, { ARIMAOptions } from 'arima'
 
 export const commonOpts: ARIMAOptions = { verbose: false }
 
+export type PredictionPayload = Payload<{ data: number }>
+
 export const configurableArima = (opts: ARIMAOptions = commonOpts, predictionSteps = 10): ForecastingMethod => {
   return (payloads: Payload[], transformers: PayloadValueTransformer[]): Payload[] => {
+    // If there's no input, there's no prediction
     if (payloads.length === 0 || transformers.length === 0) return []
+    // Transform all the values
     const values = payloads.map((payload) => transformers.map((transformer) => transformer(payload)))
-    const sequences = Array(transformers.length)
+    // Convert the parallel transformed values into serial sequences to be used as training sets
+    const trainingSets = Array(transformers.length)
       .fill(0)
       .map((_, index) => index)
       .map((index) => values.map((value) => value[index]))
-    const models = sequences.map((trainingSet) => {
+    // Train a model on each training set
+    const models = trainingSets.map((trainingSet) => {
       return new ARIMA({ ...commonOpts, ...opts }).train(trainingSet)
     })
+    // Use the trained model to predict the next N values
     const predictions = models.map((model) => model.predict(predictionSteps))
+    // Convert the predictions into payloads
     return predictions.map((prediction) => PayloadWrapper.parse({ data: prediction, schema: 'network.xyo.test' }).payload)
   }
 }
--- packages/node/packages/core/packages/forecasting/packages/arima/src/arima/configured/spec/arima.spec.ts
+++ packages/node/packages/core/packages/forecasting/packages/arima/src/arima/configured/spec/arima.spec.ts
@@ -2,14 +2,13 @@ import { Payload } from '@xyo-network/payload-model'
 
 import { arimaForecasting } from '../arima'
 
-const transformer = (payload: Payload) => {
-  return (payload as Payload<{ data: number }>)?.data || NaN
-}
+const transformer = (payload: Payload) => (payload as Payload<{ data: number }>)?.data || NaN
 
 describe('arimaForecasting', () => {
   it('should forecast', () => {
-    const dataPoints: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    const payloads = dataPoints.map((data) => {
+    const actual: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const expected: number[] = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    const payloads = actual.map((data) => {
       return { data, schema: 'network.xyo.test' }
     })
     const result = arimaForecasting(payloads, [transformer])

a4d61253b2f6b10868a213cd4f3215918f3ec98d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-21 09:39:32 -0500

    Abstract base MongoDB Forecasting Diviner

---
+++ packages/node/packages/core/packages/modules-mongo/src/Diviner/Forecasting/MongoDBForecastingDiviner.ts
@@ -0,0 +1,36 @@
+import { AbstractForecastingDiviner, ForecastingDivinerConfigSchema, ForecastingDivinerParams } from '@xyo-network/diviner'
+import { BoundWitnessWithMeta, JobQueue, PayloadWithMeta } from '@xyo-network/node-core-model'
+import { Payload } from '@xyo-network/payload-model'
+import { Promisable } from '@xyo-network/promise'
+import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
+import { Job, JobProvider } from '@xyo-network/shared'
+
+import { defineJobs, scheduleJobs } from '../../JobQueue'
+
+export type MongoDBForecastingDivinerParams = ForecastingDivinerParams & {
+  boundWitnessSdk: BaseMongoSdk<BoundWitnessWithMeta>
+  jobQueue: JobQueue
+  payloadSdk: BaseMongoSdk<PayloadWithMeta>
+}
+
+export abstract class MongoDBForecastingDiviner<TParams extends MongoDBForecastingDivinerParams = MongoDBForecastingDivinerParams>
+  extends AbstractForecastingDiviner<TParams>
+  implements JobProvider
+{
+  static override configSchema = ForecastingDivinerConfigSchema
+
+  abstract get jobs(): Job[]
+
+  override async start() {
+    await super.start()
+    const { jobQueue } = this.params
+    defineJobs(jobQueue, this.jobs)
+    jobQueue.once('ready', async () => await scheduleJobs(jobQueue, this.jobs))
+  }
+
+  protected override async stop(): Promise<this> {
+    return await super.stop()
+  }
+
+  protected abstract override getPayloadsInWindow(startTimestamp: number, stopTimestamp: number): Promisable<Payload[]>
+}

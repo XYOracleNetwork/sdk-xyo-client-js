733046636436fca7ec42b55959fc85bafddc6aaf
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-23 17:04:27 -0500

    Filter BWs by address and schema

--- packages/node/packages/core/packages/modules-mongo/src/Diviner/Forecasting/GasPrice/MongoDBGasPriceForecastingDiviner.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Diviner/Forecasting/GasPrice/MongoDBGasPriceForecastingDiviner.ts
@@ -1,6 +1,6 @@
 import { assertEx } from '@xylabs/assert'
 import { ArchivistWrapper } from '@xyo-network/archivist'
-import { AbstractForecastingDiviner, DivinerWrapper, ForecastingDivinerConfigSchema } from '@xyo-network/diviner'
+import { AbstractForecastingDiviner, ForecastingDivinerConfigSchema } from '@xyo-network/diviner'
 import { XyoEthereumGasSchema } from '@xyo-network/gas-price-payload-plugin'
 import { Payload } from '@xyo-network/payload-model'
 import { Job, JobProvider } from '@xyo-network/shared'
@@ -13,6 +13,14 @@ export class MongoDBGasPriceForecastingDiviner<TParams extends MongoDBForecastin
   implements JobProvider
 {
   static override configSchema = ForecastingDivinerConfigSchema
+
+  /**
+   * The max number of records to search during the batch query
+   */
+  protected readonly batchLimit = 1_000
+
+  protected readonly forecastingDataAddress = 'TODO'
+  protected readonly forecastingDataSchema = XyoEthereumGasSchema
 
   get jobs(): Job[] {
     return []
@@ -30,18 +38,41 @@ export class MongoDBGasPriceForecastingDiviner<TParams extends MongoDBForecastin
   // TODO: Start/stop ambiguity (which is first/last, recent/past)
   protected override async getPayloadsInWindow(startTimestamp: number, stopTimestamp: number): Promise<Payload[]> {
     const payload_schemas = XyoEthereumGasSchema
-    // TODO: Get in batches based on window windowSize
     // TODO: Filter by address injected into config
+    // TODO: Filter by payload_schema injected into config
     // TODO: Use bw diviner instead
-    const bws = await (await this.params.boundWitnessSdk.find({ payload_schemas, timestamp: { $gte: startTimestamp, $lte: stopTimestamp } }))
-      .sort({ timestamp: -1 })
-      .limit(this.params.config.windowSize)
-      .toArray()
-    if (bws.length === 0) return []
-    const hashes = bws.map((bw) => bw.payload_hashes[bw.payload_schemas.findIndex((s) => s === XyoEthereumGasSchema)])
-    const archivistMod = assertEx((await this.upResolver.resolve(this.params.config.archivist)).pop(), 'Unable to resolve archivist')
-    const archivist = ArchivistWrapper.wrap(archivistMod)
-    const payloads = await archivist.get(hashes)
+
+    // Set the initial skip value to 0
+    let skip = 0
+
+    // Flag to indicate whether or not there are more BWs to process
+    let more = true
+
+    const payloads: Payload[] = []
+
+    // Loop until there are no more BWs to process or we've got enough to satisfy the training window
+    while (more || payloads.length < this.config.windowSize) {
+      const addresses = this.forecastingDataAddress
+      const bws = await (
+        await this.params.boundWitnessSdk.find({ addresses, payload_schemas, timestamp: { $gte: startTimestamp, $lte: stopTimestamp } })
+      )
+        .sort({ timestamp: -1 })
+        .skip(skip)
+        .limit(this.batchLimit)
+        .toArray()
+
+      // Update the skip value for the next batch
+      skip += this.batchLimit
+
+      // Set the more flag to false if there are fewer documents returned than the batch size
+      more = bws.length === this.batchLimit
+
+      if (bws.length === 0) return []
+      const hashes = bws.map((bw) => bw.payload_hashes[bw.payload_schemas.findIndex((s) => s === this.forecastingDataSchema)])
+      const archivistMod = assertEx((await this.upResolver.resolve(this.config.archivist)).pop(), 'Unable to resolve archivist')
+      const archivist = ArchivistWrapper.wrap(archivistMod)
+      payloads.push((await archivist.get(hashes))[1])
+    }
     return payloads
   }
 

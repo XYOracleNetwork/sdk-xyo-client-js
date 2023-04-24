260e4959565d8a262d51f0c01ed260c477978ef4
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-24 15:06:32 -0500

    Remove specific Diviner obsoleted by configurable one

--- packages/node/packages/core/packages/modules-mongo/src/Diviner/Forecasting/MongoDBForecastingDiviner.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Diviner/Forecasting/MongoDBForecastingDiviner.ts
@@ -1,9 +1,12 @@
+import { assertEx } from '@xylabs/assert'
+import { exists } from '@xylabs/exists'
+import { ArchivistWrapper } from '@xyo-network/archivist-wrapper'
 import { AbstractForecastingDiviner, ForecastingDivinerConfigSchema, ForecastingDivinerParams } from '@xyo-network/diviner'
 import { BoundWitnessWithMeta, JobQueue, PayloadWithMeta } from '@xyo-network/node-core-model'
 import { Payload } from '@xyo-network/payload-model'
-import { Promisable } from '@xyo-network/promise'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 import { Job, JobProvider } from '@xyo-network/shared'
+import { Filter } from 'mongodb'
 
 import { defineJobs, scheduleJobs } from '../../JobQueue'
 
@@ -13,11 +16,16 @@ export type MongoDBForecastingDivinerParams = ForecastingDivinerParams & {
   payloadSdk: BaseMongoSdk<PayloadWithMeta>
 }
 
-export abstract class MongoDBForecastingDiviner<TParams extends MongoDBForecastingDivinerParams = MongoDBForecastingDivinerParams>
+export class MongoDBForecastingDiviner<TParams extends MongoDBForecastingDivinerParams = MongoDBForecastingDivinerParams>
   extends AbstractForecastingDiviner<TParams>
   implements JobProvider
 {
   static override configSchema = ForecastingDivinerConfigSchema
+
+  /**
+   * The max number of records to search during the batch query
+   */
+  protected readonly batchLimit = 1_000
 
   get jobs(): Job[] {
     return []
@@ -32,9 +40,46 @@ export abstract class MongoDBForecastingDiviner<TParams extends MongoDBForecasti
     }
   }
 
+  protected override async getPayloadsInWindow(startTimestamp: number, stopTimestamp: number): Promise<Payload[]> {
+    const addresses = this.config.witnessAddresses
+    const payload_schemas = this.config.witnessSchema
+    const payloads: Payload[] = []
+    const archivistMod = assertEx((await this.upResolver.resolve(this.config.archivist)).pop(), 'Unable to resolve archivist')
+    const archivist = ArchivistWrapper.wrap(archivistMod)
+    let skip = 0
+    let more = true
+
+    // Loop until there are no more BWs to process or we've got enough payloads to satisfy the training window
+    while (more || payloads.length < this.config.windowSize) {
+      const filter: Filter<BoundWitnessWithMeta> = { addresses, payload_schemas, timestamp: { $gte: startTimestamp, $lte: stopTimestamp } }
+      // TODO: Use bw diviner instead
+      const boundWitnesses = await (await this.params.boundWitnessSdk.find(filter))
+        .sort({ timestamp: -1 })
+        .skip(skip)
+        .limit(this.batchLimit)
+        .toArray()
+
+      // Update the skip value for the next batch
+      skip += this.batchLimit
+
+      // Set the more flag to false if there are fewer documents returned than the batch size
+      more = boundWitnesses.length === this.batchLimit
+
+      // Get the payloads from the BWs
+      const hashes = boundWitnesses
+        .map((bw) => bw.payload_hashes[bw.payload_schemas.findIndex((s) => s === this.config.witnessSchema)])
+        .filter(exists)
+
+      // Get the payloads corresponding to the BW hashes from the archivist
+      if (hashes.length === 0) {
+        const batchPayloads = (await archivist.get(hashes))[1]
+        payloads.push(batchPayloads)
+      }
+    }
+    return payloads
+  }
+
   protected override async stop(): Promise<this> {
     return await super.stop()
   }
-
-  protected abstract override getPayloadsInWindow(startTimestamp: number, stopTimestamp: number): Promisable<Payload[]>
 }

2d8d912ff328201c06b58b02b06744b1b280e040
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-26 11:36:52 -0500

    Add payload diviner models to package

--- packages/modules/packages/diviner/packages/models/src/index.ts
+++ packages/modules/packages/diviner/packages/models/src/index.ts
@@ -1,3 +1,4 @@
 export * from '@xyo-network/boundwitness-diviner-model'
 export * from '@xyo-network/diviner-model'
 export * from '@xyo-network/forecasting-diviner-model'
+export * from '@xyo-network/payloads-diviner-model'

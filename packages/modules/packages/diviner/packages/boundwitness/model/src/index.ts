4fd2869c340a599ad851a72c1ccdd1e5cde7d5ee
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-27 17:13:42 -0500

    Abstract BoundWitness Package

--- packages/modules/packages/diviner/packages/boundwitness/model/src/index.ts
+++ packages/modules/packages/diviner/packages/boundwitness/model/src/index.ts
@@ -1,5 +1,6 @@
 export * from './Config'
 export * from './Diviner'
+export * from './Params'
 export * from './Predicate'
 export * from './Query'
 export * from './Schema'
---
+++ packages/modules/packages/diviner/packages/boundwitness/src/index.ts
@@ -0,0 +1,3 @@
+export * from '@xyo-network/diviner-boundwitness-abstract'
+// export * from '@xyo-network/diviner-boundwitness-memory'
+export * from '@xyo-network/diviner-boundwitness-model'

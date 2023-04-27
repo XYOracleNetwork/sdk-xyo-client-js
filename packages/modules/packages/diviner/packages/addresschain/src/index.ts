b5b9afee46568c83d41f6e6e5fba2dad745d4210
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-27 09:05:11 -0500

    Update package name

--- packages/modules/packages/diviner/packages/addresschain/src/index.ts
+++ packages/modules/packages/diviner/packages/addresschain/src/index.ts
@@ -1,3 +1,3 @@
 export * from '@xyo-network/addresschain-diviner-model'
 export * from '@xyo-network/diviner-address-chain-abstract'
-export * from '@xyo-network/memory-addresschain-diviner'
+export * from '@xyo-network/diviner-address-chain-memory'

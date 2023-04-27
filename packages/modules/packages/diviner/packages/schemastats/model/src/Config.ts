06c4a54aae1433a98ce5db99b713c2317d60dc8d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-27 12:25:36 -0500

    Schema stats diviner

--- packages/modules/packages/diviner/packages/schemastats/model/src/Config.ts
+++ packages/modules/packages/diviner/packages/schemastats/model/src/Config.ts
@@ -1,13 +1,12 @@
+import { DivinerConfig } from '@xyo-network/diviner-model'
 import { Payload } from '@xyo-network/payload-model'
 
-import { PayloadStatsDivinerConfig } from '../PayloadStats'
 import { SchemaStatsDivinerSchema } from './Schema'
 
 export type SchemaStatsDivinerConfigSchema = `${SchemaStatsDivinerSchema}.config`
 export const SchemaStatsDivinerConfigSchema: SchemaStatsDivinerConfigSchema = `${SchemaStatsDivinerSchema}.config`
 
-export type SchemaStatsDivinerConfig<S extends string = SchemaStatsDivinerConfigSchema, T extends Payload = Payload> = PayloadStatsDivinerConfig<
-  S,
+export type SchemaStatsDivinerConfig<S extends string = string, T extends Payload = Payload> = DivinerConfig<
   T & {
     schema: S
   }

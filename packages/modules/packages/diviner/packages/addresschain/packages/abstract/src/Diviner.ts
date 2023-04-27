11d2d8136c4b7536c56fb371bb5d279437f4040d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-04-27 09:37:09 -0500

    Update abstract diviners from type to ABC

--- packages/modules/packages/diviner/packages/addresschain/packages/abstract/src/Diviner.ts
+++ packages/modules/packages/diviner/packages/addresschain/packages/abstract/src/Diviner.ts
@@ -1,4 +1,4 @@
 import { AbstractDiviner } from '@xyo-network/abstract-diviner'
 import { AddressChainDivinerParams } from '@xyo-network/diviner-address-chain-model'
 
-export type AddressChainDiviner<TParams extends AddressChainDivinerParams = AddressChainDivinerParams> = AbstractDiviner<TParams>
+export abstract class AddressChainDiviner<TParams extends AddressChainDivinerParams = AddressChainDivinerParams> extends AbstractDiviner<TParams> {}
--- packages/modules/packages/diviner/packages/addresshistory/packages/abstract/src/Diviner.ts
+++ packages/modules/packages/diviner/packages/addresshistory/packages/abstract/src/Diviner.ts
@@ -1,4 +1,6 @@
 import { AbstractDiviner } from '@xyo-network/abstract-diviner'
 import { AddressHistoryDivinerParams } from '@xyo-network/diviner-address-history-model'
 
-export type AddressHistoryDiviner<TParams extends AddressHistoryDivinerParams = AddressHistoryDivinerParams> = AbstractDiviner<TParams>
+export abstract class AddressHistoryDiviner<
+  TParams extends AddressHistoryDivinerParams = AddressHistoryDivinerParams,
+> extends AbstractDiviner<TParams> {}

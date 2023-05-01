954550d1811e8f27c157cb0d850bbd3bf375b22f
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-01 17:03:20 -0500

    Update new addresses in batches

--- packages/node/packages/core/packages/modules-mongo/src/Diviner/AddressSpace/MongoDBMerkleAddressSpaceDiviner.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Diviner/AddressSpace/MongoDBMerkleAddressSpaceDiviner.ts
@@ -2,8 +2,9 @@ import { exists } from '@xylabs/exists'
 import { Account } from '@xyo-network/account'
 import { AccountInstance } from '@xyo-network/account-model'
 import { AddressSchema } from '@xyo-network/address-payload-plugin'
+import { ArchivistWrapper } from '@xyo-network/archivist'
 import { MemoryAddressSpaceDiviner } from '@xyo-network/diviner-address-space-memory'
-import { AddressSpaceDivinerConfig, AddressSpaceDivinerConfigSchema, DivinerParams } from '@xyo-network/diviner-models'
+import { AddressSpaceDivinerConfig, DivinerParams } from '@xyo-network/diviner-models'
 import { AnyConfigSchema } from '@xyo-network/module-model'
 import { BoundWitnessWithMeta, PayloadRule } from '@xyo-network/node-core-model'
 import { PayloadBuilder } from '@xyo-network/payload-builder'
@@ -34,9 +35,10 @@ const moduleName = 'MongoDBSchemaStatsDiviner'
 export class MongoDBMerkleAddressSpaceDiviner<
   TParams extends MongoDBMerkleAddressSpaceDivinerParams = MongoDBMerkleAddressSpaceDivinerParams,
 > extends MemoryAddressSpaceDiviner<TParams> {
+  // TODO: Get via config or default
+  protected readonly batchSize = 10
   protected currentlyRunning = false
-  protected paginationAccount: AccountInstance = new Account()
-
+  protected readonly paginationAccount: AccountInstance = new Account()
   override divine(_payloads?: Payload[]): Promise<Payload[]> {
     void this.backgroundDivine()
     const response = new PayloadBuilder<CollectionPointerPayload>({ schema: CollectionPointerSchema })
@@ -66,13 +68,18 @@ export class MongoDBMerkleAddressSpaceDiviner<
       })
       // Ensure uniqueness on case
       const addresses = new Set<string>(result?.values?.map((address: string) => address?.toLowerCase()).filter(exists))
-      // TODO: Store in archivist
       const toStore = [...addresses].map((address) => {
         return { address, schema: AddressSchema }
       })
       // TODO: Filter addresses we've seen before
       const newAddresses = toStore
-      const archivists = await this.archivists()
+      if (newAddresses.length === 0) return
+      const mods = await this.archivists()
+      const archivists = mods.map((mod) => ArchivistWrapper.wrap(mod, this.paginationAccount))
+      for (let i = 0; i < newAddresses.length; i += this.batchSize) {
+        const batch = newAddresses.slice(i, i + this.batchSize)
+        await Promise.all(archivists.map((archivist) => archivist.insert(batch)))
+      }
     } catch (error) {
       this.logger?.error(`${moduleName}.BackgroundDivine: ${error}`)
     } finally {

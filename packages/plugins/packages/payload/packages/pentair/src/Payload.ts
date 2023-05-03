d8282b62b3d10f1e43e99e4403e87308d1b7f7ee
Arie Trouw <arie.trouw@xylabs.com>
2023-05-02 17:16:31 -0700

    fixed screenlogic witness

--- packages/plugins/packages/payload/packages/pentair/src/Payload.ts
+++ packages/plugins/packages/payload/packages/pentair/src/Payload.ts
@@ -1,10 +1,42 @@
 import { Payload } from '@xyo-network/payload-model'
+import type {
+  SLChemData,
+  SLCircuitNamesData,
+  SLControllerConfigData,
+  SLEquipmentConfigurationData,
+  SLEquipmentStateData,
+  SLGetCustomNamesData,
+  SLIntellichlorData,
+  SLPumpStatusData,
+  SLScheduleData,
+  SLSystemTimeData,
+  SLVersionData,
+  SLWeatherForecastData,
+} from 'node-screenlogic'
 
 import { XyoPentairScreenlogicSchema } from './Schema'
-import { PoolConfigJson, PoolStatus } from './screenlogic'
 
-export type XyoPentairScreenlogicPayload = Payload<{
-  config?: Partial<PoolConfigJson>
-  schema: XyoPentairScreenlogicSchema
-  status?: Partial<PoolStatus>
-}>
+export type XyoPentairScreenlogicPayload = Payload<
+  {
+    chem: SLChemData
+    chlor: SLIntellichlorData
+    equipment?: {
+      circuitNames?: SLCircuitNamesData
+      config?: SLEquipmentConfigurationData
+      controllerConfig?: SLControllerConfigData
+      customNames?: SLGetCustomNamesData
+      state?: SLEquipmentStateData
+      systemTime?: SLSystemTimeData
+      weatherForecast?: SLWeatherForecastData
+    }
+    pump?: {
+      status: SLPumpStatusData[]
+    }
+    schedule?: {
+      once?: SLScheduleData
+      recurring?: SLScheduleData
+    }
+    version?: SLVersionData
+  },
+  XyoPentairScreenlogicSchema
+>

dc97aa3bc1aec1fa10299fcb07e67e2eb97946f9
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-09 20:01:58 -0500

    Match other module folder structure

--- packages/modules/packages/archivist/packages/model/src/Queries/index.ts
+++ packages/modules/packages/archivist/packages/model/src/Queries/index.ts
@@ -5,4 +5,25 @@ export * from './Delete'
 export * from './Find'
 export * from './Get'
 export * from './Insert'
-export * from './types'
+
+import { ModuleQuery, ModuleQueryBase, Query } from '@xyo-network/module-model'
+
+import { ArchivistAllQuery } from './All'
+import { ArchivistClearQuery } from './Clear'
+import { ArchivistCommitQuery } from './Commit'
+import { ArchivistDeleteQuery } from './Delete'
+import { ArchivistFindQuery } from './Find'
+import { ArchivistGetQuery } from './Get'
+import { ArchivistInsertQuery } from './Insert'
+
+export type ArchivistQueryRoot =
+  | ArchivistAllQuery
+  | ArchivistClearQuery
+  | ArchivistCommitQuery
+  | ArchivistDeleteQuery
+  | ArchivistFindQuery
+  | ArchivistGetQuery
+  | ArchivistInsertQuery
+
+export type ArchivistQueries = ModuleQueryBase | ArchivistQueryRoot
+export type ArchivistQuery<TQuery extends Query | void = void> = ModuleQuery<TQuery extends Query ? ArchivistQueryRoot | TQuery : ArchivistQueryRoot>

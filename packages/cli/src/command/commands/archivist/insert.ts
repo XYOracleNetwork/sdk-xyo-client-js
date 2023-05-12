042252c4f2f6ef6cf036359370cf1ade34ca8c1b
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-12 07:41:23 -0500

    Update CLI to use default archivist

--- packages/cli/src/command/commands/archivist/insert.ts
+++ packages/cli/src/command/commands/archivist/insert.ts
@@ -1,18 +1,19 @@
+import { EmptyObject } from '@xyo-network/core'
 import { ArgumentsCamelCase, Argv, CommandBuilder, CommandModule } from 'yargs'
 
 import { printLine } from '../../../lib'
+import { ModuleArguments } from '../ModuleArguments'
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-type Arguments = {}
+type Arguments = ModuleArguments & {
+  payloads: string[]
+}
 
 export const aliases: ReadonlyArray<string> = []
 export const builder: CommandBuilder = (yargs: Argv) =>
-  yargs
-    .usage('Usage: $0 archivist insert <address> <payloads..>')
-    .positional('address', { demandOption: true, type: 'string' })
-    .positional('payloads', { array: true, demandOption: true, type: 'string' })
+  yargs.usage('Usage: $0 archivist insert <payloads..>').positional('payloads', { array: true, demandOption: true, type: 'string' })
 
-export const command = 'insert <address> <payloads..>'
+export const command = 'insert <payloads..>'
 export const deprecated = false
 export const describe = 'Insert payload(s) into the Archivist'
 export const handler = (argv: ArgumentsCamelCase<Arguments>) => {
@@ -20,7 +21,7 @@ export const handler = (argv: ArgumentsCamelCase<Arguments>) => {
   printLine(JSON.stringify(argv))
 }
 
-const mod: CommandModule = {
+const mod: CommandModule<EmptyObject, Arguments> = {
   aliases,
   command,
   deprecated,

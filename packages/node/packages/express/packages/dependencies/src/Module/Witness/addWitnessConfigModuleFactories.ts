0a6d6e3ca286a7344d7c2e0f480ef1aa4ab52cc1
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-16 06:36:00 -0500

    Convert witnesses to use CreatableModule

--- packages/node/packages/express/packages/dependencies/src/Module/Witness/addWitnessConfigModuleFactories.ts
+++ packages/node/packages/express/packages/dependencies/src/Module/Witness/addWitnessConfigModuleFactories.ts
@@ -1,23 +1,19 @@
 import { HDWallet } from '@xyo-network/account'
-import { AnyConfigSchema } from '@xyo-network/modules'
-import { ConfigModuleFactoryDictionary } from '@xyo-network/node-core-model'
+import { CreatableModuleDictionary, ModuleFactory } from '@xyo-network/modules'
 import { TYPES, WALLET_PATHS } from '@xyo-network/node-core-types'
-import { PrometheusNodeWitness, PrometheusNodeWitnessConfig } from '@xyo-network/prometheus-node-plugin'
+import { PrometheusNodeWitness } from '@xyo-network/prometheus-node-plugin'
 import { Container } from 'inversify'
 
 const getPrometheusNodeWitness = (container: Container) => {
   const mnemonic = container.get<string>(TYPES.AccountMnemonic)
   const account = HDWallet.fromMnemonic(mnemonic).derivePath(WALLET_PATHS.Witnesses.Prometheus)
-  const factory = (config: AnyConfigSchema<PrometheusNodeWitnessConfig>) => {
-    return PrometheusNodeWitness.create({
-      account,
-      config: { ...config, name: TYPES.PrometheusWitness.description, schema: PrometheusNodeWitness.configSchema },
-    })
-  }
-  return factory
+  return new ModuleFactory(PrometheusNodeWitness, {
+    account,
+    config: { name: TYPES.PrometheusWitness.description, schema: PrometheusNodeWitness.configSchema },
+  })
 }
 
 export const addWitnessConfigModuleFactories = (container: Container) => {
-  const dictionary = container.get<ConfigModuleFactoryDictionary>(TYPES.ConfigModuleFactoryDictionary)
+  const dictionary = container.get<CreatableModuleDictionary>(TYPES.ConfigModuleFactoryDictionary)
   dictionary[PrometheusNodeWitness.configSchema] = getPrometheusNodeWitness(container)
 }

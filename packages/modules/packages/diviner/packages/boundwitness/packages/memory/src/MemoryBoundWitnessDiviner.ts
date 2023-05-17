2c9023671c9263bce24a5a10b900d5f191876951
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-17 12:52:51 -0500

    Add Memory BW Diviner Implementation

--- packages/modules/packages/diviner/packages/boundwitness/packages/memory/src/MemoryBoundWitnessDiviner.ts
+++ packages/modules/packages/diviner/packages/boundwitness/packages/memory/src/MemoryBoundWitnessDiviner.ts
@@ -1,19 +1,25 @@
 import { assertEx } from '@xylabs/assert'
-import { exists } from '@xylabs/exists'
-import { BoundWitness } from '@xyo-network/boundwitness-model'
+import { ArchivistWrapper } from '@xyo-network/archivist-wrapper'
+import { isBoundWitness } from '@xyo-network/boundwitness-model'
 import { BoundWitnessDiviner } from '@xyo-network/diviner-boundwitness-abstract'
 import {
   BoundWitnessDivinerConfigSchema,
   BoundWitnessDivinerParams,
-  BoundWitnessDivinerQueryPayload,
-  BoundWitnessDivinerQuerySchema,
+  isBoundWitnessDivinerQueryPayload,
 } from '@xyo-network/diviner-boundwitness-model'
 import { Payload } from '@xyo-network/payload-model'
 
 export class MemoryBoundWitnessDiviner<TParams extends BoundWitnessDivinerParams = BoundWitnessDivinerParams> extends BoundWitnessDiviner<TParams> {
   static override configSchema = BoundWitnessDivinerConfigSchema
 
-  override divine(payloads?: Payload[]): Promise<Payload[]> {
+  override async divine(payloads?: Payload[]): Promise<Payload[]> {
+    const filter = assertEx(payloads?.filter(isBoundWitnessDivinerQueryPayload)?.pop(), 'Missing query payload')
+    if (!filter) return []
+    const archivistMod = assertEx((await this.upResolver.resolve(this.config.archivist)).pop(), 'Unable to resolve archivist')
+    const archivist = ArchivistWrapper.wrap(archivistMod)
+    const all = await archivist.all()
+    const bw = all.filter(isBoundWitness)
+    // TODO: Filter by query payload
     throw new Error('Method not implemented.')
   }
 }

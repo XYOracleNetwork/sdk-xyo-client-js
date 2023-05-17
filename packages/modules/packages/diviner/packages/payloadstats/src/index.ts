1dbd3c66c4f791b599c2b3402208b67a7d7a9cdc
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-17 16:37:03 -0500

    Export entire diviner in barrel package

--- packages/modules/packages/diviner/packages/payloadstats/src/index.ts
+++ packages/modules/packages/diviner/packages/payloadstats/src/index.ts
@@ -1,3 +1,3 @@
 export * from '@xyo-network/diviner-payload-stats-abstract'
-// export * from '@xyo-network/diviner-payload-stats-memory'
+export * from '@xyo-network/diviner-payload-stats-memory'
 export * from '@xyo-network/diviner-payload-stats-model'

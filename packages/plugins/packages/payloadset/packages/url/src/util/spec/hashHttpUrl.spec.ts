dffb0aa0bcec3cdafb74c6883a14a2f9433c6d3f
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-17 07:05:59 -0500

    Tests for http/https URLs

--- packages/plugins/packages/payloadset/packages/url/src/util/spec/hashHttpUrl.spec.ts
+++ packages/plugins/packages/payloadset/packages/url/src/util/spec/hashHttpUrl.spec.ts
@@ -2,23 +2,23 @@ import { hashHttpUrl } from '../hashHttpUrl'
 
 describe('hashHttpUrl', () => {
   describe('with existing URL', () => {
-    const uri = 'xyo.network/favicon.ico'
     it('hashes http URLs', async () => {
-      const expected = '7f5007068d2b56ea9735e2490d60cff2e72cae312024ac1f6c91158eba47d05d'
-      const url = `http://${uri}`
+      // TODO: Replace with a more static http URL
+      const expected = '6da5620880159634213e197fafca1dde0272153be3e4590818533fab8d040770'
+      const url = 'http://www.google.com/favicon.ico'
       const actual = await hashHttpUrl(url)
       expect(actual).toEqual(expected)
     })
     it('hashes https URLs', async () => {
       const expected = '5b6fb901d981dd1da84844069c3eb5aaf725a4f23e40613751eecd5ca0bc4060'
-      const url = `https://${uri}`
+      const url = 'https://xyo.network/favicon.ico'
       const actual = await hashHttpUrl(url)
       expect(actual).toEqual(expected)
     })
   })
   describe('with non-existent URL', () => {
     it('throws', () => {
-      expect(() => hashHttpUrl('missing.txt')).rejects.toThrow()
+      expect(() => hashHttpUrl('https://foo.bar.baz.quix/missing.txt')).rejects.toThrow()
     })
   })
 })

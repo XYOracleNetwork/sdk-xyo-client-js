e9a2c2edf196dfbd9de45ad524d2dd34d275b284
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-17 14:36:46 -0500

    Remove methods moved to js-sdk

--- packages/protocol/packages/core/src/lib/Address/concat.ts
+++
@@ -1,7 +0,0 @@
-import { exists } from '@xylabs/exists'
-
-import { normalizeAddress } from './normalize'
-
-export const concat = (a?: string | string[], b?: string | string[]): string[] => {
-  return ([] as (string | undefined)[]).concat(a).concat(b).filter(exists).map(normalizeAddress)
-}

0ec6b56c9a5ff0cbc8520485864eb150e154ff17
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-17 15:41:18 -0500

    Notes on deprecated filter prop

--- packages/modules/packages/diviner/packages/boundwitness/packages/memory/src/MemoryBoundWitnessDiviner.ts
+++ packages/modules/packages/diviner/packages/boundwitness/packages/memory/src/MemoryBoundWitnessDiviner.ts
@@ -1,4 +1,4 @@
-import { containsAll, flatten } from '@xylabs/array'
+import { containsAll } from '@xylabs/array'
 import { assertEx } from '@xylabs/assert'
 import { ArchivistWrapper } from '@xyo-network/archivist-wrapper'
 import { isBoundWitness } from '@xyo-network/boundwitness-model'
@@ -19,11 +19,11 @@ export class MemoryBoundWitnessDiviner<TParams extends BoundWitnessDivinerParams
     if (!filter) return []
     const archivistMod = assertEx((await this.upResolver.resolve(this.config.archivist)).pop(), 'Unable to resolve archivist')
     const archivist = ArchivistWrapper.wrap(archivistMod)
-    const { address, addresses, payload_hashes, payload_schemas, limit, offset, order } = filter
+    const { addresses, payload_hashes, payload_schemas, limit, offset, order } = filter
     const all = await archivist.all()
     let bws = all.filter(isBoundWitness)
     if (order === 'desc') bws = bws.reverse()
-    const allAddresses = flatten(address, addresses).map(normalizeAddress)
+    const allAddresses = addresses?.map(normalizeAddress)
     if (allAddresses?.length) bws = bws.filter((bw) => containsAll(bw.addresses, allAddresses))
     if (payload_hashes?.length) bws = bws.filter((bw) => containsAll(bw.payload_hashes, payload_hashes))
     if (payload_schemas?.length) bws = bws.filter((bw) => containsAll(bw.payload_schemas, payload_schemas))

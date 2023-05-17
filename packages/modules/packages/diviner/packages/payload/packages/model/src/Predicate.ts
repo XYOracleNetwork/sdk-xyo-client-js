59065950e4f38231033c1050ac112f8068cbc627
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-17 15:30:15 -0500

    Notes in deprecated query filter

--- packages/modules/packages/diviner/packages/payload/packages/model/src/Predicate.ts
+++ packages/modules/packages/diviner/packages/payload/packages/model/src/Predicate.ts
@@ -5,6 +5,10 @@ import { SortDirection } from './SortDirection'
 /* Note: Added Omit to PayloadFindDiviner for offset until we support hash based offsets */
 
 export type PayloadDivinerPredicate<T extends AnyObject = AnyObject> = Partial<{
+  /**
+   * @deprecated Use BW Diviner to find signed Payloads matching desired
+   * criteria, then get Payloads by hash directly from Archivist
+   */
   address: string | string[]
   hash: string
   limit: number

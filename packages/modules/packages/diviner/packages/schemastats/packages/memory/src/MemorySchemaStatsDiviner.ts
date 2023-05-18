65ee6bb0d40384a7674d719632d27f047befb8d7
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-18 01:22:42 -0500

    Memory Schema Stats Diviner

---
+++ packages/modules/packages/diviner/packages/schemastats/packages/memory/src/MemorySchemaStatsDiviner.ts
@@ -0,0 +1,49 @@
+import { assertEx } from '@xylabs/assert'
+import { ArchivistWrapper } from '@xyo-network/archivist-wrapper'
+import { isBoundWitness } from '@xyo-network/boundwitness-model'
+import { SchemaStatsDiviner } from '@xyo-network/diviner-schema-stats-abstract'
+import {
+  isSchemaStatsQueryPayload,
+  SchemaStatsDivinerConfigSchema,
+  SchemaStatsDivinerParams,
+  SchemaStatsDivinerSchema,
+  SchemaStatsPayload,
+  SchemaStatsQueryPayload,
+} from '@xyo-network/diviner-schema-stats-model'
+import { PayloadBuilder } from '@xyo-network/payload-builder'
+import { Payload } from '@xyo-network/payload-model'
+
+export class MemorySchemaStatsDiviner<TParams extends SchemaStatsDivinerParams = SchemaStatsDivinerParams> extends SchemaStatsDiviner<TParams> {
+  static override configSchema = SchemaStatsDivinerConfigSchema
+
+  override async divine(payloads?: Payload[]): Promise<Payload[]> {
+    const query = payloads?.find<SchemaStatsQueryPayload>(isSchemaStatsQueryPayload)
+    if (!query) return []
+    const addresses = query?.address ? (Array.isArray(query?.address) ? query.address : [query.address]) : undefined
+    const counts = addresses ? await Promise.all(addresses.map((address) => this.divineAddress(address))) : [await this.divineAllAddresses()]
+    return counts.map((count) => new PayloadBuilder<SchemaStatsPayload>({ schema: SchemaStatsDivinerSchema }).fields({ count }).build())
+  }
+
+  protected async divineAddress(address: string): Promise<Record<string, number>> {
+    const archivistMod = assertEx((await this.upResolver.resolve(this.config.archivist)).pop(), 'Unable to resolve archivist')
+    const archivist = ArchivistWrapper.wrap(archivistMod)
+    const all = await archivist.all()
+    const filtered = all.filter(isBoundWitness).filter((bw) => bw.addresses.includes(address))
+    const counts: Record<string, number> = filtered.reduce((acc, payload) => {
+      acc[payload.schema] = acc[payload.schema] ? acc[payload.schema] + 1 : 1
+      return acc
+    }, {} as Record<string, number>)
+    return counts
+  }
+
+  protected async divineAllAddresses(): Promise<Record<string, number>> {
+    const archivistMod = assertEx((await this.upResolver.resolve(this.config.archivist)).pop(), 'Unable to resolve archivist')
+    const archivist = ArchivistWrapper.wrap(archivistMod)
+    const all = await archivist.all()
+    const counts: Record<string, number> = all.reduce((acc, payload) => {
+      acc[payload.schema] = acc[payload.schema] ? acc[payload.schema] + 1 : 1
+      return acc
+    }, {} as Record<string, number>)
+    return counts
+  }
+}
--- packages/modules/packages/diviner/packages/schemastats/packages/model/src/Config.ts
+++ packages/modules/packages/diviner/packages/schemastats/packages/model/src/Config.ts
@@ -1,4 +1,5 @@
 import { DivinerConfig } from '@xyo-network/diviner-model'
+import { ModuleFilter } from '@xyo-network/module-model'
 import { Payload } from '@xyo-network/payload-model'
 
 import { SchemaStatsDivinerSchema } from './Schema'
@@ -8,6 +9,7 @@ export const SchemaStatsDivinerConfigSchema: SchemaStatsDivinerConfigSchema = `$
 
 export type SchemaStatsDivinerConfig<T extends Payload = Payload> = DivinerConfig<
   T & {
+    archivist?: ModuleFilter
     schema: SchemaStatsDivinerConfigSchema
   }
 >

630f83054cc3fe4837e9257e14767972eeb0b8b6
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-19 08:54:14 -0500

    Add tests for address history diviner

--- packages/modules/packages/diviner/packages/addresshistory/src/spec/Diviner.spec.ts
+++ packages/modules/packages/diviner/packages/addresshistory/src/spec/Diviner.spec.ts
@@ -1,6 +1,6 @@
 import { Account } from '@xyo-network/account'
 import { ArchivistWrapper, MemoryArchivist } from '@xyo-network/archivist'
-import { AddressHistoryDivinerConfigSchema } from '@xyo-network/diviner-address-history-model'
+import { AddressHistoryDivinerConfigSchema, AddressHistoryQuerySchema } from '@xyo-network/diviner-address-history-model'
 import { DivinerWrapper } from '@xyo-network/diviner-wrapper'
 import { MemoryNode } from '@xyo-network/node'
 import { PayloadWrapper } from '@xyo-network/payload-wrapper'
@@ -9,36 +9,72 @@ import { AddressHistoryDiviner } from '../Diviner'
 
 describe('AddressHistoryDiviner', () => {
   describe('divine', () => {
-    it('returns divined result', async () => {
-      const node = await MemoryNode.create()
-      const account = Account.random()
-      const archivist = ArchivistWrapper.wrap(
-        await MemoryArchivist.create({ config: { schema: MemoryArchivist.configSchema, storeQueries: true } }),
-        account,
-      )
+    describe('with query payload', () => {
+      it('returns divined result for queried addresses', async () => {
+        const node = await MemoryNode.create()
+        const account = Account.random()
+        const archivist = ArchivistWrapper.wrap(
+          await MemoryArchivist.create({ config: { schema: MemoryArchivist.configSchema, storeQueries: true } }),
+          account,
+        )
 
-      const payload1 = PayloadWrapper.parse({ index: 1, schema: 'network.xyo.test' })
-      const payload2 = PayloadWrapper.parse({ index: 2, schema: 'network.xyo.test' })
-      const payload3 = PayloadWrapper.parse({ index: 3, schema: 'network.xyo.test' })
+        const payload1 = PayloadWrapper.parse({ index: 1, schema: 'network.xyo.test' })
+        const payload2 = PayloadWrapper.parse({ index: 2, schema: 'network.xyo.test' })
+        const payload3 = PayloadWrapper.parse({ index: 3, schema: 'network.xyo.test' })
 
-      await archivist.insert([payload1.payload])
-      await archivist.insert([payload2.payload])
-      await archivist.insert([payload3.payload])
+        await archivist.insert([payload1.payload])
+        await archivist.insert([payload2.payload])
+        await archivist.insert([payload3.payload])
 
-      const all = await archivist.all()
+        const all = await archivist.all()
 
-      expect(all).toBeArrayOfSize(7)
+        expect(all).toBeArrayOfSize(7)
 
-      await node.register(archivist)
-      await node.attach(archivist.address)
-      const diviner = await AddressHistoryDiviner.create({
-        config: { address: account.addressValue.hex, schema: AddressHistoryDivinerConfigSchema },
+        await node.register(archivist)
+        await node.attach(archivist.address)
+        const diviner = await AddressHistoryDiviner.create({
+          config: { address: account.addressValue.hex, schema: AddressHistoryDivinerConfigSchema },
+        })
+        await node.register(diviner)
+        await node.attach(diviner.address)
+        const divinerWrapper = DivinerWrapper.wrap(diviner)
+        const query = { address: account.addressValue.hex, schema: AddressHistoryQuerySchema }
+        const result = await divinerWrapper.divine([query])
+        expect(result.length).toBe(1)
       })
-      await node.register(diviner)
-      await node.attach(diviner.address)
-      const divinerWrapper = DivinerWrapper.wrap(diviner)
-      const result = await divinerWrapper.divine()
-      expect(result.length).toBe(1)
+    })
+    describe('with no query payloads', () => {
+      it('returns divined result for all addresses', async () => {
+        const node = await MemoryNode.create()
+        const account = Account.random()
+        const archivist = ArchivistWrapper.wrap(
+          await MemoryArchivist.create({ config: { schema: MemoryArchivist.configSchema, storeQueries: true } }),
+          account,
+        )
+
+        const payload1 = PayloadWrapper.parse({ index: 1, schema: 'network.xyo.test' })
+        const payload2 = PayloadWrapper.parse({ index: 2, schema: 'network.xyo.test' })
+        const payload3 = PayloadWrapper.parse({ index: 3, schema: 'network.xyo.test' })
+
+        await archivist.insert([payload1.payload])
+        await archivist.insert([payload2.payload])
+        await archivist.insert([payload3.payload])
+
+        const all = await archivist.all()
+
+        expect(all).toBeArrayOfSize(7)
+
+        await node.register(archivist)
+        await node.attach(archivist.address)
+        const diviner = await AddressHistoryDiviner.create({
+          config: { address: account.addressValue.hex, schema: AddressHistoryDivinerConfigSchema },
+        })
+        await node.register(diviner)
+        await node.attach(diviner.address)
+        const divinerWrapper = DivinerWrapper.wrap(diviner)
+        const result = await divinerWrapper.divine()
+        expect(result.length).toBe(1)
+      })
     })
   })
 })

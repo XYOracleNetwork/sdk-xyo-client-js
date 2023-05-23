b0ec9ac3546a2f13e43a74fbca54dd4e941c080b
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-23 13:52:34 -0500

    Make test BW building async

--- packages/node/packages/express/packages/test/src/testUtil/Block/getNewBlock.ts
+++ packages/node/packages/express/packages/test/src/testUtil/Block/getNewBlock.ts
@@ -8,29 +8,40 @@ import { PayloadWrapper } from '@xyo-network/payload-wrapper'
 import { unitTestSigningAccount } from '../Account'
 import { getNewPayloads, knownPayload } from '../Payload'
 
-export const knownBlock = new BoundWitnessBuilder({ inlinePayloads: true })
-  .witness(unitTestSigningAccount)
-  .payload(knownPayload)
-  .build()[0] as BoundWitness & PayloadWithPartialMeta
+// export const knownBlock = new BoundWitnessBuilder({ inlinePayloads: true })
+//   .witness(unitTestSigningAccount)
+//   .payload(knownPayload)
+//   .build()[0] as BoundWitness & PayloadWithPartialMeta
 
-export const knownBlockHash = assertEx(PayloadWrapper.hash(knownBlock))
+// export const knownBlockHash = assertEx(PayloadWrapper.hash(knownBlock))
 
-export const getNewBlock = (...payloads: Payload[]): BoundWitnessWithPartialMeta & PayloadWithPartialMeta => {
-  return new BoundWitnessBuilder({ inlinePayloads: true }).witness(unitTestSigningAccount).payloads(payloads).build()[0]
+export const getNewBlock = async (...payloads: Payload[]): Promise<BoundWitnessWithPartialMeta & PayloadWithPartialMeta> => {
+  return (await new BoundWitnessBuilder({ inlinePayloads: true }).witness(unitTestSigningAccount).payloads(payloads).build())[0]
 }
 
 export const getNewBlockWithPayloads = (numPayloads = 1) => {
   return getNewBlock(...getNewPayloads(numPayloads))
 }
 
-export const getNewBlocks = (numBoundWitnesses = 1): Array<BoundWitnessWithPartialMeta & PayloadWithPartialMeta> => {
-  return new Array(numBoundWitnesses).fill(0).map(() => {
-    return new BoundWitnessBuilder({ inlinePayloads: true }).witness(unitTestSigningAccount).build()[0]
-  })
+export const getNewBlocks = async (numBoundWitnesses = 1): Promise<Array<BoundWitnessWithPartialMeta & PayloadWithPartialMeta>> => {
+  const sequence = new Array(numBoundWitnesses).fill(0)
+  const values = await Promise.all(
+    sequence.map(async () => {
+      return (await new BoundWitnessBuilder({ inlinePayloads: true }).witness(unitTestSigningAccount).build())[0]
+    }),
+  )
+  return values
 }
 
-export const getNewBlocksWithPayloads = (numBoundWitnesses = 1, numPayloads = 1): Array<BoundWitnessWithPartialMeta & PayloadWithPartialMeta> => {
-  return new Array(numBoundWitnesses).fill(0).map(() => {
-    return new BoundWitnessBuilder({ inlinePayloads: true }).witness(unitTestSigningAccount).payloads(getNewPayloads(numPayloads)).build()[0]
-  })
+export const getNewBlocksWithPayloads = async (
+  numBoundWitnesses = 1,
+  numPayloads = 1,
+): Promise<Array<BoundWitnessWithPartialMeta & PayloadWithPartialMeta>> => {
+  return await Promise.all(
+    new Array(numBoundWitnesses).fill(0).map(async () => {
+      return (
+        await new BoundWitnessBuilder({ inlinePayloads: true }).witness(unitTestSigningAccount).payloads(getNewPayloads(numPayloads)).build()
+      )[0]
+    }),
+  )
 }

bde47451f7745428d1eb45b04bb554204a04679c
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-24 14:10:57 -0500

    Minor cleanup

--- packages/protocol/packages/account/src/Key/WASMPrivateKey.ts
+++ packages/protocol/packages/account/src/Key/WASMPrivateKey.ts
@@ -8,17 +8,9 @@ import { PrivateKey } from './PrivateKey'
 // @staticImplements<PrivateKeyStatic>()
 export class WASMPrivateKey extends PrivateKey {
   override async sign(hash: DataLike) {
-    const privateKey = this._keyPair.getPrivate('hex')
-    const privateKeyBytes = toUint8Array(privateKey)
+    const privateKeyBytes = toUint8Array(this._keyPair.getPrivate('hex'))
+    const { malleateSignatureCompact, signMessageHashCompact } = await instantiateSecp256k1()
     const arrayHash = toUint8Array(hash)
-    const secp256k1 = await instantiateSecp256k1()
-    const a = secp256k1.signMessageHashDER(privateKeyBytes, arrayHash)
-    const b = secp256k1.signMessageHashCompact(privateKeyBytes, arrayHash)
-    const c = secp256k1.signMessageHashRecoverableCompact(privateKeyBytes, arrayHash)
-    // const d = secp256k1.signMessageHashSchnorr(privateKeyBytes, arrayHash)
-    const aPrime = secp256k1.malleateSignatureDER(a)
-    const bPrime = secp256k1.malleateSignatureCompact(b)
-    return bPrime
-    // return toUint8Array(signature.r.toString('hex', 64) + signature.s.toString('hex', 64))
+    return malleateSignatureCompact(signMessageHashCompact(privateKeyBytes, arrayHash))
   }
 }

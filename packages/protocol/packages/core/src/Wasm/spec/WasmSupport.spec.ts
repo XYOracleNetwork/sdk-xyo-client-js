125ae60fce33da93b535d03ffb16d8465d47aa63
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-25 13:22:58 -0500

    Tests for WASM Support Helper Class

---
+++ packages/protocol/packages/core/src/Wasm/spec/WasmSupport.spec.ts
@@ -0,0 +1,42 @@
+import { WasmFeature, WasmSupport } from '../WasmSupport'
+
+describe('WasmSupport', () => {
+  const allFeatures: WasmFeature[] = [
+    'bigInt',
+    'bulkMemory',
+    'exceptions',
+    'extendedConst',
+    'gc',
+    'memory64',
+    'multiValue',
+    'mutableGlobals',
+    'referenceTypes',
+    'relaxedSimd',
+    'saturatedFloatToInt',
+    'signExtensions',
+    'simd',
+    'streamingCompilation',
+    'tailCall',
+    'threads',
+  ]
+  const generallySupportedFeatures: WasmFeature[] = ['simd', 'bigInt']
+  describe('initialize', () => {
+    it('initializes the instance', async () => {
+      const instance = await WasmSupport.create(generallySupportedFeatures)
+      expect(instance.isInitialized).toBe(true)
+    })
+  })
+  describe('isDesiredFeatureSetSupported', () => {
+    it('Returns true if desired features are supported', async () => {
+      const instance = await WasmSupport.create(generallySupportedFeatures)
+      expect(instance.isDesiredFeatureSetSupported).toBe(true)
+    })
+  })
+  describe('featureSupport', () => {
+    it('Lists desired features and their support status', async () => {
+      const instance = await WasmSupport.create(allFeatures)
+      expect(Object.keys(instance.featureSupport)).toIncludeAllMembers(allFeatures)
+      Object.values(instance.featureSupport).map((feature) => expect(feature).toBeBoolean())
+    })
+  })
+})

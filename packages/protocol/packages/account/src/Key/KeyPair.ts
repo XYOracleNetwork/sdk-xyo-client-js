6fb66abb390483563b70951fffe7efd7540e063c
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-25 14:53:16 -0500

    Check for WASM support when instantiating key

--- packages/protocol/packages/account/src/Key/KeyPair.ts
+++ packages/protocol/packages/account/src/Key/KeyPair.ts
@@ -1,14 +1,18 @@
+import { distinct } from '@xylabs/array'
 import { staticImplements } from '@xylabs/static-implements'
-import { DataLike } from '@xyo-network/core'
+import { DataLike, WasmSupport } from '@xyo-network/core'
 import { KeyPairInstance, KeyPairStatic, PrivateKeyInstance, PublicKeyInstance } from '@xyo-network/key-model'
 
 import { PrivateKey } from './PrivateKey'
 import { WASMPrivateKey } from './WASMPrivateKey'
+import { WASMPublicKey } from './WASMPublicKey'
+
+const wasmSupportStatic = new WasmSupport([...WASMPrivateKey.wasmFeatures, ...WASMPublicKey.wasmFeatures].filter(distinct))
 
 @staticImplements<KeyPairStatic>()
 export class KeyPair implements KeyPairInstance {
-  static allowWasm = true
-  static wasmSupported = true
+  static readonly wasmInitialized = wasmSupportStatic.initialize()
+  static readonly wasmSupport = wasmSupportStatic
 
   private _isXyoKeyPair = true
   private _private?: PrivateKeyInstance
@@ -18,7 +22,7 @@ export class KeyPair implements KeyPairInstance {
   }
 
   private static get PrivateKeyKind() {
-    return KeyPair.allowWasm && KeyPair.wasmSupported ? WASMPrivateKey : PrivateKey
+    return KeyPair.wasmSupport.canUseWasm ? WASMPrivateKey : PrivateKey
   }
 
   get private(): PrivateKeyInstance {

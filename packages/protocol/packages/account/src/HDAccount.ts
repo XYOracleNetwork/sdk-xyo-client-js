3c4e56ace389adb2cdce45953221e21bfa1154a9
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-25 15:35:53 -0500

    Add signing lock to ensure previous hash updates

--- packages/protocol/packages/account/src/HDAccount.ts
+++ packages/protocol/packages/account/src/HDAccount.ts
@@ -2,11 +2,13 @@ import { HDNode } from '@ethersproject/hdnode'
 import { assertEx } from '@xylabs/assert'
 import { AccountInstance, AccountOptions } from '@xyo-network/account-model'
 import { DataLike, toUint8Array, XyoData } from '@xyo-network/core'
+import { Lock } from 'semaphore-async-await'
 
 import { KeyPair } from './Key'
 
 export class HDAccount extends KeyPair implements AccountInstance {
   private _previousHash?: XyoData
+  private readonly _signingLock = new Lock()
 
   constructor(protected readonly node: HDNode, opts?: AccountOptions) {
     const privateKeyToUse: DataLike = toUint8Array(node.privateKey.replace('0x', ''))
@@ -23,9 +25,15 @@ export class HDAccount extends KeyPair implements AccountInstance {
     return this._previousHash
   }
 
-  sign(hash: Uint8Array | string) {
-    this._previousHash = new XyoData(32, hash)
-    return this.private.sign(hash)
+  async sign(hash: Uint8Array | string): Promise<Uint8Array> {
+    await this._signingLock.acquire()
+    try {
+      const signature = this.private.sign(hash)
+      this._previousHash = new XyoData(32, hash)
+      return signature
+    } finally {
+      this._signingLock.release()
+    }
   }
 
   verify(msg: Uint8Array | string, signature: Uint8Array | string) {

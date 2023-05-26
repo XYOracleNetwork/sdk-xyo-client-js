8690c170620a8c65e5a679e9119b508ca2e3d9fb
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-26 11:34:44 -0500

    Implement Mongo-backed hash store

--- packages/node/packages/core/packages/modules-mongo/src/PreviousHashStore/MongoDBPreviousHashStore.ts
+++ packages/node/packages/core/packages/modules-mongo/src/PreviousHashStore/MongoDBPreviousHashStore.ts
@@ -1,13 +1,27 @@
 import { PreviousHashStore } from '@xyo-network/core'
+import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
+
+export type AddressPreviousHash = { address: string; previousHash: string }
 
 export class MongoDBPreviousHashStore implements PreviousHashStore {
-  getItem(key: string): string | null | Promise<string | null> {
-    throw new Error('Method not implemented.')
+  keyPrefix?: string
+
+  constructor(protected readonly payloadSdk: BaseMongoSdk<AddressPreviousHash>) {}
+
+  async getItem(key: string): Promise<string | null | Promise<string | null>> {
+    const address = this.getKeyName(key)
+    const value = await this.payloadSdk.findOne({ address })
+    return value?.previousHash ?? null
   }
-  removeItem(key: string): void | Promise<void> {
-    throw new Error('Method not implemented.')
+  async removeItem(key: string): Promise<void | Promise<void>> {
+    const address = this.getKeyName(key)
+    await this.payloadSdk.useMongo((db) => db.db('foo').collection('bar').deleteOne({ address }))
   }
-  setItem(key: string, value: string): void | Promise<void> {
-    throw new Error('Method not implemented.')
+  async setItem(key: string, previousHash: string): Promise<void | Promise<void>> {
+    const address = this.getKeyName(key)
+    await this.payloadSdk.insertOne({ address, previousHash })
+  }
+  protected getKeyName(key: string) {
+    return this.keyPrefix ? `${this.keyPrefix}${key}` : key
   }
 }

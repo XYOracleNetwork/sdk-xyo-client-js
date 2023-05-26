52feaded313ad50f1128fbd21203b76227a0c181
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-26 13:04:45 -0500

    Expose address info collection sdk helper

--- packages/node/packages/core/packages/modules-mongo/src/Mongo/getSdk.ts
+++ packages/node/packages/core/packages/modules-mongo/src/Mongo/getSdk.ts
@@ -2,8 +2,11 @@ import { BoundWitnessWithMeta, PayloadWithMeta } from '@xyo-network/node-core-mo
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 
 import { COLLECTIONS } from '../collections'
+import { AddressInfo } from './AddressInfo'
 import { getBaseMongoSdk } from './getBaseMongoSdk'
 
 export const getBoundWitnessSdk = (): BaseMongoSdk<BoundWitnessWithMeta> => getBaseMongoSdk<BoundWitnessWithMeta>(COLLECTIONS.BoundWitnesses)
 
 export const getPayloadSdk = (): BaseMongoSdk<PayloadWithMeta> => getBaseMongoSdk<PayloadWithMeta>(COLLECTIONS.Payloads)
+
+export const getAddressSdk = (): BaseMongoSdk<AddressInfo> => getBaseMongoSdk<AddressInfo>(COLLECTIONS.Address)

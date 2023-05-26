db10873d65f09c0cbcaae234cb05683185050541
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-26 11:34:17 -0500

    Add optional key prefix to interface

--- packages/protocol/packages/account/src/PreviousHashStore.ts
+++ packages/protocol/packages/account/src/PreviousHashStore.ts
@@ -1,4 +1,5 @@
 export interface PreviousHashStore {
+  keyPrefix?: string
   getItem(key: string): string | null | Promise<string | null>
   removeItem(key: string): void | Promise<void>
   setItem(key: string, value: string): void | Promise<void>

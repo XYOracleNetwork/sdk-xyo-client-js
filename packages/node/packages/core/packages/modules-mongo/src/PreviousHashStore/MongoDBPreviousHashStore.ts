8daa0a0d01af689e03920a1d034b79a124a6c461
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-26 12:55:52 -0500

    Update return types

--- packages/node/packages/core/packages/modules-mongo/src/PreviousHashStore/MongoDBPreviousHashStore.ts
+++ packages/node/packages/core/packages/modules-mongo/src/PreviousHashStore/MongoDBPreviousHashStore.ts
@@ -1,4 +1,4 @@
-import { PreviousHashStore } from '@xyo-network/core'
+import { PreviousHashStore } from '@xyo-network/account'
 import { BaseMongoSdk } from '@xyo-network/sdk-xyo-mongo-js'
 
 export type AddressPreviousHash = { address: string; previousHash: string }
@@ -8,16 +8,16 @@ export class MongoDBPreviousHashStore implements PreviousHashStore {
 
   constructor(protected readonly payloadSdk: BaseMongoSdk<AddressPreviousHash>) {}
 
-  async getItem(key: string): Promise<string | null | Promise<string | null>> {
+  async getItem(key: string): Promise<string | null> {
     const address = this.getKeyName(key)
     const value = await this.payloadSdk.findOne({ address })
     return value?.previousHash ?? null
   }
-  async removeItem(key: string): Promise<void | Promise<void>> {
+  async removeItem(key: string): Promise<void> {
     const address = this.getKeyName(key)
     await this.payloadSdk.useMongo((db) => db.db('foo').collection('bar').deleteOne({ address }))
   }
-  async setItem(key: string, previousHash: string): Promise<void | Promise<void>> {
+  async setItem(key: string, previousHash: string): Promise<void> {
     const address = this.getKeyName(key)
     await this.payloadSdk.insertOne({ address, previousHash })
   }

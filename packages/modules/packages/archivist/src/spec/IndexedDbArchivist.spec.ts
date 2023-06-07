f06de9d3a0d3feaf2ecb6be102254ffb7710db2d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-07 16:44:12 -0400

    Round Trip/All tests for IndexedDB Archivist

--- packages/modules/packages/archivist/src/spec/IndexedDbArchivist.spec.ts
+++ packages/modules/packages/archivist/src/spec/IndexedDbArchivist.spec.ts
@@ -7,7 +7,9 @@ import { indexedDB } from 'fake-indexeddb'
 import { IndexedDbArchivist, IndexedDbArchivistConfigSchema } from '../IndexedDbArchivist'
 import { testArchivistAll, testArchivistRoundTrip } from './testArchivist'
 
+const name = 'IndexedDB'
+
 window.indexedDB = indexedDB
 
-testArchivistRoundTrip(IndexedDbArchivist.create({ config: { namespace: 'test', schema: IndexedDbArchivistConfigSchema } }), 'cookie')
-testArchivistAll(IndexedDbArchivist.create({ config: { namespace: 'test', schema: IndexedDbArchivistConfigSchema } }), 'cookie')
+testArchivistRoundTrip(IndexedDbArchivist.create({ config: { namespace: 'test', schema: IndexedDbArchivistConfigSchema } }), name)
+testArchivistAll(IndexedDbArchivist.create({ config: { namespace: 'test', schema: IndexedDbArchivistConfigSchema } }), name)

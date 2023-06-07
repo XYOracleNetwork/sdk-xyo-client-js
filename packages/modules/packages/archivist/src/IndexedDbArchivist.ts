8ef7995e93b04920009977a731b4aed51cf9d536
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-07 16:42:21 -0400

    Implement get/all

--- packages/modules/packages/archivist/src/IndexedDbArchivist.ts
+++ packages/modules/packages/archivist/src/IndexedDbArchivist.ts
@@ -4,31 +4,41 @@ import { BoundWitness } from '@xyo-network/boundwitness-model'
 import { PayloadHasher } from '@xyo-network/core'
 import { AnyConfigSchema, creatableModule } from '@xyo-network/module'
 import { Payload } from '@xyo-network/payload-model'
-import { setMany, UseStore } from 'idb-keyval'
+import { entries, getMany, setMany, UseStore } from 'idb-keyval'
 
-export type IndexedDBArchivistConfigSchema = 'network.xyo.module.config.archivist.storage'
-export const IndexedDBArchivistConfigSchema: IndexedDBArchivistConfigSchema = 'network.xyo.module.config.archivist.storage'
+export type IndexedDbArchivistConfigSchema = 'network.xyo.module.config.archivist.storage'
+export const IndexedDbArchivistConfigSchema: IndexedDbArchivistConfigSchema = 'network.xyo.module.config.archivist.storage'
 
-export type IndexedDBArchivistConfig = ArchivistConfig<{
+export type IndexedDbArchivistConfig = ArchivistConfig<{
   maxEntries?: number
   maxEntrySize?: number
   namespace?: string
-  schema: IndexedDBArchivistConfigSchema
+  schema: IndexedDbArchivistConfigSchema
 }>
 
-export type IndexedDBArchivistParams = ArchivistParams<
-  AnyConfigSchema<IndexedDBArchivistConfig>,
+export type IndexedDbArchivistParams = ArchivistParams<
+  AnyConfigSchema<IndexedDbArchivistConfig>,
   {
-    indexedDB?: UseStore
+    IndexedDb?: UseStore
   }
 >
 
 @creatableModule()
-export class IndexedDBArchivist<
-  TParams extends IndexedDBArchivistParams = IndexedDBArchivistParams,
+export class IndexedDbArchivist<
+  TParams extends IndexedDbArchivistParams = IndexedDbArchivistParams,
   TEventData extends ArchivistModuleEventData = ArchivistModuleEventData,
 > extends AbstractArchivist<TParams, TEventData> {
-  static override configSchema = IndexedDBArchivistConfigSchema
+  static override configSchema = IndexedDbArchivistConfigSchema
+
+  override async all(): Promise<Payload[]> {
+    const result = await entries<string, Payload>(this.params.IndexedDb ?? undefined)
+    return result.map<Payload>(([_hash, payload]) => payload)
+  }
+
+  override async get(hashes: string[]): Promise<Payload[]> {
+    const result = await getMany<Payload>(hashes, this.params.IndexedDb ?? undefined)
+    return result
+  }
 
   async insert(payloads: Payload[]): Promise<BoundWitness[]> {
     const entries = await Promise.all(
@@ -37,7 +47,7 @@ export class IndexedDBArchivist<
         return [hash, payload]
       }),
     )
-    await setMany(entries, this.params.indexedDB)
+    await setMany(entries, this.params.IndexedDb ?? undefined)
     const result = await this.bindQueryResult({ payloads, schema: ArchivistInsertQuerySchema }, payloads)
     return [result[0]]
   }

a6739a3e5d0c3100c74a9a8eb934704bb2bb8e15
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-08 13:20:25 -0400

    Enable naming via config

--- packages/modules/packages/archivist/packages/indexeddb/src/IndexedDbArchivist.ts
+++ packages/modules/packages/archivist/packages/indexeddb/src/IndexedDbArchivist.ts
@@ -12,24 +12,24 @@ import { BoundWitness } from '@xyo-network/boundwitness-model'
 import { PayloadHasher } from '@xyo-network/core'
 import { AnyConfigSchema, creatableModule } from '@xyo-network/module'
 import { Payload } from '@xyo-network/payload-model'
-import { clear, delMany, entries, getMany, setMany, UseStore } from 'idb-keyval'
+import { clear, createStore, delMany, entries, getMany, setMany, UseStore } from 'idb-keyval'
 
 export type IndexedDbArchivistConfigSchema = 'network.xyo.module.config.archivist.storage'
 export const IndexedDbArchivistConfigSchema: IndexedDbArchivistConfigSchema = 'network.xyo.module.config.archivist.storage'
 
 export type IndexedDbArchivistConfig = ArchivistConfig<{
-  maxEntries?: number
-  maxEntrySize?: number
-  namespace?: string
+  /**
+   * The database name
+   */
+  dbName?: string
   schema: IndexedDbArchivistConfigSchema
+  /**
+   * The name of the object store
+   */
+  storeName?: string
 }>
 
-export type IndexedDbArchivistParams = ArchivistParams<
-  AnyConfigSchema<IndexedDbArchivistConfig>,
-  {
-    indexedDB?: UseStore
-  }
->
+export type IndexedDbArchivistParams = ArchivistParams<AnyConfigSchema<IndexedDbArchivistConfig>>
 
 @creatableModule()
 export class IndexedDbArchivist<
@@ -38,26 +38,41 @@ export class IndexedDbArchivist<
 > extends AbstractArchivist<TParams, TEventData> {
   static override configSchema = IndexedDbArchivistConfigSchema
 
+  private _db: UseStore | undefined
+
+  get dbName() {
+    return this.config?.dbName ?? 'xyo-archivist'
+  }
+
   override get queries() {
     return [ArchivistAllQuerySchema, ArchivistClearQuerySchema, ArchivistDeleteQuerySchema, ArchivistInsertQuerySchema, ...super.queries]
   }
 
+  get storeName() {
+    return this.config?.storeName ?? 'xyo-archivist'
+  }
+
+  private get db(): UseStore {
+    this._db = this._db ?? createStore('db3', 'keyval')
+    return this._db
+  }
+
   override async all(): Promise<Payload[]> {
-    const result = await entries<string, Payload>(this.params.indexedDB ?? undefined)
+    const result = await entries<string, Payload>(this.db)
     return result.map<Payload>(([_hash, payload]) => payload)
   }
 
   override async clear(): Promise<void> {
-    await clear(this.params.indexedDB ?? undefined)
+    await clear(this.db)
   }
 
   override async delete(hashes: string[]): Promise<boolean[]> {
-    await delMany(hashes, this.params.indexedDB ?? undefined)
+    await delMany(hashes, this.db)
     return hashes.map((_) => true)
   }
 
   override async get(hashes: string[]): Promise<Payload[]> {
-    const result = await getMany<Payload>(hashes, this.params.indexedDB ?? undefined)
+    const result = await getMany<Payload>(hashes, this.db)
     return result
   }
 

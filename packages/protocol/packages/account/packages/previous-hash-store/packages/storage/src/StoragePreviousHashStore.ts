5b9b7d103f0b3cd3134f47af8f696e0124554a2f
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-14 09:35:38 -0400

    Local Storage previous hash store

--- packages/protocol/packages/account/packages/previous-hash-store/packages/storage/src/StoragePreviousHashStore.ts
+++ packages/protocol/packages/account/packages/previous-hash-store/packages/storage/src/StoragePreviousHashStore.ts
@@ -1,14 +1,46 @@
 import { PreviousHashStore } from '@xyo-network/previous-hash-store-model'
+import store, { StoreBase } from 'store2'
+
+export type StorageKind = 'local' | 'session' | 'page'
+
+export type StoragePreviousHashOpts = {
+  namespace?: string
+  type?: StorageKind
+}
 
 export class StoragePreviousHashStore implements PreviousHashStore {
   keyPrefix?: string | undefined
-  getItem(address: string): string | Promise<string | null> | null {
-    throw new Error('Method not implemented.')
+  private _namespace = 'xyo-previous-hash-store'
+  private _storage: StoreBase | undefined
+  private _type: StorageKind = 'local'
+
+  constructor(opts: StoragePreviousHashOpts) {
+    if (opts?.namespace) this._namespace = opts.namespace
+    if (opts?.type) this._type = opts.type
+    this._storage = store[this.type].namespace(this.namespace)
   }
-  removeItem(address: string): void | Promise<void> {
-    throw new Error('Method not implemented.')
+
+  get namespace() {
+    return this._namespace
   }
-  setItem(address: string, previousHash: string): void | Promise<void> {
-    throw new Error('Method not implemented.')
+
+  get type(): StorageKind {
+    return this._type
+  }
+
+  private get storage(): StoreBase {
+    if (!this?._storage) this._storage = store[this.type].namespace(this.namespace)
+    return this._storage
+  }
+
+  async getItem(address: string): Promise<string | null> {
+    const value = await this.storage.get(address)
+    return value?.previousHash ?? null
+  }
+  async removeItem(address: string): Promise<void> {
+    await this.storage.remove(address)
+  }
+  async setItem(address: string, previousHash: string): Promise<void> {
+    await this.storage.set(address, previousHash)
   }
 }

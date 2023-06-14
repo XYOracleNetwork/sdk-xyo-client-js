ffbece564de731cb5d4b38510a19401b25011aa1
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-14 16:56:25 -0400

    Remove keyPrefix
    Let implementations figure this out if necessary

--- packages/protocol/packages/account/packages/previous-hash-store/packages/model/src/PreviousHashStore.ts
+++ packages/protocol/packages/account/packages/previous-hash-store/packages/model/src/PreviousHashStore.ts
@@ -1,5 +1,4 @@
 export interface PreviousHashStore {
-  keyPrefix?: string
   getItem(address: string): string | null | Promise<string | null>
   removeItem(address: string): void | Promise<void>
   setItem(address: string, previousHash: string): void | Promise<void>

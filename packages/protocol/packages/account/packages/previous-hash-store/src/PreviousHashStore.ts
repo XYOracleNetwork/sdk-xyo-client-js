dfa60dcad721bcb8bc4f13bdddc2af8697cdbebd
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-14 09:04:02 -0400

    Remove unused files

--- packages/protocol/packages/account/packages/previous-hash-store/src/PreviousHashStore.ts
+++
@@ -1,6 +0,0 @@
-export interface PreviousHashStore {
-  keyPrefix?: string
-  getItem(address: string): string | null | Promise<string | null>
-  removeItem(address: string): void | Promise<void>
-  setItem(address: string, previousHash: string): void | Promise<void>
-}

5f47f6b588b950a5c124d90ea02e33204929cde6
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-20 15:02:07 -0500

    Add tests for wallet NFTs

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/spec/pricesFromUniswap3.spec.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/spec/pricesFromUniswap3.spec.ts
@@ -1,12 +1,12 @@
-import { InfuraProvider } from '@ethersproject/providers'
 import { describeIf } from '@xylabs/jest-helpers'
 
 import { getNftsOwnedByAddress } from '../getNftsOwnedByAddress'
 
 describeIf(process.env.INFURA_PROJECT_ID && process.env.INFURA_PROJECT_SECRET)('getNftsOwnedByAddress', () => {
-  test.todo('observe', async () => {
-    const provider = new InfuraProvider('homestead', { projectId: process.env.INFURA_PROJECT_ID, projectSecret: process.env.INFURA_PROJECT_SECRET })
-    const pairs = await getNftsOwnedByAddress('0x000000', 'chain', '0x000000', provider)
+  const address = '0xacdaEEb57ff6886fC8e203B9Dd4C2b241DF89b7a'
+  const chainId = 1
+  test('observe', async () => {
+    const pairs = await getNftsOwnedByAddress(address, chainId)
     expect(pairs.length).toBeGreaterThan(1)
   })
 })

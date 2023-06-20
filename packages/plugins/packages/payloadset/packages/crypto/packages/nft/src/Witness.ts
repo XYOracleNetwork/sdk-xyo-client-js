40a6f021cc0d6b487febe04b2dd02b024dfa0bc6
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-20 15:01:37 -0500

    Remove provider injection

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/Witness.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/Witness.ts
@@ -1,4 +1,3 @@
-import { Provider } from '@ethersproject/providers'
 import { assertEx } from '@xylabs/assert'
 import { CryptoWalletNftPayload, CryptoWalletNftSchema, CryptoWalletNftWitnessConfigSchema } from '@xyo-network/crypto-wallet-nft-payload-plugin'
 import { AnyConfigSchema } from '@xyo-network/modules'
@@ -8,30 +7,19 @@ import { AbstractWitness, WitnessParams } from '@xyo-network/witness'
 import { CryptoWalletNftWitnessConfig } from './Config'
 import { getNftsOwnedByAddress } from './lib'
 
-export type CryptoWalletNftWitnessParams = WitnessParams<
-  AnyConfigSchema<CryptoWalletNftWitnessConfig>,
-  {
-    provider?: Provider
-  }
->
+export type CryptoWalletNftWitnessParams = WitnessParams<AnyConfigSchema<CryptoWalletNftWitnessConfig>>
 
 export class CryptoWalletNftWitness<TParams extends CryptoWalletNftWitnessParams = CryptoWalletNftWitnessParams> extends AbstractWitness<TParams> {
   static override configSchema = CryptoWalletNftWitnessConfigSchema
 
-  protected get provider() {
-    return this.params.provider
-  }
-
   override async observe(): Promise<Payload[]> {
     this.started('throw')
     const address = assertEx(this.config.address, 'params.address is required')
-    const chainId = assertEx(this.config.chainId, 'params.chain is required')
-    const network = assertEx(this.config.network, 'params.network is required')
-    const provider = assertEx(this.provider, 'params.provider is required')
-    const nfts = await getNftsOwnedByAddress(address, network, chainId, provider)
+    const chainId = assertEx(this.config.chainId, 'params.chainId is required')
+    const nfts = await getNftsOwnedByAddress(address, chainId)
     const timestamp = Date.now()
 
-    const payload: CryptoWalletNftPayload = { address, chainId, network, nfts, schema: CryptoWalletNftSchema, timestamp }
+    const payload: CryptoWalletNftPayload = { address, chainId, nfts, schema: CryptoWalletNftSchema, timestamp }
 
     return super.observe([payload])
   }

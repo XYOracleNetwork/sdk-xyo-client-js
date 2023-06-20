ca384b0b50ef2d5780aea26fd3bc516a5de0d086
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-20 18:17:43 -0500

    Update test to assert known NFT fields

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/spec/getNftsOwnedByAddress.spec.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/spec/getNftsOwnedByAddress.spec.ts
@@ -6,7 +6,6 @@ import { getNftsOwnedByAddress } from '../getNftsOwnedByAddress'
 describeIf(process.env.INFURA_PROJECT_ID && process.env.INFURA_PROJECT_SECRET)('getNftsOwnedByAddress', () => {
   const address = '0xacdaEEb57ff6886fC8e203B9Dd4C2b241DF89b7a'
   const chainId = 1
-  const isStringOrNull = (value: never) => typeof value === 'string' || value === null
   const account = Account.random()
   test('observe', async () => {
     const nfts = await getNftsOwnedByAddress(address, chainId, account.private.hex)
@@ -14,9 +13,12 @@ describeIf(process.env.INFURA_PROJECT_ID && process.env.INFURA_PROJECT_SECRET)('
     for (let i = 0; i < nfts.length; i++) {
       const nft = nfts[i]
       expect(nft.contract).toBeString()
-      expect(nft.name).toSatisfy(isStringOrNull)
-      expect(nft.symbol).toSatisfy(isStringOrNull)
-      expect(nft.tokenType).toBeString()
+      expect(nft.supply).toBeString()
+      if (nft?.metadata) {
+        expect(nft.metadata).toBeObject()
+      }
+      expect(nft.tokenId).toBeString()
+      expect(nft.type).toBeString()
     }
   })
 })

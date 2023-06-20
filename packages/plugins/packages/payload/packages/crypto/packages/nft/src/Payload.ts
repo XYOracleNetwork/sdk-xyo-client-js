46da24d2c921a78939f2a4a30514c5a50a10860f
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-20 18:17:16 -0500

    Update types to capture pseudo-standard types

--- packages/plugins/packages/payload/packages/crypto/packages/nft/src/Payload.ts
+++ packages/plugins/packages/payload/packages/crypto/packages/nft/src/Payload.ts
@@ -2,10 +2,21 @@ import { Payload } from '@xyo-network/payload-model'
 
 import { CryptoWalletNftSchema } from './Schema'
 
+export interface NftAttribute {
+  [key: string]: unknown
+  display_type?: unknown
+  trait_type?: unknown
+  value?: unknown
+}
+
 export interface NftInfo {
   contract: string
   metadata?: {
     [key: string]: unknown
+    attributes?: NftAttribute[] | unknown
+    description?: unknown
+    image?: unknown
+    name?: unknown
   }
   supply: string
   tokenId: string

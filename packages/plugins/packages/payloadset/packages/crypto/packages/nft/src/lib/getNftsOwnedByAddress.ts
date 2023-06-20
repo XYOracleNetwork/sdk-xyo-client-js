cc0c18bbbbed140c05e30acaa4b581a4707dd196
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-20 15:01:55 -0500

    Use infrua to query supplied wallet

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/getNftsOwnedByAddress.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/getNftsOwnedByAddress.ts
@@ -1,8 +1,46 @@
-import { Provider } from '@ethersproject/providers'
+import { ExternalProvider, JsonRpcFetchFunc } from '@ethersproject/providers'
+import { Auth, SDK } from '@infura/sdk'
 import { NftInfo } from '@xyo-network/crypto-wallet-nft-payload-plugin'
 
-export const getNftsOwnedByAddress = async (address: string, network: string, chainId: string, provider: Provider): Promise<NftInfo[]> => {
-  const nfts = await Promise.resolve([])
-  // TODO: Implement
+export type ProviderType = ExternalProvider | JsonRpcFetchFunc
+
+type IPFSType = {
+  apiKeySecret: string | undefined
+  projectId: string | undefined
+}
+
+type AuthOptions = {
+  chainId: number | undefined
+  ipfs?: IPFSType
+  privateKey?: string | undefined
+  projectId: string | undefined
+  provider?: ProviderType
+  rpcUrl?: string | undefined
+  secretId: string | undefined
+}
+
+export const getNftsOwnedByAddress = async (address: string, chainId: number, provider?: ProviderType): Promise<NftInfo[]> => {
+  // Create Auth object
+  const opts: AuthOptions = {
+    chainId,
+    // ipfs: {
+    //   apiKeySecret: process.env.INFURA_IPFS_PROJECT_SECRET,
+    //   projectId: process.env.INFURA_IPFS_PROJECT_ID,
+    // },
+    // privateKey: process.env.WALLET_PRIVATE_KEY,
+    projectId: process.env.INFURA_PROJECT_ID,
+    provider,
+    // rpcUrl: process.env.EVM_RPC_URL,
+    secretId: process.env.INFURA_PROJECT_SECRET,
+  }
+  const auth = new Auth(opts)
+
+  // Instantiate SDK
+  const sdk = new SDK(auth)
+  // TODO: Handle pagination
+  const result = await sdk.api.getCollectionsByWallet({
+    walletAddress: address,
+  })
+  const nfts = result.collections.map<NftInfo>((collection) => collection)
   return nfts
 }

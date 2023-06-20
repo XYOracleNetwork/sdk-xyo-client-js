c2f4ea523105c11b474ff4dd70b03af4e868c77e
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-20 18:32:27 -0500

    Remove no-config witness tests

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/spec/Witness.spec.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/spec/Witness.spec.ts
@@ -2,52 +2,20 @@
 const now = new Date()
 jest.useFakeTimers().setSystemTime(now)
 
-import { InfuraProvider } from '@ethersproject/providers'
 import { CryptoWalletNftPayload, CryptoWalletNftWitnessConfigSchema } from '@xyo-network/crypto-wallet-nft-payload-plugin'
 import { PayloadWrapper } from '@xyo-network/payload-wrapper'
 
 import { CryptoWalletNftWitness } from '../Witness'
 
 describe('CryptoWalletNftWitness', () => {
+  const address = '0xacdaEEb57ff6886fC8e203B9Dd4C2b241DF89b7a'
+  const chainId = 1
   test('observe', async () => {
-    const provider = new InfuraProvider('homestead', { projectId: process.env.INFURA_PROJECT_ID, projectSecret: process.env.INFURA_PROJECT_SECRET })
-    const witness = await CryptoWalletNftWitness.create({
-      config: {
-        schema: CryptoWalletNftWitnessConfigSchema,
-      },
-      provider,
-    })
+    const witness = await CryptoWalletNftWitness.create({ config: { address, chainId, schema: CryptoWalletNftWitnessConfigSchema } })
     const [observation] = (await witness.observe()) as CryptoWalletNftPayload[]
     expect(observation.nfts.length).toBeGreaterThan(1)
     expect(observation.timestamp).toBe(+now)
-
     const answerWrapper = PayloadWrapper.wrap(observation)
     expect(await answerWrapper.getValid()).toBe(true)
-  })
-  test('observe [no config]', async () => {
-    const provider = new InfuraProvider('homestead', { projectId: process.env.INFURA_PROJECT_ID, projectSecret: process.env.INFURA_PROJECT_SECRET })
-    const witness = await CryptoWalletNftWitness.create({
-      config: {
-        schema: CryptoWalletNftWitnessConfigSchema,
-      },
-      provider,
-    })
-    const [observation] = (await witness.observe()) as CryptoWalletNftPayload[]
-    expect(observation.nfts.length).toBeGreaterThan(1)
-    expect(observation.timestamp).toBe(+now)
-
-    const answerWrapper = PayloadWrapper.wrap(observation)
-    expect(await answerWrapper.getValid()).toBe(true)
-  })
-  test('observe [no params]', async () => {
-    const didThrow = async () => {
-      try {
-        await CryptoWalletNftWitness.create()
-        return false
-      } catch {
-        return true
-      }
-    }
-    expect(await didThrow()).toBe(true)
   })
 })

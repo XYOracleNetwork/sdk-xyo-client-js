3bfe8c728ece22048a6ccefeb76c19753f2d0936
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-21 11:53:58 -0500

    Update tests to use Provider

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/spec/Witness.spec.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/spec/Witness.spec.ts
@@ -2,16 +2,22 @@
 const now = new Date()
 jest.useFakeTimers().setSystemTime(now)
 
+import { ExternalProvider } from '@ethersproject/providers'
+import { describeIf } from '@xylabs/jest-helpers'
 import { CryptoWalletNftPayload, CryptoWalletNftWitnessConfigSchema } from '@xyo-network/crypto-wallet-nft-payload-plugin'
 import { PayloadWrapper } from '@xyo-network/payload-wrapper'
+import { HttpProvider } from 'web3-providers-http'
 
 import { CryptoWalletNftWitness } from '../Witness'
 
-describe('CryptoWalletNftWitness', () => {
+describeIf(process.env.INFURA_API_KEY)('CryptoWalletNftWitness', () => {
   const address = '0xacdaEEb57ff6886fC8e203B9Dd4C2b241DF89b7a'
   const chainId = 1
+  const network = 'homestead'
   test('observe', async () => {
-    const witness = await CryptoWalletNftWitness.create({ config: { address, chainId, schema: CryptoWalletNftWitnessConfigSchema } })
+    const apiKey = process.env.INFURA_API_KEY
+    const provider = new HttpProvider(`https://${network}.infura.io/v3/${apiKey}`) as unknown as ExternalProvider
+    const witness = await CryptoWalletNftWitness.create({ config: { address, chainId, schema: CryptoWalletNftWitnessConfigSchema }, provider })
     const [observation] = (await witness.observe()) as CryptoWalletNftPayload[]
     expect(observation.nfts.length).toBeGreaterThan(1)
     expect(observation.timestamp).toBe(+now)

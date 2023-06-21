0f3472c58a4d2010a2c05ae503dd38576c583f66
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-21 17:54:43 -0500

    Use existing ENV VAR

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/spec/getNftsOwnedByAddress.spec.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/spec/getNftsOwnedByAddress.spec.ts
@@ -4,11 +4,11 @@ import { HttpProvider } from 'web3-providers-http'
 
 import { getNftsOwnedByAddress } from '../getNftsOwnedByAddress'
 
-describeIf(process.env.INFURA_API_KEY)('getNftsOwnedByAddress', () => {
+describeIf(process.env.INFURA_PROJECT_ID)('getNftsOwnedByAddress', () => {
   const address = '0xacdaEEb57ff6886fC8e203B9Dd4C2b241DF89b7a'
   const chainId = 1
   const network = 'homestead'
-  const apiKey = process.env.INFURA_API_KEY
+  const apiKey = process.env.INFURA_PROJECT_ID
   const provider = new HttpProvider(`https://${network}.infura.io/v3/${apiKey}`) as unknown as ExternalProvider
   test('observe', async () => {
     const nfts = await getNftsOwnedByAddress(address, chainId, provider)
--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/spec/Witness.spec.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/spec/Witness.spec.ts
@@ -10,12 +10,12 @@ import { HttpProvider } from 'web3-providers-http'
 
 import { CryptoWalletNftWitness } from '../Witness'
 
-describeIf(process.env.INFURA_API_KEY)('CryptoWalletNftWitness', () => {
+describeIf(process.env.INFURA_PROJECT_ID)('CryptoWalletNftWitness', () => {
   const address = '0xacdaEEb57ff6886fC8e203B9Dd4C2b241DF89b7a'
   const chainId = 1
   const network = 'homestead'
   test('observe', async () => {
-    const apiKey = process.env.INFURA_API_KEY
+    const apiKey = process.env.INFURA_PROJECT_ID
     const provider = new HttpProvider(`https://${network}.infura.io/v3/${apiKey}`) as unknown as ExternalProvider
     const witness = await CryptoWalletNftWitness.create({ config: { address, chainId, schema: CryptoWalletNftWitnessConfigSchema }, provider })
     const [observation] = (await witness.observe()) as CryptoWalletNftPayload[]

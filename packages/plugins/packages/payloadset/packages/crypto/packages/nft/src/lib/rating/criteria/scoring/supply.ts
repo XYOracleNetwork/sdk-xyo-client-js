2333cac1898e9ec2f7a60674d62d5b5456771e48
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-23 14:00:05 -0500

    Check tokenId/supply numerical validity

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/rating/criteria/scoring/supply.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/rating/criteria/scoring/supply.ts
@@ -1,9 +1,15 @@
 import { NftInfo } from '@xyo-network/crypto-wallet-nft-payload-plugin'
 
 import { FAIL, PASS, PassFailScoringFunction } from '../../score'
+import { isValidPositiveBigNumber } from './lib'
 
 export const scoreSupply: PassFailScoringFunction = (nft: NftInfo) => {
-  if (nft instanceof Array) return FAIL
-  if (nft.constructor === Object) return PASS
-  return FAIL
+  if (!nft.supply) return FAIL
+  if (typeof nft.supply !== 'string') return FAIL
+  try {
+    // Check for positive BigNumber since data type is uint256
+    return isValidPositiveBigNumber(nft.supply) ? PASS : FAIL
+  } catch (_error) {
+    return FAIL
+  }
 }
--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/rating/criteria/scoring/tokenId.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/rating/criteria/scoring/tokenId.ts
@@ -1,9 +1,21 @@
 import { NftInfo } from '@xyo-network/crypto-wallet-nft-payload-plugin'
 
 import { FAIL, PASS, PassFailScoringFunction } from '../../score'
+import { isValidPositiveBigNumber } from './lib'
 
+/**
+ * Callers SHALL NOT assume that ID numbers have any specific pattern to them, and
+ * MUST treat the ID as a "black box"
+ * @param nft
+ * @returns
+ */
 export const scoreTokenId: PassFailScoringFunction = (nft: NftInfo) => {
-  if (nft instanceof Array) return FAIL
-  if (nft.constructor === Object) return PASS
-  return FAIL
+  if (!nft.tokenId) return FAIL
+  if (typeof nft.tokenId !== 'string') return FAIL
+  try {
+    // Check for positive BigNumber since data type is uint256
+    return isValidPositiveBigNumber(nft.tokenId) ? PASS : FAIL
+  } catch (_error) {
+    return FAIL
+  }
 }

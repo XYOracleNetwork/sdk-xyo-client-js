f1ba9a9753dc07d09f831bfa1aaacd38526cad50
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-23 11:38:11 -0500

    Placeholder NFT Rating Diviner evaluation method

---
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/evaluateNft.ts
@@ -0,0 +1,10 @@
+import { NftInfo } from '@xyo-network/crypto-wallet-nft-payload-plugin'
+
+export const evaluateNft = async (
+  /**
+   * The NFT
+   */
+  nft: NftInfo,
+): Promise<number> => {
+  return await Promise.resolve(100)
+}

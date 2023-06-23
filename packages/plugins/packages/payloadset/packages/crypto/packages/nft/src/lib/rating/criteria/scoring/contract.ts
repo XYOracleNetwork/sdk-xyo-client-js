498e432207f8412b7ca970078de802dc3cf554bc
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-23 13:23:42 -0500

    Validate contract address

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/rating/criteria/scoring/contract.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/rating/criteria/scoring/contract.ts
@@ -3,7 +3,7 @@ import { NftInfo } from '@xyo-network/crypto-wallet-nft-payload-plugin'
 
 import { FAIL, PASS, PassFailScoringFunction } from '../../score'
 
-export const scoreContractHash: PassFailScoringFunction = (nft: NftInfo) => {
+export const scoreContractAddress: PassFailScoringFunction = (nft: NftInfo) => {
   if (!nft.contract) return FAIL
   if (typeof nft.contract !== 'string') return FAIL
   if (!isAddress(nft.contract)) return FAIL
--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/rating/criteria/scoringCriteria.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/rating/criteria/scoringCriteria.ts
@@ -1,10 +1,10 @@
 import { WeightedScoringCriteria } from '../score'
-import { scoreContractHash, scoreSupply, scoreTokenId, scoreType } from './scoring'
+import { scoreContractAddress, scoreSupply, scoreTokenId, scoreType } from './scoring'
 
 const metadataScoringCriteria: Record<string, WeightedScoringCriteria> = {}
 
 export const scoringCriteria: Record<string, WeightedScoringCriteria> = {
-  'Contract.Hash': { score: scoreContractHash, weight: 1 },
+  'Contract.Address': { score: scoreContractAddress, weight: 1 },
   Supply: { score: scoreSupply, weight: 1 },
   'Token Id': { score: scoreTokenId, weight: 1 },
   Type: { score: scoreType, weight: 1 },

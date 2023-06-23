e5ec5150a5cf14f261d0fe09ad923ea29e1edcb9
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-23 11:50:43 -0500

    Extensible rating criteria

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/evaluateNft.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/evaluateNft.ts
@@ -1,10 +1,33 @@
 import { NftInfo } from '@xyo-network/crypto-wallet-nft-payload-plugin'
 
+type RatingFunction = (nft: NftInfo) => boolean | Promise<boolean>
+
+interface WeightedRating {
+  criteria: RatingFunction
+  weight: number
+}
+
+export const ratingCriteria: Record<string, WeightedRating> = {}
+
+export const maxPossibleRating = Object.values(ratingCriteria).reduce((sum, { weight }) => sum + weight, 0)
+
+export type Rating = {
+  [key: keyof typeof ratingCriteria]: boolean
+}
+
 export const evaluateNft = async (
   /**
    * The NFT
    */
   nft: NftInfo,
 ): Promise<number> => {
-  return await Promise.resolve(100)
+  const score = (
+    await Promise.all(
+      Object.entries(ratingCriteria).map(([key, { criteria, weight }]) => {
+        return [criteria(nft), weight] as const
+      }),
+    )
+  ).reduce((sum, [passed, weight]) => (passed ? sum + weight : sum), 0)
+  const rating = score / maxPossibleRating
+  return rating
 }

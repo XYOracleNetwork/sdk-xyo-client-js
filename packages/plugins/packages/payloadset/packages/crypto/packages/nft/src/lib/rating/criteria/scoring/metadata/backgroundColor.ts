f83f8d445e0eb89b48562df147d0993cbe752f17
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-24 08:11:10 -0500

    Check for hex background colors

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/rating/criteria/scoring/metadata/backgroundColor.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/rating/criteria/scoring/metadata/backgroundColor.ts
@@ -1,7 +1,16 @@
 import { NftInfo, OpenSeaNftInfo } from '@xyo-network/crypto-wallet-nft-payload-plugin'
 
-import { PASS, ScaledScore } from '../../../score'
+import { incrementPossible, incrementTotalAndPossible, ScaledScore } from '../../../score'
+
+const isHexColor = /^[0-9A-F]{6}$/i
 
 export const scoreBackgroundColor = (nft: NftInfo | OpenSeaNftInfo): ScaledScore => {
-  return PASS
+  const score: ScaledScore = [0, 0]
+  if (!nft.metadata?.background_color) return score
+  incrementPossible(score)
+  if (typeof nft.metadata.background_color !== 'string') return score
+  incrementTotalAndPossible(score)
+  if (!nft.metadata.background_color.toUpperCase().match(isHexColor)) return score
+  incrementTotalAndPossible(score)
+  return score
 }

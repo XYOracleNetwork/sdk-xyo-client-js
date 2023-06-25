03cf2182c518fd37d6961ab641cb7d78087f482c
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-24 23:31:10 -0500

    score increment helpers

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/rating/score.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/rating/score.ts
@@ -16,18 +16,18 @@ export interface WeightedScoringCriteria {
   weight: number
 }
 
-export const incrementTotal = (score: ScaledScore): ScaledScore => {
-  score[0]++
+export const incrementTotal = (score: ScaledScore, by = 1): ScaledScore => {
+  score[0] += by
   return score
 }
 
-export const incrementPossible = (score: ScaledScore): ScaledScore => {
-  score[1]++
+export const incrementPossible = (score: ScaledScore, by = 1): ScaledScore => {
+  score[1] += by
   return score
 }
 
-export const incrementTotalAndPossible = (score: ScaledScore): ScaledScore => {
-  score[0]++
-  score[1]++
+export const incrementTotalAndPossible = (score: ScaledScore, totalBy = 1, possibleBy = 1): ScaledScore => {
+  score[0] += totalBy
+  score[1] += possibleBy
   return score
 }

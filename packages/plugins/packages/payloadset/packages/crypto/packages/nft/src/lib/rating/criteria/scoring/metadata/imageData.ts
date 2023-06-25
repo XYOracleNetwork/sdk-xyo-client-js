a61d4cdf841b37d5204251499d18c7a4ef9997c1
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-24 20:10:34 -0500

    Image Data Tests

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/rating/criteria/scoring/metadata/imageData.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/rating/criteria/scoring/metadata/imageData.ts
@@ -1,7 +1,23 @@
 import { NftInfo, OpenSeaNftInfo } from '@xyo-network/crypto-wallet-nft-payload-plugin'
 
-import { PASS, ScaledScore } from '../../../score'
+import { incrementTotal, PASS, ScaledScore } from '../../../score'
 
-export const scoreImageData = (nft: NftInfo | OpenSeaNftInfo): ScaledScore => {
-  return PASS
+const MaxPossibleImageDataScore = 1
+
+const isValidImageData = (image_data: string): boolean => {
+  return false
+}
+
+export const scoreNftImageData = (nft: NftInfo | OpenSeaNftInfo): ScaledScore => {
+  if (!nft?.metadata?.image_data) {
+    return nft.metadata?.image ? PASS : [0, MaxPossibleImageDataScore]
+  } else {
+    return scoreImageData(nft.metadata?.image_data)
+  }
+}
+
+export const scoreImageData = (image_data: unknown): ScaledScore => {
+  const score: ScaledScore = [0, MaxPossibleImageDataScore]
+  if (!image_data || typeof image_data !== 'string' || !isValidImageData(image_data)) return score
+  return incrementTotal(score)
 }
---
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/src/lib/rating/criteria/scoring/metadata/spec/imageData.spec.ts
@@ -0,0 +1,29 @@
+import { scoreImageData } from '../imageData'
+import { expectMaxPossibleScore, expectMiniumScore } from './testHelpers'
+
+const valid = [
+  "<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='2000' height='2000'><style>.m1 #c{fill: #fff;}.m1 #r{fill: #000;}.m2 #c{fill: #fc3;}.m2 #r{fill: #000;}.m3 #c{fill: #fff;}.m3 #r{fill: #33f;}.m4 #c{fill: #fff;}.m4 #r{fill: #f33;}.a #c{fill: #000 !important;}.a #r{fill: #fff !important;}</style><g class='m1 c3'><rect id='r' width='2000' height='2000'/><circle id='c' cx='1000' cy='1000' r='146.2449'/></g></svg>",
+]
+const invalid = ['', 'not an SVG', {}]
+const missing = [undefined, null]
+
+describe('scoreImageData', () => {
+  describe('with valid image data', () => {
+    it.each(valid)('return max possible score', (value) => {
+      const score = scoreImageData(value)
+      expectMaxPossibleScore(score)
+    })
+  })
+  describe('with invalid image data', () => {
+    it.each(invalid)('returns minimum score', (value) => {
+      const score = scoreImageData(value)
+      expectMiniumScore(score)
+    })
+  })
+  describe('with no image data', () => {
+    it.each(missing)('returns minimum score', (value) => {
+      const score = scoreImageData(value)
+      expectMiniumScore(score)
+    })
+  })
+})

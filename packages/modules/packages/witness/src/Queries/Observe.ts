d559a5430c7fae55b4e7468d87efbc91cd11236b
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-06-28 08:13:31 -0500

    Export witness via barrel package

--- packages/modules/packages/witness/src/Queries/Observe.ts
+++
@@ -1,9 +0,0 @@
-import { Query } from '@xyo-network/module-model'
-
-export type WitnessObserveQuerySchema = 'network.xyo.query.witness.observe'
-export const WitnessObserveQuerySchema: WitnessObserveQuerySchema = 'network.xyo.query.witness.observe'
-
-export type WitnessObserveQuery = Query<{
-  payloads?: string[]
-  schema: WitnessObserveQuerySchema
-}>

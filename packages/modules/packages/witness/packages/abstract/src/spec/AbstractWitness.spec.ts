180b461c89778a77c12d18ac57779953ad81c48e
Arie Trouw <arie.trouw@xylabs.com>
2023-06-28 14:48:42 -0700

    2.63.14

--- packages/modules/packages/witness/packages/abstract/src/spec/AbstractWitness.spec.ts
+++ packages/modules/packages/witness/packages/abstract/src/spec/AbstractWitness.spec.ts
@@ -2,11 +2,10 @@ import { Account } from '@xyo-network/account'
 import { AccountInstance } from '@xyo-network/account-model'
 import { Module, ModuleParams } from '@xyo-network/module'
 import { PayloadBuilder } from '@xyo-network/payload-builder'
+import { WitnessConfig, WitnessConfigSchema, WitnessModule } from '@xyo-network/witness-model'
+import { WitnessWrapper } from '@xyo-network/witness-wrapper'
 
 import { AbstractWitness } from '../AbstractWitness'
-import { WitnessConfig, WitnessConfigSchema } from '@xyo-network/witness-model'
-import { WitnessModule } from '@xyo-network/witness-model'
-import { WitnessWrapper } from '@xyo-network/witness-wrapper'
 
 describe('Witness', () => {
   const config: WitnessConfig = { schema: WitnessConfigSchema }

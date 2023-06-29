d677e4004f0e911c6ad4eb121f808a634f8ea705
Arie Trouw <arie.trouw@xylabs.com>
2023-06-29 09:35:32 -0700

    Removed indirect module support from manifest to support round tripping

--- packages/manifest/src/spec/Manifest.spec.ts
+++ packages/manifest/src/spec/Manifest.spec.ts
@@ -4,23 +4,10 @@ import { NodeWrapper } from '@xyo-network/node'
 
 import { ManifestWrapper } from '../ManifestWrapper'
 import { ManifestPayload } from '../Payload'
-import simpleNodeIndirectManifest from './simple-node-indirect-manifest.json'
 import simpleNodeInlineManifest from './simple-node-inline-manifest.json'
 
 describe('Manifest', () => {
   describe('Create Node from Manifest', () => {
-    test('Simple Node [Indirect]', async () => {
-      const mnemonic = 'later puppy sound rebuild rebuild noise ozone amazing hope broccoli crystal grief'
-      const wallet = await HDWallet.fromMnemonic(mnemonic)
-      const manifest = new ManifestWrapper(simpleNodeIndirectManifest as ManifestPayload, wallet)
-      const [node] = await manifest.loadDapps()
-      expect(node).toBeDefined()
-      const wrapper = NodeWrapper.wrap(node)
-      const discover = await wrapper.discover()
-      const discoveredAddresses = discover.filter((item) => item.schema === AddressSchema)
-      expect(discoveredAddresses).toBeArrayOfSize(4)
-      expect(await node.downResolver.resolve()).toBeArrayOfSize(3)
-    })
     test('Simple Node [Inline]', async () => {
       const mnemonic = 'later puppy sound rebuild rebuild noise ozone amazing hope broccoli crystal grief'
       const wallet = await HDWallet.fromMnemonic(mnemonic)

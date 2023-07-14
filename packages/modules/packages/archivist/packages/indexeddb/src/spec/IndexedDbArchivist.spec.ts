6d4af63ddf4e3404131b6192446aa654f67091c2
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-14 11:05:15 -0500

    Passing tests

--- packages/modules/packages/archivist/packages/indexeddb/src/spec/IndexedDbArchivist.spec.ts
+++ packages/modules/packages/archivist/packages/indexeddb/src/spec/IndexedDbArchivist.spec.ts
@@ -2,10 +2,39 @@
  * @jest-environment jsdom
  */
 
-import { indexedDB } from 'fake-indexeddb'
+import {
+  IDBCursor,
+  IDBCursorWithValue,
+  IDBDatabase,
+  IDBFactory,
+  IDBIndex,
+  IDBKeyRange,
+  IDBObjectStore,
+  IDBOpenDBRequest,
+  IDBRequest,
+  IDBTransaction,
+  IDBVersionChangeEvent,
+  indexedDB,
+} from 'fake-indexeddb'
 
 import { IndexedDbArchivist, IndexedDbArchivistConfigSchema } from '../IndexedDbArchivist'
 import { testArchivistAll, testArchivistRoundTrip } from './testArchivist'
+
+// Shim via fake-indexeddb
+window.indexedDB = indexedDB
+
+// Augment window with prototypes to ensure instance of comparisons work
+window.IDBCursor = IDBCursor
+window.IDBCursorWithValue = IDBCursorWithValue
+window.IDBDatabase = IDBDatabase
+window.IDBFactory = IDBFactory
+window.IDBIndex = IDBIndex
+window.IDBKeyRange = IDBKeyRange
+window.IDBObjectStore = IDBObjectStore
+window.IDBOpenDBRequest = IDBOpenDBRequest
+window.IDBRequest = IDBRequest
+window.IDBTransaction = IDBTransaction
+window.IDBVersionChangeEvent = IDBVersionChangeEvent
 
 window.indexedDB = indexedDB
 

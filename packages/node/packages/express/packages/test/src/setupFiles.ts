02f0dcc20a06aa9e257ac2d097ae89f914f3fd1a
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-27 08:50:09 -0500

    Await inactive forgets after each test

--- packages/node/packages/express/packages/test/src/setupFiles.ts
+++ packages/node/packages/express/packages/test/src/setupFiles.ts
@@ -1,7 +1,10 @@
+import { ForgetPromise } from '@xylabs/forget'
+
 beforeAll(() => {
   // Can be async, before each test file
 })
 
-afterAll(() => {
+afterAll(async () => {
   // Can be async, after each test file
+  await ForgetPromise.awaitInactive()
 })

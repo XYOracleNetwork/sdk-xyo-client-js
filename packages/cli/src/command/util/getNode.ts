91198977a46caa481077dd3ef49f8d4477654b4d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-31 21:51:46 -0500

    Use bridge helper

--- packages/cli/src/command/util/getNode.ts
+++ packages/cli/src/command/util/getNode.ts
@@ -1,16 +1,14 @@
 import { assertEx } from '@xylabs/assert'
-import { HttpBridge } from '@xyo-network/http-bridge'
 import { asNodeInstance, NodeInstance } from '@xyo-network/node-model'
 
 import { printError } from '../../lib'
 import { BaseArguments } from '../BaseArguments'
-import { getBridgeConfig } from './getBridgeConfig'
+import { getBridge } from './getBridge'
 
 export const getNode = async (args: BaseArguments): Promise<NodeInstance> => {
   const { verbose } = args
   try {
-    const config = await getBridgeConfig(args)
-    const bridge = await HttpBridge.create({ config })
+    const bridge = await getBridge(args)
     const node = assertEx((await bridge.resolve({ address: [await bridge.getRootAddress()] }))?.pop(), 'Failed to resolve rootNode')
     return asNodeInstance(node, 'Not a NodeModule')
   } catch (error) {

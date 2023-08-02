867a31734707a69429ca6171cbbfa21f55ea528d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-08-01 20:02:03 -0500

    Nest payload plugin in barrel package

---
+++ packages/plugins/packages/payload/packages/crypto/packages/nft/packages/nft/src/index.ts
@@ -0,0 +1,12 @@
+import { NftInfoPayloadPlugin } from './Plugin'
+
+export * from './Diviner'
+export * from './Payload'
+export * from './Schema'
+export * from './Template'
+export * from './Witness'
+
+export { NftInfoPayloadPlugin }
+
+// eslint-disable-next-line import/no-default-export
+export default NftInfoPayloadPlugin

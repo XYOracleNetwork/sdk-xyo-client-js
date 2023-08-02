5e8b9102c0c08374b1c07dc6c1803a3682fe8d9c
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-08-02 17:35:02 -0500

    Do as much in parallel as we can

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/packages/collection/witness/src/Witness.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/packages/collection/witness/src/Witness.ts
@@ -27,14 +27,18 @@ export class CryptoNftCollectionWitness<
       queries.map<Promise<NftCollectionInfoPayload>>(async (query) => {
         const address = assertEx(query?.address || this.config.address, 'params.address is required')
         const chainId = assertEx(query?.chainId || this.config.chainId, 'params.chainId is required')
-        const [info, total, nfts, writeArchivist] = await Promise.all([
+        const [info, total, nfts, archivist] = await Promise.all([
           getNftCollectionInfo(address, chainId, this.account.private.hex),
           getNftCollectionTotalNfts(address, chainId, this.account.private.hex),
           getNftCollectionNfts(address, chainId, this.account.private.hex, 10),
           this.writeArchivist(),
         ])
-        const sources = await Promise.all(nfts.map((nft) => PayloadHasher.hashAsync(nft)))
-        if (writeArchivist) await writeArchivist.insert(nfts)
+        const [sources] = await Promise.all([
+          // Hash all the payloads
+          Promise.all(nfts.map((nft) => PayloadHasher.hashAsync(nft))),
+          // Insert them into the archivist if we have one
+          archivist ? archivist.insert(nfts) : Promise.resolve(),
+        ])
         return { ...info, schema: NftCollectionSchema, sources, total }
       }),
     )

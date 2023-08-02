1f2fec2d00f6300fc661213b074704d829b9f169
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-08-02 07:19:37 -0500

    Update test with real data

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/packages/collection/witness/src/lib/spec/getNftCollectionInfo.spec.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/packages/collection/witness/src/lib/spec/getNftCollectionInfo.spec.ts
@@ -4,21 +4,11 @@ import { HDWallet } from '@xyo-network/account'
 import { getNftCollectionInfo } from '../getNftCollectionInfo'
 
 describeIf(process.env.INFURA_PROJECT_ID)('getNftCollectionInfo', () => {
-  const address = '0xacdaEEb57ff6886fC8e203B9Dd4C2b241DF89b7a'
+  const address = '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'
   const chainId = 1
   test('gets NFTs owned by the address', async () => {
     const { privateKey } = await HDWallet.random()
-    const nfts = await getNftCollectionInfo(address, chainId, privateKey)
-    expect(nfts.length).toBeGreaterThan(0)
-    for (let i = 0; i < nfts.length; i++) {
-      const nft = nfts[i]
-      expect(nft.contract).toBeString()
-      expect(nft.supply).toBeString()
-      if (nft?.metadata) {
-        expect(nft.metadata).toBeObject()
-      }
-      expect(nft.tokenId).toBeString()
-      expect(nft.type).toBeString()
-    }
+    const result = await getNftCollectionInfo(address, chainId, privateKey)
+    expect(result).toBeObject()
   })
 })

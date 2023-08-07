8763d57b4fa17627d31c99bf7d8d7fa0660ff870
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-08-07 15:51:56 -0500

    Edge cases for probability distributions

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/packages/collection/witness/src/lib/collectionMetrics/lib/probablityDistributions/binomial/spec/calculateBinomialParams.spec.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/packages/collection/witness/src/lib/collectionMetrics/lib/probablityDistributions/binomial/spec/calculateBinomialParams.spec.ts
@@ -4,6 +4,9 @@ describe('calculateBinomialParams', () => {
   const data: [values: number[], outcome: BinomialDistributionParameters][] = [
     [[1, 0], { mean: 1, p: 0.5, stdDev: 0.7071067811865476, variance: 0.5 }],
     [[1, 1, 0, 0], { mean: 2, p: 0.5, stdDev: 1, variance: 1 }],
+    [[1, 0, 0, 0], { mean: 1, p: 0.25, stdDev: 0.8660254037844386, variance: 0.75 }],
+    [[1, 1, 1, 1], { mean: 4, p: 1, stdDev: 0, variance: 0 }],
+    [[0, 0, 0, 0], { mean: 0, p: 0, stdDev: 0, variance: 0 }],
   ]
   it.each(data)('calculates the params', (values, outcome) => {
     const result = calculateBinomialParams(values)

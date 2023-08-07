fda4988daf005b6e5a9b8ba1c21d8f8e81e29042
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-08-07 17:09:35 -0500

    Calculate binomial probability of individual trait

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/packages/collection/witness/src/lib/collectionMetrics/getNftCollectionMetrics.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/packages/collection/witness/src/lib/collectionMetrics/getNftCollectionMetrics.ts
@@ -1,34 +1,50 @@
 import { NftInfo, OpenSeaNftAttribute } from '@xyo-network/crypto-nft-payload-plugin'
 
-import { BinomialDistributionParameters, calculateAllPropertiesDistribution, Distribution } from './lib'
+import { BinomialDistributionParameters, calculateAllPropertiesDistribution } from './lib'
 
-export interface NftAttributeMetrics {
-  binomial: BinomialDistributionParameters
+export type Foo = { [key: string]: NftTraitMetrics }
+
+export interface NftTraitMetrics {
+  binomial: Partial<BinomialDistributionParameters>
   count: number
 }
 
 export interface NftCollectionMetrics {
-  distribution: {
+  metrics: {
     metadata: {
-      attributes: Distribution<{
-        [key: string]: string | number
-      }>
+      attributes: {
+        [trait: string]: {
+          [value: string]: NftTraitMetrics
+        }
+      }
     }
   }
 }
 
 export const getNftCollectionMetrics = (nfts: NftInfo[]): NftCollectionMetrics => {
-  const attributes = nfts
+  const traits = nfts
     .map((nft) => nft.metadata?.attributes as OpenSeaNftAttribute[])
     .map((attributes) => {
       return Object.fromEntries(attributes.map((attribute) => [attribute.trait_type, attribute.value]))
     })
-  const distribution = calculateAllPropertiesDistribution(attributes)
-  return {
-    distribution: {
-      metadata: {
-        attributes: distribution,
-      },
-    },
-  }
+  const distribution = calculateAllPropertiesDistribution(traits)
+  const attributes: { [trait: string]: { [value: string]: NftTraitMetrics } } = Object.fromEntries(
+    Object.entries(distribution)
+      .filter((v): v is [string, { [key: string]: number }] => {
+        return v[1] !== undefined
+      })
+      .map(([trait, value]) => {
+        const values = Object.fromEntries(
+          Object.entries(value).map(([value, count]) => {
+            const binomial = {
+              p: count / nfts.length,
+            }
+            const metrics: NftTraitMetrics = { binomial, count }
+            return [value, metrics]
+          }),
+        )
+        return [trait, { ...values }]
+      }),
+  )
+  return { metrics: { metadata: { attributes } } }
 }

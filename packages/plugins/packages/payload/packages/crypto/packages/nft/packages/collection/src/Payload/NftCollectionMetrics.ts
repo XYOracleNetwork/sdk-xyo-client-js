d039a2443f3bd98d15b1f97bedb3b289711cdf53
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-08-07 22:29:39 -0500

    Remove doubly-nested property

--- packages/plugins/packages/payload/packages/crypto/packages/nft/packages/collection/src/Payload/NftCollectionMetrics.ts
+++ packages/plugins/packages/payload/packages/crypto/packages/nft/packages/collection/src/Payload/NftCollectionMetrics.ts
@@ -1,14 +1,12 @@
 import { NftTraitMetrics } from './NftTraitMetrics'
 
 export interface NftCollectionMetrics {
-  metrics: {
-    metadata: {
-      attributes: {
-        [trait: string]: {
-          metrics: NftTraitMetrics
-          values: {
-            [value: string]: NftTraitMetrics
-          }
+  metadata: {
+    attributes: {
+      [trait: string]: {
+        metrics: NftTraitMetrics
+        values: {
+          [value: string]: NftTraitMetrics
         }
       }
     }

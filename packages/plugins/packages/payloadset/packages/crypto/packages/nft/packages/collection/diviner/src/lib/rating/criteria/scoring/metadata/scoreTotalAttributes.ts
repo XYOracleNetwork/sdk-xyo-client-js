32366aa35be88068aca6a715d35cf5cf4ff7891d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-08-08 09:07:05 -0500

    Normalize possible attribute score to range

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/packages/collection/diviner/src/lib/rating/criteria/scoring/metadata/scoreTotalAttributes.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/packages/collection/diviner/src/lib/rating/criteria/scoring/metadata/scoreTotalAttributes.ts
@@ -1,11 +1,16 @@
 import { NftCollectionAttributeMetrics } from '@xyo-network/crypto-nft-collection-payload-plugin'
 import { Score } from '@xyo-network/crypto-nft-score-model'
 
+const maxScore = 10
+
 export const scoreTotalAttributes = (info: NftCollectionAttributeMetrics): Score => {
   const { attributes } = info.metrics.metadata
   const scores = Object.entries(attributes).map<Score>(([_trait, { metrics }]) => {
     const rarity = Math.floor((1 - metrics.binomial.p) * 100)
     return [rarity, 100]
   })
-  return scores.reduce<Score>(([a, b], [c, d]) => [a + c, b + d], [0, 0])
+  const total = scores.reduce<Score>(([a, b], [c, d]) => [a + c, b + d], [0, 0])
+  const [totalScore, totalPossible] = total
+  const score = Math.min(Math.round((totalScore / totalPossible) * maxScore), maxScore)
+  return [score, maxScore]
 }

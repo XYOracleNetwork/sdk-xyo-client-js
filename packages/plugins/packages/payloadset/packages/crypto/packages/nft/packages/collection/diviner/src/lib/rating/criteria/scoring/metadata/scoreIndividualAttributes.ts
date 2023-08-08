7d56b8aa9c0bc39cfd178bbe547123c86366a561
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-08-07 22:26:00 -0500

    Score individual and total attributes

--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/packages/collection/diviner/src/lib/rating/criteria/scoring/metadata/scoreIndividualAttributes.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/packages/collection/diviner/src/lib/rating/criteria/scoring/metadata/scoreIndividualAttributes.ts
@@ -1,6 +1,11 @@
 import { NftCollectionAttributeMetrics } from '@xyo-network/crypto-nft-collection-payload-plugin'
 import { Score } from '@xyo-network/crypto-nft-score-model'
 
-export const scoreIndividualAttributes = (_attribute: NftCollectionAttributeMetrics): Score => {
-  return [0, 1]
+export const scoreIndividualAttributes = (info: NftCollectionAttributeMetrics): Score => {
+  const { values } = info.metrics.metrics.metadata.attributes
+  const scores = Object.entries(values.values).map<Score>(([_, metrics]) => {
+    const rarity = 1 - metrics.binomial.p
+    return [rarity * 100, 100]
+  })
+  return scores.reduce<Score>(([a, b], [c, d]) => [a + c, b + d], [0, 0])
 }
--- packages/plugins/packages/payloadset/packages/crypto/packages/nft/packages/collection/diviner/src/lib/rating/criteria/scoring/metadata/scoreTotalAttributes.ts
+++ packages/plugins/packages/payloadset/packages/crypto/packages/nft/packages/collection/diviner/src/lib/rating/criteria/scoring/metadata/scoreTotalAttributes.ts
@@ -1,6 +1,8 @@
 import { NftCollectionAttributeMetrics } from '@xyo-network/crypto-nft-collection-payload-plugin'
 import { Score } from '@xyo-network/crypto-nft-score-model'
 
-export const scoreTotalAttributes = (_attribute: NftCollectionAttributeMetrics): Score => {
-  return [0, 1]
+export const scoreTotalAttributes = (info: NftCollectionAttributeMetrics): Score => {
+  const { metrics } = info.metrics.metrics.metadata.attributes
+  const rarity = 1 - metrics.metrics.binomial.p
+  return [rarity * 100, 100]
 }

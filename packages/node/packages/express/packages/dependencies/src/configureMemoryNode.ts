afdefd9d4f1f92788486326d99816a2a1b449d67
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-08-10 06:51:46 -0500

    Enable diviners

--- packages/node/packages/express/packages/dependencies/src/configureMemoryNode.ts
+++ packages/node/packages/express/packages/dependencies/src/configureMemoryNode.ts
@@ -5,6 +5,8 @@ import { ArchivistConfigSchema, ArchivistInsertQuerySchema, isArchivistInstance,
 import { PayloadHasher } from '@xyo-network/core'
 import { NftCollectionScoreDivinerConfigSchema, NftCollectionWitnessConfigSchema } from '@xyo-network/crypto-nft-collection-payload-plugin'
 import { NftScoreDivinerConfigSchema, NftWitnessConfigSchema } from '@xyo-network/crypto-nft-payload-plugin'
+import { BoundWitnessDivinerConfigSchema } from '@xyo-network/diviner-boundwitness-model'
+import { PayloadDivinerConfigSchema } from '@xyo-network/diviner-payload-model'
 import { ImageThumbnailWitnessConfigSchema } from '@xyo-network/image-thumbnail-plugin'
 import { AnyConfigSchema, CreatableModuleDictionary, ModuleConfig } from '@xyo-network/module-model'
 import { TYPES } from '@xyo-network/node-core-types'
@@ -24,11 +26,11 @@ const diviners: ModuleConfigWithVisibility[] = [
   // [{ schema: AddressHistoryDivinerConfigSchema }, true],
   // [{ schema: AddressSpaceDivinerConfigSchema }, true],
   // [{ schema: AddressSpaceBatchDivinerConfigSchema }, true],
-  // [{ schema: BoundWitnessDivinerConfigSchema }, true],
+  [{ schema: BoundWitnessDivinerConfigSchema }, true],
   // [{ schema: BoundWitnessStatsDivinerConfigSchema }, true],
   [{ schema: NftCollectionScoreDivinerConfigSchema }, true],
   [{ schema: NftScoreDivinerConfigSchema }, true],
-  // [{ schema: PayloadDivinerConfigSchema }, true],
+  [{ schema: PayloadDivinerConfigSchema }, true],
   // [{ schema: PayloadStatsDivinerConfigSchema }, true],
   // [{ schema: SchemaListDivinerConfigSchema }, true],
   // [{ schema: SchemaStatsDivinerConfigSchema }, true],

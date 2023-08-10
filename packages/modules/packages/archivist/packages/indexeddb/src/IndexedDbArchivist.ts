e38aa05c1a410ae04b2e8c5b08d46ea22b23b3bc
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-08-10 14:56:28 -0500

    Indexed DB read/write through optimizations

--- packages/modules/packages/archivist/packages/indexeddb/src/IndexedDbArchivist.ts
+++ packages/modules/packages/archivist/packages/indexeddb/src/IndexedDbArchivist.ts
@@ -98,6 +98,9 @@ export class IndexedDbArchivist<
 
   protected override async getHandler(hashes: string[]): Promise<Payload[]> {
     const found = await getMany<Payload>(hashes, this.db)
+    // If we found everything or we don't have parents, return what we have
+    if (found.length === hashes.length || !(await this.parents())?.read) return found
+    // Otherwise, check parents for any hashes we were unable to find
     const foundHashes = await Promise.all(found.map(PayloadHasher.hashAsync))
     const notfound = hashes.filter((hash) => !foundHashes.includes(hash))
     const parentFound = notfound.length > 0 ? await super.getHandler(notfound) : []

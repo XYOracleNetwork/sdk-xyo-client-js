aaef37c9d551aa5bb91ffa4efb75af120e4586e8
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-08-11 17:34:15 -0500

    Add test cases for real-world use cases

--- packages/plugins/packages/payloadset/packages/image-thumbnail/src/spec/Witness.spec.ts
+++ packages/plugins/packages/payloadset/packages/image-thumbnail/src/spec/Witness.spec.ts
@@ -2,9 +2,16 @@ import { ImageThumbnailWitness } from '../Witness'
 
 describe('Witness', () => {
   describe('checkIpfsUrl', () => {
-    const cases: [uri: string, expected: string][] = [['', '']]
+    const cases: [uri: string, expected: string][] = [
+      [
+        'ipfs://ipfs/QmewywDQGqz9WuWfT11ueSR3Mu86MejfD64v3KtFRzGP2G/image.jpeg',
+        'https://cloudflare-ipfs.com/ipfs/QmewywDQGqz9WuWfT11ueSR3Mu86MejfD64v3KtFRzGP2G/image.jpeg',
+      ],
+      ['ipfs://QmWX3Kx2NX3AK8WxTQwktVYLMFHX3pHm77ThynhgmU8dP8', 'https://cloudflare-ipfs.com/ipfs/QmWX3Kx2NX3AK8WxTQwktVYLMFHX3pHm77ThynhgmU8dP8'],
+    ]
     it.each(cases)('%s', (input, expected) => {
-      ImageThumbnailWitness.checkIpfsUrl(input)
+      const actual = ImageThumbnailWitness.checkIpfsUrl(input)
+      expect(actual).toBe(expected)
     })
   })
 })

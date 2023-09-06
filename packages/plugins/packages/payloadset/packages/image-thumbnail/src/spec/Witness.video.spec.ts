888bc5a9174746522dede718cae1fd2b2a4690ca
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-09-06 17:34:48 -0500

    Passing video conversion tests

--- packages/plugins/packages/payloadset/packages/image-thumbnail/src/spec/Witness.video.spec.ts
+++ packages/plugins/packages/payloadset/packages/image-thumbnail/src/spec/Witness.video.spec.ts
@@ -14,29 +14,33 @@ describeIfHasBin('magick')('ImageThumbnailWitness', () => {
     beforeAll(async () => {
       witness = await ImageThumbnailWitness.create({ account: await HDWallet.random() })
     })
-    const mimeTypes = [
-      'video/3gpp',
-      'video/3gpp2',
-      'video/h264',
-      'video/h265',
-      'video/mp2t',
-      'video/mp4',
-      'video/mpeg',
-      'video/ogg',
-      'video/quicktime',
-      'video/vnd.rn-realvideo',
-      'video/webm',
-      'video/x-flv',
-      'video/x-m4v',
-      'video/x-matroska',
-      'video/x-ms-wmv',
-      'video/x-msvideo',
+    const cases: [mime: string, url: string][] = [
+      // ['video/3gpp', 'https://filesamples.com/samples/video/3gp/sample_640x360.3gp'],
+      // ['video/3gpp2', ''],
+      // ['video/h264', ''],
+      // ['video/h265', ''],
+      // MPEG-2 Transport Stream
+      // ['video/mp2t', 'https://filesamples.com/samples/video/m2ts/sample_640x360.m2ts'],
+      ['video/mp4', 'https://cdn-longterm.mee6.xyz/assets/avatars-presale.mp4'],
+      // MPEG Moving Picture Experts Group Phase 1
+      ['video/mpeg', 'https://filesamples.com/samples/video/mpeg/sample_640x360.mpeg'],
+      // ['video/ogg', 'https://filesamples.com/samples/video/ogv/sample_640x360.ogv'],
+      // ['video/quicktime', 'https://filesamples.com/samples/video/mov/sample_640x360.mov'],
+      // ['video/vnd.rn-realvideo', ''],
+      ['video/webm', 'https://filesamples.com/samples/video/webm/sample_640x360.webm'],
+      // ['video/x-flv', ''],
+      // MPEG-4 Video
+      // ['video/x-m4v', 'https://filesamples.com/samples/video/m4v/sample_640x360.m4v'],
+      // ['video/x-matroska', 'https://filesamples.com/samples/video/mkv/sample_640x360.mkv'],
+      // Windows Media Video
+      ['video/x-ms-wmv', 'https://filesamples.com/samples/video/wmv/sample_640x360.wmv'],
+      // AVI (video/vnd.avi, video/avi, video/msvideo, video/x-msvideo)
+      ['video/x-msvideo', 'https://filesamples.com/samples/video/avi/sample_640x360.avi'],
     ]
-    const cases = [
-      'https://cdn-longterm.mee6.xyz/assets/avatars-presale.mp4',
+    const problemCases = [
       // 'https://media.niftygateway.com/video/upload/v1649189105/Abigail/FEWO/Paint/Paint/006266_paint_hf9cft.mp4',
     ]
-    testIfHasBin('ffmpeg').each(cases)('HTTPS [large/mp4 (animated)]', async (url) => {
+    testIfHasBin('ffmpeg').each(cases)('video [mime (%s)]', async (mime, url) => {
       const httpsPayload: UrlPayload = { schema: UrlSchema, url }
       const result = (await witness.observe([httpsPayload])) as ImageThumbnail[]
       expect(result.length).toBe(1)

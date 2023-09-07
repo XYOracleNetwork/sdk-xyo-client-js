5ee5aa4c3e9f68576c9e33b4a334308c65b69e8c
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-09-06 22:20:43 -0500

    Handle EOF error via temp file

--- packages/plugins/packages/payloadset/packages/image-thumbnail/src/Witness/ffmpeg/fluent/getVideoFrameAsImageFluent.ts
+++ packages/plugins/packages/payloadset/packages/image-thumbnail/src/Witness/ffmpeg/fluent/getVideoFrameAsImageFluent.ts
@@ -1,55 +1,51 @@
+import { uuid } from '@xyo-network/core'
 import ffmpeg from 'fluent-ffmpeg'
-import { Readable, Writable } from 'stream'
-
+import { unlink, writeFile } from 'fs/promises'
+import { tmpdir } from 'os'
+import { Writable } from 'stream'
 /**
  * Execute FFmpeg using fluent API with provided input buffer and video thumbnail image.
  * @param videoBuffer Input video buffer.
  * @returns Output buffer containing the video thumbnail image.
  */
 export const getVideoFrameAsImageFluent = async (videoBuffer: Buffer) => {
-  const imageBuffer = await new Promise<Buffer>((resolve, reject) => {
-    // const videoStream = new PassThrough()
-    // videoStream.end(videoBuffer)
+  const tmpFile = `/${tmpdir()}/${uuid()}.mp4`
+  try {
+    await writeFile(tmpFile, videoBuffer, { encoding: 'binary' })
+    const imageBuffer = await new Promise<Buffer>((resolve, reject) => {
+      // Initialize empty array to collect PNG chunks
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const pngChunks: any[] = []
 
-    const videoStream = new Readable({
-      read() {
-        this.push(videoBuffer)
-        this.push(null)
-      },
+      // Create a Writable stream to collect PNG output from ffmpeg
+      const writableStream = new Writable({
+        write(chunk, encoding, callback) {
+          pngChunks.push(chunk)
+          callback()
+        },
+      })
+
+      const command = ffmpeg()
+        // Uncomment to debug CLI args to ffmpeg
+        // .on('start', (commandLine) => console.log('Spawned Ffmpeg with command: ' + commandLine))
+        .input(tmpFile)
+        .takeFrames(1)
+        .withNoAudio()
+        .on('error', (err) => reject(err.message))
+        // Listen for the 'end' event to combine the chunks and create a PNG buffer
+        .on('end', () => resolve(Buffer.concat(pngChunks)))
+        .outputOptions('-f image2pipe')
+
+      // Start processing
+      command.pipe(writableStream)
     })
-
-    // Initialize empty array to collect PNG chunks
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const pngChunks: any[] = []
-
-    // Create a Writable stream to collect PNG output from ffmpeg
-    const writableStream = new Writable({
-      write(chunk, encoding, callback) {
-        pngChunks.push(chunk)
-        callback()
-      },
-    })
-
-    const command = ffmpeg()
-      // Uncomment to debug CLI args to ffmpeg
-      // .on('start', (commandLine) => console.log('Spawned Ffmpeg with command: ' + commandLine))
-      .input(videoStream)
-      .takeFrames(1)
-      .withNoAudio()
-      // Exactly as their docs but does not work
-      // .setStartTime('00:00:00')
-      // .seekInput(0)
-      .on('error', (err) => reject(err.message))
-      // Listen for the 'end' event to combine the chunks and create a PNG buffer
-      .on('end', () => resolve(Buffer.concat(pngChunks)))
-      .on('data', (chunk) => pngChunks.push(chunk))
-      // .toFormat('png')
-      .outputOptions('-f image2pipe')
-    // .outputOptions('-vcodec png')
-    // .output(writableStream, { end: true })
-
-    // Start processing
-    command.pipe(writableStream)
-  })
-  return imageBuffer
+    return imageBuffer
+  } finally {
+    // Temp file cleanup
+    try {
+      await unlink(tmpFile)
+    } catch {
+      // No error here since file doesn't exist
+    }
+  }
 }

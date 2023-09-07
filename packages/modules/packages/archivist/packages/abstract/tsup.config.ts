5902d4807e9e00e63cc979e4ffa1ffbef3ad4e3d
Arie Trouw <arie.trouw@xylabs.com>
2023-09-07 12:24:51 -0700

    tsup experiment in one package

---
+++ packages/modules/packages/archivist/packages/abstract/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup'
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+  bundle: false,
+  cjsInterop: true,
+  clean: true,
+  dts: true,
+  entry: ['src'],
+  format: ['cjs', 'esm'],
+  sourcemap: true,
+  splitting: false,
+  tsconfig: 'tsconfig.json',
+})

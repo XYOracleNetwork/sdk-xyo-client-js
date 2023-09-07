0d92ae4a7e82e99da780c273884a6d2df7f84bdb
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-09-06 23:38:24 -0500

    Comments & documentation

--- packages/plugins/packages/payloadset/packages/image-thumbnail/src/Witness/ffmpeg/fluent/getVideoFrameAsImageFluent.ts
+++ packages/plugins/packages/payloadset/packages/image-thumbnail/src/Witness/ffmpeg/fluent/getVideoFrameAsImageFluent.ts
@@ -9,40 +9,43 @@ import { Writable } from 'stream'
  * @returns Output buffer containing the video thumbnail image.
  */
 export const getVideoFrameAsImageFluent = async (videoBuffer: Buffer) => {
+  // Get a temp file name
   const tmpFile = `/${tmpdir()}/${uuid()}.mp4`
   try {
+    // Write videoBuffer to temp file for use as input to ffmpeg to
+    // avoid issues with ffmpeg inferring premature EOF from buffer
+    // passed via stdin (happens when ffmpeg is trying to infer
+    // input video format)
     await writeFile(tmpFile, videoBuffer, { encoding: 'binary' })
     const imageBuffer = await new Promise<Buffer>((resolve, reject) => {
       // Initialize empty array to collect PNG chunks
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const pngChunks: any[] = []
+      const pngChunks: Uint8Array[] = []
 
       // Create a Writable stream to collect PNG output from ffmpeg
-      const writableStream = new Writable({
-        write(chunk, encoding, callback) {
+      const ffmpegPngOutput = new Writable({
+        write(chunk, _encoding, callback) {
           pngChunks.push(chunk)
           callback()
         },
       })
 
-      const command = ffmpeg()
-        // Uncomment to debug CLI args to ffmpeg
+      ffmpeg()
+        // NOTE: Uncomment to debug CLI args to ffmpeg
         // .on('start', (commandLine) => console.log('Spawned Ffmpeg with command: ' + commandLine))
         .on('error', (err) => reject(err.message))
-        // Listen for the 'end' event to combine the chunks and create a PNG buffer
+        // Listen for the 'end' event to combine the chunks to buffer holding the PNG image
         .on('end', () => resolve(Buffer.concat(pngChunks)))
-        .input(tmpFile)
-        .takeFrames(1)
-        .withNoAudio()
-        .outputOptions('-f image2pipe')
-        .videoCodec('png')
-
-      // Start processing
-      command.pipe(writableStream)
+        .input(tmpFile) // Use temp file as input
+        .takeFrames(1) // Only take 1st video frame
+        .withNoAudio() // Don't include audio
+        .outputOptions('-f image2pipe') // Write output to stdout
+        .videoCodec('png') // Force PNG output
+        // Start processing and direct ffmpeg stdout to writable stream
+        .pipe(ffmpegPngOutput)
     })
     return imageBuffer
   } finally {
-    // Temp file cleanup
+    // Cleanup temp file
     try {
       await unlink(tmpFile)
     } catch {

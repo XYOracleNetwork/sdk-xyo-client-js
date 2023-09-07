6bf5ac646aad1e22c299bdd40f0c20cad323d87f
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-09-06 22:58:29 -0500

    Support flash video

--- packages/plugins/packages/payloadset/packages/image-thumbnail/src/spec/Witness.video.spec.ts
+++ packages/plugins/packages/payloadset/packages/image-thumbnail/src/spec/Witness.video.spec.ts
@@ -1,6 +1,7 @@
 import { HDWallet } from '@xyo-network/account'
 import { ImageThumbnail, ImageThumbnailSchema } from '@xyo-network/image-thumbnail-payload-plugin'
 import { UrlPayload, UrlSchema } from '@xyo-network/url-payload-plugin'
+import FileType from 'file-type'
 import { sync as hasbin } from 'hasbin'
 
 import { ImageThumbnailWitness } from '../Witness'
@@ -9,12 +10,16 @@ const describeIfHasBin = (bin: string) => (hasbin(bin) ? describe : describe.ski
 
 type MimeWithUrl = [mime: string, url: string]
 
-const testVideo = async (witness: ImageThumbnailWitness, url: string) => {
+const testVideoFormat = async (witness: ImageThumbnailWitness, url: string) => {
   const httpsPayload: UrlPayload = { schema: UrlSchema, url }
   const result = (await witness.observe([httpsPayload])) as ImageThumbnail[]
   expect(result.length).toBe(1)
-  expect(result[0].url?.length).toBeLessThan(64000)
   expect(result[0].schema).toBe(ImageThumbnailSchema)
+  expect(result[0].url?.length).toBeLessThan(64000)
+  const imageData = result[0].url?.split(',')[1] ?? ''
+  const buffer = Buffer.from(Uint8Array.from(atob(imageData), (c) => c.charCodeAt(0)))
+  const fileType = await FileType.fromBuffer(buffer)
+  expect(fileType?.mime).toBe('image/png')
 }
 
 describeIfHasBin('magick')('ImageThumbnailWitness', () => {
@@ -28,35 +33,35 @@ describeIfHasBin('magick')('ImageThumbnailWitness', () => {
         // Cases
         ['video/3gpp', 'https://filesamples.com/samples/video/3gp/sample_640x360.3gp'],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
     describe.skip('3GPP2', () => {
       const cases: MimeWithUrl[] = [
         // Cases
         ['video/3gpp2', ''],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
     describe.skip('Advanced Video Coding / H.264', () => {
       const cases: MimeWithUrl[] = [
         // Cases
         ['video/h264', ''],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
     describe.skip('High Efficiency Video Coding / H.265', () => {
       const cases: MimeWithUrl[] = [
         // Cases
         ['video/h265', ''],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
     describe.skip('MPEG-2 Transport Stream', () => {
       const cases: MimeWithUrl[] = [
         // Cases
         ['video/mp2t', 'https://filesamples.com/samples/video/m2ts/sample_640x360.m2ts'],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
     describe('MPEG-4 Part 14', () => {
       const cases: MimeWithUrl[] = [
@@ -64,70 +69,70 @@ describeIfHasBin('magick')('ImageThumbnailWitness', () => {
         ['video/mp4', 'https://cdn-longterm.mee6.xyz/assets/avatars-presale.mp4'],
         ['video/mp4;codecs=avc1', 'https://media.niftygateway.com/video/upload/v1649189105/Abigail/FEWO/Paint/Paint/006266_paint_hf9cft.mp4'],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
     describe('MPEG Moving Picture Experts Group Phase 1', () => {
       const cases: MimeWithUrl[] = [
         // Cases
         ['video/mpeg', 'https://filesamples.com/samples/video/mpeg/sample_640x360.mpeg'],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
     describe.skip('OGG', () => {
       const cases: MimeWithUrl[] = [
         // Cases
         ['video/ogg', 'https://filesamples.com/samples/video/ogv/sample_640x360.ogv'],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
     describe('QuickTime File Format (QTFF)', () => {
       const cases: MimeWithUrl[] = [
         // Cases
         ['video/quicktime', 'https://filesamples.com/samples/video/mov/sample_640x360.mov'],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
     describe.skip('RealMedia', () => {
       const cases: MimeWithUrl[] = [
         // Cases
         ['video/vnd.rn-realvideo', '.rm'],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
     describe('WebM', () => {
       const cases: MimeWithUrl[] = [
         // Cases
         ['video/webm', 'https://filesamples.com/samples/video/webm/sample_640x360.webm'],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
-    describe.skip('Flash Video', () => {
+    describe('Flash Video', () => {
       const cases: MimeWithUrl[] = [
         // Cases
-        ['video/x-flv', ''],
+        ['video/x-flv', 'https://filesamples.com/samples/video/flv/sample_640x360.flv'],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
     describe('MPEG-4 Video', () => {
       const cases: MimeWithUrl[] = [
         // Cases
         ['video/x-m4v', 'https://filesamples.com/samples/video/m4v/sample_640x360.m4v'],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
     describe.skip('Matroska Multimedia Container', () => {
       const cases: MimeWithUrl[] = [
         // Cases
         ['video/x-matroska', 'https://filesamples.com/samples/video/mkv/sample_640x360.mkv'],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
     describe('Windows Media Video', () => {
       const cases: MimeWithUrl[] = [
         // Cases
         ['video/x-ms-wmv', 'https://filesamples.com/samples/video/wmv/sample_640x360.wmv'],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
     describe('Audio Video Interleave', () => {
       const cases: MimeWithUrl[] = [
@@ -137,7 +142,7 @@ describeIfHasBin('magick')('ImageThumbnailWitness', () => {
         // ['video/msvideo', 'https://filesamples.com/samples/video/avi/sample_640x360.avi'],
         // ['video/x-msvideo', 'https://filesamples.com/samples/video/avi/sample_640x360.avi'],
       ]
-      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideo(witness, url))
+      it.each(cases)('video [mime (%s)]', async (_mime, url) => await testVideoFormat(witness, url))
     })
   })
 })

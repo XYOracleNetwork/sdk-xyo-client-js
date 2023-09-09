69fe694e854bafb2c37ba95618c1f37c7a183bdf
Arie Trouw <arie.trouw@xylabs.com>
2023-09-08 18:12:57 -0700

    more tsup done

---
+++ packages/sdk/packages/logger/tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup'
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+  bundle: true,
+  cjsInterop: true,
+  clean: true,
+  dts: {
+    entry: ['src/index.ts'],
+  },
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  sourcemap: true,
+  splitting: false,
+  tsconfig: 'tsconfig.json',
+})

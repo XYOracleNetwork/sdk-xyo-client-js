a2e6aae58e9451f0dd4d0c6514af44f05e4306a4
Arie Trouw <arie.trouw@xylabs.com>
2023-09-09 11:10:24 -0700

    payload set plugin tsup

---
+++ packages/plugins/packages/payloadset/packages/blockchain/packages/ethereum/packages/gas/tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup'
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+  bundle: true,
+  cjsInterop: true,
+  clean: false,
+  dts: {
+    entry: ['src/index.ts'],
+  },
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  sourcemap: true,
+  splitting: false,
+  tsconfig: 'tsconfig.json',
+})

f4ec4ea1737a0c56dc7d989f75bfa5a1aeb78fe4
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-09-11 06:54:42 -0500

    Remove async from boot

--- packages/node/packages/express/packages/dependencies/src/Module/addMemoryModules.ts
+++ packages/node/packages/express/packages/dependencies/src/Module/addMemoryModules.ts
@@ -6,10 +6,8 @@ import { addSentinelModuleFactories } from './Sentinel'
 import { addWitnessModuleFactories } from './Witness'
 
 export const addMemoryModules = async (container: Container) => {
-  // TODO: Prepare for async init
-  await Promise.resolve()
-  await addArchivistModuleFactories(container)
-  await addDivinerModuleFactories(container)
+  addArchivistModuleFactories(container)
+  addDivinerModuleFactories(container)
   await addWitnessModuleFactories(container)
   await addSentinelModuleFactories(container)
 }

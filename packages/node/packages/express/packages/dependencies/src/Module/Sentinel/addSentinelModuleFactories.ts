08ad32fa1a41fbe7076a10ea67f3b17529af29ae
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-09-11 08:00:44 -0500

    Remove hardcoded sentinel params

--- packages/node/packages/express/packages/dependencies/src/Module/Sentinel/addSentinelModuleFactories.ts
+++ packages/node/packages/express/packages/dependencies/src/Module/Sentinel/addSentinelModuleFactories.ts
@@ -1,21 +1,13 @@
 import { CreatableModuleDictionary, ModuleFactory } from '@xyo-network/module-model'
-import { TYPES, WALLET_PATHS } from '@xyo-network/node-core-types'
+import { TYPES } from '@xyo-network/node-core-types'
 import { MemorySentinel, SentinelConfigSchema } from '@xyo-network/sentinel'
 import { Container } from 'inversify'
 
-const getImageThumbnailSentinel = () => {
-  return new ModuleFactory(MemorySentinel, {
-    config: {
-      accountDerivationPath: WALLET_PATHS.Sentinels.ImageThumbnailSentinel,
-      archivist: 'ThumbnailArchivist',
-      name: TYPES.ThumbnailSentinel.description,
-      schema: SentinelConfigSchema,
-      witnesses: ['ImageThumbnailWitness', 'TimestampWitness'],
-    },
-  })
+const getSentinel = () => {
+  return new ModuleFactory(MemorySentinel, { config: { schema: SentinelConfigSchema } })
 }
 
 export const addSentinelModuleFactories = (container: Container) => {
   const dictionary = container.get<CreatableModuleDictionary>(TYPES.CreatableModuleDictionary)
-  dictionary[MemorySentinel.configSchema] = getImageThumbnailSentinel()
+  dictionary[MemorySentinel.configSchema] = getSentinel()
 }

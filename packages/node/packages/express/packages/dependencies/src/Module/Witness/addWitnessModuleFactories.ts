a905e923da34f17ff0286b3b49f8119b4357c6a6
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-09-11 07:45:26 -0500

    remove wallet imports in favor of config injection

--- packages/node/packages/express/packages/dependencies/src/Module/Witness/addWitnessModuleFactories.ts
+++ packages/node/packages/express/packages/dependencies/src/Module/Witness/addWitnessModuleFactories.ts
@@ -1,17 +1,11 @@
-import { HDWallet } from '@xyo-network/account'
 import { CryptoNftCollectionWitness } from '@xyo-network/crypto-nft-collection-witness-plugin'
 import { CryptoWalletNftWitness } from '@xyo-network/crypto-nft-witness-wallet-plugin'
 import { ImageThumbnailWitness } from '@xyo-network/image-thumbnail-plugin'
 import { CreatableModuleDictionary, ModuleFactory } from '@xyo-network/module-model'
-import { TYPES, WALLET_PATHS } from '@xyo-network/node-core-types'
+import { TYPES } from '@xyo-network/node-core-types'
 import { PrometheusNodeWitness } from '@xyo-network/prometheus-node-plugin'
 import { TimestampWitness, TimestampWitnessConfigSchema } from '@xyo-network/witness-timestamp'
 import { Container } from 'inversify'
-
-const getWallet = (container: Container) => {
-  const mnemonic = container.get<string>(TYPES.AccountMnemonic)
-  return HDWallet.fromMnemonic(mnemonic)
-}
 
 const getCryptoWalletNftWitness = () => {
   return new ModuleFactory(CryptoWalletNftWitness, { config: { schema: CryptoWalletNftWitness.configSchema } })

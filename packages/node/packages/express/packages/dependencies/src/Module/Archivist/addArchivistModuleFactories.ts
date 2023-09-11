d0450a9cbc1dc7e2b040afe60d68161acf73ab0c
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-09-11 06:54:14 -0500

    Remove hardcoded archivist params

--- packages/node/packages/express/packages/dependencies/src/Module/Archivist/addArchivistModuleFactories.ts
+++ packages/node/packages/express/packages/dependencies/src/Module/Archivist/addArchivistModuleFactories.ts
@@ -1,22 +1,17 @@
-import { HDWallet } from '@xyo-network/account'
 import { MemoryArchivist } from '@xyo-network/archivist'
 import { CreatableModuleDictionary, ModuleFactory } from '@xyo-network/module-model'
 import { MongoDBDeterministicArchivistConfigSchema } from '@xyo-network/node-core-modules-mongo'
-import { TYPES, WALLET_PATHS } from '@xyo-network/node-core-types'
+import { TYPES } from '@xyo-network/node-core-types'
 import { Container } from 'inversify'
 
-const getMongoDBArchivistFactory = async (container: Container) => {
-  const mnemonic = container.get<string>(TYPES.AccountMnemonic)
-  const wallet = await HDWallet.fromMnemonic(mnemonic)
-  const accountDerivationPath = WALLET_PATHS.Archivists.Archivist
+const getMongoDBArchivistFactory = () => {
   return new ModuleFactory(MemoryArchivist, {
-    config: { accountDerivationPath, name: TYPES.Archivist.description, schema: MemoryArchivist.configSchema },
-    wallet,
+    config: { schema: MemoryArchivist.configSchema },
   })
 }
 
-export const addArchivistModuleFactories = async (container: Container) => {
+export const addArchivistModuleFactories = (container: Container) => {
   const dictionary = container.get<CreatableModuleDictionary>(TYPES.CreatableModuleDictionary)
-  dictionary[MongoDBDeterministicArchivistConfigSchema] = await getMongoDBArchivistFactory(container)
-  dictionary[MemoryArchivist.configSchema] = await getMongoDBArchivistFactory(container)
+  dictionary[MongoDBDeterministicArchivistConfigSchema] = getMongoDBArchivistFactory()
+  dictionary[MemoryArchivist.configSchema] = getMongoDBArchivistFactory()
 }

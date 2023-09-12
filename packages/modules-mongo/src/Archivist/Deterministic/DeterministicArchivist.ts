2d1603352d0eee7b2218701dad3a31ce4c600b6d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-09-12 09:14:18 -0400

    Add mongo storage class labels

--- packages/modules-mongo/src/Archivist/Deterministic/DeterministicArchivist.ts
+++ packages/modules-mongo/src/Archivist/Deterministic/DeterministicArchivist.ts
@@ -8,6 +8,14 @@ export type MongoDBDeterministicArchivistConfig = MongoDBArchivistConfig
 
 export type MongoDBDeterministicArchivistParams = MongoDBArchivistParams
 
+interface Labels<T extends Record<string, string>> {
+  labels: T
+}
+
+interface MongoStorageClass {
+  storageClass: 'mongodb'
+}
+
 export class MongoDBDeterministicArchivist<
   TParams extends MongoDBDeterministicArchivistParams = MongoDBDeterministicArchivistParams,
 > extends MongoDBArchivist<TParams> {

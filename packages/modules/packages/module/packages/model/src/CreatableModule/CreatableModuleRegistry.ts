5ae545b0ad0ce159dc6247c0acbd81140ee01880
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-09-13 09:19:26 -0400

    Transformer for conversion between dict/registry

--- packages/modules/packages/module/packages/model/src/CreatableModule/CreatableModuleRegistry.ts
+++ packages/modules/packages/module/packages/model/src/CreatableModule/CreatableModuleRegistry.ts
@@ -1,6 +1,14 @@
 import { CreatableModuleFactory } from './CreatableModule'
+import { CreatableModuleDictionary } from './CreatableModuleDictionary'
 import { LabeledCreatableModuleFactory } from './LabeledCreatableModuleFactory'
 
 export interface CreatableModuleRegistry {
   [key: string]: (CreatableModuleFactory | LabeledCreatableModuleFactory)[] | undefined
+}
+
+export const toCreatableModuleRegistry = (dict: CreatableModuleDictionary): CreatableModuleRegistry => {
+  return Object.entries(dict).reduce((registry, [schema, factory]) => {
+    registry[schema] = [factory]
+    return registry
+  }, {} as CreatableModuleRegistry)
 }

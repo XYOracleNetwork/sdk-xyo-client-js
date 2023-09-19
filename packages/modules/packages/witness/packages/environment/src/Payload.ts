66de1bc8bae2452cff75234831867096a401947b
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-09-19 06:14:01 -0500

    Add environment witness

--- packages/modules/packages/witness/packages/environment/src/Payload.ts
+++ packages/modules/packages/witness/packages/environment/src/Payload.ts
@@ -3,9 +3,13 @@ import { Payload } from '@xyo-network/payload-model'
 export const EnvironmentSchema = 'network.xyo.environment'
 export type EnvironmentSchema = typeof EnvironmentSchema
 
-export type TimeStamp = Payload<
+export interface EnvironmentVariables {
+  [key: string]: string | undefined
+}
+
+export type Environment = Payload<
   {
-    environment: number
+    environment: EnvironmentVariables
   },
   EnvironmentSchema
 >
--- packages/modules/packages/witness/packages/environment/src/Witness.ts
+++ packages/modules/packages/witness/packages/environment/src/Witness.ts
@@ -5,13 +5,13 @@ import { WitnessConfigSchema } from '@xyo-network/witness-model'
 
 import { EnvironmentWitnessConfigSchema } from './Config'
 import { EnvironmentWitnessParams } from './Params'
-import { EnvironmentSchema, TimeStamp } from './Payload'
+import { Environment, EnvironmentSchema } from './Payload'
 
 export class EnvironmentWitness<P extends EnvironmentWitnessParams = EnvironmentWitnessParams> extends AbstractWitness<P> {
   static override configSchemas = [EnvironmentWitnessConfigSchema, WitnessConfigSchema]
   protected override async observeHandler(payloads?: Payload[]): Promise<Payload[]> {
     const sources = await PayloadWrapper.hashes(payloads ?? [])
-    const payload: TimeStamp = { environment: Date.now(), schema: EnvironmentSchema, sources }
+    const payload: Environment = { environment: process.env, schema: EnvironmentSchema, sources }
     return [payload]
   }
 }

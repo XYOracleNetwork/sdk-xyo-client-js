7baa54c6ef52c8c6461e1b3c082d02f4d260478a
Arie Trouw <arie.trouw@xylabs.com>
2023-09-20 14:43:39 -0700

    Payload type including PayloadFields

--- packages/protocol/packages/payload/packages/model/src/Payload.ts
+++ packages/protocol/packages/payload/packages/model/src/Payload.ts
@@ -22,16 +22,16 @@ export type WithPayload<T extends EmptyPayload | void = void> = WithSchema<T ext
 export type Payload<T extends void | object | WithSchema = void, S extends Schema | void = void> = T extends WithSchema
   ? S extends Schema
     ? /* T (w/Schema) & S provided */
-      WithPayload<Omit<T, 'schema'> & { schema: S }>
+      WithPayload<Omit<T, 'schema'> & { schema: S } & PayloadFields>
     : /* Only T (w/Schema) provided */
       WithPayload<T>
   : T extends object
   ? S extends Schema
     ? /* T (w/o Schema) & S provided */
-      WithPayload<T & { schema: S }>
+      WithPayload<T & { schema: S } & PayloadFields>
     : /* Only T (w/o Schema) provided */
-      WithPayload<T & { schema: Schema }>
+      WithPayload<T & { schema: Schema } & PayloadFields>
   : /* Either just S or neither S or T provided */
     {
       schema: S extends Schema ? S : Schema
-    }
+    } & PayloadFields

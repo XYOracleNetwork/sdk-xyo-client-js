ae5d7118884d968f82f75dc8b193d720f77afb0d
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-09-20 23:03:53 -0400

    EIP-1193 event listeners

--- packages/sdk/packages/metamask-connector/src/MetaMaskConnector.ts
+++ packages/sdk/packages/metamask-connector/src/MetaMaskConnector.ts
@@ -1,11 +1,12 @@
-import { Listener, Web3Provider } from '@ethersproject/providers'
-import { MetaMaskInpageProvider } from '@metamask/providers'
+import { Listener, Web3Provider } from '@ethersproject/providers';
+import { MetaMaskInpageProvider } from '@metamask/providers';
 
 export class MetaMaskConnector {
   private account = ''
   private ethereum = window.ethereum as MetaMaskInpageProvider
 
   private listeners: Listener[] = []
+  private providerListeners: [event: string, listener: Listener][] = []
   private provider: Web3Provider | undefined
 
   constructor(provider?: Web3Provider) {
@@ -40,6 +41,15 @@ export class MetaMaskConnector {
     }
   }
 
+  async requestAccounts(): Promise<string[] | null> {
+    if (!this.provider) {
+      this.logProviderMissing()
+      return
+    }
+
+    return await this.provider.send('eth_requestAccounts', [])
+  }
+
   isMetaMaskInstalled() {
     return this.ethereum && this.ethereum.isMetaMask
   }
@@ -52,6 +62,7 @@ export class MetaMaskConnector {
     return false
   }
 
+  /** Web3Provider Listeners - https://docs.ethers.org/v5/api/providers/provider/#Provider--events */
   on(event: string, listener: Listener) {
     this.provider?.on(event, listener)
     this.listeners.push(listener)
@@ -64,6 +75,41 @@ export class MetaMaskConnector {
 
   removeListeners() {
     this.provider?.removeAllListeners()
+  }
+
+  /** 
+   * EIP-1193 Event Listeners
+   * 
+   * .on in Web3Provider does not understand EIP-1193 events
+   * see - https://github.com/ethers-io/ethers.js/discussions/1560#discussioncomment-730893
+   */
+  onAccountsChanged(listener: Listener) {
+    this.ethereum?.on('accountsChanged', listener)
+    this.providerListeners.push(['accountsChanged', listener])
+  }
+
+  onDisconnect(listener: Listener) {
+    this.ethereum?.on('disconnect', listener)
+    this.providerListeners.push(['disconnect', listener])
+  }
+
+  onChainChanged(listener: Listener) {
+    this.ethereum?.on('chainChanged', listener)
+    this.providerListeners.push(['chainChanged', listener])
+  }
+
+  onConnect(listener: Listener) {
+    this.ethereum?.on('connect', listener)
+    this.providerListeners.push(['connect', listener])
+  }
+
+  removeProviderListener(event: string, listener: Listener) {
+    this.ethereum?.removeListener(event, listener)
+    this.providerListeners = this.providerListeners.filter(([,savedListener]) => listener !== savedListener)
+  }
+
+  removeProviderListeners() {
+    this.providerListeners.forEach(([event, listener]) => this.ethereum?.removeListener(event, listener))
   }
 
   async signMessage(message: string) {

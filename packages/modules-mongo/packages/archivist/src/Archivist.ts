b32cc2cc1d4b5322131b3c5b364e32ebc1550b2d
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-09-21 06:39:57 -0500

    Use mixin for augmenting archivist

--- packages/modules-mongo/packages/archivist/src/Archivist.ts
+++ packages/modules-mongo/packages/archivist/src/Archivist.ts
@@ -1,64 +1,14 @@
-import { assertEx } from '@xylabs/assert'
 import { exists } from '@xylabs/exists'
-import { merge } from '@xylabs/lodash'
 import { fulfilledValues } from '@xylabs/promise'
-import { staticImplements } from '@xylabs/static-implements'
 import { AbstractArchivist } from '@xyo-network/archivist-abstract'
 import { ArchivistConfigSchema, ArchivistInsertQuerySchema } from '@xyo-network/archivist-model'
-import { MongoDBArchivistConfigSchema, MongoDBArchivistParams } from '@xyo-network/archivist-model-mongodb'
-import { Module } from '@xyo-network/module'
-import { MongoDBStorageClassLabels } from '@xyo-network/module-model-mongodb'
-import { BoundWitnessWithMeta, PayloadWithMeta, PayloadWithPartialMeta } from '@xyo-network/node-core-model'
+import { MongoDBArchivistConfigSchema } from '@xyo-network/archivist-model-mongodb'
+import { MongoDBModuleMixin } from '@xyo-network/module-abstract-mongodb'
+import { PayloadWithPartialMeta } from '@xyo-network/node-core-model'
 import { Payload } from '@xyo-network/payload-model'
 import { PayloadWrapper } from '@xyo-network/payload-wrapper'
-import { BaseMongoSdk, BaseMongoSdkConfig } from '@xyo-network/sdk-xyo-mongo-js'
 
 import { toBoundWitnessWithMeta, toPayloadWithMeta, toReturnValue, validByType } from './lib'
-
-export interface MongoDBModuleStatic<T extends MongoDBStorageClassLabels = MongoDBStorageClassLabels> {
-  labels: T
-}
-export interface MongoDBModule {
-  get boundWitnessSdkConfig(): BaseMongoSdkConfig
-  get boundWitnesses(): BaseMongoSdk<BoundWitnessWithMeta>
-  get payloadSdkConfig(): BaseMongoSdkConfig
-  get payloads(): BaseMongoSdk<PayloadWithMeta>
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type AnyModule<TParams extends MongoDBArchivistParams = MongoDBArchivistParams> = abstract new (...args: any[]) => Module<TParams>
-
-const MongoDBModuleMixin = <TModule extends AnyModule = AnyModule>(ModuleBase: TModule) => {
-  @staticImplements<MongoDBModuleStatic>()
-  abstract class MongoModuleBase extends ModuleBase implements MongoDBModule {
-    static labels = MongoDBStorageClassLabels
-    _boundWitnessSdk: BaseMongoSdk<BoundWitnessWithMeta> | undefined
-    _payloadSdk: BaseMongoSdk<PayloadWithMeta> | undefined
-
-    get boundWitnessSdkConfig(): BaseMongoSdkConfig {
-      return merge({}, this.params.boundWitnessSdkConfig, this.config.boundWitnessSdkConfig, {
-        collection: this.config.boundWitnessSdkConfig?.collection ?? this.params.boundWitnessSdkConfig?.collection ?? 'bound_witnesses',
-      })
-    }
-
-    get boundWitnesses() {
-      this._boundWitnessSdk = this._boundWitnessSdk ?? new BaseMongoSdk<BoundWitnessWithMeta>(this.boundWitnessSdkConfig)
-      return assertEx(this._boundWitnessSdk)
-    }
-
-    get payloadSdkConfig(): BaseMongoSdkConfig {
-      return merge({}, this.params.payloadSdkConfig, this.config.payloadSdkConfig, {
-        collection: this.config.payloadSdkConfig?.collection ?? this.params.payloadSdkConfig?.collection ?? 'payload',
-      })
-    }
-
-    get payloads() {
-      this._payloadSdk = this._payloadSdk ?? new BaseMongoSdk<PayloadWithMeta>(this.payloadSdkConfig)
-      return assertEx(this._payloadSdk)
-    }
-  }
-  return MongoModuleBase
-}
 
 const MongoDBArchivistBase = MongoDBModuleMixin(AbstractArchivist)
 

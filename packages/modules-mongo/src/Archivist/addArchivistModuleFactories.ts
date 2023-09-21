b2e42b857a87195aa49fee5952ef0dee95b11c3f
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-09-21 12:32:13 -0500

    Remove factory in favor of raw ctor

--- packages/modules-mongo/src/Archivist/addArchivistModuleFactories.ts
+++ packages/modules-mongo/src/Archivist/addArchivistModuleFactories.ts
@@ -1,22 +1,9 @@
 import { MongoDBArchivist } from '@xyo-network/archivist-mongodb'
-import { ModuleFactory, ModuleFactoryLocator } from '@xyo-network/module'
+import { ModuleFactoryLocator } from '@xyo-network/module'
 import { TYPES } from '@xyo-network/node-core-types'
-import { BaseMongoSdkPrivateConfig } from '@xyo-network/sdk-xyo-mongo-js'
 import { Container } from 'inversify'
-
-import { getBaseMongoSdkPrivateConfig } from '../Mongo'
-
-const getMongoDBArchivistFactory = () => {
-  const boundWitnessSdkConfig: BaseMongoSdkPrivateConfig = getBaseMongoSdkPrivateConfig()
-  const payloadSdkConfig: BaseMongoSdkPrivateConfig = getBaseMongoSdkPrivateConfig()
-  return ModuleFactory.withParams(MongoDBArchivist, {
-    boundWitnessSdkConfig,
-    config: { schema: MongoDBArchivist.configSchema },
-    payloadSdkConfig,
-  })
-}
 
 export const addArchivistModuleFactories = (container: Container) => {
   const locator = container.get<ModuleFactoryLocator>(TYPES.ModuleFactoryLocator)
-  locator.register(getMongoDBArchivistFactory(), MongoDBArchivist.labels)
+  locator.register(MongoDBArchivist)
 }

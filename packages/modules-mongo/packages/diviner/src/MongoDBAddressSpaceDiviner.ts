fc0fe8be247b459fb34ca7b8c06fe7abd53c6b09
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-09-26 06:24:18 -0500

    Use collection distinct operator

--- packages/modules-mongo/packages/diviner/src/MongoDBAddressSpaceDiviner.ts
+++ packages/modules-mongo/packages/diviner/src/MongoDBAddressSpaceDiviner.ts
@@ -2,7 +2,7 @@ import { exists } from '@xylabs/exists'
 import { AddressSchema } from '@xyo-network/address-payload-plugin'
 import { AddressSpaceDiviner } from '@xyo-network/diviner-address-space-abstract'
 import { AddressSpaceDivinerConfigSchema } from '@xyo-network/diviner-models'
-import { COLLECTIONS, DATABASES, DefaultMaxTimeMS, MongoDBModuleMixin } from '@xyo-network/module-abstract-mongodb'
+import { DefaultMaxTimeMS, MongoDBModuleMixin } from '@xyo-network/module-abstract-mongodb'
 import { Payload } from '@xyo-network/payload-model'
 
 const MongoDBDivinerBase = MongoDBModuleMixin(AddressSpaceDiviner)
@@ -12,17 +12,11 @@ export class MongoDBAddressSpaceDiviner extends MongoDBDivinerBase {
 
   protected override async divineHandler(_payloads?: Payload[]): Promise<Payload[]> {
     // TODO: Most Recently Used, Most Frequently Used, Addresses of Value/Importance to Me
-    const result = await this.boundWitnesses.useMongo((db) => {
-      return db.db(DATABASES.Archivist).command(
-        {
-          distinct: COLLECTIONS.BoundWitnesses,
-          key: 'addresses',
-        },
-        { maxTimeMS: DefaultMaxTimeMS },
-      )
+    const result = await this.boundWitnesses.useCollection(async (collection) => {
+      return await collection.distinct('addresses', {}, { maxTimeMS: DefaultMaxTimeMS })
     })
     // Ensure uniqueness on case
-    const addresses = new Set<string>(result?.values?.map((address: string) => address?.toLowerCase()).filter(exists))
+    const addresses = new Set<string>(result?.map((address: string) => address?.toLowerCase()).filter(exists))
     return [...addresses].map((address) => {
       return { address, schema: AddressSchema }
     })

f654a81af985499ae2cdc40c533f78f9d1918ec5
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-02 07:41:45 -0500

    Codify Index Description

---
+++ packages/modules-mongo/packages/module/packages/abstract/src/IndexDescription.ts
@@ -0,0 +1,26 @@
+/**
+ * The index direction (1 for ascending, -1 for descending)
+ */
+export type IndexDirection = -1 | 1
+
+/**
+ * Description of index(es) to be created on a store
+ */
+export type IndexDescription = {
+  /**
+   * The key(s) to index
+   */
+  key:
+    | {
+        [key: string]: IndexDirection
+      }
+    | Map<string, IndexDirection>
+  /**
+   * The name of the index
+   */
+  name?: string
+  /**
+   * If true, the index must enforce uniqueness on the key
+   */
+  unique?: boolean
+}

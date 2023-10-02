bdff0ebb6da59da7f06e554484846b72448b4b17
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-02 11:55:01 -0500

    Remove lint warning for test

--- packages/plugins/packages/payloadset/packages/image-thumbnail/src/Diviner/spec/Diviner.spec.ts
+++ packages/plugins/packages/payloadset/packages/image-thumbnail/src/Diviner/spec/Diviner.spec.ts
@@ -1,9 +1,5 @@
-import { MemoryNode } from '@xyo-network/node-memory'
-
-import { ImageThumbnailDiviner } from '../Diviner'
-
 describe('ImageThumbnailDiviner', () => {
-  let _node: MemoryNode
-  let _diviner: ImageThumbnailDiviner
+  // let _node: MemoryNode
+  // let _diviner: ImageThumbnailDiviner
   it.skip('should have tests', async () => {})
 })

9e05f1afce4ab091dd889047320a091d60c46fb1
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-04 07:07:05 -0500

    Scaffold test architecture

--- packages/plugins/packages/payloadset/packages/image-thumbnail/src/Witness/spec/Witness.sentinel.spec.ts
+++ packages/plugins/packages/payloadset/packages/image-thumbnail/src/Witness/spec/Witness.sentinel.spec.ts
@@ -1,29 +1,42 @@
+import { HDWallet } from '@xyo-network/account'
+import { MemoryNode } from '@xyo-network/node-memory'
 import { MemorySentinel } from '@xyo-network/sentinel'
+import { TimestampWitness } from '@xyo-network/witness-timestamp'
 
 import { ImageThumbnailWitness } from '../Witness'
 
 describe('Witness', () => {
   describe('when behind sentinel', () => {
-    const cases: [uri: string, expected: string][] = [
-      [
-        'ipfs://ipfs/QmewywDQGqz9WuWfT11ueSR3Mu86MejfD64v3KtFRzGP2G/image.jpeg',
-        'https://5d7b6582.beta.decentralnetworkservices.com/ipfs/QmewywDQGqz9WuWfT11ueSR3Mu86MejfD64v3KtFRzGP2G/image.jpeg',
-      ],
-      [
-        'ipfs://QmWX3Kx2NX3AK8WxTQwktVYLMFHX3pHm77ThynhgmU8dP8',
-        'https://5d7b6582.beta.decentralnetworkservices.com/ipfs/QmWX3Kx2NX3AK8WxTQwktVYLMFHX3pHm77ThynhgmU8dP8',
-      ],
-    ]
+    let thumbnailWitness: ImageThumbnailWitness
+    let timestampWitness: TimestampWitness
     let sentinel: MemorySentinel
+    let node: MemoryNode
     beforeAll(async () => {
+      thumbnailWitness = await ImageThumbnailWitness.create({
+        config: { schema: ImageThumbnailWitness.configSchema },
+        wallet: await HDWallet.random(),
+      })
+      timestampWitness = await TimestampWitness.create({
+        config: { schema: TimestampWitness.configSchema },
+        wallet: await HDWallet.random(),
+      })
       sentinel = await MemorySentinel.create({
         config: { schema: MemorySentinel.configSchema },
+        wallet: await HDWallet.random(),
       })
+      const modules = [timestampWitness, thumbnailWitness, sentinel]
+      node = await MemoryNode.create({
+        config: { schema: MemoryNode.configSchema },
+        wallet: await HDWallet.random(),
+      })
+      await node.start()
+      await Promise.all(
+        modules.map(async (mod) => {
+          await node.register(mod)
+          await node.attach(mod.address, true)
+        }),
+      )
     })
-    it.each(cases)('%s', async (input, expected) => {
-      const witness = await ImageThumbnailWitness.create()
-      const actual = witness.checkIpfsUrl(input)
-      expect(actual).toBe(expected)
-    })
+    it('witnesses thumbnail for url', async () => {})
   })
 })

ef66b475f2be33bd5b698f678f40df91b9064c79
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-04 11:33:40 -0500

    Ensure payloads exist in archivist

--- packages/plugins/packages/payloadset/packages/image-thumbnail/src/Witness/spec/Witness.sentinel.spec.ts
+++ packages/plugins/packages/payloadset/packages/image-thumbnail/src/Witness/spec/Witness.sentinel.spec.ts
@@ -1,18 +1,21 @@
 import { HDWallet } from '@xyo-network/account'
 import { isImageThumbnail } from '@xyo-network/image-thumbnail-payload-plugin'
+import { MemoryArchivist } from '@xyo-network/memory-archivist'
 import { MemoryNode } from '@xyo-network/node-memory'
 import { PayloadBuilder } from '@xyo-network/payload-builder'
 import { MemorySentinel } from '@xyo-network/sentinel'
 import { UrlSchema } from '@xyo-network/url-payload-plugin'
 import { isTimestamp, TimestampWitness } from '@xyo-network/witness-timestamp'
-import { mock, MockProxy } from 'jest-mock-extended'
+import { mock } from 'jest-mock-extended'
 
 import { ImageThumbnailWitness } from '../Witness'
 
 describe('Witness', () => {
   describe('when behind sentinel', () => {
+    const archivistName = 'archivist'
     let thumbnailWitness: ImageThumbnailWitness
     let timestampWitness: TimestampWitness
+    let archivist: MemoryArchivist
     let sentinel: MemorySentinel
     let node: MemoryNode
     const logger = mock<Console>()
@@ -28,12 +31,16 @@ describe('Witness', () => {
         logger,
         wallet: await HDWallet.random(),
       })
+      archivist = await MemoryArchivist.create({
+        config: { name: archivistName, schema: MemoryArchivist.configSchema },
+        wallet: await HDWallet.random(),
+      })
       sentinel = await MemorySentinel.create({
-        config: { schema: MemorySentinel.configSchema },
+        config: { archivists: [archivistName], schema: MemorySentinel.configSchema },
         logger,
         wallet: await HDWallet.random(),
       })
-      const modules = [timestampWitness, thumbnailWitness, sentinel]
+      const modules = [timestampWitness, thumbnailWitness, archivist, sentinel]
       node = await MemoryNode.create({
         config: { schema: MemoryNode.configSchema },
         logger,
@@ -60,6 +67,8 @@ describe('Witness', () => {
       expect(thumbnails.length).toBe(1)
       const thumbnail = thumbnails[0]
       expect(thumbnail.sourceUrl).toBe(url)
+      const payloads = await archivist?.all()
+      expect(payloads?.length).toBeGreaterThan(0)
     })
   })
 })

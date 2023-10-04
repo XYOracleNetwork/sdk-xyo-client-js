6a7d9da7f4ffb24b754b604c3ed2033e0b84b1cc
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-04 16:32:54 -0500

    Consume config/params from SDK

--- packages/plugins/packages/payloadset/packages/image-thumbnail/packages/diviner/src/Diviner/Params.ts
+++
@@ -1,6 +0,0 @@
-import { DivinerParams } from '@xyo-network/diviner-model'
-import { AnyConfigSchema } from '@xyo-network/module'
-
-import { ImageThumbnailDivinerConfig } from './Config'
-
-export type ImageThumbnailDivinerParams = DivinerParams<AnyConfigSchema<ImageThumbnailDivinerConfig>>

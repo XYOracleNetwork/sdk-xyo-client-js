4301c9b886cb766602a0dcd019545263cc0e0600
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-10-05 07:52:29 -0500

    Implement custom query type for image thumbnail diviner

--- packages/plugins/packages/payload/packages/image-thumbnail/src/Diviner/Query.ts
+++ packages/plugins/packages/payload/packages/image-thumbnail/src/Diviner/Query.ts
@@ -1,15 +1,10 @@
+import { PayloadDivinerPredicate } from '@xyo-network/diviner-payload-model'
 import { Payload } from '@xyo-network/payload-model'
-import { UrlPayload } from '@xyo-network/url-payload-plugin'
 
 import { ImageThumbnailDivinerSchema } from './Schema'
 
 export type ImageThumbnailDivinerQuerySchema = `${ImageThumbnailDivinerSchema}.query`
 export const ImageThumbnailDivinerQuerySchema: ImageThumbnailDivinerQuerySchema = `${ImageThumbnailDivinerSchema}.query`
 
-export type ImageThumbnailDivinerQueryPayload = UrlPayload &
-  Payload<
-    {
-      foo: string
-    },
-    ImageThumbnailDivinerQuerySchema
-  >
+export type ImageThumbnailDivinerQueryPayload = Pick<PayloadDivinerPredicate, 'limit' | 'offset' | 'order'> &
+  Payload<{ status?: boolean; url: string }, ImageThumbnailDivinerQuerySchema>

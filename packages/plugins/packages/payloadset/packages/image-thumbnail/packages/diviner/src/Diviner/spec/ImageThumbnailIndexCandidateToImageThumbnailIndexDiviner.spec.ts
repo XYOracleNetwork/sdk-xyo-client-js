a7157715b5b3315fd7204f8076d9833b7af9e46f
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-11-07 14:53:15 -0600

    Add tests for single & multiple data

--- packages/plugins/packages/payloadset/packages/image-thumbnail/packages/diviner/src/Diviner/spec/ImageThumbnailIndexCandidateToImageThumbnailIndexDiviner.spec.ts
+++ packages/plugins/packages/payloadset/packages/image-thumbnail/packages/diviner/src/Diviner/spec/ImageThumbnailIndexCandidateToImageThumbnailIndexDiviner.spec.ts
@@ -8,35 +8,63 @@ import { ImageThumbnailIndexCandidateToImageThumbnailIndexDiviner } from '../Ima
 
 describe('ImageThumbnailIndexCandidateToImageThumbnailIndexDiviner', () => {
   let diviner: ImageThumbnailIndexCandidateToImageThumbnailIndexDiviner
-  const timestamp = 1234567890
-  const results: ImageThumbnail[] = [
-    {
-      http: {
-        status: 200,
-      },
-      schema: ImageThumbnailSchema,
-      sourceUrl: 'https://xyo.network',
-      url: 'data',
+  const timestampA = 1234567890
+  const timestampPayloadA: TimeStamp = { schema: TimestampSchema, timestamp: timestampA }
+  const imageThumbnailPayloadA: ImageThumbnail = {
+    http: {
+      status: 200,
     },
-  ]
-  const timestampPayload: TimeStamp = { schema: TimestampSchema, timestamp }
+    schema: ImageThumbnailSchema,
+    sourceUrl: 'https://xyo.network',
+    url: 'data',
+  }
+  const timestampB = 1234567891
+  const timestampPayloadB: TimeStamp = { schema: TimestampSchema, timestamp: timestampB }
+  const imageThumbnailPayloadB: ImageThumbnail = {
+    http: {
+      status: 500,
+    },
+    schema: ImageThumbnailSchema,
+    sourceUrl: 'https://xyo.network',
+  }
   beforeAll(async () => {
     diviner = await ImageThumbnailIndexCandidateToImageThumbnailIndexDiviner.create()
   })
   describe('divine', () => {
-    it.each(results)('transforms', async (imageThumbnailPayload) => {
-      const [boundWitness] = await new BoundWitnessBuilder().payloads([imageThumbnailPayload, timestampPayload]).build()
-      const result = await diviner.divine([boundWitness, imageThumbnailPayload, timestampPayload])
-      const payloadDictionary = await PayloadHasher.toMap([boundWitness, imageThumbnailPayload, timestampPayload])
-      expect(result).toBeArrayOfSize(1)
-      expect(result.filter(isImageThumbnailResultIndex)).toBeArrayOfSize(1)
-      const [index] = result.filter(isImageThumbnailResultIndex)
-      const key = await PayloadHasher.hashAsync({ schema: UrlSchema, url: imageThumbnailPayload.sourceUrl })
-      expect(index.key).toBe(key)
-      expect(index.sources).toEqual(Object.keys(payloadDictionary))
-      expect(index.success).toBe(true)
-      expect(index.timestamp).toBe(1234567890)
-      expect(index.status).toBe(200)
+    const cases: [ImageThumbnail, TimeStamp][] = [
+      [imageThumbnailPayloadA, timestampPayloadA],
+      [imageThumbnailPayloadB, timestampPayloadB],
+    ]
+    describe('with single result', () => {
+      it.each(cases)('transforms single result', async (thumbnail, timestamp) => {
+        const [boundWitness] = await new BoundWitnessBuilder().payloads([thumbnail, timestamp]).build()
+        const result = await diviner.divine([boundWitness, thumbnail, timestamp])
+        const payloadDictionary = await PayloadHasher.toMap([boundWitness, thumbnail, timestamp])
+        expect(result).toBeArrayOfSize(1)
+        expect(result.filter(isImageThumbnailResultIndex)).toBeArrayOfSize(1)
+        const [index] = result.filter(isImageThumbnailResultIndex)
+        const key = await PayloadHasher.hashAsync({ schema: UrlSchema, url: thumbnail.sourceUrl })
+        expect(index.key).toBe(key)
+        expect(index.sources).toEqual(Object.keys(payloadDictionary))
+        expect(index.success).toBe(thumbnail.http?.status === 200)
+        expect(index.timestamp).toBe(timestamp.timestamp)
+        expect(index.status).toBe(thumbnail.http?.status)
+      })
+    })
+    describe('with multiple results', () => {
+      it('transforms multiple results', async () => {
+        const data = (
+          await Promise.all(
+            cases.map(async (payloads) => {
+              const [bw] = await new BoundWitnessBuilder().payloads(payloads).build()
+              return [bw, ...payloads]
+            }),
+          )
+        ).flat()
+        const result = await diviner.divine(data)
+        const payloadDictionary = await PayloadHasher.toMap(data)
+        expect(result).toBeArrayOfSize(2)
+      })
     })
   })
 })

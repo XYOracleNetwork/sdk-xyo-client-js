6d45ad7645717965fb85c7ca2caad8ec91c8f2b3
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-11-07 08:32:58 -0600

    Query transform diviner tests

--- packages/plugins/packages/payloadset/packages/image-thumbnail/packages/diviner/src/Diviner/spec/ImageThumbnailQueryToImageThumbnailIndexQueryDiviner.spec.ts
+++ packages/plugins/packages/payloadset/packages/image-thumbnail/packages/diviner/src/Diviner/spec/ImageThumbnailQueryToImageThumbnailIndexQueryDiviner.spec.ts
@@ -1,42 +1,35 @@
-import { BoundWitnessBuilder } from '@xyo-network/boundwitness-builder'
 import { PayloadHasher } from '@xyo-network/core'
-import { ImageThumbnail, ImageThumbnailSchema, isImageThumbnailResultIndex } from '@xyo-network/image-thumbnail-payload-plugin'
+import { ImageThumbnailDivinerQuery, ImageThumbnailDivinerQuerySchema } from '@xyo-network/image-thumbnail-payload-plugin'
 import { UrlSchema } from '@xyo-network/url-payload-plugin'
-import { TimeStamp, TimestampSchema } from '@xyo-network/witness-timestamp'
 
-import { ImageThumbnailIndexCandidateToImageThumbnailIndexDiviner } from '../ImageThumbnailIndexCandidateToImageThumbnailIndexDiviner'
+import { ImageThumbnailQueryToImageThumbnailIndexQueryDiviner } from '../ImageThumbnailQueryToImageThumbnailIndexQueryDiviner'
+import { isImageThumbnailResultQuery } from '../ImageThumbnailResultQuery'
 
 describe('ImageThumbnailQueryToImageThumbnailIndexQueryDiviner', () => {
-  let diviner: ImageThumbnailIndexCandidateToImageThumbnailIndexDiviner
-  const timestamp = 1234567890
-  const results: ImageThumbnail[] = [
+  let diviner: ImageThumbnailQueryToImageThumbnailIndexQueryDiviner
+  const cases: ImageThumbnailDivinerQuery[] = [
     {
-      http: {
-        status: 200,
-      },
-      schema: ImageThumbnailSchema,
-      sourceUrl: 'https://xyo.network',
-      url: 'data',
+      schema: ImageThumbnailDivinerQuerySchema,
+      url: 'https://xyo.network',
     },
   ]
-  const timestampPayload: TimeStamp = { schema: TimestampSchema, timestamp }
   beforeAll(async () => {
-    diviner = await ImageThumbnailIndexCandidateToImageThumbnailIndexDiviner.create()
+    diviner = await ImageThumbnailQueryToImageThumbnailIndexQueryDiviner.create()
   })
   describe('divine', () => {
-    it.each(results)('transforms', async (imageThumbnailPayload) => {
-      const [boundWitness] = await new BoundWitnessBuilder().payloads([imageThumbnailPayload, timestampPayload]).build()
-      const result = await diviner.divine([boundWitness, imageThumbnailPayload, timestampPayload])
-      const payloadDictionary = await PayloadHasher.toMap([boundWitness, imageThumbnailPayload, timestampPayload])
-      expect(result).toBeArrayOfSize(1)
-      expect(result.filter(isImageThumbnailResultIndex)).toBeArrayOfSize(1)
-      const [index] = result.filter(isImageThumbnailResultIndex)
-      const key = await PayloadHasher.hashAsync({ schema: UrlSchema, url: imageThumbnailPayload.sourceUrl })
-      expect(index.key).toBe(key)
-      expect(index.sources).toEqual(Object.keys(payloadDictionary))
-      expect(index.success).toBe(true)
-      expect(index.timestamp).toBe(1234567890)
-      expect(index.status).toBe(200)
+    it.each(cases)('transforms', async (imageThumbnailDivinerQuery) => {
+      const results = await diviner.divine([imageThumbnailDivinerQuery])
+      const actual = results.filter(isImageThumbnailResultQuery)
+      expect(actual).toBeArrayOfSize(1)
+      const { url } = imageThumbnailDivinerQuery
+      const key = await PayloadHasher.hashAsync({ schema: UrlSchema, url })
+      expect(actual?.[0]).toEqual({
+        key,
+        limit: 1,
+        offset: 0,
+        order: 'desc',
+        schema: 'network.xyo.diviner.payload.query',
+      })
     })
   })
 })

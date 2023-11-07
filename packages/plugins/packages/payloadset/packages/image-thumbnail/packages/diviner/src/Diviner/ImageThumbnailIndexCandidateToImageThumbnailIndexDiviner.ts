be2b1bc425088d5d3ef9c71a407374fee1971e59
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-11-07 15:22:22 -0600

    Handle multiple indexes at once

--- packages/plugins/packages/payloadset/packages/image-thumbnail/packages/diviner/src/Diviner/ImageThumbnailIndexCandidateToImageThumbnailIndexDiviner.ts
+++ packages/plugins/packages/payloadset/packages/image-thumbnail/packages/diviner/src/Diviner/ImageThumbnailIndexCandidateToImageThumbnailIndexDiviner.ts
@@ -7,12 +7,13 @@ import {
   ImageThumbnailResultIndex,
   ImageThumbnailResultIndexFields,
   ImageThumbnailResultIndexSchema,
+  ImageThumbnailSchema,
   isImageThumbnail,
 } from '@xyo-network/image-thumbnail-payload-plugin'
 import { PayloadBuilder } from '@xyo-network/payload-builder'
 import { Payload } from '@xyo-network/payload-model'
 import { UrlSchema } from '@xyo-network/url-payload-plugin'
-import { isTimestamp, TimeStamp } from '@xyo-network/witness-timestamp'
+import { isTimestamp, TimeStamp, TimestampSchema } from '@xyo-network/witness-timestamp'
 
 import { ImageThumbnailDivinerLabels, ImageThumbnailDivinerStageLabels } from './ImageThumbnailDivinerLabels'
 
@@ -27,23 +28,42 @@ export class ImageThumbnailIndexCandidateToImageThumbnailIndexDiviner extends Ab
   }
 
   protected override async divineHandler(payloads: Payload[] = []): Promise<ImageThumbnailResultIndex[]> {
-    const bw: BoundWitness | undefined = payloads.find(isBoundWitness)
-    const imageThumbnailPayload: ImageThumbnail | undefined = payloads.find(isImageThumbnail)
-    const timestampPayload: TimeStamp | undefined = payloads.find(isTimestamp)
-    if (bw && imageThumbnailPayload && timestampPayload) {
-      const { sourceUrl: url } = imageThumbnailPayload
-      const { timestamp } = timestampPayload
-      const status = imageThumbnailPayload.http?.status
-      const success = !!imageThumbnailPayload.url // Call anything with a thumbnail url a success
-      const sources = (await PayloadHasher.hashPairs([bw, imageThumbnailPayload, timestampPayload])).map(([, hash]) => hash)
-      const urlPayload = { schema: UrlSchema, url }
-      const key = await PayloadHasher.hashAsync(urlPayload)
-      const fields: ImageThumbnailResultIndexFields = { key, sources, success, timestamp }
-      if (status) fields.status = status
-      const result: ImageThumbnailResultIndex = new PayloadBuilder<ImageThumbnailResultIndex>({ schema: ImageThumbnailResultIndexSchema })
-        .fields(fields)
-        .build()
-      return [result]
+    const bws: BoundWitness[] = payloads.filter(isBoundWitness)
+    const imageThumbnailPayloads: ImageThumbnail[] = payloads.filter(isImageThumbnail)
+    const timestampPayloads: TimeStamp[] = payloads.filter(isTimestamp)
+    if (bws.length && imageThumbnailPayloads.length && timestampPayloads.length) {
+      const payloadDictionary = await PayloadHasher.toMap(payloads)
+      const tuples: [BoundWitness, ImageThumbnail, TimeStamp][] = bws.reduce<[BoundWitness, ImageThumbnail, TimeStamp][]>(
+        (acc, curr) => {
+          const imageThumbnailIndex = curr.payload_schemas?.findIndex((schema) => schema === ImageThumbnailSchema)
+          const timestampIndex = curr.payload_schemas?.findIndex((schema) => schema === TimestampSchema)
+          const imageThumbnailHash = curr.payload_hashes?.[imageThumbnailIndex]
+          const timestampHash = curr.payload_hashes?.[timestampIndex]
+          const imageThumbnailPayload = [payloadDictionary[imageThumbnailHash]].find(isImageThumbnail)
+          const timestampPayload = [payloadDictionary[timestampHash]].find(isTimestamp)
+          if (imageThumbnailPayload && timestampPayload) acc.push([curr, imageThumbnailPayload, timestampPayload])
+          return acc
+        },
+        [] as [BoundWitness, ImageThumbnail, TimeStamp][],
+      )
+      const indexes = await Promise.all(
+        tuples.map(async ([bw, imageThumbnailPayload, timestampPayload]) => {
+          const { sourceUrl: url } = imageThumbnailPayload
+          const { timestamp } = timestampPayload
+          const status = imageThumbnailPayload.http?.status
+          const success = !!imageThumbnailPayload.url // Call anything with a thumbnail url a success
+          const sources = (await PayloadHasher.hashPairs([bw, imageThumbnailPayload, timestampPayload])).map(([, hash]) => hash)
+          const urlPayload = { schema: UrlSchema, url }
+          const key = await PayloadHasher.hashAsync(urlPayload)
+          const fields: ImageThumbnailResultIndexFields = { key, sources, success, timestamp }
+          if (status) fields.status = status
+          const result: ImageThumbnailResultIndex = new PayloadBuilder<ImageThumbnailResultIndex>({ schema: ImageThumbnailResultIndexSchema })
+            .fields(fields)
+            .build()
+          return [result]
+        }),
+      )
+      return indexes.flat()
     }
     return Promise.resolve([])
   }

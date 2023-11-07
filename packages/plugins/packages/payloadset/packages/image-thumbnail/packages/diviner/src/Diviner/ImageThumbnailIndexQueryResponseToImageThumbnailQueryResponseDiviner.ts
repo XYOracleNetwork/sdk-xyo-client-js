8bb6de49746bb04dc1aa161e10b6054d193bdd7e
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-11-07 08:11:07 -0600

    Support multiple inputs to diviner

--- packages/plugins/packages/payloadset/packages/image-thumbnail/packages/diviner/src/Diviner/ImageThumbnailIndexQueryResponseToImageThumbnailQueryResponseDiviner.ts
+++ packages/plugins/packages/payloadset/packages/image-thumbnail/packages/diviner/src/Diviner/ImageThumbnailIndexQueryResponseToImageThumbnailQueryResponseDiviner.ts
@@ -1,4 +1,6 @@
+import { exists } from '@xylabs/exists'
 import { AbstractDiviner } from '@xyo-network/abstract-diviner'
+import { PayloadHasher } from '@xyo-network/core'
 import { DivinerConfigSchema } from '@xyo-network/diviner-model'
 import {
   ImageThumbnailResult,
@@ -9,6 +11,7 @@ import {
 } from '@xyo-network/image-thumbnail-payload-plugin'
 import { PayloadBuilder } from '@xyo-network/payload-builder'
 import { Payload } from '@xyo-network/payload-model'
+import { UrlSchema } from '@xyo-network/url-payload-plugin'
 
 import { ImageThumbnailDivinerLabels, ImageThumbnailDivinerStageLabels } from './ImageThumbnailDivinerLabels'
 
@@ -22,15 +25,33 @@ export class ImageThumbnailIndexQueryResponseToImageThumbnailQueryResponseDivine
     'network.xyo.diviner.stage': 'indexQueryResponseToDivinerQueryResponseDiviner',
   }
 
-  protected override divineHandler(payloads: Payload[] = []): Promise<ImageThumbnailResult[]> {
-    const imageThumbnailDivinerQuery = payloads.find(isImageThumbnailDivinerQuery)
-    const imageThumbnailResultIndex = payloads.find(isImageThumbnailResultIndex)
-    if (imageThumbnailDivinerQuery && imageThumbnailResultIndex) {
-      const { url } = imageThumbnailDivinerQuery
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { key, schema, ...commonFields } = imageThumbnailResultIndex
-      const fields: ImageThumbnailResultFields = { ...commonFields, url }
-      return Promise.resolve([new PayloadBuilder<ImageThumbnailResult>({ schema: ImageThumbnailResultSchema }).fields(fields).build()])
+  protected override async divineHandler(payloads: Payload[] = []): Promise<ImageThumbnailResult[]> {
+    const imageThumbnailDivinerQueries = payloads.filter(isImageThumbnailDivinerQuery)
+    const imageThumbnailResultIndexes = payloads.filter(isImageThumbnailResultIndex)
+
+    if (imageThumbnailDivinerQueries.length && imageThumbnailResultIndexes.length) {
+      const keyToUrlDictionary = Object.fromEntries(
+        await Promise.all(
+          imageThumbnailDivinerQueries.map(async (imageThumbnailDivinerQuery) => {
+            const { url } = imageThumbnailDivinerQuery
+            const urlPayload = new PayloadBuilder<ImageThumbnailResult>({ schema: UrlSchema }).fields({ url }).build()
+            const key = await PayloadHasher.hashAsync(urlPayload)
+            return [key, url] as const
+          }),
+        ),
+      )
+      const results = imageThumbnailResultIndexes
+        .map((imageThumbnailResultIndex) => {
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          const { key, schema, ...commonFields } = imageThumbnailResultIndex
+          const url = keyToUrlDictionary?.[key]
+          if (url) {
+            const fields: ImageThumbnailResultFields = { ...commonFields, url }
+            return new PayloadBuilder<ImageThumbnailResult>({ schema: ImageThumbnailResultSchema }).fields(fields).build()
+          }
+        })
+        .filter(exists)
+      return results
     }
     return Promise.resolve([])
   }

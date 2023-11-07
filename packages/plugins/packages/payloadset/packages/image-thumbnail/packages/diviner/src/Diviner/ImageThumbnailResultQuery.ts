de73fe95d9e937c7b47e3a3aa6d034879698f121
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-11-07 08:32:37 -0600

    Add identity function

--- packages/plugins/packages/payloadset/packages/image-thumbnail/packages/diviner/src/Diviner/ImageThumbnailResultQuery.ts
+++ packages/plugins/packages/payloadset/packages/image-thumbnail/packages/diviner/src/Diviner/ImageThumbnailResultQuery.ts
@@ -1,5 +1,6 @@
-import { PayloadDivinerQueryPayload } from '@xyo-network/diviner-payload-model'
+import { PayloadDivinerQueryPayload, PayloadDivinerQuerySchema } from '@xyo-network/diviner-payload-model'
 import { ImageThumbnailResultIndex } from '@xyo-network/image-thumbnail-payload-plugin'
+import { isPayloadOfSchemaType } from '@xyo-network/payload-model'
 
 /**
  * The fields that will need to be indexed on in the underlying store
@@ -10,3 +11,9 @@ export type QueryableImageThumbnailResultProperties = Extract<keyof ImageThumbna
  * The query that will be used to retrieve the results from the underlying store
  */
 export type ImageThumbnailResultQuery = PayloadDivinerQueryPayload & Pick<ImageThumbnailResultIndex, QueryableImageThumbnailResultProperties>
+
+/**
+ * A type guard for ImageThumbnailResultQuery
+ */
+// TODO: Use a more derived schema than PayloadDivinerQuerySchema
+export const isImageThumbnailResultQuery = isPayloadOfSchemaType<ImageThumbnailResultQuery>(PayloadDivinerQuerySchema)

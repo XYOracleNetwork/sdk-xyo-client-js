8a0117ee3b8721382e47e34917b715abb29c36d4
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-11-07 19:22:54 -0600

    Add test permutation matrix

--- packages/plugins/packages/payloadset/packages/image-thumbnail/packages/diviner/src/Diviner/spec/ImageThumbnailQueryToImageThumbnailIndexQueryDiviner.spec.ts
+++ packages/plugins/packages/payloadset/packages/image-thumbnail/packages/diviner/src/Diviner/spec/ImageThumbnailQueryToImageThumbnailIndexQueryDiviner.spec.ts
@@ -3,32 +3,133 @@ import { ImageThumbnailDivinerQuery, ImageThumbnailDivinerQuerySchema } from '@x
 import { UrlSchema } from '@xyo-network/url-payload-plugin'
 
 import { ImageThumbnailQueryToImageThumbnailIndexQueryDiviner } from '../ImageThumbnailQueryToImageThumbnailIndexQueryDiviner'
-import { isImageThumbnailResultQuery } from '../ImageThumbnailResultQuery'
+import { ImageThumbnailResultQuery, isImageThumbnailResultQuery } from '../ImageThumbnailResultQuery'
 
 describe('ImageThumbnailQueryToImageThumbnailIndexQueryDiviner', () => {
   let diviner: ImageThumbnailQueryToImageThumbnailIndexQueryDiviner
-  const cases: ImageThumbnailDivinerQuery[] = [
+  const queries: ImageThumbnailDivinerQuery[] = [
     {
       schema: ImageThumbnailDivinerQuerySchema,
       url: 'https://xyo.network',
     },
+    {
+      limit: 10,
+      offset: 10,
+      order: 'asc',
+      schema: ImageThumbnailDivinerQuerySchema,
+      status: 200,
+      success: true,
+      url: 'https://xyo.network',
+    },
+    {
+      limit: 10,
+      schema: ImageThumbnailDivinerQuerySchema,
+      url: 'https://explore.xyo.network',
+    },
+    {
+      offset: 10,
+      schema: ImageThumbnailDivinerQuerySchema,
+      url: 'https://explore.xyo.network',
+    },
+    {
+      order: 'asc',
+      schema: ImageThumbnailDivinerQuerySchema,
+      url: 'https://explore.xyo.network',
+    },
+    {
+      schema: ImageThumbnailDivinerQuerySchema,
+      status: 200,
+      url: 'https://explore.xyo.network',
+    },
+    {
+      schema: ImageThumbnailDivinerQuerySchema,
+      success: true,
+      url: 'https://explore.xyo.network',
+    },
+    {
+      schema: ImageThumbnailDivinerQuerySchema,
+      success: false,
+      url: 'https://explore.xyo.network',
+    },
   ]
+  const expected: ImageThumbnailResultQuery[] = [
+    {
+      key: 'setInBeforeAll',
+      limit: 1,
+      offset: 0,
+      order: 'desc',
+      schema: 'network.xyo.diviner.payload.query',
+    } as unknown as ImageThumbnailResultQuery,
+    {
+      key: 'setInBeforeAll',
+      limit: 10,
+      offset: 10,
+      order: 'asc',
+      schema: 'network.xyo.diviner.payload.query',
+      status: 200,
+      success: true,
+    } as unknown as ImageThumbnailResultQuery,
+    {
+      key: 'setInBeforeAll',
+      limit: 10,
+      offset: 0,
+      order: 'desc',
+      schema: 'network.xyo.diviner.payload.query',
+    } as unknown as ImageThumbnailResultQuery,
+    {
+      key: 'setInBeforeAll',
+      limit: 1,
+      offset: 10,
+      order: 'desc',
+      schema: 'network.xyo.diviner.payload.query',
+    } as unknown as ImageThumbnailResultQuery,
+    {
+      key: 'setInBeforeAll',
+      limit: 1,
+      offset: 0,
+      order: 'asc',
+      schema: 'network.xyo.diviner.payload.query',
+    } as unknown as ImageThumbnailResultQuery,
+    {
+      key: 'setInBeforeAll',
+      limit: 1,
+      offset: 0,
+      order: 'desc',
+      schema: 'network.xyo.diviner.payload.query',
+      status: 200,
+    } as unknown as ImageThumbnailResultQuery,
+    {
+      key: 'setInBeforeAll',
+      limit: 1,
+      offset: 0,
+      order: 'desc',
+      schema: 'network.xyo.diviner.payload.query',
+      success: true,
+    } as unknown as ImageThumbnailResultQuery,
+    {
+      key: 'setInBeforeAll',
+      limit: 1,
+      offset: 0,
+      order: 'desc',
+      schema: 'network.xyo.diviner.payload.query',
+      success: false,
+    } as unknown as ImageThumbnailResultQuery,
+  ]
+  const cases: [ImageThumbnailDivinerQuery, ImageThumbnailResultQuery][] = queries.map((query, i) => [query, expected[i]])
   beforeAll(async () => {
     diviner = await ImageThumbnailQueryToImageThumbnailIndexQueryDiviner.create()
+    queries.forEach(async (query, i) => {
+      const key = await PayloadHasher.hashAsync({ schema: UrlSchema, url: query.url })
+      expected[i].key = key
+    })
   })
   describe('divine', () => {
-    it.each(cases)('transforms', async (imageThumbnailDivinerQuery) => {
-      const results = await diviner.divine([imageThumbnailDivinerQuery])
-      const actual = results.filter(isImageThumbnailResultQuery)
-      expect(actual).toBeArrayOfSize(1)
-      const { url } = imageThumbnailDivinerQuery
-      const key = await PayloadHasher.hashAsync({ schema: UrlSchema, url })
-      expect(actual?.[0]).toEqual({
-        key,
-        limit: 1,
-        offset: 0,
-        order: 'desc',
-        schema: 'network.xyo.diviner.payload.query',
+    describe('with single query', () => {
+      it.each(cases)('transforms', async (query, expected) => {
+        const results = await diviner.divine([query])
+        const actual = results.filter(isImageThumbnailResultQuery)
+        expect(actual).toBeArrayOfSize(1)
+        expect(actual?.[0]).toEqual(expected)
       })
     })
   })

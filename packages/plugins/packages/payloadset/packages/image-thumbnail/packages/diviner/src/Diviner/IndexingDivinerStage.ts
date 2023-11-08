eb0fe7d671ef295c649dc0cddad71e8c02896ebd
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-11-07 20:20:10 -0600

    Notes for stages

--- packages/plugins/packages/payloadset/packages/image-thumbnail/packages/diviner/src/Diviner/IndexingDivinerStage.ts
+++ packages/plugins/packages/payloadset/packages/image-thumbnail/packages/diviner/src/Diviner/IndexingDivinerStage.ts
@@ -5,7 +5,19 @@ export const DivinerStageSchema: DivinerStageSchema = 'network.xyo.diviner.stage
  * The diviners for each stage of an indexing diviner
  */
 export type IndexingDivinerStage =
+  /**
+   * Uses the current state to determine the next batch of index candidates
+   */
   | 'stateToIndexCandidateDiviner'
+  /**
+   * Transforms the index candidates into indexes
+   */
   | 'indexCandidateToIndexDiviner'
+  /**
+   * Transforms a diviner query into an index query
+   */
   | 'divinerQueryToIndexQueryDiviner'
+  /**
+   * Transforms an index query response into a diviner divine query response
+   */
   | 'indexQueryResponseToDivinerQueryResponseDiviner'

73b219dde2647f61ab24c632ecf755d7092076aa
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-11-17 19:37:05 -0600

    Comment implementation

--- packages/modules/packages/diviner/packages/indexing/packages/temporal/packages/memory/src/IndexQueryResponseToDivinerQueryResponseDiviner.ts
+++ packages/modules/packages/diviner/packages/indexing/packages/temporal/packages/memory/src/IndexQueryResponseToDivinerQueryResponseDiviner.ts
@@ -1,18 +1,8 @@
-import { exists } from '@xylabs/exists'
 import { AbstractDiviner } from '@xyo-network/abstract-diviner'
-import { PayloadHasher } from '@xyo-network/core'
 import { DivinerConfigSchema } from '@xyo-network/diviner-model'
 import { isPayloadDivinerQueryPayload } from '@xyo-network/diviner-payload-model'
-import {
-  ImageThumbnailResult,
-  ImageThumbnailResultFields,
-  ImageThumbnailResultSchema,
-  isImageThumbnailResultIndex,
-} from '@xyo-network/image-thumbnail-payload-plugin'
 import { Labels } from '@xyo-network/module-model'
-import { PayloadBuilder } from '@xyo-network/payload-builder'
 import { Payload } from '@xyo-network/payload-model'
-import { UrlSchema } from '@xyo-network/url-payload-plugin'
 
 /**
  * Transforms an ImageThumbnailIndex response into an ImageThumbnailResponse response
@@ -23,38 +13,9 @@ export class TemporalIndexingDivinerIndexQueryResponseToDivinerQueryResponseDivi
     'network.xyo.diviner.stage': 'indexQueryResponseToDivinerQueryResponseDiviner',
   }
 
-  protected override async divineHandler(payloads: Payload[] = []): Promise<ImageThumbnailResult[]> {
-    // Filter out the two operands
+  protected override async divineHandler(payloads: Payload[] = []): Promise<Payload[]> {
+    await Promise.resolve()
     const imageThumbnailDivinerQueries = payloads.filter(isPayloadDivinerQueryPayload)
-
-    const imageThumbnailResultIndexes = payloads.filter(isImageThumbnailResultIndex)
-
-    // If we have operands
-    if (imageThumbnailDivinerQueries.length && imageThumbnailResultIndexes.length) {
-      // Create a dictionary to translate index keys to the urls that represent them
-      const keyToUrlDictionary = Object.fromEntries(
-        await Promise.all(
-          imageThumbnailDivinerQueries.map(async (imageThumbnailDivinerQuery) => {
-            const { url } = imageThumbnailDivinerQuery
-            const urlPayload = new PayloadBuilder<ImageThumbnailResult>({ schema: UrlSchema }).fields({ url }).build()
-            const key = await PayloadHasher.hashAsync(urlPayload)
-            return [key, url] as const
-          }),
-        ),
-      )
-      // Map the indexes to responses using the dictionary
-      return imageThumbnailResultIndexes
-        .map((imageThumbnailResultIndex) => {
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          const { key, schema, ...commonFields } = imageThumbnailResultIndex
-          const url = keyToUrlDictionary?.[key]
-          if (url) {
-            const fields: ImageThumbnailResultFields = { ...commonFields, url }
-            return new PayloadBuilder<ImageThumbnailResult>({ schema: ImageThumbnailResultSchema }).fields(fields).build()
-          }
-        })
-        .filter(exists)
-    }
-    return []
+    return imageThumbnailDivinerQueries
   }
 }

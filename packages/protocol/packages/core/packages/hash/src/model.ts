d8b24d5f12163d4e54d93d12bb32b40c4440b5e9
Arie Trouw <arie.trouw@xylabs.com>
2023-11-22 13:02:28 -0800

    updo

--- packages/protocol/packages/core/packages/hash/src/model.ts
+++ packages/protocol/packages/core/packages/hash/src/model.ts
@@ -1,1 +1,7 @@
-export type Hash = string
+const hexRegex = /[0-9a-f]+/i
+
+export type Hex = string
+
+export type Hash = Hex
+
+export const isHex = (value: string): value is Hex => hexRegex.test(value)

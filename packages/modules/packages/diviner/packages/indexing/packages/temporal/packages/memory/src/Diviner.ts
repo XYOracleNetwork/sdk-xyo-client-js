b39f05c50c704598e94b49872d9e895225603d5f
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-11-27 17:50:47 -0600

    Get diviner from config for backwards compatibility

--- packages/modules/packages/diviner/packages/indexing/packages/temporal/packages/memory/src/Diviner.ts
+++ packages/modules/packages/diviner/packages/indexing/packages/temporal/packages/memory/src/Diviner.ts
@@ -1,3 +1,4 @@
+import { assertEx } from '@xylabs/assert'
 import { IndexingDiviner } from '@xyo-network/diviner-indexing-memory'
 import { IndexingDivinerConfigSchema, IndexingDivinerStage } from '@xyo-network/diviner-indexing-model'
 import { DivinerConfigSchema, DivinerInstance, DivinerModule, DivinerModuleEventData } from '@xyo-network/diviner-model'
@@ -23,23 +24,6 @@ export class TemporalIndexingDiviner<
   private _indexQueryResponseToDivinerQueryResponseDiviner: DivinerInstance | undefined
   private _stateToIndexCandidateDiviner: DivinerInstance | undefined
 
-  private get divinerQueryToIndexQueryDiviner(): DivinerInstance {
-    if (this._divinerQueryToIndexQueryDiviner) return this._divinerQueryToIndexQueryDiviner
-    throw new Error('TODO: Create diviner')
-  }
-  private get indexCandidateToIndexDiviner(): DivinerInstance {
-    if (this._indexCandidateToIndexDiviner) return this._indexCandidateToIndexDiviner
-    throw new Error('TODO: Create diviner')
-  }
-  private get indexQueryResponseToDivinerQueryResponseDiviner(): DivinerInstance {
-    if (this._indexQueryResponseToDivinerQueryResponseDiviner) return this._indexQueryResponseToDivinerQueryResponseDiviner
-    throw new Error('TODO: Create diviner')
-  }
-  private get stateToIndexCandidateDiviner(): DivinerInstance {
-    if (this._stateToIndexCandidateDiviner) return this._stateToIndexCandidateDiviner
-    throw new Error('TODO: Create diviner')
-  }
-
   /**
    * Gets the Diviner for the supplied Indexing Diviner stage
    * @param transform The Indexing Diviner stage
@@ -48,18 +32,63 @@ export class TemporalIndexingDiviner<
   protected override getIndexingDivinerStage(transform: IndexingDivinerStage) {
     switch (transform) {
       case 'divinerQueryToIndexQueryDiviner':
-        return Promise.resolve(this.divinerQueryToIndexQueryDiviner)
+        return this.getDivinerQueryToIndexQueryDiviner()
       case 'indexCandidateToIndexDiviner':
-        return Promise.resolve(this.indexCandidateToIndexDiviner)
+        return this.getIndexCandidateToIndexDiviner()
       case 'indexQueryResponseToDivinerQueryResponseDiviner':
-        return Promise.resolve(this.indexQueryResponseToDivinerQueryResponseDiviner)
+        return this.getIndexQueryResponseToDivinerQueryResponseDiviner()
       case 'stateToIndexCandidateDiviner':
-        return Promise.resolve(this.stateToIndexCandidateDiviner)
+        return this.getStateToIndexCandidateDiviner()
     }
   }
 
   protected override async startHandler(): Promise<boolean> {
     await super.startHandler()
     return true
+  }
+
+  private async getDivinerQueryToIndexQueryDiviner(): Promise<DivinerInstance> {
+    if (!this._divinerQueryToIndexQueryDiviner) {
+      const name = this.config.indexingDivinerStages?.divinerQueryToIndexQueryDiviner
+      if (name) {
+        this._divinerQueryToIndexQueryDiviner = await this.resolve(name)
+      } else {
+        throw new Error('TODO: Create diviner')
+      }
+    }
+    return assertEx(this._divinerQueryToIndexQueryDiviner, 'divinerQueryToIndexQueryDiviner')
+  }
+  private async getIndexCandidateToIndexDiviner(): Promise<DivinerInstance> {
+    if (!this._indexCandidateToIndexDiviner) {
+      const name = this.config.indexingDivinerStages?.indexCandidateToIndexDiviner
+      if (name) {
+        this._indexCandidateToIndexDiviner = await this.resolve(name)
+      } else {
+        throw new Error('TODO: Create diviner')
+      }
+    }
+    return assertEx(this._indexCandidateToIndexDiviner, 'indexCandidateToIndexDiviner')
+  }
+  private async getIndexQueryResponseToDivinerQueryResponseDiviner(): Promise<DivinerInstance> {
+    if (!this._indexQueryResponseToDivinerQueryResponseDiviner) {
+      const name = this.config.indexingDivinerStages?.indexQueryResponseToDivinerQueryResponseDiviner
+      if (name) {
+        this._indexQueryResponseToDivinerQueryResponseDiviner = await this.resolve(name)
+      } else {
+        throw new Error('TODO: Create diviner')
+      }
+    }
+    return assertEx(this._indexQueryResponseToDivinerQueryResponseDiviner, 'indexQueryResponseToDivinerQueryResponseDiviner')
+  }
+  private async getStateToIndexCandidateDiviner(): Promise<DivinerInstance> {
+    if (!this._stateToIndexCandidateDiviner) {
+      const name = this.config.indexingDivinerStages?.stateToIndexCandidateDiviner
+      if (name) {
+        this._stateToIndexCandidateDiviner = await this.resolve(name)
+      } else {
+        throw new Error('TODO: Create diviner')
+      }
+    }
+    return assertEx(this._stateToIndexCandidateDiviner, 'stateToIndexCandidateDiviner')
   }
 }

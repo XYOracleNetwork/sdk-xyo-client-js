eaaaf904e79b34be874101029acb4160e7dfdd2b
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-11-29 22:22:25 -0600

    Documentation

--- packages/modules/packages/diviner/packages/indexing/packages/temporal/packages/memory/src/jsonpath/reducePayloads.ts
+++ packages/modules/packages/diviner/packages/indexing/packages/temporal/packages/memory/src/jsonpath/reducePayloads.ts
@@ -3,11 +3,18 @@ import { SchemaToPayloadTransformersDictionary } from '@xyo-network/diviner-temp
 import { PayloadBuilder } from '@xyo-network/payload-builder'
 import { Payload, PayloadFields } from '@xyo-network/payload-model'
 
-export const reducePayloads = async (
+/**
+ * Reduces the payloads to a single payload using the supplied transformers
+ * @param payloads The payloads to reduce
+ * @param payloadTransformers The transformers to use to convert the payloads to the destination payload
+ * @param destinationSchema The schema of the destination payload
+ * @returns The reduced payload
+ */
+export const reducePayloads = async <T extends Payload = Payload>(
   payloads: Payload[],
   payloadTransformers: SchemaToPayloadTransformersDictionary,
   destinationSchema: string,
-): Promise<Payload> => {
+): Promise<T> => {
   // Use the payload transformers to convert the fields from the source payloads to the destination fields
   const indexFields = payloads
     .map<PayloadFields[]>((payload) => {
@@ -20,17 +27,24 @@ export const reducePayloads = async (
   // Include all the sources for reference
   const sources = (await PayloadHasher.hashPairs([...payloads])).map(([, hash]) => hash)
   // Build and return the index
-  return new PayloadBuilder<Payload>({ schema: destinationSchema }).fields(Object.assign({ sources }, ...indexFields)).build()
+  return new PayloadBuilder<T>({ schema: destinationSchema }).fields(Object.assign({ sources }, ...indexFields)).build()
 }
 
-export const reducePayloadsArray = async (
+/**
+ * Reduces the arrays of payload arrays to an array of payloads using the supplied transformers
+ * @param payloadsArray The arrays of payloads to reduce
+ * @param payloadTransformers The transformers to use to convert the payloads to the destination payloads
+ * @param destinationSchema The schema of the destination payloads
+ * @returns The reduced payloads
+ */
+export const reducePayloadsArray = async <T extends Payload = Payload>(
   payloadsArray: Payload[][],
   payloadTransformers: SchemaToPayloadTransformersDictionary,
   destinationSchema: string,
-): Promise<Payload[]> => {
+): Promise<T[]> => {
   return await Promise.all(
     payloadsArray.map(async (payloads) => {
-      return await reducePayloads(payloads, payloadTransformers, destinationSchema)
+      return await reducePayloads<T>(payloads, payloadTransformers, destinationSchema)
     }),
   )
 }

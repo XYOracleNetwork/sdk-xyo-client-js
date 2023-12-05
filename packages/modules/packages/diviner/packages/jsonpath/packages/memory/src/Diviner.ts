70d64f3495b686fb05fcff3b16d2447bf9cb6c8b
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-12-05 14:46:14 -0600

    JSON Path payload building

--- packages/modules/packages/diviner/packages/jsonpath/packages/memory/src/Diviner.ts
+++ packages/modules/packages/diviner/packages/jsonpath/packages/memory/src/Diviner.ts
@@ -1,9 +1,12 @@
 import { assertEx } from '@xylabs/assert'
-import { exists } from '@xylabs/exists'
 import { AbstractDiviner } from '@xyo-network/abstract-diviner'
-import { JsonPathDivinerConfigSchema, JsonPathDivinerParams } from '@xyo-network/diviner-jsonpath-model'
+import { JsonPathDivinerConfigSchema, JsonPathDivinerParams, PayloadTransformer } from '@xyo-network/diviner-jsonpath-model'
 import { DivinerModule, DivinerModuleEventData } from '@xyo-network/diviner-model'
-import { Payload } from '@xyo-network/payload-model'
+import { PayloadHasher } from '@xyo-network/hash'
+import { PayloadBuilder } from '@xyo-network/payload-builder'
+import { Payload, PayloadFields } from '@xyo-network/payload-model'
+
+import { toPayloadTransformer } from './jsonpath'
 
 export class JsonPathDiviner<
   TParams extends JsonPathDivinerParams = JsonPathDivinerParams,
@@ -13,21 +16,33 @@ export class JsonPathDiviner<
 > extends AbstractDiviner<TParams, TIn, TOut, TEventData> {
   static override configSchemas = [JsonPathDivinerConfigSchema]
 
-  protected get operations(): readonly Operation[] {
-    return assertEx(this.config?.operations, 'JsonPathDiviner: invalid operations')
+  protected _transforms: PayloadTransformer[] | undefined
+
+  /**
+   * Dictionary of schemas to payload transformers for creating indexes
+   * from the payloads within a Bound Witness
+   */
+  protected get transforms(): PayloadTransformer[] {
+    if (!this._transforms) {
+      const transforms = assertEx(this.config.transforms, 'config.transforms is not defined')
+      this._transforms = transforms.map(toPayloadTransformer)
+    }
+    return this._transforms
   }
 
   protected override async divineHandler(payloads?: TIn[]): Promise<TOut[]> {
-    const results = payloads
-      ?.map((payload) => {
-        try {
-          const result = applyPatch(payload, this.operations, { mutate: false })
-          return result.res?.[0]?.doc as TOut
-        } catch (e) {
-          return undefined
-        }
-      })
-      .filter(exists)
-    return await Promise.resolve(results ?? [])
+    if (!payloads) return []
+    // Create the indexes from the tuples
+    const results = await Promise.all(
+      payloads.map<Promise<TOut>>(async (payload) => {
+        // Use the payload transformers to convert the fields from the source payloads to the destination fields
+        const indexFields: PayloadFields[] = this.transforms.map((transform) => transform(payload))
+        // Include all the sources for reference
+        const sources = Object.keys(await PayloadHasher.toMap([payload]))
+        // Build and return the index
+        return new PayloadBuilder<TOut>({ schema: 'todo' }).fields(Object.assign({ sources }, ...indexFields)).build()
+      }),
+    )
+    return results
   }
 }

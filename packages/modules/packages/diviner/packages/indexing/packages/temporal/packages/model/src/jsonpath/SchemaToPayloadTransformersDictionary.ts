7369ac196a53122dea07162423297a3c226d3521
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-12-20 12:21:53 -0600

    Remove local types

--- packages/modules/packages/diviner/packages/indexing/packages/temporal/packages/model/src/jsonpath/SchemaToPayloadTransformersDictionary.ts
+++
@@ -1,6 +0,0 @@
-import { PayloadTransformer } from './PayloadTransformer'
-
-/**
- * A dictionary of schema to payload transformers
- */
-export type SchemaToPayloadTransformersDictionary = { [schema: string]: PayloadTransformer[] }

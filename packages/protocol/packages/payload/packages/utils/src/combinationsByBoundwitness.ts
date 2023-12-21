304812ff4638337abeba1e97991ed4218e534b44
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-12-21 14:08:42 -0600

    Add bound witness packetizer

---
+++ packages/protocol/packages/payload/packages/utils/src/combinationsByBoundwitness.ts
@@ -0,0 +1,25 @@
+import { exists } from '@xylabs/exists'
+import { difference } from '@xylabs/set'
+import { BoundWitness, isBoundWitness } from '@xyo-network/boundwitness'
+import { PayloadHasher } from '@xyo-network/hash'
+import { Payload } from '@xyo-network/payload-model'
+
+/**
+ * Returns tuples of bound witnesses and their payloads from the supplied payloads. Omits
+ * bound witnesses where the payloads were not included in the supplied payloads.
+ * @param payloads An array of bound witnesses and payloads
+ * @returns An array of tuples of bound witnesses and their payloads
+ */
+export const combinationsByBoundwitness = async (payloads: Payload[]): Promise<[BoundWitness, ...Payload[]][]> => {
+  await Promise.resolve()
+  const bws = new Set(payloads.filter(isBoundWitness))
+  const remaining = difference(new Set(payloads), bws)
+  const payloadDictionary = await PayloadHasher.toMap([...remaining])
+  return [...bws]
+    .map((bw) => {
+      const { payload_hashes } = bw
+      const p = payload_hashes.map((h) => payloadDictionary[h])
+      return [bw, ...p] as [BoundWitness, ...Payload[]]
+    })
+    .filter(exists)
+}

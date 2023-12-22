dae16c2946a3bf44ca4a0a7ff6235dd4aa1c932c
Arie Trouw <arie.trouw@xylabs.com>
2023-12-22 11:34:05 -0800

    deploy 2.85.3

--- packages/modules/packages/diviner/packages/range/packages/model/src/Config/Config.ts
+++ packages/modules/packages/diviner/packages/range/packages/model/src/Config/Config.ts
@@ -4,17 +4,13 @@ import { isPayloadOfSchemaType } from '@xyo-network/payload-model'
 export const RangeDivinerConfigSchema = 'network.xyo.diviner.range.config'
 export type RangeDivinerConfigSchema = typeof RangeDivinerConfigSchema
 
-import { Hex } from '@xylabs/hex'
-
 import { RangePayload } from '../Payload'
 
-export type RangeDivinerConfigBase<T> = DivinerConfig<
+export type RangeDivinerConfig = DivinerConfig<
   {
     ranges?: RangePayload[]
   },
   RangeDivinerConfigSchema
 >
-
-export type RangeDivinerConfig = RangeDivinerConfigBase<number> | RangeDivinerConfigBase<Hex>
 
 export const isRangeDivinerConfig = isPayloadOfSchemaType<RangeDivinerConfig>(RangeDivinerConfigSchema)

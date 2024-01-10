9155047284040e35efafc0d24be80df2677c4dd9
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-01-10 15:26:05 -0600

    Add start code for diviner

--- packages/modules/packages/diviner/packages/indexeddb/packages/payload/src/Diviner.ts
+++ packages/modules/packages/diviner/packages/indexeddb/packages/payload/src/Diviner.ts
@@ -1,9 +1,10 @@
 import { assertEx } from '@xylabs/assert'
+import { IndexDescription } from '@xyo-network/archivist-model'
 import { DivinerModule, DivinerModuleEventData } from '@xyo-network/diviner-model'
 import { PayloadDiviner } from '@xyo-network/diviner-payload-abstract'
 import { isPayloadDivinerQueryPayload, PayloadDivinerQueryPayload } from '@xyo-network/diviner-payload-model'
 import { Payload } from '@xyo-network/payload-model'
-import { IDBPDatabase } from 'idb'
+import { IDBPDatabase, openDB } from 'idb'
 
 import { IndexedDbPayloadDivinerConfigSchema } from './Config'
 import { IndexedDbPayloadDivinerParams } from './Params'
@@ -22,6 +23,7 @@ export class IndexedDbPayloadDiviner<
   static defaultDbName = 'archivist'
   static defaultDbVersion = 1
   static defaultStoreName = 'payloads'
+  static schemaIndex: Required<IndexDescription> = { key: { schema: 1 }, name: 'IX_schema', unique: false }
 
   private _db: IDBPDatabase<PayloadStore> | undefined
 
@@ -67,7 +69,36 @@ export class IndexedDbPayloadDiviner<
     const archivist = assertEx(await this.getArchivist(), 'Unable to resolve archivist')
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { schemas, limit, offset, hash, order, schema, ...props } = filter
-
+    // TODO: Conditionally on schemas
+    // TODO: How to handle offset?
     return []
+  }
+  protected override async startHandler() {
+    await super.startHandler()
+    // NOTE: We could defer this creation to first access but we
+    // want to fail fast here in case something is wrong
+    const { storeName, dbName, dbVersion } = this
+    this._db = await openDB<PayloadStore>(dbName, dbVersion, {
+      async upgrade(database) {
+        await Promise.resolve() // Async to match spec
+        // Create the store
+        const store = database.createObjectStore(storeName, {
+          // If it isn't explicitly set, create a value by auto incrementing.
+          autoIncrement: true,
+        })
+        // Name the store
+        store.name = storeName
+        // TODO: Create indexes from config as well
+        const indexesToCreate = [IndexedDbPayloadDiviner.schemaIndex]
+        for (const { key, name, unique } of indexesToCreate) {
+          const indexKeys = Object.keys(key)
+          const keys = indexKeys.length === 1 ? indexKeys[0] : indexKeys
+          const indexName = name ?? `IX_${indexKeys.join('_')}`
+          store.createIndex(indexName, keys, { unique })
+        }
+      },
+    })
+
+    return true
   }
 }

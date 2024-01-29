0f3b58a9bd6545ddd59f62316c523b6bd5aeb852
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-01-29 15:26:34 -0600

    Add remote state to config

--- packages/modules/packages/bridge/packages/pub-sub/src/Config.ts
+++ packages/modules/packages/bridge/packages/pub-sub/src/Config.ts
@@ -6,9 +6,16 @@ import { PubSubBridgeSchema } from './Schema'
 export const PubSubBridgeConfigSchema = `${PubSubBridgeSchema}.config`
 export type PubSubBridgeConfigSchema = typeof PubSubBridgeConfigSchema
 
+interface CacheModuleConfig {
+  archivist: string
+  boundWitnessDiviner: string
+}
+
 export type PubSubBridgeConfig<TConfig extends EmptyObject = EmptyObject, TSchema extends string | void = void> = BridgeConfig<
   {
-    nodeUrl?: string
+    bridge?: string
+    queryCache?: CacheModuleConfig
+    responseCache?: CacheModuleConfig
     schema: PubSubBridgeConfigSchema
   } & TConfig,
   TSchema extends string ? TSchema : PubSubBridgeConfigSchema

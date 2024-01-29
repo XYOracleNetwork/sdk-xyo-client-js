47c88c85ded0a4591b8c47ce259b7b1c4571728b
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-01-29 14:57:27 -0600

    Simplify types

--- packages/modules/packages/bridge/packages/pub-sub/src/Bridge.ts
+++ packages/modules/packages/bridge/packages/pub-sub/src/Bridge.ts
@@ -1,11 +1,10 @@
 import { Promisable } from '@xylabs/promise'
-import { RetryConfig } from '@xylabs/retry'
 import { AbstractBridge } from '@xyo-network/abstract-bridge'
-import { BoundWitnessFields } from '@xyo-network/boundwitness-model'
+import { QueryBoundWitness } from '@xyo-network/boundwitness-model'
 import { BridgeModule } from '@xyo-network/bridge-model'
-import { ModuleManifest } from '@xyo-network/manifest-model'
-import { ArchivingModuleConfig, CosigningAddressSet, creatableModule, Labels, ModuleEventData, ModuleQueryResult } from '@xyo-network/module-model'
-import { QueryFields, SchemaFields } from '@xyo-network/payload-model'
+import { ModuleManifestPayload } from '@xyo-network/manifest-model'
+import { creatableModule, ModuleConfig, ModuleEventData, ModuleQueryResult } from '@xyo-network/module-model'
+import { Payload, QueryFields, SchemaFields } from '@xyo-network/payload-model'
 
 import { PubSubBridgeConfigSchema } from './Config'
 import { PubSubBridgeParams } from './Params'
@@ -15,84 +14,6 @@ export class PubSubBridge<TParams extends PubSubBridgeParams, TEventData extends
   extends AbstractBridge<TParams, TEventData>
   implements BridgeModule<TParams, TEventData>
 {
-  override getRootAddress(): Promisable<string> {
-    throw new Error('Method not implemented.')
-  }
-  override targetConfig(address: string): SchemaFields &
-    object & {
-      readonly archivist?: string | undefined
-      readonly labels?: Labels | undefined
-      readonly name?: string | undefined
-      readonly paging?: Record<string, { size?: number | undefined }> | undefined
-      readonly retry?: RetryConfig | undefined
-      schema: 'network.xyo.module.config'
-      readonly security?:
-        | {
-            readonly allowAnonymous?: boolean | undefined
-            readonly allowed?: Record<string, (string | CosigningAddressSet)[]> | undefined
-            readonly disallowed?: Record<string, string[]> | undefined
-          }
-        | undefined
-      readonly sign?: boolean | undefined
-      readonly storeQueries?: boolean | undefined
-      readonly timestamp?: boolean | undefined
-    } & ArchivingModuleConfig {
-    throw new Error('Method not implemented.')
-  }
-  override targetDiscover(address?: string | undefined, maxDepth?: number | undefined): Promisable<({ schema: string } & object)[]> {
-    throw new Error('Method not implemented.')
-  }
-  override targetManifest(
-    address: string,
-    maxDepth?: number | undefined,
-  ): Promisable<
-    | (SchemaFields & object & ModuleManifest & { schema: 'network.xyo.module.manifest' })
-    | (SchemaFields & object & ModuleManifest & { schema: 'network.xyo.node.manifest' })
-  > {
-    throw new Error('Method not implemented.')
-  }
-  override targetQueries(address: string): string[] {
-    throw new Error('Method not implemented.')
-  }
-  override targetQuery(
-    address: string,
-    query: SchemaFields & object & QueryFields & { schema: string },
-    payloads?: ({ schema: string } & object)[] | undefined,
-  ): Promisable<ModuleQueryResult> {
-    throw new Error('Method not implemented.')
-  }
-  override targetQueryable(
-    address: string,
-    query: SchemaFields &
-      object &
-      Omit<BoundWitnessFields & { query: string; resultSet?: string | undefined; schema: 'network.xyo.boundwitness' }, 'schema'> & {
-        schema: 'network.xyo.boundwitness'
-      },
-    payloads?: ({ schema: string } & object)[] | undefined,
-    queryConfig?:
-      | (SchemaFields &
-          object & {
-            readonly archivist?: string | undefined
-            readonly labels?: Labels | undefined
-            readonly name?: string | undefined
-            readonly paging?: Record<string, { size?: number | undefined }> | undefined
-            readonly retry?: RetryConfig | undefined
-            schema: 'network.xyo.module.config'
-            readonly security?:
-              | {
-                  readonly allowAnonymous?: boolean | undefined
-                  readonly allowed?: Record<string, (string | CosigningAddressSet)[]> | undefined
-                  readonly disallowed?: Record<string, string[]> | undefined
-                }
-              | undefined
-            readonly sign?: boolean | undefined
-            readonly storeQueries?: boolean | undefined
-            readonly timestamp?: boolean | undefined
-          } & ArchivingModuleConfig)
-      | undefined,
-  ): boolean {
-    throw new Error('Method not implemented.')
-  }
   static override configSchemas = [PubSubBridgeConfigSchema]
 
   // TODO: Get from config
@@ -123,6 +44,49 @@ export class PubSubBridge<TParams extends PubSubBridgeParams, TEventData extends
     await Promise.resolve()
     this.gateway = undefined
     this.connected = false
+    return true
+  }
+
+  override getRootAddress(): Promisable<string> {
+    throw new Error('Method not implemented.')
+  }
+
+  override targetConfig(_address: string): ModuleConfig {
+    if (!this.connected) {
+      throw new Error('Not connected')
+    }
+    throw new Error('Method not implemented.')
+  }
+  override targetDiscover(_address?: string | undefined, _maxDepth?: number | undefined): Promise<Payload[]> {
+    if (!this.connected) {
+      throw new Error('Not connected')
+    }
+    throw new Error('Method not implemented.')
+  }
+  override targetManifest(_address: string, _maxDepth?: number | undefined): Promise<ModuleManifestPayload> {
+    if (!this.connected) {
+      throw new Error('Not connected')
+    }
+    throw new Error('Method not implemented.')
+  }
+  override targetQueries(_address: string): string[] {
+    if (!this.connected) {
+      throw new Error('Not connected')
+    }
+    throw new Error('Method not implemented.')
+  }
+  override async targetQuery(
+    _address: string,
+    _query: SchemaFields & object & QueryFields & { schema: string },
+    _payloads?: Payload[] | undefined,
+  ): Promise<ModuleQueryResult> {
+    if (!this.connected) {
+      throw new Error('Not connected')
+    }
+    await this.started('throw')
+    throw new Error('Method not implemented.')
+  }
+  override targetQueryable(_address: string, _query: QueryBoundWitness, _payloads?: Payload[], _queryConfig?: ModuleConfig): boolean {
     return true
   }
 }

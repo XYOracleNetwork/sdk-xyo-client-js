5b53e8af7691fa04e09cc10ff4036a25f6c00bfe
Arie Trouw <arie.trouw@xylabs.com>
2024-02-06 09:28:39 -0800

    WIP

--- packages/modules/packages/bridge/packages/pub-sub/src/Config.ts
+++ packages/modules/packages/bridge/packages/pub-sub/src/Config.ts
@@ -9,75 +9,77 @@ export type PubSubBridgeConfigSchema = typeof PubSubBridgeConfigSchema
 /**
  * Configuration for searchable storage of local state
  */
-export interface SearchableStorage {
+export interface SearchableStorage<TArchivist = string, TDiviner = string> {
   /**
    * Name/Address of the archivist where intermediate communications are stored
    */
-  archivist: string
+  archivist: TArchivist
   /**
    * Name/Address of the diviner where intermediate communications are filtered
    */
-  boundWitnessDiviner: string
+  boundWitnessDiviner: TDiviner
 }
 
 /**
  * Configuration for the mutually accessible
  * modules between the modules being connected
  */
-export interface IntermediaryConfig {
+export interface Intermediary<TArchivist = string, TDiviner = string> {
   /**
    * Name/Address of the archivist where intermediate communications are stored
    */
-  archivist: string
+  archivist: TArchivist
   /**
    * Name/Address of the diviner where intermediate communications are filtered
    */
-  boundWitnessDiviner: string
+  boundWitnessDiviner: TDiviner
+}
+
+export interface AsyncQueryBusConfig<TArchivist = string, TDiviner = string> {
+  /**
+   * How many queries to process at once when retrieving queries
+   * for an address
+   */
+  individualAddressBatchQueryLimit?: number
+  /**
+   * How often to poll for new queries/responses
+   */
+  pollFrequency?: number
+  /**
+   * Configuration for intermediary query storage
+   */
+  queries?: Intermediary<TArchivist, TDiviner>
+
+  /**
+   * Configuration for intermediary response storage
+   */
+  queryCache?: CacheConfig | true
+
+  /**
+   * Configuration for intermediary response storage
+   */
+  responses?: Intermediary<TArchivist, TDiviner>
+
+  /**
+   * The root address to connect the bridge to
+   */
+  rootAddress?: string
+
+  /**
+   * The configurations schema for the module
+   */
+  schema: PubSubBridgeConfigSchema
+
+  /**
+   * Where the archivist should persist its internal state
+   */
+  stateStore?: SearchableStorage<TArchivist, TDiviner>
 }
 
 /**
  * Configuration for the PubSubBridge
  */
 export type PubSubBridgeConfig<TConfig extends EmptyObject = EmptyObject, TSchema extends string | void = void> = BridgeConfig<
-  {
-    /**
-     * How many queries to process at once when retrieving queries
-     * for an address
-     */
-    individualAddressBatchQueryLimit?: number
-    /**
-     * How often to poll for new queries/responses
-     */
-    pollFrequency?: number
-    /**
-     * Configuration for intermediary query storage
-     */
-    queries?: IntermediaryConfig
-
-    /**
-     * Configuration for intermediary response storage
-     */
-    queryCache?: CacheConfig | true
-
-    /**
-     * Configuration for intermediary response storage
-     */
-    responses?: IntermediaryConfig
-
-    /**
-     * The root address to connect the bridge to
-     */
-    rootAddress?: string
-
-    /**
-     * The configurations schema for the module
-     */
-    schema: PubSubBridgeConfigSchema
-
-    /**
-     * Where the archivist should persist its internal state
-     */
-    stateStore?: SearchableStorage
-  } & TConfig,
+  AsyncQueryBusConfig & TConfig,
   TSchema extends string ? TSchema : PubSubBridgeConfigSchema
 >
--- packages/modules/packages/bridge/packages/pub-sub/src/Params.ts
+++ packages/modules/packages/bridge/packages/pub-sub/src/Params.ts
@@ -1,9 +1,19 @@
+import { BaseParams } from '@xylabs/object'
+import { ArchivistInstance } from '@xyo-network/archivist-model'
 import { BridgeParams } from '@xyo-network/bridge-model'
-import { AnyConfigSchema } from '@xyo-network/module-model'
+import { DivinerInstance } from '@xyo-network/diviner-model'
+import { AnyConfigSchema, ModuleInstance } from '@xyo-network/module-model'
 
-import { PubSubBridgeConfig } from './Config'
+import { AsyncQueryBusConfig, PubSubBridgeConfig } from './Config'
 
 /**
  * The parameters for the PubSubBridge
  */
 export type PubSubBridgeParams<TConfig extends AnyConfigSchema<PubSubBridgeConfig> = AnyConfigSchema<PubSubBridgeConfig>> = BridgeParams<TConfig>
+
+export type AsyncQueryBusParams = BaseParams<
+  {
+    config: Omit<AsyncQueryBusConfig<ArchivistInstance, DivinerInstance>, 'responses' | 'queries' | 'schema'>
+    listeningModules: () => Promise<ModuleInstance[]>
+  } & Required<Pick<AsyncQueryBusConfig<ArchivistInstance, DivinerInstance>, 'responses' | 'queries'>>
+>

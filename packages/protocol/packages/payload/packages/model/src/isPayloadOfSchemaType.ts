9c2f5caded349eda7de4d5ae73de1a5c5f59c8a5
Arie Trouw <arie.trouw@xylabs.com>
2024-02-13 07:47:41 -0800

    type test code

--- packages/protocol/packages/payload/packages/model/src/isPayloadOfSchemaType.ts
+++ packages/protocol/packages/payload/packages/model/src/isPayloadOfSchemaType.ts
@@ -1,5 +1,5 @@
 import { isAnyPayload } from './isPayload'
-import { Payload, WithMeta } from './Payload'
+import { Payload, PayloadWithMeta, WithMeta } from './Payload'
 
 export const isPayloadOfSchemaType = <T extends Payload>(schema: string) => {
   return (x?: unknown | null): x is T => isAnyPayload(x) && x?.schema === schema
@@ -12,3 +12,29 @@ export const isPayloadOfSchemaTypeWithMeta = <T extends Payload>(schema: string)
 export const notPayloadOfSchemaType = <T extends Payload>(schema: string) => {
   return (x?: unknown | null): x is T => !isAnyPayload(x) || x?.schema !== schema
 }
+
+//test types -- keep for future validation, but comment out
+
+/*
+type TestSchema = 'network.xyo.test'
+const TestSchema: TestSchema = 'network.xyo.test'
+
+type Test = Payload<{ field: string }, TestSchema>
+
+const testPayload: Test = { field: 'test', schema: TestSchema }
+const testWithMeta: WithMeta<Test> = { $hash: '1234abcd', $meta: { timestamp: Date.now() }, field: 'test', schema: TestSchema }
+
+const testPayloads: Payload[] = [testPayload]
+const testMetaPayloads: WithMeta<Payload>[] = [testWithMeta]
+
+const testType: Test = testWithMeta
+
+const isTest: Test[] = testPayloads.filter(isPayloadOfSchemaType(TestSchema))
+const isTestFromMeta: Payload[] = testMetaPayloads.filter(isPayloadOfSchemaType(TestSchema))
+const isTestFromMetaWithMeta: PayloadWithMeta[] = testMetaPayloads.filter(isPayloadOfSchemaType(TestSchema))
+const isTestFromMetaTyped: Test[] = testMetaPayloads.filter(isPayloadOfSchemaType(TestSchema))
+const isTestFromMetaTypedWithMeta: WithMeta<Test>[] = testMetaPayloads.filter(isPayloadOfSchemaType(TestSchema))
+
+const isTestWithMeta: WithMeta<Test>[] = testPayloads.filter(isPayloadOfSchemaTypeWithMeta(TestSchema))
+const isTestWithMetaFromMeta: WithMeta<Test>[] = testMetaPayloads.filter(isPayloadOfSchemaTypeWithMeta(TestSchema))
+*/

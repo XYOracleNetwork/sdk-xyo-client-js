49263730908d326c9568510ca181be7784cd90d4
Arie Trouw <arie.trouw@xylabs.com>
2024-02-20 09:02:01 -0800

    Multi Segment resolve tests passes

--- packages/modules/src/spec/MultiSegmentResolve.spec.ts
+++ packages/modules/src/spec/MultiSegmentResolve.spec.ts
@@ -13,7 +13,7 @@ const moduleBName = 'module'
  */
 
 describe('SimpleModuleResolverDeep', () => {
-  test.skip('two archivists same name, different nodes', async () => {
+  test('two archivists same name, different nodes', async () => {
     const rootNode = await MemoryNode.create({ account: 'random' })
     const nodeA = await MemoryNode.create({ account: 'random', config: { name: nodeAName, schema: NodeConfigSchema } })
     const nodeB = await MemoryNode.create({ account: 'random', config: { name: nodeBName, schema: NodeConfigSchema } })
@@ -21,24 +21,30 @@ describe('SimpleModuleResolverDeep', () => {
     const moduleB = await MemoryArchivist.create({ account: 'random', config: { name: moduleBName, schema: ArchivistConfigSchema } })
 
     await nodeA.register(moduleA)
-    await nodeA.attach(moduleA.address)
+    await nodeA.attach(moduleA.address, true)
+
+    const resolvedModuleAFromNodeA = await nodeA.resolve(moduleAName)
+    expect(resolvedModuleAFromNodeA?.address).toBe(moduleA.address)
 
     await nodeB.register(moduleB)
-    await nodeB.attach(moduleB.address)
+    await nodeB.attach(moduleB.address, true)
+
+    const resolvedModuleBFromNodeB = await nodeB.resolve(moduleBName)
+    expect(resolvedModuleBFromNodeB?.address).toBe(moduleB.address)
 
     await rootNode.register(nodeA)
-    await rootNode.attach(nodeA.address)
+    await rootNode.attach(nodeA.address, true)
     await rootNode.register(nodeB)
-    await rootNode.attach(nodeB.address)
+    await rootNode.attach(nodeB.address, true)
 
-    const resolvedModuleA = rootNode.resolve(nodeAName)
-    expect(resolvedModuleA).toBe(moduleA.address)
-    const resolvedModuleB = rootNode.resolve(nodeBName)
-    expect(resolvedModuleB).toBe(moduleB.address)
+    const resolvedModuleA = await rootNode.resolve(moduleAName)
+    expect([moduleA.address, moduleB.address]).toInclude(resolvedModuleA?.address ?? '')
+    const resolvedModuleB = await rootNode.resolve(moduleBName)
+    expect([moduleA.address, moduleB.address]).toInclude(resolvedModuleB?.address ?? '')
 
-    const resolvedModuleAMulti = rootNode.resolve(`${nodeAName}:${moduleAName}`)
-    expect(resolvedModuleAMulti).toBe(moduleA.address)
-    const resolvedModuleBMulti = rootNode.resolve(`${nodeBName}:${moduleBName}`)
-    expect(resolvedModuleBMulti).toBe(moduleB.address)
+    const resolvedModuleAMulti = await rootNode.resolve(`${nodeAName}:${moduleAName}`)
+    expect(resolvedModuleAMulti?.address).toBe(moduleA.address)
+    const resolvedModuleBMulti = await rootNode.resolve(`${nodeBName}:${moduleBName}`)
+    expect(resolvedModuleBMulti?.address).toBe(moduleB.address)
   })
 })

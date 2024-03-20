06531608b5a1b1b932406c374d43ddaee601cccc
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-03-20 06:33:14 -0500

    Allow for allow for desired output types

--- packages/modules/packages/diviner/packages/hash/src/Diviner.ts
+++ packages/modules/packages/diviner/packages/hash/src/Diviner.ts
@@ -1,6 +1,6 @@
 import { AbstractDiviner } from '@xyo-network/diviner-abstract'
 import { creatableModule } from '@xyo-network/module-model'
-import { Payload, WithSources } from '@xyo-network/payload-model'
+import { Payload } from '@xyo-network/payload-model'
 
 import { HashLeaseEstimateDivinerConfigSchema } from './Config'
 import { HashLeaseEstimateDivinerParams } from './Params'
@@ -10,11 +10,12 @@ import { HashLeaseEstimate, HashLeaseEstimateSchema } from './Payload'
 export class HashLeaseEstimateDiviner<
   TParams extends HashLeaseEstimateDivinerParams = HashLeaseEstimateDivinerParams,
   TIn extends Payload = Payload,
-> extends AbstractDiviner<TParams, TIn, HashLeaseEstimate> {
+  TOut extends HashLeaseEstimate = HashLeaseEstimate,
+> extends AbstractDiviner<TParams, TIn, TOut> {
   static override configSchemas = [HashLeaseEstimateDivinerConfigSchema]
   static override targetSchema = HashLeaseEstimateSchema
 
-  protected override async divineHandler(_payloads: TIn[] = []): Promise<WithSources<HashLeaseEstimate>[]> {
+  protected override async divineHandler(_payloads: TIn[] = []): Promise<TOut[]> {
     return await Promise.resolve([])
   }
 }

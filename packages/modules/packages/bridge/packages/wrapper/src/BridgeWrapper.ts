7a12a60cfb5cb7f5b3f9b1112fd1b8019e9abd4b
Arie Trouw <arie.trouw@xylabs.com>
2024-03-25 11:50:28 -0700

    deploy 2.93.0-rc.1

--- packages/modules/packages/bridge/packages/wrapper/src/BridgeWrapper.ts
+++ packages/modules/packages/bridge/packages/wrapper/src/BridgeWrapper.ts
@@ -14,7 +14,7 @@ import {
   ModuleFilterPayload,
   ModuleFilterPayloadSchema,
 } from '@xyo-network/bridge-model'
-import { ModuleIdentifier } from '@xyo-network/module-model'
+import { ModuleIdentifier, ModuleInstance } from '@xyo-network/module-model'
 import { constructableModuleWrapper, ModuleWrapper } from '@xyo-network/module-wrapper'
 
 constructableModuleWrapper()
@@ -25,17 +25,19 @@ export class BridgeWrapper<TWrappedModule extends BridgeModule = BridgeModule>
   static override instanceIdentityCheck = isBridgeInstance
   static override moduleIdentityCheck = isBridgeModule
 
-  async expose(id: ModuleIdentifier, options?: BridgeExposeOptions): Promise<Address[]> {
+  async expose(id: ModuleIdentifier, options?: BridgeExposeOptions): Promise<ModuleInstance[]> {
     const filterPayload: ModuleFilterPayload = { id, schema: ModuleFilterPayloadSchema, ...options }
-    return (await this.sendQuery<BridgeExposeQuery, ModuleFilterPayload, AddressPayload>({ schema: BridgeExposeQuerySchema }, [filterPayload])).map(
-      ({ address }) => address,
-    )
+    const addresses = (
+      await this.sendQuery<BridgeExposeQuery, ModuleFilterPayload, AddressPayload>({ schema: BridgeExposeQuerySchema }, [filterPayload])
+    ).map(({ address }) => address)
+    return await Promise.all((addresses as Address[]).map((address) => this.resolve(address)))
   }
 
-  async unexpose(id: ModuleIdentifier, options?: BridgeUnexposeOptions): Promise<Address[]> {
+  async unexpose(id: ModuleIdentifier, options?: BridgeUnexposeOptions): Promise<ModuleInstance[]> {
     const filterPayload: ModuleFilterPayload = { id, schema: ModuleFilterPayloadSchema, ...options }
-    return (
+    const addresses = (
       await this.sendQuery<BridgeUnexposeQuery, ModuleFilterPayload, AddressPayload>({ schema: BridgeUnexposeQuerySchema }, [filterPayload])
     ).map(({ address }) => address)
+    return addresses ? [] : []
   }
 }

8b3d4d91854384a2d98d6dde36a2f46b51efc929
Arie Trouw <arie.trouw@xylabs.com>
2024-03-29 00:32:45 -0700

    witness types

--- packages/modules/packages/witness/packages/model/src/EventsModels/ObserveStart.ts
+++ packages/modules/packages/witness/packages/model/src/EventsModels/ObserveStart.ts
@@ -1,14 +1,14 @@
 import { EventData } from '@xyo-network/module-events'
-import { Module, ModuleEventArgs } from '@xyo-network/module-model'
+import { ModuleEventArgs, ModuleInstance } from '@xyo-network/module-model'
 import { Payload } from '@xyo-network/payload-model'
 
-export type ObserveStartEventArgs<T extends Module = Module, TIn extends Payload = Payload> = ModuleEventArgs<
+export type ObserveStartEventArgs<T extends ModuleInstance = ModuleInstance, TIn extends Payload = Payload> = ModuleEventArgs<
   T,
   {
     inPayloads?: TIn[]
   }
 >
 
-export interface ObserveStartEventData<T extends Module = Module, TIn extends Payload = Payload> extends EventData {
+export interface ObserveStartEventData<T extends ModuleInstance = ModuleInstance, TIn extends Payload = Payload> extends EventData {
   observeStart: ObserveStartEventArgs<T, TIn>
 }

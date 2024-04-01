f0d566a5e5a43cc20d0bbe5daf731ad204418193
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-04-01 17:22:26 -0500

    Different default port for tests

--- packages/modules/packages/bridge/packages/websocket/src/spec/WebsocketBridge.spec.ts
+++ packages/modules/packages/bridge/packages/websocket/src/spec/WebsocketBridge.spec.ts
@@ -9,7 +9,7 @@ import { BridgeCommands, createServer } from '../socketServer'
 describe('WebsocketBridge', () => {
   let ioServer: { start: () => void; stop: () => void }
   let moduleClientA: Socket
-  const port = 3000
+  const port = 3001
   const serverUrl = `http://localhost:${port}`
   const moduleAddressA = 'f4f4fa193a3b785bcf3f9902d031d49f1cf01a11'
   const moduleAddressB = '0d8cf1ea18281a34c166624ff9d4e5a473d05ae5'

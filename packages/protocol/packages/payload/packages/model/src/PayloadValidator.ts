799c5b86330c846c9af99623d34520d863b64530
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-04-09 15:09:22 -0500

    Add PayloadValidator

---
+++ packages/protocol/packages/payload/packages/model/src/PayloadValidator.ts
@@ -0,0 +1,48 @@
+import { Payload } from './Payload'
+
+/**
+ * Type corresponding to the properties of a payload
+ */
+export type PayloadProperty<
+  /**
+   * The type of payload
+   */
+  T extends Payload = Payload,
+> = keyof T
+
+/**
+ * Type corresponding to the payload values
+ */
+export type PayloadValue<
+  /**
+   * The type of payload
+   */
+  T extends Payload = Payload,
+  /**
+   * The property of the payload to select
+   */
+  Key extends PayloadProperty<T> = PayloadProperty<T>,
+> = T[Key]
+
+/**
+ * An expressions that selects a property value from the payload
+ */
+export type PayloadValueExpression<
+  /**
+   * The type of payload
+   */
+  T extends Payload = Payload,
+  /**
+   * The property of the payload to select
+   */
+  Key extends PayloadProperty<T> = PayloadProperty<T>,
+  /**
+   * The type of the selected property's value
+   */
+  TValue = PayloadValue<T, Key>,
+> = (payload: Payload) => TValue
+
+/**
+ * A function that validates the supplied payload
+ */
+export type PayloadValidationFunction<T extends Payload = Payload> = (payload: T) => boolean | Promise<boolean>

0936c6949d26aaae2c96a29e96c4b7a86c4f2564
Arie Trouw <arie.trouw@xylabs.com>
2024-05-21 13:24:28 -0700

    ChildAddressPayload

--- packages/core-payload-plugins/packages/address/src/Payload.ts
+++ packages/core-payload-plugins/packages/address/src/Payload.ts
@@ -1,10 +1,23 @@
 import { Address } from '@xylabs/hex'
 import { Payload } from '@xyo-network/payload-model'
 
-import { AddressSchema } from './Schema'
+import { AddressChildSchema, AddressSchema } from './Schema'
 
 export interface AddressFields {
   address: Address
 }
 
 export type AddressPayload = Payload<AddressFields, AddressSchema>
+
+export interface AddressChildFields extends AddressFields {
+  /**
+   * The derivation path of the child address
+   */
+  path?: string
+  /**
+   * The public address of the root of the hierarchy
+   */
+  root?: Address
+}
+
+export type AddressChildPayload = Payload<AddressChildFields, AddressChildSchema>
--- packages/core-payload-plugins/packages/address/src/Schema.ts
+++ packages/core-payload-plugins/packages/address/src/Schema.ts
@@ -1,2 +1,5 @@
-export type AddressSchema = 'network.xyo.address'
-export const AddressSchema: AddressSchema = 'network.xyo.address'
+export const AddressSchema = 'network.xyo.address'
+export type AddressSchema = typeof AddressSchema
+
+export const AddressChildSchema = 'network.xyo.address.child'
+export type AddressChildSchema = typeof AddressChildSchema

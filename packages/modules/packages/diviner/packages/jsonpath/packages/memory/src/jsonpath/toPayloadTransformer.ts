b453745e63c07e1a6c7f8600cdb8594869a4da8a
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-06-26 12:26:18 -0500

    Add example helper method to determine JSON Path results

--- packages/modules/packages/diviner/packages/jsonpath/packages/memory/src/jsonpath/toPayloadTransformer.ts
+++ packages/modules/packages/diviner/packages/jsonpath/packages/memory/src/jsonpath/toPayloadTransformer.ts
@@ -3,6 +3,22 @@ import { Payload } from '@xyo-network/payload-model'
 import jsonpath from 'jsonpath'
 
 /**
+ * Determines if a JSON path expression is a scalar expression (targeting
+ * a single property in the JSON object) or a multiple value expression
+ * @param jsonPath The JSON path expression to check
+ * @returns 
+ */
+const isScalarJsonPathExpression = (jsonPath: string)=> {
+  const multipleValueIndicators = ['*', '..', ',', ':'];
+  for (let indicator of multipleValueIndicators) {
+      if (jsonPath.includes(indicator)) {
+          return false;
+      }
+  }
+  return true;
+}
+
+/**
  * Converts a JSON Path transform expression to a payload transformer
  * @param transformExpression The transform expression to convert
  * @returns The payload transformer for the JSON Path transform expression
@@ -10,7 +26,7 @@ import jsonpath from 'jsonpath'
 export const toPayloadTransformer = (transformExpression: JsonPathTransformExpression) => {
   const { defaultValue, destinationField, sourcePathExpression } = transformExpression
   const transformer: PayloadTransformer = (x: Payload) => {
-    const source = jsonpath.value(x, sourcePathExpression)
+    const source = jsonpath.query(x, sourcePathExpression)
     const transformed: Record<string, unknown> = {}
     // Assign the source value to the destination field or the default value if the source is undefined
     const destinationValue = source === undefined ? defaultValue : source

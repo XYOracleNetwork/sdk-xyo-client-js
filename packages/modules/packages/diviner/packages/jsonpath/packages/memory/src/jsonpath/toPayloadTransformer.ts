580597a3e521801a2b4809333ecee8594de9a42e
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-06-28 07:32:50 -0500

    Add implementation note

--- packages/modules/packages/diviner/packages/jsonpath/packages/memory/src/jsonpath/toPayloadTransformer.ts
+++ packages/modules/packages/diviner/packages/jsonpath/packages/memory/src/jsonpath/toPayloadTransformer.ts
@@ -3,22 +3,6 @@ import { Payload } from '@xyo-network/payload-model'
 import jsonpath from 'jsonpath'
 
 /**
- * Determines if a JSON path expression is a scalar expression (targeting
- * a single property in the JSON object) or a multiple value expression
- * @param jsonPath The JSON path expression to check
- * @returns 
- */
-const isScalarJsonPathExpression = (jsonPath: string)=> {
-  const multipleValueIndicators = ['*', '..', ',', ':'];
-  for (let indicator of multipleValueIndicators) {
-      if (jsonPath.includes(indicator)) {
-          return false;
-      }
-  }
-  return true;
-}
-
-/**
  * Converts a JSON Path transform expression to a payload transformer
  * @param transformExpression The transform expression to convert
  * @returns The payload transformer for the JSON Path transform expression
@@ -26,7 +10,13 @@ const isScalarJsonPathExpression = (jsonPath: string)=> {
 export const toPayloadTransformer = (transformExpression: JsonPathTransformExpression) => {
   const { defaultValue, destinationField, sourcePathExpression } = transformExpression
   const transformer: PayloadTransformer = (x: Payload) => {
-    const source = jsonpath.query(x, sourcePathExpression)
+    const source = jsonpath.value(x, sourcePathExpression)
+    // TODO: If the expression should return multiple values, use jsonpath.query
+    // instead of jsonpath.value
+    // const source =
+    //   isScalarJsonPathExpression(transformExpression.sourcePathExpression) ?
+    //     jsonpath.value(x, sourcePathExpression)
+    //   : jsonpath.query(x, sourcePathExpression)
     const transformed: Record<string, unknown> = {}
     // Assign the source value to the destination field or the default value if the source is undefined
     const destinationValue = source === undefined ? defaultValue : source

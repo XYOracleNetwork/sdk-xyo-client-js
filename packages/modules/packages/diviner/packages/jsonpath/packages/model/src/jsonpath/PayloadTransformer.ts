303227515d98f16f257d2101b21c270d83915149
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-06-28 08:13:20 -0500

    Remove arrayed definition of payload transformer for now

--- packages/modules/packages/diviner/packages/jsonpath/packages/model/src/jsonpath/PayloadTransformer.ts
+++ packages/modules/packages/diviner/packages/jsonpath/packages/model/src/jsonpath/PayloadTransformer.ts
@@ -1,3 +1,5 @@
 import { Payload } from '@xyo-network/payload-model'
 
-export type PayloadTransformer = (x: Payload) => Partial<Payload> | Partial<Payload>[]
+// TODO: Support for multiple return values from a single transformer
+// export type PayloadTransformer = (x: Payload) => Partial<Payload> | Partial<Payload>[]
+export type PayloadTransformer = (x: Payload) => Partial<Payload>

1567168506efcd95fb3ec2e72f3c523954ba8923
Arie Trouw <arie.trouw@xylabs.com>
2024-07-24 14:43:37 -0700

    Archivist Labels

---
+++ packages/modules/packages/archivist/packages/model/src/Labels.ts
@@ -0,0 +1,5 @@
+import { Labels } from '@xyo-network/module-model'
+
+export interface ArchivistLabels extends Labels {
+  'network.xyo.archivist.persistence.scope': 'memory' | 'thread' | 'process' | 'device' | 'network'
+}

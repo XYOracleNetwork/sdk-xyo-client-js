d4011cc9ba3818c8cd1cfbd400cc2e55b93bd94d
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-07-30 09:34:49 -0500

    Add payload pointer resolution rules

---
+++ packages/modules/packages/diviner/packages/payloadpointer/packages/memory/src/findPayload.ts
@@ -0,0 +1,67 @@
+import { ArchivistInstance } from '@xyo-network/archivist-model'
+import { BoundWitnessWrapper } from '@xyo-network/boundwitness-wrapper'
+import { BoundWitnessDiviner } from '@xyo-network/diviner-boundwitness-abstract'
+import { BoundWitnessDivinerQueryPayload, BoundWitnessDivinerQuerySchema } from '@xyo-network/diviner-boundwitness-model'
+import { PayloadDiviner } from '@xyo-network/diviner-payload-abstract'
+import { PayloadDivinerQueryPayload, PayloadDivinerQuerySchema } from '@xyo-network/diviner-payload-model'
+import { isBoundWitnessPointer, PayloadSearchCriteria, PointerPayload } from '@xyo-network/diviner-payload-pointer-model'
+import { Payload, Schema } from '@xyo-network/payload-model'
+import { PayloadWrapper } from '@xyo-network/payload-wrapper'
+
+import { combineRules } from './combineRules.js'
+
+const limit = 1
+
+const createBoundWitnessFilterFromSearchCriteria = (searchCriteria: PayloadSearchCriteria): BoundWitnessDivinerQueryPayload[] => {
+  const { addresses, order = 'desc', schemas, timestamp } = searchCriteria
+  const query: BoundWitnessDivinerQueryPayload = {
+    addresses,
+    limit,
+    order,
+    payload_schemas: schemas,
+    schema: BoundWitnessDivinerQuerySchema,
+    timestamp,
+  }
+  return [query]
+}
+
+const createPayloadFilterFromSearchCriteria = (searchCriteria: PayloadSearchCriteria): Payload[] => {
+  const { order = 'desc', schemas, timestamp } = searchCriteria
+  const query: PayloadDivinerQueryPayload = { limit, order, schema: PayloadDivinerQuerySchema, schemas, timestamp }
+  return [query]
+}
+
+export const findPayload = async (
+  archivist: ArchivistInstance,
+  boundWitnessDiviner: BoundWitnessDiviner,
+  payloadDiviner: PayloadDiviner,
+  pointer: PointerPayload,
+): Promise<Payload | undefined> => {
+  const searchCriteria = combineRules(pointer.reference)
+  const { addresses } = searchCriteria
+  const findWitnessedPayload = addresses?.length
+  const returnBoundWitness = isBoundWitnessPointer(pointer)
+  if (returnBoundWitness || findWitnessedPayload) {
+    const filter = createBoundWitnessFilterFromSearchCriteria(searchCriteria)
+    const result = await boundWitnessDiviner.divine(filter)
+    const bw = result?.[0] ? BoundWitnessWrapper.parse(result[0]) : undefined
+    if (bw) {
+      if (returnBoundWitness) return bw.payload
+      const { schemas, order = 'desc' } = searchCriteria
+      let payloadIndex = order === 'asc' ? 0 : bw.payloadHashes.length - 1
+      if (schemas) {
+        const schemaInSearchCriteria = (schema: Schema) => schemas.includes(schema)
+        payloadIndex = order === 'asc' ? bw.payloadSchemas.findIndex(schemaInSearchCriteria) : bw.payloadSchemas.findLastIndex(schemaInSearchCriteria)
+      }
+      const hash = bw.payloadHashes[payloadIndex]
+      const result = await archivist.get([hash])
+      return result?.[0] ? PayloadWrapper.wrap(result?.[0]).payload : undefined
+    }
+  }
+  // Find payload
+  else {
+    const filter = createPayloadFilterFromSearchCriteria(searchCriteria)
+    const result = await payloadDiviner.divine(filter)
+    return result?.[0] ? PayloadWrapper.wrap(result?.[0]).payload : undefined
+  }
+}
---
+++ packages/modules/packages/diviner/packages/payloadpointer/packages/memory/src/spec/combineRules.spec.ts
@@ -0,0 +1,74 @@
+import { PayloadAddressRule, PayloadRule, PayloadSchemaRule, PayloadTimestampOrderRule } from '@xyo-network/diviner-payload-pointer-model'
+
+import { combineRules } from '../combineRules.js'
+
+// Mock Date.now
+const now = new Date()
+jest.useFakeTimers().setSystemTime(now)
+
+const validRules = (): PayloadRule[][] => {
+  return [[{ schema: 'network.xyo.debug' }], [{ order: 'desc', timestamp: Date.now() }]]
+}
+
+describe('combineRules', () => {
+  describe('with no rules', () => {
+    it('should throw', () => {
+      expect(() => {
+        combineRules([])
+      }).toThrow()
+    })
+    it('for addresses defaults to an empty array', () => {
+      const rules = validRules()
+      const actual = combineRules(rules)
+      expect(actual.addresses.length).toBe(0)
+    })
+    it('for schema should throw', () => {
+      expect(() => {
+        const rules = validRules().filter((rule) => !(rule?.[0] as PayloadSchemaRule)?.schema)
+        combineRules(rules)
+      }).toThrow()
+    })
+    describe('for timestamp defaults to', () => {
+      it('timestamp set to current time', () => {
+        const rules = validRules().filter((rule) => !(rule?.[0] as PayloadTimestampOrderRule)?.timestamp)
+        const actual = combineRules(rules)
+        expect(actual.timestamp).toBe(+now)
+      })
+      it('direction defaults to desc', () => {
+        const rules = validRules().filter((rule) => !(rule?.[0] as PayloadTimestampOrderRule)?.timestamp)
+        const actual = combineRules(rules)
+        expect(actual.order).toBe('desc')
+      })
+    })
+  })
+  describe('with PayloadAddressRule rules', () => {
+    it('combines multiple rules', () => {
+      const rules = validRules()
+      const addressRules: PayloadAddressRule[] = [{ address: 'foo' }, { address: 'bar' }]
+      rules.push(addressRules)
+      const actual = combineRules(rules)
+      expect(actual.addresses.sort()).toEqual(['bar', 'foo'])
+    })
+  })
+  describe('with PayloadSchemaRule rules', () => {
+    it('combines multiple rules', () => {
+      const rules: PayloadRule[][] = [[{ order: 'desc', timestamp: Date.now() }], [{ schema: 'network.xyo.test' }, { schema: 'network.xyo.debug' }]]
+      const actual = combineRules(rules)
+      expect(actual.schemas.sort()).toEqual(['network.xyo.debug', 'network.xyo.test'])
+    })
+  })
+  describe('with PayloadTimestampDirectionRule rules', () => {
+    it('should only allow one rule', () => {
+      const rules: PayloadRule[][] = [
+        [{ schema: 'network.xyo.debug' }],
+        [
+          { order: 'desc', timestamp: Date.now() },
+          { order: 'asc', timestamp: Date.now() },
+        ],
+      ]
+      expect(() => {
+        combineRules(rules)
+      }).toThrow()
+    })
+  })
+})

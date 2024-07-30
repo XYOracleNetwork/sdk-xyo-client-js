46b1790e861ec81f013c021433d051c35f87b25e
Arie Trouw <arie.trouw@xylabs.com>
2024-07-30 14:23:28 -0700

    Fixed nested objects in arrays in payloads not removing undefined fields

--- packages/protocol/packages/core/packages/hash/src/removeEmptyFields.ts
+++ packages/protocol/packages/core/packages/hash/src/removeEmptyFields.ts
@@ -2,7 +2,11 @@ import { AnyObject, EmptyObject } from '@xylabs/object'
 import { typeOf } from '@xylabs/typeof'
 
 export const removeEmptyFields = <T extends EmptyObject>(obj: T): T => {
-  if (obj === null || Array.isArray(obj)) return obj
+  if (obj === null) return obj
+
+  if (Array.isArray(obj)) {
+    return obj.map((value) => (typeof value === 'object' ? removeEmptyFields(value) : value)) as T
+  }
 
   const newObject: AnyObject = {}
   for (const [key, value] of Object.entries(obj)) {

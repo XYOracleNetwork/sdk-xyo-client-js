4ad059bad534533ef1f716d79e5214bdf872000c
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-07-30 09:46:33 -0500

    Implement diviner

--- packages/modules/packages/diviner/packages/payloadpointer/packages/memory/src/Diviner.ts
+++ packages/modules/packages/diviner/packages/payloadpointer/packages/memory/src/Diviner.ts
@@ -1,11 +1,22 @@
+import { ArchivistInstance, asArchivistInstance } from '@xyo-network/archivist-model'
 import { AbstractDiviner } from '@xyo-network/diviner-abstract'
-import { DivinerInstance, DivinerModuleEventData } from '@xyo-network/diviner-model'
-import { PayloadPointerDivinerConfigSchema, PayloadPointerDivinerParams } from '@xyo-network/diviner-payload-pointer-model'
+import { BoundWitnessDiviner } from '@xyo-network/diviner-boundwitness-abstract'
+import { asDivinerInstance, DivinerInstance, DivinerModuleEventData } from '@xyo-network/diviner-model'
+import { PayloadDiviner } from '@xyo-network/diviner-payload-abstract'
+import {
+  isPayloadPointer,
+  isPointerPayload,
+  PayloadPointerDivinerConfigSchema,
+  PayloadPointerDivinerParams,
+  PointerPayload,
+} from '@xyo-network/diviner-payload-pointer-model'
 import { Payload, Schema } from '@xyo-network/payload-model'
+
+import { findPayload } from './findPayload.js'
 
 export class JsonPathDiviner<
   TParams extends PayloadPointerDivinerParams = PayloadPointerDivinerParams,
-  TIn extends Payload = Payload,
+  TIn extends PointerPayload = PointerPayload,
   TOut extends Payload = Payload,
   TEventData extends DivinerModuleEventData<DivinerInstance<TParams, TIn, TOut>, TIn, TOut> = DivinerModuleEventData<
     DivinerInstance<TParams, TIn, TOut>,
@@ -17,13 +28,15 @@ export class JsonPathDiviner<
   static override readonly defaultConfigSchema: Schema = PayloadPointerDivinerConfigSchema
 
   protected override async divineHandler(payloads?: TIn[]): Promise<TOut[]> {
-    if (!payloads) return []
-    // Create the indexes from the tuples
-    return await Promise.all(
-      payloads.map<Promise<TOut>>(async () => {
-        await Promise.resolve()
-        throw new Error('Not implemented')
-      }),
-    )
+    const pointer = payloads?.find(isPointerPayload)
+    await Promise.resolve()
+    const archivist = asArchivistInstance(await this.resolve(this.config.archivist))
+    const boundWitnessDiviner = asDivinerInstance(await this.resolve(this.config.boundWitnessDiviner)) as BoundWitnessDiviner
+    const payloadDiviner = asDivinerInstance(await this.resolve(this.config.payloadDiviner)) as PayloadDiviner
+    if (archivist && boundWitnessDiviner && payloadDiviner && pointer) {
+      const result = (await findPayload(archivist, boundWitnessDiviner, payloadDiviner, pointer)) as TOut | undefined
+      if (result) return [result]
+    }
+    return []
   }
 }

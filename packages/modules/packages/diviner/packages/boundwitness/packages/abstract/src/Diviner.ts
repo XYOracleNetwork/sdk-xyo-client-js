59964478ad6ad5fb4abeab01b39c9ec55da66e4b
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-08-01 16:11:56 -0500

    Implement schemas for BoundWitness Diviner

--- packages/modules/packages/diviner/packages/boundwitness/packages/abstract/src/Diviner.ts
+++ packages/modules/packages/diviner/packages/boundwitness/packages/abstract/src/Diviner.ts
@@ -1,7 +1,8 @@
 import { BoundWitness } from '@xyo-network/boundwitness-model'
 import { AbstractDiviner } from '@xyo-network/diviner-abstract'
-import { BoundWitnessDivinerParams, BoundWitnessDivinerQueryPayload } from '@xyo-network/diviner-boundwitness-model'
+import { BoundWitnessDivinerConfigSchema, BoundWitnessDivinerParams, BoundWitnessDivinerQueryPayload } from '@xyo-network/diviner-boundwitness-model'
 import { DivinerInstance, DivinerModuleEventData } from '@xyo-network/diviner-model'
+import { Schema } from '@xyo-network/payload-model'
 
 export abstract class BoundWitnessDiviner<
   TParams extends BoundWitnessDivinerParams = BoundWitnessDivinerParams,
@@ -12,4 +13,7 @@ export abstract class BoundWitnessDiviner<
     TIn,
     TOut
   >,
-> extends AbstractDiviner<TParams, TIn, TOut, TEventData> {}
+> extends AbstractDiviner<TParams, TIn, TOut, TEventData> {
+  static override readonly configSchemas: Schema[] = [...super.configSchemas, BoundWitnessDivinerConfigSchema]
+  static override readonly defaultConfigSchema: Schema = BoundWitnessDivinerConfigSchema
+}

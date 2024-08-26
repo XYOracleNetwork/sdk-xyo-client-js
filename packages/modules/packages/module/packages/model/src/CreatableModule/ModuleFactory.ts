54886e033369f9a4bcb530a657e43e3f1b2402bb
Arie Trouw <arie.trouw@xylabs.com>
2024-08-26 14:29:08 -0700

    params not optional

--- packages/modules/packages/module/packages/model/src/CreatableModule/ModuleFactory.ts
+++ packages/modules/packages/module/packages/model/src/CreatableModule/ModuleFactory.ts
@@ -58,14 +58,14 @@ export class ModuleFactory<TModule extends AttachableModuleInstance> implements 
     assertEx(thisFunc === rootFunc, () => `Override not allowed for [${functionName}] - override ${functionName}Handler instead`)
   }
 
-  create<T extends AttachableModuleInstance>(this: CreatableModuleFactory<T>, params: TModule['params'] | undefined): Promise<T> {
+  create<T extends AttachableModuleInstance>(this: CreatableModuleFactory<T>, params: TModule['params']): Promise<T> {
     const factory = this as ModuleFactory<T>
     const schema = factory.creatableModule.defaultConfigSchema
     const mergedParams: TModule['params'] = merge(
       {},
       factory.defaultParams,
       params,
-      { config: merge({}, factory.defaultParams?.config, params?.config, { schema }) },
+      { config: merge({}, factory.defaultParams?.config, params.config, { schema }) },
     )
     return factory.creatableModule.create<T>(mergedParams)
   }

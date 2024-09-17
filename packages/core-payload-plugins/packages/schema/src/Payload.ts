db632fe60093f1ed5e16dd311e018dda13367ac2
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-09-17 06:35:45 -0500

    Add schema identity helpers & documentation

--- packages/core-payload-plugins/packages/schema/src/Payload.ts
+++ packages/core-payload-plugins/packages/schema/src/Payload.ts
@@ -1,15 +1,42 @@
-import type { Payload } from '@xyo-network/payload-model'
+import {
+  isPayloadOfSchemaType, isPayloadOfSchemaTypeWithMeta, isPayloadOfSchemaTypeWithSources, type Payload,
+} from '@xyo-network/payload-model'
 
-import type { SchemaSchema } from './Schema.ts'
+import { SchemaSchema } from './Schema.ts'
 
 export type SchemaPayload = Payload<{
+  /**
+   * The schema definition
+   */
   definition: {
     [key: string]: unknown
     $id?: string
   }
+  /**
+   * The schema this schema extends (if any)
+   */
   extends?: string
+
   /** @deprecated use definition.$id instead */
   name?: string
 
+  /**
+   * Identifies this payload as a Schema
+   */
   schema: SchemaSchema
 }>
+
+/**
+ * Identity function for determining if an object is an Schema
+ */
+export const isSchema = isPayloadOfSchemaType<SchemaPayload>(SchemaSchema)
+
+/**
+ * Identity function for determining if an object is an Schema with sources
+ */
+export const isSchemaWithSources = isPayloadOfSchemaTypeWithSources<SchemaPayload>(SchemaSchema)
+
+/**
+ * Identity function for determining if an object is an Schema with meta
+ */
+export const isSchemaWithMeta = isPayloadOfSchemaTypeWithMeta<SchemaPayload>(SchemaSchema)

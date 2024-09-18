9dd8e956a5ba19e16b5643157a5d88eaacd382e2
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-09-18 09:18:08 -0500

    Add more narrow definitions to all TS to better infer promisable

--- packages/protocol/packages/payload/packages/model/src/PayloadValidationFunction.ts
+++ packages/protocol/packages/payload/packages/model/src/PayloadValidationFunction.ts
@@ -1,6 +1,16 @@
 import type { Payload } from './Payload.ts'
 
 /**
+ * A function that validates the supplied payload synchronously
+ */
+export type SyncPayloadValidationFunction<T extends Payload = Payload> = (payload: T) => boolean
+
+/**
+ * A function that validates the supplied payload asynchronously
+ */
+export type AsyncPayloadValidationFunction<T extends Payload = Payload> = (payload: T) => Promise<boolean>
+
+/**
  * A function that validates the supplied payload
  */
-export type PayloadValidationFunction<T extends Payload = Payload> = (payload: T) => boolean | Promise<boolean>
+export type PayloadValidationFunction<T extends Payload = Payload> = SyncPayloadValidationFunction<T> | AsyncPayloadValidationFunction<T>

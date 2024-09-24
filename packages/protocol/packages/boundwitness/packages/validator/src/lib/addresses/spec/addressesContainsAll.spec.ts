9626a38079f8572d009f9ba6b3c8c174ef9d711b
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-09-23 21:03:36 -0500

    Update descriptions

--- packages/protocol/packages/boundwitness/packages/validator/src/lib/addresses/spec/addressesContainsAll.spec.ts
+++ packages/protocol/packages/boundwitness/packages/validator/src/lib/addresses/spec/addressesContainsAll.spec.ts
@@ -9,47 +9,59 @@ describe('BoundWitnessValidator', () => {
   const payload = { schema: 'network.xyo.test', value: Date.now() }
   const oneWallet: WalletInstance[] = []
   const twoWallets: WalletInstance[] = []
-  const cases: [string, () => WalletInstance[]][] = [
-    ['with single signer', () => oneWallet],
-    ['with multiple signers', () => twoWallets],
-  ]
   beforeAll(async () => {
     oneWallet.push(await HDWallet.random())
     twoWallets.push(await HDWallet.random(), await HDWallet.random())
   })
-  describe.each(cases)('%s', (_, signers) => {
-    let addresses: Address[]
-    beforeAll(() => {
-      addresses = signers().map(x => x.address)
-    })
-    describe('returns true', () => {
-      it('when addresses empty', async () => {
+  describe('returns true', () => {
+    const cases: [string, () => WalletInstance[]][] = [
+      ['for no wallet', () => []],
+      ['for single wallet', () => oneWallet],
+      ['for multiple wallets', () => twoWallets],
+    ]
+    describe.each(cases)('%s when all sign', (_, signers) => {
+      let addresses: Address[]
+      beforeAll(() => {
+        addresses = signers().map(x => x.address)
+      })
+      it('addresses empty', async () => {
         const all = signers()
         const [bw] = await new BoundWitnessBuilder().signers(all).payload(payload).build()
         expect(addressesContainsAll(bw, [])).toBeTrue()
       })
-      it('when all addresses present in boundwitness addresses', async () => {
+      it('all addresses present in boundwitness addresses', async () => {
         const all = signers()
         const [bw] = await new BoundWitnessBuilder().signers(all).payload(payload).build()
         expect(addressesContainsAll(bw, addresses)).toBeTrue()
       })
-      it('when all addresses present in boundwitness addresses with extra addresses', async () => {
+      it('with extra signers and all addresses present in boundwitness addresses', async () => {
         const extra = [...signers(), await HDWallet.random()]
         const [bw] = await new BoundWitnessBuilder().signers(extra).payload(payload).build()
         expect(addressesContainsAll(bw, addresses)).toBeTrue()
       })
     })
-    describe('returns false', () => {
-      it('when no addresses in boundwitness addresses', async () => {
+  })
+  describe('returns false', () => {
+    const cases: [string, () => WalletInstance[]][] = [
+      // ['for no wallet', () => []], // This case intentionally skipped because empty addresses will always return true
+      ['for single wallet', () => oneWallet],
+      ['for multiple wallets', () => twoWallets],
+    ]
+    describe.each(cases)('%s', (_, signers) => {
+      let addresses: Address[]
+      beforeAll(() => {
+        addresses = signers().map(x => x.address)
+      })
+      it('with no signers', async () => {
         const [bw] = await new BoundWitnessBuilder().payload(payload).build()
         expect(addressesContainsAll(bw, addresses)).toBeFalse()
       })
-      it('when missing one address in boundwitness addresses', async () => {
+      it('with all signers but one', async () => {
         const less = [...signers().slice(0, -1), await HDWallet.random()]
         const [bw] = await new BoundWitnessBuilder().signers(less).payload(payload).build()
         expect(addressesContainsAll(bw, addresses)).toBeFalse()
       })
-      it('when missing all addresses in boundwitness addresses', async () => {
+      it('with all different signers', async () => {
         const none = [await HDWallet.random(), await HDWallet.random()]
         const [bw] = await new BoundWitnessBuilder().signers(none).payload(payload).build()
         expect(addressesContainsAll(bw, addresses)).toBeFalse()

58201d11a2db8dc26478ef60db4bfcdd41018961
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-09-23 20:11:27 -0500

    Add addresses helpers

---
+++ packages/protocol/packages/boundwitness/packages/validator/src/lib/addresses/addressesContainsAll.ts
@@ -0,0 +1,8 @@
+import type { Address } from '@xylabs/hex'
+import type { BoundWitnessFields } from '@xyo-network/boundwitness-model'
+import { BoundWitness } from '@xyo-network/boundwitness-model'
+
+export const addressesContainsAll = (boundwitness: Partial<BoundWitnessFields>, addresses: Address[]): boolean => {
+  // TODO
+  return false
+}
---
+++ packages/protocol/packages/boundwitness/packages/validator/src/lib/addresses/index.ts
@@ -0,0 +1,1 @@
+export * from './addressesContainsAll.ts'

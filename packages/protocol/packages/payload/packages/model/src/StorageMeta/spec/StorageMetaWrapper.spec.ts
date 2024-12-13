d318fad2001dc76a4349668b1b8797a315088a22
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-12-13 15:19:25 -0600

    Test individual values

--- packages/protocol/packages/payload/packages/model/src/StorageMeta/spec/StorageMetaWrapper.spec.ts
+++ packages/protocol/packages/payload/packages/model/src/StorageMeta/spec/StorageMetaWrapper.spec.ts
@@ -11,13 +11,19 @@ import { StorageMetaWrapper } from '../StorageMetaWrapper.ts'
 describe('StorageMetaWrapper', () => {
   const hash: Hash = toHex('1269b95d3ebf1b1258a82ccca0b365fabf4b8c99bf8fc852e5045e30ad20fbb1')
   const address: Address = 'b36d327210f67ad98be881ddf6ad1f1b3e2c5137'
+  const timestamp = 1_234_567_890_123
   describe('from', () => {
     it('parse', () => {
-      const now = Date.now()
-      const wrapper = StorageMetaWrapper.from(now, hash, address)
-      expect(wrapper.epoch).toBe(StorageMetaWrapper.timestampToEpoch(now))
+      const wrapper = StorageMetaWrapper.from(timestamp, hash, address)
+      expect(wrapper.epoch).toBe(StorageMetaWrapper.timestampToEpoch(timestamp))
       expect(wrapper.nonce).toBe(StorageMetaWrapper.hashToNonce(hash))
+      expect(wrapper.nonce).toBe(hash.slice(-8 * 2))
       expect(wrapper.address).toBe(address)
+      expect (wrapper.localSequence).toBe(`${wrapper.epoch}${wrapper.nonce}`)
+      expect (wrapper.localSequence).toBe('0000011f71fb04cbe5045e30ad20fbb1')
+      expect (wrapper.qualifiedSequence).toBe(`${wrapper.epoch}${wrapper.nonce}${wrapper.address}`)
+      expect (wrapper.qualifiedSequence).toBe(`${wrapper.localSequence}${wrapper.address}`)
+      expect (wrapper.qualifiedSequence).toBe('0000011f71fb04cbe5045e30ad20fbb1b36d327210f67ad98be881ddf6ad1f1b3e2c5137')
     })
   })
   describe.skip('with Fully Qualified Sequence', () => {

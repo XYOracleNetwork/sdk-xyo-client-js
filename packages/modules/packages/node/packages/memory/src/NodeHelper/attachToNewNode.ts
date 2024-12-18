b8f0c7018538f20e57b1099c4ee3a76c67c2b886
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-12-17 18:30:12 -0600

    Simplify helper method signature

--- packages/modules/packages/node/packages/memory/src/NodeHelper/attachToNewNode.ts
+++ packages/modules/packages/node/packages/memory/src/NodeHelper/attachToNewNode.ts
@@ -1,11 +1,17 @@
 import type { ModuleIdentifier } from '@xyo-network/module-model'
-import type { NodeInstance } from '@xyo-network/node-model'
+import { NodeConfigSchema, type NodeInstance } from '@xyo-network/node-model'
 
 import type { MemoryNodeParams } from '../MemoryNode.ts'
 import { MemoryNode } from '../MemoryNode.ts'
 import { attachToExistingNode } from './attachToExistingNode.ts'
 
-export const attachToNewNode = async (source: NodeInstance, id: ModuleIdentifier, destinationParams: MemoryNodeParams): Promise<NodeInstance> => {
+const DEFAULT_NODE_PARAMS = { config: { schema: NodeConfigSchema } }
+
+export const attachToNewNode = async (
+  source: NodeInstance,
+  id: ModuleIdentifier,
+  destinationParams: MemoryNodeParams = DEFAULT_NODE_PARAMS,
+): Promise<NodeInstance> => {
   const destination = await MemoryNode.create(destinationParams)
   return await attachToExistingNode(source, id, destination)
 }

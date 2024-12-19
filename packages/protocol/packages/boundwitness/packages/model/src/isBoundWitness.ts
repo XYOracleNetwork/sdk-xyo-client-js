27a4242e40e83c32c41fcf88722eab875440146e
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-12-18 18:33:39 -0600

    Notes on future symmetry

--- packages/protocol/packages/boundwitness/packages/model/src/isBoundWitness.ts
+++ packages/protocol/packages/boundwitness/packages/model/src/isBoundWitness.ts
@@ -8,14 +8,18 @@ import type { BoundWitness, UnsignedBoundWitness } from './BoundWitness/index.ts
 import { BoundWitnessSchema } from './BoundWitness/index.ts'
 
 export const isBoundWitness = (value: unknown): value is BoundWitness => isPayloadOfSchemaType<BoundWitness>(BoundWitnessSchema)(value)
+
+export const notBoundWitness = notPayloadOfSchemaType<BoundWitness>(BoundWitnessSchema)
+
+// TODO: Use AsObjectFactory here to match standard pattern
+// TODO: Other as identity functions throw if not valid, this one returns undefined with is more of a asOptional behavior
+export const asBoundWitness = <T extends BoundWitness<{ schema: string }> = BoundWitness>(payload?: unknown) =>
+  isBoundWitness(payload) ? (payload as T) : undefined
+
 export const isBoundWitnessWithStorageMeta = (value: unknown): value is WithStorageMeta<BoundWitness> =>
   isPayloadOfSchemaType<BoundWitness>(BoundWitnessSchema)(value) && isStorageMeta(value)
+export const asBoundWitnessWithStorageMeta = AsObjectFactory.createOptional<WithStorageMeta<BoundWitness>>(isBoundWitnessWithStorageMeta)
 export const asOptionalBoundWitnessWithStorageMeta = AsObjectFactory.createOptional<WithStorageMeta<BoundWitness>>(isBoundWitnessWithStorageMeta)
 
 export const isUnsignedBoundWitness = (value: unknown): value is UnsignedBoundWitness =>
   isPayloadOfSchemaType<UnsignedBoundWitness>(BoundWitnessSchema)(value)
-export const notBoundWitness = notPayloadOfSchemaType<BoundWitness>(BoundWitnessSchema)
-
-// TODO: Use AsObjectFactory here to match standard patter
-export const asBoundWitness = <T extends BoundWitness<{ schema: string }> = BoundWitness>(payload?: unknown) =>
-  isBoundWitness(payload) ? (payload as T) : undefined

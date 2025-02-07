e0ca79b6c9cd6d5fac53167a28ec7306dd1dd32a
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-02-07 14:08:43 -0600

    Add test for default next setting

--- packages/modules/packages/archivist/packages/acceptance-tests/src/query/next/next.ts
+++ packages/modules/packages/archivist/packages/acceptance-tests/src/query/next/next.ts
@@ -67,6 +67,29 @@ export const generateArchivistNextTests = (
           expect(id?.salt).toBe(expected?.salt)
         })
       })
+      describe('with open undefined', () => {
+        const open = undefined
+        it.each(['asc', 'desc'] as const)('returns payloads without cursor [order: %s]', async (order) => {
+          const bound = order === 'asc' ? payloads.at(0) : payloads.at(-1)
+          expect(bound).toBeDefined()
+          const expected = order === 'asc' ? payloads.at(1) : payloads.at(-2)
+          expect(expected).toBeDefined()
+          const cursor = bound?._sequence
+          expect(cursor).toBeDefined()
+          const response = await sut.next({
+            open, order, limit: 1, cursor,
+          })
+          expect(response).toBeDefined()
+          expect(response.length).toBe(1)
+          const ids = filterAs(response, asOptionalId)
+          expect(ids).toBeDefined()
+          expect(ids.length).toBe(1)
+          const id = ids.at(0)
+          expect(id).toBeDefined()
+          expect(id?.salt).toBeDefined()
+          expect(id?.salt).toBe(expected?.salt)
+        })
+      })
     })
   })
 }

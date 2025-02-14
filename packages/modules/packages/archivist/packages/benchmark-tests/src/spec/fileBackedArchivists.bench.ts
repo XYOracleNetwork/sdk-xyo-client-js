7949dc3039ee276f12256a16a23e038ab1b5c9f3
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-02-13 20:20:30 -0600

    Get multiple tests

--- packages/modules/packages/archivist/packages/benchmark-tests/src/spec/fileBackedArchivists.bench.ts
+++ packages/modules/packages/archivist/packages/benchmark-tests/src/spec/fileBackedArchivists.bench.ts
@@ -10,7 +10,16 @@ import type { WithSources } from '@xyo-network/payload-model'
 import { bench, describe } from 'vitest'
 
 describe('LMDB vs LevelDB', () => {
-  const missingHash = 'a2069e044136fdf5decb4580b81d2b8ae5c26101d621ec8e2d1c0bbf2f4fbb90'
+  const missingHash: Hash = 'a2069e044136fdf5decb4580b81d2b8ae5c26101d621ec8e2d1c0bbf2f4fbb90'
+  const missingHashes: Hash[] = [
+    'a2069e044136fdf5decb4580b81d2b8ae5c26101d621ec8e2d1c0bbf2f4fbb90',
+    '66f07db88d4dbaef3c49559ad10604593cabd54431255f8c3a7163f562570b9d',
+    '5482f54dca13a1ecfb0c36bd8cf0471045dbaef38f05021a2ea6f13545a7c062',
+    '6f4b1a669a95a4cb2f1f62e1f018c1a39add570caf76476d8171e349444544ba',
+    '1876f2127ac778881fac7fb5354a2c114478fd467e3917835b3281432df1cbf6',
+    '655ed3ef5b03601f5ede740607c10281aa982671700d094e50c31b38798931a9',
+    '7b1a7ce000163cf0bebe74596b338a1086438f64bbf8f03cb1e9902fd804fa45',
+  ]
   const totalTestPayloads = 100
   let levelDbArchivist: LevelDbArchivist
   const levelDBArchivistSetup = async () => {
@@ -22,7 +31,9 @@ describe('LMDB vs LevelDB', () => {
     })
     const payload = payloads[Math.trunc(totalTestPayloads / 2)]
     hash = await PayloadBuilder.hash(payload)
+    hashes = await Promise.all(payloads.slice(10).map(p => PayloadBuilder.hash(p)))
     dataHash = await PayloadBuilder.dataHash(payload)
+    dataHashes = await Promise.all(payloads.slice(10).map(p => PayloadBuilder.dataHash(p)))
   }
   const levelDBArchivistSetupWithData = async () => {
     await levelDBArchivistSetup()
@@ -38,14 +49,18 @@ describe('LMDB vs LevelDB', () => {
     })
     const payload = payloads[Math.trunc(totalTestPayloads / 2)]
     hash = await PayloadBuilder.hash(payload)
+    hashes = await Promise.all(payloads.slice(10).map(p => PayloadBuilder.hash(p)))
     dataHash = await PayloadBuilder.dataHash(payload)
+    dataHashes = await Promise.all(payloads.slice(10).map(p => PayloadBuilder.dataHash(p)))
   }
   const lmdbArchivistSetupWithData = async () => {
     await lmdbArchivistSetup()
     await lmdbArchivist.insert(payloads)
   }
   let hash: Hash
+  let hashes: Hash[]
   let dataHash: Hash
+  let dataHashes: Hash[]
 
   const payloads = Array.from(
     { length: totalTestPayloads },
@@ -103,6 +118,35 @@ describe('LMDB vs LevelDB', () => {
 
       bench('LmdbArchivist', async () => {
         await lmdbArchivist.get([missingHash])
+      }, { setup: lmdbArchivistSetupWithData })
+    })
+  })
+  describe('get (multiple)', () => {
+    describe('get (by hash)', () => {
+      bench('LevelDbArchivist', async () => {
+        await levelDbArchivist.get(hashes)
+      }, { setup: levelDBArchivistSetupWithData })
+
+      bench('LmdbArchivist', async () => {
+        await lmdbArchivist.get(hashes)
+      }, { setup: lmdbArchivistSetupWithData })
+    })
+    describe('get (by dataHash)', () => {
+      bench('LevelDbArchivist', async () => {
+        await levelDbArchivist.get(dataHashes)
+      }, { setup: levelDBArchivistSetupWithData })
+
+      bench('LmdbArchivist', async () => {
+        await lmdbArchivist.get(dataHashes)
+      }, { setup: lmdbArchivistSetupWithData })
+    })
+    describe('get (missing hash)', () => {
+      bench('LevelDbArchivist', async () => {
+        await levelDbArchivist.get(missingHashes)
+      }, { setup: levelDBArchivistSetupWithData })
+
+      bench('LmdbArchivist', async () => {
+        await lmdbArchivist.get(missingHashes)
       }, { setup: lmdbArchivistSetupWithData })
     })
   })

642b6c232ade930d048ae01979ec1a25f5a56855
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-02-13 19:39:57 -0600

    Notes on bench

--- packages/modules/packages/archivist/packages/benchmark-tests/src/spec/fileBackedArchivists.bench.ts
+++ packages/modules/packages/archivist/packages/benchmark-tests/src/spec/fileBackedArchivists.bench.ts
@@ -1,16 +1,11 @@
 import { tmpdir } from 'node:os'
-import { before } from 'node:test'
 
-import type { Hash } from '@xylabs/hex'
 import { LevelDbArchivist, LevelDbArchivistConfigSchema } from '@xyo-network/archivist-leveldb'
 import { LmdbArchivist, LmdbArchivistConfigSchema } from '@xyo-network/archivist-lmdb'
 import type { Id } from '@xyo-network/id-payload-plugin'
 import { IdSchema } from '@xyo-network/id-payload-plugin'
 import { PayloadBuilder } from '@xyo-network/payload-builder'
-import type { WithHashStorageMeta } from '@xyo-network/payload-model'
-import {
-  beforeAll, bench, describe,
-} from 'vitest'
+import { bench, describe } from 'vitest'
 
 describe('LMDB vs LevelDB', () => {
   let levelDbArchivist: LevelDbArchivist
@@ -18,7 +13,7 @@ describe('LMDB vs LevelDB', () => {
     levelDbArchivist = await LevelDbArchivist.create({
       account: 'random',
       config: {
-        location: tmpdir(), dbName: 'test1.db', storeName: 'payloads', clearStoreOnStart: true, schema: LevelDbArchivistConfigSchema,
+        location: tmpdir(), dbName: 'levelDbArchivist.bench.db', storeName: 'payloads', clearStoreOnStart: true, schema: LevelDbArchivistConfigSchema,
       },
     })
   }
@@ -27,7 +22,7 @@ describe('LMDB vs LevelDB', () => {
     lmdbArchivist = await LmdbArchivist.create({
       account: 'random',
       config: {
-        location: tmpdir(), dbName: 'test1.db', storeName: 'payloads', clearStoreOnStart: true, schema: LmdbArchivistConfigSchema,
+        location: tmpdir(), dbName: 'lmdbArchivist.bench.db', storeName: 'payloads', clearStoreOnStart: true, schema: LmdbArchivistConfigSchema,
       },
     })
   }
@@ -38,6 +33,7 @@ describe('LMDB vs LevelDB', () => {
 
   )
 
+  // NOTE: Pre-hashed  since bench does not support async beforeAll setup for local vars
   // const hash = await PayloadBuilder.hash({ schema: 'network.xyo.id', salt: '1' })
   const hash = '3a3b8deca568ff820b0b7c8714fbdf82b40fb54f4b15aca8745e06b81291558e'
   // const dataHash = await PayloadBuilder.dataHash({ schema: 'network.xyo.id', salt: '1' })

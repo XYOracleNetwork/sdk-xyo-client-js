304209014af6fd243e9dc036ce14df5f0ec2a20d
Arie Trouw <arie.trouw@xylabs.com>
2025-02-28 15:39:18 -0800

    Schema Properties pull out

--- packages/protocol/packages/boundwitness/packages/wrapper/src/SchemaType.ts
+++ packages/protocol/packages/boundwitness/packages/wrapper/src/SchemaType.ts
@@ -7,23 +7,30 @@ import type { JSONSchemaType } from 'ajv'
 
 export const SignatureRegEx = HexRegEx(32)
 
+export const boundWitnessProperties: JSONSchemaType<BoundWitness>['properties'] = {
+  ...payloadJsonSchema.properties,
+  addresses: { items: { type: 'string', pattern: AddressRegEx }, type: 'array' },
+  payload_hashes: { items: { type: 'string', pattern: HashRegEx }, type: 'array' },
+  payload_schemas: { items: { type: 'string', pattern: SchemaRegEx }, type: 'array' },
+  previous_hashes: {
+    items: {
+      type: 'string', pattern: HashRegEx, nullable: true,
+    },
+    type: 'array',
+  },
+  root: { type: 'string', pattern: HashRegEx },
+  $destination: { items: { type: 'string', pattern: HashRegEx }, type: 'array' },
+  $sourceQuery: { type: 'string', pattern: HashRegEx },
+  $sources: { items: { type: 'string', pattern: HashRegEx }, type: 'array' },
+  $opCodes: { items: { type: 'string' }, type: 'array' },
+  $signatures: { items: { type: 'string', pattern: HexRegEx(64) }, type: 'array' },
+}
+
 export const boundWitnessJsonSchema: JSONSchemaType<BoundWitness> = {
   ...payloadJsonSchema,
   $id: 'https://schemas.xyo.network/2.0/boundwitness',
   additionalProperties: false,
-  properties: {
-    ...payloadJsonSchema.properties,
-    addresses: { items: { type: 'string', pattern: AddressRegEx }, type: 'array' },
-    payload_hashes: { items: { type: 'string', pattern: HashRegEx }, type: 'array' },
-    payload_schemas: { items: { type: 'string', pattern: SchemaRegEx }, type: 'array' },
-    previous_hashes: { items: { type: 'string', pattern: HashRegEx }, type: 'array' },
-    root: { type: 'string', pattern: HashRegEx },
-    $destination: { items: { type: 'string', pattern: HashRegEx }, type: 'array' },
-    $sourceQuery: { type: 'string', pattern: HashRegEx },
-    $sources: { items: { type: 'string', pattern: HashRegEx }, type: 'array' },
-    $opCodes: { items: { type: 'string' }, type: 'array' },
-    $signatures: { items: { type: 'string', pattern: SignatureRegEx }, type: 'array' },
-  },
+  properties: boundWitnessProperties,
   required: [...payloadJsonSchema.required, 'addresses', 'payload_hashes', 'payload_schemas', 'previous_hashes'],
   type: 'object',
 }
--- packages/protocol/packages/payload/packages/wrapper/src/SchemaType.ts
+++ packages/protocol/packages/payload/packages/wrapper/src/SchemaType.ts
@@ -6,20 +6,22 @@ export const HexRegEx = (bytes?: number) => bytes ? `^[a-f0-9]{${bytes * 2}}$` :
 export const HashRegEx = HexRegEx(32)
 export const AddressRegEx = HexRegEx(20)
 
+export const payloadPropertiesJsonSchema: JSONSchemaType<Payload>['properties'] = {
+  $sources: {
+    type: 'array',
+    items: { type: 'string', pattern: HashRegEx },
+    nullable: true,
+  },
+  $opCodes: {
+    items: { type: 'string' }, type: 'array', nullable: true,
+  },
+  schema: { pattern: SchemaRegEx, type: 'string' },
+}
+
 export const payloadJsonSchema: JSONSchemaType<Payload> = {
   $id: 'https://schemas.xyo.network/2.0/payload',
   additionalProperties: true,
-  properties: {
-    $sources: {
-      type: 'array',
-      items: { type: 'string', pattern: HashRegEx },
-      nullable: true,
-    },
-    $opCodes: {
-      items: { type: 'string' }, type: 'array', nullable: true,
-    },
-    schema: { pattern: SchemaRegEx, type: 'string' },
-  },
+  properties: payloadPropertiesJsonSchema,
   required: ['schema'],
   type: 'object',
 }

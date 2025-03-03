7198a10add6695f70ab3c47fc9745ac9d431e912
Arie Trouw <arie.trouw@xylabs.com>
2025-03-03 12:06:25 -0800

    Deploy
    3.9.27

--- packages/protocol/packages/payload/packages/wrapper/src/SchemaType.ts
+++ packages/protocol/packages/payload/packages/wrapper/src/SchemaType.ts
@@ -2,12 +2,13 @@ import type { Payload } from '@xyo-network/payload-model'
 import { SchemaRegEx } from '@xyo-network/payload-model'
 import type { JSONSchemaType } from 'ajv'
 
-export const HexRegEx = (minBytes = 0, maxBytes?: number) => {
-  return `^[a-f0-9]{${minBytes * 2},${maxBytes ? minBytes * 2 : ''}}$`
+export const HexRegExMinMax = (minBytes = 0, maxBytes?: number) => {
+  return `^[a-f0-9]{${minBytes * 2},${maxBytes ? maxBytes * 2 : ''}}$`
 }
-export const HashRegEx = HexRegEx(32)
-export const AddressRegEx = HexRegEx(20)
-export const Uint256RegEx = HexRegEx(0, 32)
+export const HexRegEx = HexRegExMinMax()
+export const HashRegEx = HexRegExMinMax(32, 32)
+export const AddressRegEx = HexRegExMinMax(20, 20)
+export const Uint256RegEx = HexRegExMinMax(0, 32)
 
 export const payloadPropertiesJsonSchema: JSONSchemaType<Payload>['properties'] = {
   $sources: {

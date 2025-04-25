d271331d7bd21083e1fbb5a7d201829b1cab25bb
Arie Trouw <arie.trouw@xylabs.com>
2025-04-25 11:19:30 -0700

    reduced the barrel exports for archivist/diviner/sentinel/witness

--- packages/modules/packages/sentinel/src/index.ts
+++ packages/modules/packages/sentinel/src/index.ts
@@ -1,3 +1,2 @@
 export * from '@xyo-network/sentinel-abstract'
-export * from '@xyo-network/sentinel-memory'
 export * from '@xyo-network/sentinel-model'
--- packages/modules/packages/witness/src/index.ts
+++ packages/modules/packages/witness/src/index.ts
@@ -1,5 +1,3 @@
 export * from '@xyo-network/abstract-witness'
-export * from '@xyo-network/witness-evm'
 export * from '@xyo-network/witness-model'
-export * from '@xyo-network/witness-timestamp'
 export * from '@xyo-network/witness-wrapper'

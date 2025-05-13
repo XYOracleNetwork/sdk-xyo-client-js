b8829cdfd591b756cb9307f541af59107b7aa109
Arie Trouw <arie.trouw@xylabs.com>
2025-05-13 15:34:43 -0700

    updo

--- packages/modules/packages/module/packages/emitter/src/ModuleBaseEmitter.ts
+++ packages/modules/packages/module/packages/emitter/src/ModuleBaseEmitter.ts
@@ -1,7 +1,5 @@
 import type { BaseParams } from '@xylabs/base'
-import type { EventData } from '@xylabs/events'
-
-import { BaseEmitter } from './BaseEmitter.ts'
+import { BaseEmitter, type EventData } from '@xylabs/events'
 
 export class ModuleBaseEmitter<TParams extends BaseParams = BaseParams, TEventData extends EventData = EventData>
   extends BaseEmitter<TParams, TEventData> {}

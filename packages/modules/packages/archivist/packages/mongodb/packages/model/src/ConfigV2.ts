8fde48286062745efe8a9bb5d19623cbf825e683
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-06-04 15:37:12 -0500

    Move params to base mixin config

--- packages/modules/packages/archivist/packages/mongodb/packages/model/src/ConfigV2.ts
+++ packages/modules/packages/archivist/packages/mongodb/packages/model/src/ConfigV2.ts
@@ -4,13 +4,6 @@ import type { ArchivistConfig } from '@xyo-network/archivist-model'
 import type { MongoDBArchivistConfigSchema } from './Schema.ts'
 
 export type MongoDBArchivistConfigV2 = ArchivistConfig<{
-  /**
-   * The maximum number of payloads to store in the archivist.
-   * If not specified, there is no limit. If specified, the archivist
-   * will automatically clear old payloads when the limit is reached
-   * in a LIFO fashion.
-   */
-  max?: number
   payloadSdkConfig?: Partial<BaseMongoSdkPublicConfig>
   schema: MongoDBArchivistConfigSchema
 }>

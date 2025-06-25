dbbb234be123a7387c135d0396101bfe2bbae5ad
Arie Trouw <arie.trouw@xylabs.com>
2025-06-24 17:11:21 -0700

    exporting manifest json schemas

--- packages/manifest/src/index.ts
+++ packages/manifest/src/index.ts
@@ -1,2 +1,4 @@
+export { default as DappJsonSchema } from './dapp-package-manifest-schema.json' with {type: 'json' }
+export { default as PackageJsonSchema } from './schema.json' with { type: 'json' }
 export * from '@xyo-network/manifest-model'
 export * from '@xyo-network/manifest-wrapper'

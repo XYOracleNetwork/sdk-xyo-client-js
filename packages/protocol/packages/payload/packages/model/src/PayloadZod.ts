4a4e1f208028adc573da44b72979489b6d1a7ba7
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-10-10 16:26:23 -0500

    Add Payload Zod helpers

--- packages/protocol/packages/payload/packages/model/src/PayloadZod.ts
+++ packages/protocol/packages/payload/packages/model/src/PayloadZod.ts
@@ -24,3 +24,10 @@ export type AnyPayloadWithStorageMeta = z.infer<typeof AnyPayloadWithStorageMeta
 export function WithStorageMetaZod<T extends typeof PayloadZod>(valueZod: T) {
   return StorageMetaZod.extend(valueZod.shape)
 }
+
+export const PayloadZodStrict = z.strictObject({ schema: SchemaZod })
+export type PayloadZodStrict = typeof PayloadZodStrict
+export const PayloadZodLoose: PayloadZodStrict = z.looseObject({ schema: SchemaZod })
+
+export const PayloadZodStrictOfSchema = <S extends string>(schema: S) => PayloadZodStrict.extend({ schema: z.literal(schema) })
+export const PayloadZodLooseOfSchema = <S extends string>(schema: S) => PayloadZodLoose.extend({ schema: z.literal(schema) })

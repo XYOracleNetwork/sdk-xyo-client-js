a85377a1651af9056848e2c402ffa013f4ab0a4c
Arie Trouw <arie.trouw@xylabs.com>
2025-12-03 14:09:23 -0800

    Module extends CreatableInstance

--- packages/modules/packages/module/packages/model/src/module/Module.ts
+++ packages/modules/packages/module/packages/model/src/module/Module.ts
@@ -1,9 +1,9 @@
-import type { EventEmitter } from '@xylabs/events'
+import type { CreatableInstance } from '@xylabs/creatable'
 
 import type { ModuleEventData } from '../EventsModels/index.ts'
 import type { ModuleParams } from '../ModuleParams.ts'
 import type { ModuleFields } from './ModuleFields.ts'
 
 export interface Module<TParams extends ModuleParams = ModuleParams, TEventData extends ModuleEventData<object> = ModuleEventData<object>>
-  extends ModuleFields<TParams>,
-  EventEmitter<TEventData> {}
+  extends ModuleFields<TParams>, CreatableInstance<TParams, TEventData> {
+}
